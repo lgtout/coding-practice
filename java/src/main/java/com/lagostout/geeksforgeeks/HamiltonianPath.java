27ecfd3ca658d5238c21cb91de7911b575897685
lagos tout <lagos.tout@gmail.com>
2017-01-22 19:23:15 -0800

    Hamiltonian path initial commit

---
+++ java/src/main/java/com/lagostout/geeksforgeeks/HamiltonianPath.java
@@ -0,0 +1,9 @@
+package com.lagostout.geeksforgeeks;
+
+import java.util.List;
+
+public class HamiltonianPath {
+    public List<Integer> findPathUsingBacktracking() {
+        return null;
+    }
+}
---
+++ java/src/test/groovy/com/lagostout/geeksforgeeks/HamiltonianPathSpec.groovy
@@ -0,0 +1,12 @@
+package com.lagostout.geeksforgeeks
+
+import spock.lang.Specification
+import spock.lang.Unroll
+
+class HamiltonianPathSpec extends Specification {
+    @Unroll
+    'finds hamiltonian path using backtracking'() {
+//        where:
+//        [graph]
+    }
+}
