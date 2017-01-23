b05b6910c2e7cc51478c6a6f44a123ac010d3fb4
lagos tout <lagos.tout@gmail.com>
2017-01-22 19:24:05 -0800

    Queue Reconstruction - in progress

--- java/src/main/groovy/com/lagostout/leetcode/QueueReconstruction.java
+++ java/src/main/groovy/com/lagostout/leetcode/QueueReconstruction.java
@@ -3,59 +3,54 @@ package com.lagostout.leetcode;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
-import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 class QueueReconstruction {
     Gson gson = new GsonBuilder().create();
     public int[][] reconstructQueue(int[][] people) {
-        List<int[]> result = new LinkedList<>();
-        Map<int[], Integer> numberAllowedInFrontOfPerson = new HashMap<>();
-        if (people.length == 0) return new int[0][];
-        for (int i = 0; i < people.length; i++) {
-            int[] person = people[i];
-            int resultPersonIndex = 0;
-            int numberAllowed = person[1];
-            while (numberAllowed > 0 && resultPersonIndex < result.size()) {
-                if (result.get(resultPersonIndex)[0] >= person[0]) {
-                    --numberAllowed;
-                }
-                resultPersonIndex++;
-            }
-            numberAllowedInFrontOfPerson.put(person, numberAllowed);
-            result.add(resultPersonIndex, person);
-            resultPersonIndex++;
-            int[] addedPerson = person;
-            while (resultPersonIndex < result.size()) {
-                person = result.get(resultPersonIndex);
-                if (person[0] <= addedPerson[0]) {
-                    numberAllowedInFrontOfPerson.put(
-                            person, numberAllowedInFrontOfPerson.get(person)-1);
-                }
-                if (numberAllowedInFrontOfPerson.get(person) < 0) {
-                    System.out.println("swapping " + gson.toJson(person));
-                    result.remove(person);
-                    int tempResultPersonIndex = resultPersonIndex;
-                    numberAllowed = numberAllowedInFrontOfPerson.get(person);
-                    while (numberAllowed < 0) {
-                        tempResultPersonIndex--;
-                        int[] otherPerson = result.get(tempResultPersonIndex);
-                        if (otherPerson[0] >= person[0]) {
-                            numberAllowed++;
-                        }
-                    }
-                    numberAllowedInFrontOfPerson.put(person, numberAllowed);
-                    result.add(tempResultPersonIndex, person);
-                    System.out.println(gson.toJson(result));
-                }
-                resultPersonIndex++;
-            }
-            System.out.println(gson.toJson(addedPerson));
-            System.out.println(gson.toJson(result));
+        int[][] result = new int[people.length][];
+        Map<Integer, Set<Integer>> atLeastAsTallToHeightMap = new HashMap<>();
+        for (int[] person : people) {
+            Set<Integer> heights = atLeastAsTallToHeightMap.computeIfAbsent(
+                    person[1], k -> new TreeSet<>());
+            heights.add(person[0]);
         }
-        int[][] resultArray = new int[result.size()][];
-        return result.toArray(resultArray);
+        System.out.println(gson.toJson(atLeastAsTallToHeightMap));
+        List<int[]> ranges = new ArrayList<>();
+        // TODO
+        // The range max isn't necessarily the size of the queue!
+        ranges.add(new int[]{1, 500, 0});
+//        ranges.add(new int[]{1, people.length, 0});
+        for (int j = 0; j < people.length; ++j){
+            for (int[] range : ranges) {
+
+                // Find next person
+                System.out.println("range: " + gson.toJson(range));
+                Set<Integer> heights = atLeastAsTallToHeightMap.get(range[2]);
+                if (heights == null || heights.isEmpty())
+                    continue;
+                Iterator<Integer> iterator = heights.iterator();
+                int height = iterator.next();
+                int[] nextPerson = new int[]{height, range[2]};
+                System.out.println("nextPerson: " + gson.toJson(nextPerson));
+                iterator.remove();
+                System.out.println(gson.toJson(atLeastAsTallToHeightMap));
+
+                // Update ranges
+                int rangesSize = ranges.size();
+                int[] lastRange = ranges.get(rangesSize-1);
+                int[] nextRange = Arrays.copyOf(lastRange, 3);
+                nextRange[0] = nextPerson[0] + 1;
+                lastRange[1] = nextPerson[0];
+                for (int[] range1 : ranges) {
+                    range1[2]++;
+                }
+                ranges.add(nextRange);
+                System.out.println("ranges: " + gson.toJson(ranges));
+                result[j] = nextPerson;
+                break;
+            }
+        }
+        return result;
     }
 }
