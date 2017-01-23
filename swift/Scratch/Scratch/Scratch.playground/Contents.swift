0901636e6250f0a0842822d6e5bf48d68d0e7855
lagos tout <lagos.tout@gmail.com>
2017-01-22 19:28:11 -0800

    Add Swift/Xcode Scratch project

---
+++ swift/Scratch/Scratch/Scratch.playground/Contents.swift
@@ -0,0 +1,4 @@
+import UIKit
+import Dollar
+
+var str = "Hello, playground"
---
+++ swift/Scratch/Scratch/main.swift
@@ -0,0 +1,8 @@
+import Foundation
+
+func findMaxConsecutiveOnes(_ nums: [Int]) -> Int {
+    return 0
+}
+print("foo")
+
+
