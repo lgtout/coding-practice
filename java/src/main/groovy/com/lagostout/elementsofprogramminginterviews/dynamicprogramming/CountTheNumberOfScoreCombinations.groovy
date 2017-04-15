0401a1157047de860b222ebbb136e414fdc488b8
lgtout <lagos.tout@gmail.com>
2017-04-15 12:57:05 -0700

    EP-G 17.1 Count the number of score combinations - faster!
    O(sn) instead of O(sn^2) time complexity.

--- java/src/main/groovy/com/lagostout/elementsofprogramminginterviews/dynamicprogramming/CountTheNumberOfScoreCombinations.groovy
+++ java/src/main/groovy/com/lagostout/elementsofprogramminginterviews/dynamicprogramming/CountTheNumberOfScoreCombinations.groovy
@@ -5,11 +5,38 @@ package com.lagostout.elementsofprogramminginterviews.dynamicprogramming
  */
 class CountTheNumberOfScoreCombinations {
 
+    /**
+     * Faster implementation: O(sn)
+     * @param score
+     * @param points
+     * @return
+     */
     static int scoreCombinationCount(int score, int[] points) {
         int[][] scores = new int[score+1][points.length]
         scores[0] = [1,1,1]
-        println scores
-        println "i j k"
+        for (int i = 1; i < scores.length; i++) {
+            for (int j = 0; j < points.size(); j++) {
+                int prevScoreIndexWithCurrPoints = i - points[j]
+                int ways = prevScoreIndexWithCurrPoints >= 0 ?
+                        scores[prevScoreIndexWithCurrPoints][j] : 0
+                int prevPointsIndexWithCurrPoints = j - 1
+                ways += prevPointsIndexWithCurrPoints >= 0 ?
+                        scores[i][prevPointsIndexWithCurrPoints] : 0
+                scores[i][j] += ways
+            }
+        }
+        scores[score][points.length - 1]
+    }
+
+    /**
+     * Slower implementation: O(sn^2)
+     * @param score
+     * @param points
+     * @return
+     */
+    static int scoreCombinationCount_v1(int score, int[] points) {
+        int[][] scores = new int[score+1][points.length]
+        scores[0] = [1,1,1]
         for (int i = 1; i < scores.length; i++) {
             for (int j = 0; j < points.size(); j++) {
                 int ways = 0
