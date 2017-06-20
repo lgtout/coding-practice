c28204c823159bbe40c30661cd9de8673b745a08
lgtout <lagos.tout@gmail.com>
2017-06-20 11:32:18 -0700

    EP-G 17.1.4 Distinct scoring sequences of game score
    Close out groovy solution - too complex.

--- java/src/main/groovy/com/lagostout/elementsofprogramminginterviews/dynamicprogramming/CountNumberOfDistinctSequencesOfPlayPointsOfScoreOfGameWithTwoTeams.groovy
+++ java/src/main/groovy/com/lagostout/elementsofprogramminginterviews/dynamicprogramming/CountNumberOfDistinctSequencesOfPlayPointsOfScoreOfGameWithTwoTeams.groovy
@@ -2,23 +2,27 @@ package com.lagostout.elementsofprogramminginterviews.dynamicprogramming
 
 class CountNumberOfDistinctSequencesOfPlayPointsOfScoreOfGameWithTwoTeams {
 
-    static enum Team {
-        A, B
-    }
-
     int compute(int score) {
         0
     }
 
-    static class PlayPoints {
+    enum Team {
+        A, B
+    }
 
+    class PlayPoints {
         int points
         Team team
-
         String toString() {
             "(${team.name()},$points)"
-//            ReflectionToStringBuilder.toString(this,
-//                    new MultilineShortPrefixRecursiveToStringStyle())
+        }
+
+        static List<List<PlayPoints>> toPlayPointSequences(List<List<List>> sequences) {
+            sequences.collect {
+                it.collect {
+                    [team:it[0], points:it[1]] as PlayPoints
+                }
+            }
         }
 
         boolean equals(o) {
@@ -38,14 +42,6 @@ class CountNumberOfDistinctSequencesOfPlayPointsOfScoreOfGameWithTwoTeams {
             result = points
             result = 31 * result + team.hashCode()
             return result
-        }
-
-        static List<List<PlayPoints>> toPlayPointSequences(List<List<List>> sequences) {
-            sequences.collect {
-                it.collect {
-                    [team:it[0], points:it[1]] as PlayPoints
-                }
-            }
         }
     }
 
--- java/src/test/groovy/com/lagostout/elementsofprogramminginterviews/dynamicprogramming/CountNumberOfDistinctSequencesOfPlayPointsOfScoreOfGameWithTwoTeamsSpec.groovy
+++ java/src/test/groovy/com/lagostout/elementsofprogramminginterviews/dynamicprogramming/CountNumberOfDistinctSequencesOfPlayPointsOfScoreOfGameWithTwoTeamsSpec.groovy
@@ -3,12 +3,17 @@ package com.lagostout.elementsofprogramminginterviews.dynamicprogramming
 import com.lagostout.elementsofprogramminginterviews.dynamicprogramming.CountNumberOfDistinctSequencesOfPlayPointsOfScoreOfGameWithTwoTeams.PlayPoints
 import com.lagostout.elementsofprogramminginterviews.dynamicprogramming.CountNumberOfDistinctSequencesOfPlayPointsOfScoreOfGameWithTwoTeams.Team
 import org.apache.commons.collections4.CollectionUtils
+import org.apache.commons.collections4.iterators.PermutationIterator
+import org.apache.commons.math3.util.CombinatoricsUtils
 import spock.lang.Specification
 import spock.lang.Unroll
 
+import static com.lagostout.elementsofprogramminginterviews.dynamicprogramming.CountNumberOfDistinctSequencesOfPlayPointsOfScoreOfGameWithTwoTeams.PlayPoints.toPlayPointSequences
+import static com.lagostout.elementsofprogramminginterviews.dynamicprogramming.CountNumberOfDistinctSequencesOfPlayPointsOfScoreOfGameWithTwoTeamsSpec.BruteForceHelper.computeScoreSequencesOfBothTeamsUsingPermutationGenerator1
 import static com.lagostout.elementsofprogramminginterviews.dynamicprogramming.
         CountNumberOfDistinctSequencesOfPlayPointsOfScoreOfGameWithTwoTeamsSpec.
-        BruteForceHelper.scoreSequences
+        BruteForceHelper.computeScoreSequencesOfOneTeam
+import static org.apache.commons.collections4.CollectionUtils.isEqualCollection
 
 class CountNumberOfDistinctSequencesOfPlayPointsOfScoreOfGameWithTwoTeamsSpec extends Specification {
 
@@ -25,26 +30,18 @@ class CountNumberOfDistinctSequencesOfPlayPointsOfScoreOfGameWithTwoTeamsSpec ex
          ]
     }
 
-    int countNumberOfDistinctSequencesWithBruteForce(
-            int firstTeamScore, int secondTeamScore,
-            List<Integer> allPlayPoints) {
-        def firstTeamSequences = scoreSequences(
-                firstTeamScore, allPlayPoints)
-        0
-    }
-
-
     static class BruteForceHelperSpec extends Specification {
 
-        // TODO
-        // Test 2-score brute force sequence generator
-
-        def 'computes sequences of a score using brute force'(
+        @Unroll
+        def 'computes sequences of a single score using brute force'(
                 int score, List<Integer> allPlayPoints, List<Integer> expected) {
 
-            expect:
-            CollectionUtils.isEqualCollection(
-                    scoreSequences(score, allPlayPoints), expected)
+            when:
+            def sequences = computeScoreSequencesOfOneTeam(
+                    score, allPlayPoints)
+
+            then:
+            isEqualCollection(sequences, expected)
 
             where:
             [score, allPlayPoints, expected] << [
@@ -58,40 +55,128 @@ class CountNumberOfDistinctSequencesOfPlayPointsOfScoreOfGameWithTwoTeamsSpec ex
             ]
 
         }
+
+        def 'computes sequences of game scores of two teams using brute force'() {
+            expect:
+            isEqualCollection(
+                    computeScoreSequencesOfBothTeamsUsingPermutationGenerator1(2,4, [0,2]),
+                    toPlayPointSequences([
+                            [[Team.A, 2],[Team.B, 2],[Team.B, 2]],
+                            [[Team.B, 2],[Team.A, 2],[Team.B, 2]],
+                            [[Team.B, 2],[Team.B, 2],[Team.A, 2]]]))
+            // More cases
+            //assert isEqualCollection(scoreSequences(2,0, [0]), [[]])
+            //assert isEqualCollection(scoreSequences(2,0, [0,2]),
+            //        toPlayPointSequences([[[Team.A, 2]]]))
+            //assert isEqualCollection(scoreSequences(2,2, [0,2]),
+            //        toPlayPointSequences([
+            //                [[Team.A, 2],[Team.B, 2]],
+            //                [[Team.B, 2],[Team.A, 2]]]))
+            //assert isEqualCollection(scoreSequences(2,1, [0,2]),
+            //        toPlayPointSequences([]))
+        }
+
     }
 
     static class BruteForceHelper {
 
-        static Set<List<PlayPoints>> scoreSequences(
+        // TODO Purely recursive 2-team sequence generator, without using permutation generator.
+        static Set<List<PlayPoints>> computeScoreSequencesOfBothTeams(
                 int firstScore, int secondScore,
                 List<Integer> allPlayPoints) {
+
+        }
+
+        static Set<List<PlayPoints>> computeScoreSequencesOfBothTeamsUsingPermutationGenerator1(
+                int firstScore, int secondScore, List<Integer> allPlayPoints) {
+            computeScoreSequencesOfBothTeamsUsingPermutationGenerator(
+                    firstScore, secondScore, [allPlayPoints, allPlayPoints].combinations() - [[0,0]])
+        }
+
+        /**
+         * Brute force approach.
+         * Computes sequences of two team scores.
+         * Uses permutation generator to add subtracted play-points back into
+         * subscores of both teams.
+         * @param firstScore
+         * @param secondScore
+         * @param combinationsOfPointsToSubtract
+         * @return
+         */
+        static Set<List<PlayPoints>> computeScoreSequencesOfBothTeamsUsingPermutationGenerator(
+                int firstScore, int secondScore,
+                List<List<Integer>> combinationsOfPointsToSubtract) {
+            println "first score $firstScore second score $secondScore " +
+                    "combinations of points to subtract $combinationsOfPointsToSubtract"
+            if (combinationsOfPointsToSubtract.isEmpty()) return [[]]
             if (firstScore == 0 && secondScore == 0) return [[]]
-            else if (firstScore < 0 || secondScore < 0) return []
-            def sequences = [allPlayPoints, allPlayPoints].combinations().collectMany {
-                List<Integer> pointsCombination ->
-                    def firstTeamPoints = [points:pointsCombination[0], team:Team.A] as PlayPoints
-                    def secondTeamPoints = [points:pointsCombination[1], team:Team.B] as PlayPoints
-                    def nextFirstTeamScore = firstScore == 0 ? 0 : firstScore - firstTeamPoints.points
-                    def nextSecondTeamScore = secondScore == 0 ? 0 : secondScore - secondTeamPoints.points
-                    scoreSequences(nextFirstTeamScore, nextSecondTeamScore, allPlayPoints).collectMany {
-                        List<PlayPoints> sequence ->
-                            def permutationGenerator =
-                                    new PermutationGenerator(sequence +
-                                            [firstTeamPoints, secondTeamPoints])
-                            new HashSet<Integer>(permutationGenerator.collect())
-                    }
+            if (firstScore < 0 || secondScore < 0) return []
+            def sequences = combinationsOfPointsToSubtract.collectMany {
+                List<Integer> pointsToSubtract ->
+                    println "points to subtract $pointsToSubtract"
+                    def subtractedPoints = []
+                    def firstTeamPoints = [points:pointsToSubtract[0], team:Team.A] as PlayPoints
+                    def firstTeamSubscore = firstScore - firstTeamPoints.points
+                    if (pointsToSubtract[0] != 0)
+                        subtractedPoints << firstTeamPoints
+                    def secondTeamPoints = [points:pointsToSubtract[1], team:Team.B] as PlayPoints
+                    def secondTeamSubscore = secondScore - secondTeamPoints.points
+                    if (pointsToSubtract[1] != 0)
+                        subtractedPoints << secondTeamPoints
+                    // Insert every permutation of subtracted points in every way
+                    // into each subSequence.
+                    println "subtracted points $subtractedPoints"
+                    def sequencesWithSubtractedPointsAddedBackIn =
+                            computeScoreSequencesOfBothTeamsUsingPermutationGenerator(
+                                    firstTeamSubscore, secondTeamSubscore,
+                                    combinationsOfPointsToSubtract).collectMany {
+                                List<PlayPoints> sequenceWithPointsSubtracted ->
+                                    def pointsCount = subtractedPoints.size()
+                                    def subSequenceSize = sequenceWithPointsSubtracted.size()
+                                    def combinationsIterator = CombinatoricsUtils.combinationsIterator(
+                                            subSequenceSize + pointsCount, pointsCount)
+                                    // For every way of picking pointsCount items from the complete sequence...
+                                    combinationsIterator.collectMany {
+                                        int[] sequenceIndicesAtWhichToInsertPoints ->
+                                            // For every way of arranging pointsCount items in pointsCount places...
+                                            new PermutationIterator(subtractedPoints).collect {
+                                                subtractedPointsPermutation ->
+                                                    def sequence = [].withDefault { null }
+                                                    // Add in subseqence play points
+                                                    sequence.addAll(sequenceWithPointsSubtracted)
+                                                    // Insert subtracted play points
+                                                    [subtractedPointsPermutation, sequenceIndicesAtWhichToInsertPoints]
+                                                            .transpose().each {
+                                                        PlayPoints points, int indexAtWhichToInsertPointInSequence ->
+                                                            sequence.add(indexAtWhichToInsertPointInSequence, points)
+                                                    }
+                                                    sequence
+                                            }
+                                    }
+                            }
+                    println "sequences with subtracted points added back in " +
+                            "$sequencesWithSubtractedPointsAddedBackIn"
+                    sequencesWithSubtractedPointsAddedBackIn
             }
             sequences
         }
 
-        static Set<List<Integer>> scoreSequences(int score, List<Integer> allPlayPoints) {
+        /**
+         * Brute force approach.
+         * Computes sequences of a single team score.
+         * @param score
+         * @param allPlayPoints
+         * @return
+         */
+        static Set<List<Integer>> computeScoreSequencesOfOneTeam(
+                int score, List<Integer> allPlayPoints) {
             if (score == 0) return [[]]
             else if (score < 0) return []
             def sequences = allPlayPoints.collectMany { int points ->
-                scoreSequences(score - points, allPlayPoints).collectMany {
+                computeScoreSequencesOfOneTeam(
+                        score - points, allPlayPoints).collect {
                     List<Integer> sequence ->
-                        def permutationGenerator = new PermutationGenerator(sequence + points)
-                        new HashSet<List<Integer>>(permutationGenerator.collect())
+                        sequence + points
                 }
             }
             sequences
