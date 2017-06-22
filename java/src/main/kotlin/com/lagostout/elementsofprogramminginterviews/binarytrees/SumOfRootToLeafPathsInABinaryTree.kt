2acbb647b7144cd4b6b2d8ccf3aebf3216b304e7
lgtout <lagos.tout@gmail.com>
2017-06-22 15:15:19 -0700

    EP-K 10.5 Sum of root-to-leaf paths in binary tree - WIP

--- java/src/main/kotlin/com/lagostout/elementsofprogramminginterviews/binarytrees/SumOfRootToLeafPathsInABinaryTree.kt
+++ java/src/main/kotlin/com/lagostout/elementsofprogramminginterviews/binarytrees/SumOfRootToLeafPathsInABinaryTree.kt
@@ -1,12 +1,40 @@
 package com.lagostout.elementsofprogramminginterviews.binarytrees
 
 import com.lagostout.common.BinaryTreeNode
+import java.util.*
 
 fun sumOfRootToLeafPaths(root: BinaryTreeNode<Boolean>): Int {
 
-    fun foo () {
+    data class Frame(var step: Int = 0, val node: BinaryTreeNode<Boolean>)
 
-    }
-    foo()
-    return 0
+    val stack = ArrayDeque<Frame>()
+
+    return ({
+        stack.push(Frame(node = root))
+        var sum = 0
+        var number = 0
+        while (stack.isNotEmpty()) {
+            val frame = stack.peek()
+            number = number shl 1
+            when (frame.step) {
+                0,1 -> {
+                    number = when (frame.node.value) {
+                        true -> number and 1
+                        else -> number }
+                    val nextNode = when (frame.step) {
+                        0 -> frame.node.left
+                        else -> frame.node.right }
+                    frame.step++
+                    stack.push(Frame(node = nextNode))
+                }
+                else -> {
+                    sum += number
+                    number ushr 1
+                    stack.pop()
+                }
+            }
+        }
+        sum
+    }())
+
 }
--- java/src/test/kotlin/com/lagostout/elementsofprogramminginterviews/binarytrees/SumOfRootToLeafPathsInABinaryTreeSpek.kt
+++ java/src/test/kotlin/com/lagostout/elementsofprogramminginterviews/binarytrees/SumOfRootToLeafPathsInABinaryTreeSpek.kt
@@ -1,19 +1,18 @@
 package com.lagostout.elementsofprogramminginterviews.binarytrees
 
 import com.lagostout.common.BinaryTreeNode
-import org.apache.commons.math3.random.RandomDataGenerator
 import org.jetbrains.spek.api.Spek
+import kotlin.test.assertEquals
 
 class SumOfRootToLeafPathsInABinaryTreeSpek : Spek({
-    given("a binary tree in which each node contains a binary digit") {
-        println(randomData(1, numberCountRange = 3..3, numberRange = 1..3))
-//        randomData().forEach {
-//            on("computing the sum of root to leaf paths") {
-//                it("returns ") {
-//
-//                }
-//            }
-//        }
+    staticData.forEach {
+        given("a binary tree in which each node contains a binary digit") {
+            on("computing the sum of root to leaf paths") {
+                it("returns ${it.expected}") {
+                    assertEquals(it.expected, sumOfRootToLeafPaths(it.root))
+                }
+            }
+        }
     }
 })
 
@@ -23,71 +22,48 @@ private data class DataRow(val root: BinaryTreeNode<Boolean>, val expected: Int)
  * It's assumed that all numbers are positive.
  * The root is always the 1-bit in the most significant position
  * of each number.
- * It's expected that numberRange will always contain at least as
- * many possible numbers as numberCountRange.
+ * Numbers cannot be prefixes of other numbers, otherwise they'll
+ * be lost in the tree.
  */
 
-// TODO Numbers cannot be prefixes of other numbers, otherwise they're lost in the tree.
-// Would it be easier to use static test data instead?
-
-private fun randomData(dataCount: Int = 5,
-                       numberCountRange: IntRange = 1..5,
-                       numberRange: IntRange = 1..9): List<DataRow> {
-    if (numberCountRange.last > numberRange.count()) {
-        throw IllegalArgumentException("The maximum number " +
-                "of numbers that can be randomly picked must be " +
-                "less than or equal to the range of numbers that " +
-                "can be picked from")
-    }
-    val random = RandomDataGenerator()
-    random.reSeed(1)
-    val data: MutableList<DataRow> = mutableListOf()
-    1.rangeTo(dataCount).forEach {
-        val numberCount = random.nextInt(
-                numberCountRange.first, numberCountRange.last)
-        val root = BinaryTreeNode(true)
-        var sum = 0
-        var numbers: MutableSet<Int> = mutableSetOf()
-        while (numbers.size < numberCount) {
-            numbers.add(random.nextInt(
-                    numberRange.first, numberRange.last))
-        }
-//        println("numbers $numbers")
-        numbers.forEach {
-            var number = it
-//            println("number $number")
-            sum += number
-            var currNode = root
-            var mask = 1
-            // Align mask with the most significant bit.
-            while (mask < number) {
-                mask = mask shl 1
-            }
-            // Make sure we didn't overshoot.
-            mask = if (mask > number) mask shr 1 else mask
-            // We've already got most significant 1 bit as the root,
-            // so let's start from the second most significant bit.
-            mask = mask shr 1
-            while (mask > 0) {
-                val bit = (number and mask) != 0
-                // 0/false bit is always the left node and 1/true
-                // bit the right one
-                if (!bit) {
-                    if (currNode.left == null) {
-                        currNode.left = BinaryTreeNode(bit)
-                    }
-                    currNode = currNode.left
-                } else {
-                    if (currNode.right == null) {
-                        currNode.right = BinaryTreeNode(bit)
-                    }
-                    currNode = currNode.right
+private val staticData: List<DataRow>
+    get() {
+        return listOf(listOf(0b110, 0b111, 0b101),
+                listOf(0b10, 0b1100, 0b1101, 0b111)).map {
+            numbers: List<Int> ->
+            val root = BinaryTreeNode(true)
+            var sum = 0
+            numbers.forEach {
+                number :Int ->
+//                println("number $number")
+                sum += number
+                var shiftedNumber = number
+                val bits: MutableList<Boolean> = mutableListOf()
+                while (shiftedNumber > 1) {
+//                println("shiftedNumber")
+                    bits.add(shiftedNumber and 1 == 1)
+                    shiftedNumber = shiftedNumber ushr 1
                 }
-                mask = mask ushr 1
+                var currNode = root
+//            println("bits ${bits.reversed()}")
+                bits.reversed().forEach {
+                    bit: Boolean ->
+                    when (bit) {
+                        false -> {
+                            if (currNode.left == null) {
+                                currNode.left = BinaryTreeNode(bit)
+                            }
+                            currNode = currNode.left
+                        }
+                        else -> {
+                            if (currNode.right == null) {
+                                currNode.right = BinaryTreeNode(bit)
+                            }
+                            currNode = currNode.right
+                        }
+                    }
+                }
             }
+            DataRow(root, sum)
         }
-        println(numbers)
-        data.add(DataRow(root, sum))
     }
-    return data
-}
