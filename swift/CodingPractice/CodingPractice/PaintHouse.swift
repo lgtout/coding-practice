f027fabcce3302c5501648454826026274bf7e71
lgtout <lagos.tout@gmail.com>
2017-06-24 13:54:57 -0700

    LC-S Paint House - WIP

---
+++ swift/CodingPractice/CodingPractice/PaintHouse.swift
@@ -0,0 +1,5 @@
+//  Copyright © 2017 lagostout. All rights reserved.
+
+import Foundation
+
+/// [Problem](https://leetcode.com/problems/paint-house/#/description)
---
+++ swift/CodingPractice/CodingPracticeTests/PaintHouseSpec.swift
@@ -0,0 +1,3 @@
+//  Copyright © 2017 lagostout. All rights reserved.
+
+import Foundation
