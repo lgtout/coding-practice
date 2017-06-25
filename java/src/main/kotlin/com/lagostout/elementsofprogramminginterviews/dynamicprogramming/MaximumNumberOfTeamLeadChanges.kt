fc81400ede8d3745030b11058fdc368259bf57a1
lgtout <lagos.tout@gmail.com>
2017-06-25 12:32:45 -0700

    EP-K 17.1.5 Maximum number of team lead changes - WIP

---
+++ java/src/main/kotlin/com/lagostout/elementsofprogramminginterviews/dynamicprogramming/MaximumNumberOfTeamLeadChanges.kt
@@ -0,0 +1,8 @@
+package com.lagostout.elementsofprogramminginterviews.dynamicprogramming
+
+/**
+ * Problem 17.1.5 page 313
+ */
+fun maximumNumberOfTeamLeadChanges(firstTeamScore: Int, secondTeamScore: Int): Int {
+    return 0
+}
