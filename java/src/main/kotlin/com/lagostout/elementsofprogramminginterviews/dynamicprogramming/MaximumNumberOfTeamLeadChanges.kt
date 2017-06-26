e903f8629923f98a3f5a272cc777dad50f1edaaf
lgtout <lagos.tout@gmail.com>
2017-06-26 15:04:29 -0700

    EP-K 17.1.5 Maximum number of team lead changes - WIP

--- java/src/main/kotlin/com/lagostout/elementsofprogramminginterviews/dynamicprogramming/MaximumNumberOfTeamLeadChanges.kt
+++ java/src/main/kotlin/com/lagostout/elementsofprogramminginterviews/dynamicprogramming/MaximumNumberOfTeamLeadChanges.kt
@@ -4,5 +4,20 @@ package com.lagostout.elementsofprogramminginterviews.dynamicprogramming
  * Problem 17.1.5 page 313
  */
 fun maximumNumberOfTeamLeadChanges(firstTeamScore: Int, secondTeamScore: Int): Int {
+    data class TeamPointCounts(val firstTeam: Int, val secondTeam: Int)
+    val cache: MutableList<List<TeamPointCounts>> = mutableListOf()
+    (0..firstTeamScore).forEach {
+        firstTeamSubscore ->
+        val row: MutableList<TeamPointCounts> = mutableListOf()
+        cache.add(row)
+        secondTeamScoreLoop@ (0..secondTeamScore).forEach  {
+            secondTeamSubscore ->
+            if (firstTeamSubscore == 0 && secondTeamSubscore == 0) {
+                row[secondTeamSubscore] = TeamPointCounts(0, 0)
+                return@secondTeamScoreLoop
+            }
+//            for
+        }
+    }
     return 0
 }
