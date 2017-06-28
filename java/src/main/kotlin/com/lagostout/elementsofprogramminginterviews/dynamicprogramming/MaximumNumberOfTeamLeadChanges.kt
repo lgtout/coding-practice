b4e1cafcd5b3a31f9c44227802904d3a440cd18f
lgtout <lagos.tout@gmail.com>
2017-06-28 16:26:50 -0700

    EP-K 17.1.5 Maximum number of team lead changes - WIP

--- java/src/main/kotlin/com/lagostout/elementsofprogramminginterviews/dynamicprogramming/MaximumNumberOfTeamLeadChanges.kt
+++ java/src/main/kotlin/com/lagostout/elementsofprogramminginterviews/dynamicprogramming/MaximumNumberOfTeamLeadChanges.kt
@@ -11,74 +11,47 @@ fun maximumNumberOfTeamLeadChanges(firstTeamScore: Int, secondTeamScore: Int,
                 return Math.min(firstTeam, secondTeam) + 1
             }
     }
-    val cache: MutableList<List<TeamPointCounts>> = mutableListOf()
+    val cache: MutableList<List<TeamPointCounts?>> = mutableListOf()
     (0..firstTeamScore).forEach {
         currentFirstTeamScore ->
         val secondTeamScoresForCurrentFirstTeamScore:
-                MutableList<TeamPointCounts> = mutableListOf()
+                MutableList<TeamPointCounts?> = mutableListOf()
         cache.add(secondTeamScoresForCurrentFirstTeamScore)
         (0..secondTeamScore).forEach secondTeamScoreLoop@ {
             currentSecondTeamScore ->
             if (currentFirstTeamScore == 0 &&
                     currentSecondTeamScore == 0) {
-                secondTeamScoresForCurrentFirstTeamScore[currentSecondTeamScore] =
-                        TeamPointCounts(0, 0)
+                secondTeamScoresForCurrentFirstTeamScore[
+                        currentSecondTeamScore] = TeamPointCounts(0, 0)
                 return@secondTeamScoreLoop
             }
-            // I'm not sure if I'm happy with this approach to look backward in score.
-            // It's much less readable than I'd like.
             // Alternate approach would be to subtract combinations of play points
             // from both scores at once. So we'd be moving in 2 dimensions at once,
             // rather than one dimension as in the current approach. For this we'd
             // need to add 0 to the possible play points to allow for consecutive
             // point scoring by the same team.  But we'd need to not count those 0
             // point scores as lead changes.
-            // Is the alternate approach worth exploring? :\  Not sure. Maybe get
-            // current approach completely tested then revisit the question.
-            fun improveLeadChangeCountIfPossible(
-                    teamSubscore: Int,
-                    getSubscoreTeamPointCounts: (Int) -> TeamPointCounts,
-                    getCurrentScoreTeamPointCounts: (TeamPointCounts) -> TeamPointCounts,
-                    teamPointCountsOfSubscoreWithGreatestLeadChangeCount: TeamPointCounts?): TeamPointCounts? {
-                if (teamSubscore > 0) {
-                    val subscoreTeamPointCounts = getSubscoreTeamPointCounts(teamSubscore)
-                    if (teamPointCountsOfSubscoreWithGreatestLeadChangeCount == null) {
-                                return subscoreTeamPointCounts
+            secondTeamScoresForCurrentFirstTeamScore[currentSecondTeamScore] = listOf(
+                    possiblePlayPoints.map {
+                        currentFirstTeamScore - it
+                    }.map {
+                        if (it < 0) null
+                        else {
+                            val teamPointCounts = cache[it][currentSecondTeamScore]
+                            teamPointCounts?.copy(firstTeam = teamPointCounts.firstTeam + 1)
+                        }
+                    },
+                    possiblePlayPoints.map {
+                        currentSecondTeamScore - it
+                    }.map {
+                        if (it < 0) null
+                        else {
+                            val teamPointCounts = cache[currentFirstTeamScore][it]
+                            teamPointCounts?.copy(secondTeam = teamPointCounts.secondTeam + 1)
+                        }
                     }
-                    else if (subscoreTeamPointCounts.leadChangeCount >
-                            teamPointCountsOfSubscoreWithGreatestLeadChangeCount.leadChangeCount) {
-                        return getCurrentScoreTeamPointCounts(
-                                teamPointCountsOfSubscoreWithGreatestLeadChangeCount)
-                    }
-                }
-                return null
-            }
-            var teamPointCountsOfSubscoreWithGreatestLeadChangeCount: TeamPointCounts? = null
-            possiblePlayPoints.forEach {
-                teamPointCountsOfSubscoreWithGreatestLeadChangeCount =
-                        improveLeadChangeCountIfPossible(
-                                currentFirstTeamScore - it,
-                                { teamSubscore ->
-                                    cache[teamSubscore][currentSecondTeamScore]
-                                },
-                                { currentTeamPointCountsOfSubscoreWithGreatestLeadChangeCount ->
-                                    currentTeamPointCountsOfSubscoreWithGreatestLeadChangeCount.copy(
-                                            firstTeam =
-                                            currentTeamPointCountsOfSubscoreWithGreatestLeadChangeCount.firstTeam + 1)
-                                },
-                                teamPointCountsOfSubscoreWithGreatestLeadChangeCount)
-                teamPointCountsOfSubscoreWithGreatestLeadChangeCount =
-                        improveLeadChangeCountIfPossible(
-                                currentSecondTeamScore - it,
-                                { teamSubscore ->
-                                    cache[currentFirstTeamScore][teamSubscore]
-                                },
-                                { currentTeamPointCountsOfSubscoreWithGreatestLeadChangeCount ->
-                                    currentTeamPointCountsOfSubscoreWithGreatestLeadChangeCount.copy(
-                                            secondTeam =
-                                            currentTeamPointCountsOfSubscoreWithGreatestLeadChangeCount.secondTeam + 1)
-                                },
-                                teamPointCountsOfSubscoreWithGreatestLeadChangeCount)
+            ).flatten().filterNotNull().reduce { acc, value ->
+                if (value.leadChangeCount > acc.leadChangeCount) value else acc
             }
         }
     }
