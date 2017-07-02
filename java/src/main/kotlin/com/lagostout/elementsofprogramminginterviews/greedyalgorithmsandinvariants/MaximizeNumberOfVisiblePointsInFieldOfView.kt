8966528557ef1ebb9d2bd980c4a7b8e98fa44386
lgtout <lagos.tout@gmail.com>
2017-07-02 14:29:33 -0700

    EP-K 18.3.3 Maximize number of visible points in field-of-view - WIP

--- java/src/main/kotlin/com/lagostout/elementsofprogramminginterviews/greedyalgorithmsandinvariants/MaximizeNumberOfVisiblePointsInFieldOfView.kt
+++ java/src/main/kotlin/com/lagostout/elementsofprogramminginterviews/greedyalgorithmsandinvariants/MaximizeNumberOfVisiblePointsInFieldOfView.kt
@@ -5,15 +5,44 @@ package com.lagostout.elementsofprogramminginterviews.greedyalgorithmsandinvaria
  */
 class MaximizeNumberOfVisiblePointsInFieldOfView {
     companion object {
-        class FieldOfView(val start: Int, val stop: Int) {
+        data class FieldOfView(val start: Int, val stop: Int) {
             fun contains(other: FieldOfView):Boolean {
                 return start <= other.start && stop >= other.stop
             }
+            fun rotate(): FieldOfView {
+                return copy(start = start+1, stop = stop+1)
+            }
+            val length: Int
+                get() = stop - start + 1
         }
-        data class Result(val fieldOfView: FieldOfView, val pointCount: Int)
+        data class Result(val fieldOfView: FieldOfView, val visiblePointCount: Int)
         fun maximumNumberOfVisiblePointsInFieldOfView(
                 points: List<Int>, fieldOfView: FieldOfView): Result {
-            return Result(FieldOfView(1,1), -1)
+            val pointPositionToPointCountMap = mutableMapOf<Int, Int>().withDefault { 0 }
+            points.forEach {
+                pointPositionToPointCountMap.put(
+                        it, pointPositionToPointCountMap[it]!!.plus(1))
+            }
+            var currentFieldOfView = fieldOfView.copy(
+                    start = -fieldOfView.length, stop = -1)
+            var maximumCountOfPointsInFieldOfView = 0
+            var fieldOfViewWithMaximumCountOfPoints = currentFieldOfView
+            var countOfPointsInCurrentFieldOfView = 0
+            while (currentFieldOfView.stop <= 99) {
+                if (maximumCountOfPointsInFieldOfView <
+                        countOfPointsInCurrentFieldOfView) {
+                    maximumCountOfPointsInFieldOfView =
+                            countOfPointsInCurrentFieldOfView
+                    fieldOfViewWithMaximumCountOfPoints = currentFieldOfView
+                }
+                countOfPointsInCurrentFieldOfView -=
+                        pointPositionToPointCountMap[currentFieldOfView.start]!!
+                currentFieldOfView = currentFieldOfView.rotate()
+                countOfPointsInCurrentFieldOfView +=
+                        pointPositionToPointCountMap[currentFieldOfView.stop]!!
+            }
+            return Result(fieldOfViewWithMaximumCountOfPoints,
+                    countOfPointsInCurrentFieldOfView)
         }
     }
 }
--- java/src/test/kotlin/com/lagostout/elementsofprogramminginterviews/dynamicprogramming/MaximizeNumberOfVisiblePointsInFieldOfViewSpek.kt
+++ java/src/test/kotlin/com/lagostout/elementsofprogramminginterviews/dynamicprogramming/MaximizeNumberOfVisiblePointsInFieldOfViewSpek.kt
@@ -1,6 +1,7 @@
 package com.lagostout.elementsofprogramminginterviews.dynamicprogramming
 
 import com.lagostout.elementsofprogramminginterviews.greedyalgorithmsandinvariants.MaximizeNumberOfVisiblePointsInFieldOfView.Companion.FieldOfView
+import com.lagostout.elementsofprogramminginterviews.greedyalgorithmsandinvariants.MaximizeNumberOfVisiblePointsInFieldOfView.Companion.Result
 import com.lagostout.elementsofprogramminginterviews.greedyalgorithmsandinvariants.MaximizeNumberOfVisiblePointsInFieldOfView.Companion.maximumNumberOfVisiblePointsInFieldOfView
 import org.jetbrains.spek.api.Spek
 import org.jetbrains.spek.api.dsl.describe
@@ -9,6 +10,7 @@ import org.jetbrains.spek.api.dsl.it
 import org.junit.platform.runner.JUnitPlatform
 import org.junit.runner.RunWith
 import java.util.*
+import kotlin.test.assertEquals
 import kotlin.test.assertTrue
 
 @RunWith(JUnitPlatform::class)
@@ -18,10 +20,15 @@ class MaximizeNumberOfVisiblePointsInFieldOfView : Spek({
             (points, fieldOfView, expected) ->
             given("points $points") {
                 it("computes $expected") {
-                    val (optimalFieldOfView, _) =
+                    val (optimalFieldOfView, visiblePoints) =
                             maximumNumberOfVisiblePointsInFieldOfView(
                                     points, fieldOfView)
+                    // TODO
+                    // Not sure about this.  What does "contains" mean?
+                    // How's it different from "equals"?
+                    // Is it sufficient?
                     assertTrue(fieldOfView.contains(optimalFieldOfView))
+                    assertEquals(expected.visiblePointCount, visiblePoints)
                 }
             }
         }
@@ -30,24 +37,37 @@ class MaximizeNumberOfVisiblePointsInFieldOfView : Spek({
     companion object {
         data class TestCase(val points: List<Int> = listOf(),
                             val fieldOfView: FieldOfView,
-                            val expected: FieldOfView) {
+                            val expected: Result) {
             init {
                 Collections.shuffle(points)
             }
         }
         val testCases = listOf(
-                TestCase(fieldOfView = FieldOfView(0, 49), expected = FieldOfView(0,99)),
-                TestCase(listOf(0), FieldOfView(0,19), FieldOfView(81,19)),
-                TestCase(listOf(0,0,0), FieldOfView(0,19), FieldOfView(81,19)),
-                TestCase(listOf(0,1), FieldOfView(0,19), FieldOfView(82,19)),
-                TestCase(listOf(0,1), FieldOfView(0,19), FieldOfView(82,19)),
-                TestCase(listOf(0,1,10), FieldOfView(0,19), FieldOfView(91,19)),
-                TestCase(listOf(0,1,10,40), FieldOfView(0,19), FieldOfView(91,19)),
-                TestCase(listOf(0,1,10,40,41), FieldOfView(0,19), FieldOfView(91,19)),
-                TestCase(listOf(0,1,10,40,41,42), FieldOfView(0,19), FieldOfView(91,19)),
-                TestCase(listOf(0,1,10,40,41,42,43), FieldOfView(0,19), FieldOfView(24,59)),
-                TestCase(listOf(0,1,10,40,41,42,43,70,71), FieldOfView(0,19), FieldOfView(24,59)),
-                TestCase(listOf(0,1,10,40,41,42,43,70,70,80,80,70), FieldOfView(0,19), FieldOfView(61,89)),
+                TestCase(fieldOfView = FieldOfView(0, 49),
+                        expected =
+                        Result(FieldOfView(0,99), 0)),
+                TestCase(listOf(0), FieldOfView(0,19),
+                        Result(FieldOfView(81,19), 1)),
+                TestCase(listOf(0,0,0), FieldOfView(0,19),
+                        Result(FieldOfView(81,19), 3)),
+                TestCase(listOf(0,1), FieldOfView(0,19),
+                        Result(FieldOfView(82,19), 2)),
+                TestCase(listOf(0,1), FieldOfView(0,19),
+                        Result(FieldOfView(82,19), 2)),
+                TestCase(listOf(0,1,10), FieldOfView(0,19),
+                        Result(FieldOfView(91,19), 3)),
+                TestCase(listOf(0,1,10,40), FieldOfView(0,19),
+                        Result(FieldOfView(91,19), 4)),
+                TestCase(listOf(0,1,10,40,41), FieldOfView(0,19),
+                        Result(FieldOfView(91,19), 3)),
+                TestCase(listOf(0,1,10,40,41,42), FieldOfView(0,19),
+                        Result(FieldOfView(91,19), 3)),
+                TestCase(listOf(0,1,10,40,41,42,43), FieldOfView(0,19),
+                        Result(FieldOfView(24,59), 4)),
+                TestCase(listOf(0,1,10,40,41,42,43,70,71), FieldOfView(0,19),
+                        Result(FieldOfView(24,59), 4)),
+                TestCase(listOf(0,1,10,40,41,42,43,70,70,80,80,70), FieldOfView(0,19),
+                        Result(FieldOfView(61,89), 5)),
                 null
         ).filterNotNull()
     }
