7ad2099608b5df117d6515e107cbefa3f34e8a54
lgtout <lagos.tout@gmail.com>
2017-07-03 12:28:53 -0700

    EP-K 13.7.4 Max length of shortest subarray of distinct values - WIP

--- java/src/test/kotlin/com/lagostout/elementsofprogramminginterviews/hashtables/RearrangeArrayToMaximizeLengthOfShortestSubarrayCoveringAllValuesSpek.kt
+++ java/src/test/kotlin/com/lagostout/elementsofprogramminginterviews/hashtables/RearrangeArrayToMaximizeLengthOfShortestSubarrayCoveringAllValuesSpek.kt
@@ -1,12 +1,16 @@
 package com.lagostout.elementsofprogramminginterviews.hashtables
 
-import org.apache.commons.math3.util.CombinatoricsUtils.combinationsIterator
+import org.apache.commons.collections4.iterators.PermutationIterator
 import org.jetbrains.spek.api.Spek
 import org.jetbrains.spek.api.dsl.describe
 import org.jetbrains.spek.api.dsl.given
 import org.jetbrains.spek.api.dsl.it
+import org.junit.platform.runner.JUnitPlatform
+import org.junit.runner.RunWith
+import java.util.*
 import kotlin.test.assertEquals
 
+@RunWith(JUnitPlatform::class)
 class RearrangeArrayToMaximizeLengthOfShortestSubarrayCoveringAllValuesSpek : Spek({
     describe("maximumLengthOfShortestSubarrayCoveringAllValues") {
         testCases.forEach {
@@ -22,15 +26,15 @@ class RearrangeArrayToMaximizeLengthOfShortestSubarrayCoveringAllValuesSpek : Sp
     }
 }) {
     companion object {
-        data class TestCase(val array: List<Int>) {
-            val subarrayLength: Int = if (array.isEmpty()) 0
+        data class TestCase(val values: List<Int>) {
+            val subarrayLength: Int = if (values.isEmpty()) 0
             else {
-                val count = array.size
-                combinationsIterator(count, count)
+                val count = values.size
+                PermutationIterator(values)
                         .asSequence().map {
                     it.map {
                         index ->
-                        array[index]
+                        values[index]
                     }
                 }.map {
                     // Safe to use !! here because we've
@@ -40,12 +44,13 @@ class RearrangeArrayToMaximizeLengthOfShortestSubarrayCoveringAllValuesSpek : Sp
                     rangeOfShortestSubarrayOfDistinctValues(it)!!.count()
                 }.reduce {
                     acc, value ->
-                    if (acc < value) acc else value
+                    if (value > acc) value else acc
                 }
             }
             operator fun component2() = subarrayLength
         }
         val testCases = listOf(
+                // TODO More cases
                 TestCase(listOf(1,1,2,3)),
                 null
         ).filterNotNull()
