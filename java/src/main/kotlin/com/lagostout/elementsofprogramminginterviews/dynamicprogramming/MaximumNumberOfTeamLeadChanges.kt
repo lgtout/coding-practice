4eb0c83654f4df8ab958c4a24ba94aefb5658b73
lgtout <lagos.tout@gmail.com>
2017-07-04 11:47:01 -0700

    EP-K 17.1.5 Maximum number of team lead changes - WIP

--- java/src/main/kotlin/com/lagostout/elementsofprogramminginterviews/dynamicprogramming/MaximumNumberOfTeamLeadChanges.kt
+++ java/src/main/kotlin/com/lagostout/elementsofprogramminginterviews/dynamicprogramming/MaximumNumberOfTeamLeadChanges.kt
@@ -8,87 +8,90 @@ package com.lagostout.elementsofprogramminginterviews.dynamicprogramming
 // But it allows the cache to contain information about both the lead change
 // count and reachability.  That's all the better, though not strictly relevant.
 
-fun maximumNumberOfTeamLeadChanges(firstTeamGameScore: Int, secondTeamGameScore: Int,
-                                   possiblePlayScores: List<Int>): Int {
-    data class GameScoreLeadChanges(val countOfFirstTeamScoringPlays: Int,
-                                    val countOfSecondTeamScoringPlays: Int) {
-        val leadChangeCount:Int
-            get() {
-                return if (countOfFirstTeamScoringPlays == 0 ||
-                        countOfSecondTeamScoringPlays == 0)
-                    0
-                else if (countOfFirstTeamScoringPlays ==
-                        countOfSecondTeamScoringPlays)
-                    countOfFirstTeamScoringPlays
-                else
-                    Math.min(countOfFirstTeamScoringPlays,
-                        countOfSecondTeamScoringPlays) + 1
-            }
-    }
-    val cacheOfGameScoreLeadChanges: MutableList<List<GameScoreLeadChanges?>> =
-            mutableListOf()
-    (0..firstTeamGameScore).forEach {
-        firstTeamScore ->
-        val secondTeamLeadChanges: MutableList<GameScoreLeadChanges?> =
-                mutableListOf()
-        cacheOfGameScoreLeadChanges.add(secondTeamLeadChanges)
-        // We only need to compute firstTeamLeadChanges once per
-        // row i.e. once per firstTeamSubscore.  It is unchanged
-        // from the firstTeamLeadChanges computed for the 0th
-        // column of each row.
-        val firstTeamLeadChanges = possiblePlayScores.map {
-            firstTeamScore - it
-        }.map {
-            firstTeamSubscore ->
-            if (firstTeamSubscore < 0)
-                null
-            else {
-                cacheOfGameScoreLeadChanges[firstTeamSubscore][0]?.let {
-                    it.copy(countOfFirstTeamScoringPlays =
-                    it.countOfFirstTeamScoringPlays + 1)
-                }
+class  MaximumNumberOfTeamLeadChanges {
+
+    companion object {
+
+        enum class Team {
+            A, B
+        }
+
+        data class GameScoreLeadChanges(
+                val teamThatMostRecentlyScored: Team? = null,
+                val leadChangeCount: Int = 0) {
+
+            fun playScoredBy(team: Team): GameScoreLeadChanges {
+                return copy(teamThatMostRecentlyScored = team,
+                        leadChangeCount =
+                            leadChangeCount +
+                                if (teamThatMostRecentlyScored != null &&
+                                        teamThatMostRecentlyScored != team)
+                                    1 else 0)
             }
         }
-        (0..secondTeamGameScore).forEach secondTeamScoreLoop@ {
-            secondTeamScore ->
-            if (firstTeamScore == 0 &&
-                    secondTeamScore == 0) {
-                secondTeamLeadChanges.add(GameScoreLeadChanges(
-                        0, 0))
-                return@secondTeamScoreLoop
-            }
-            // Alternate approach would be to subtract combinations of play points
-            // from both scores at once. So we'd be moving in 2 dimensions at once,
-            // rather than one dimension as in the current approach. For this we'd
-            // need to include 0 in the possible play points to allow for consecutive
-            // point scoring by the same team.  But later we'd need to ignore those
-            // 0 point scores as lead changes.
-            secondTeamLeadChanges.add(listOf(
-                    firstTeamLeadChanges,
-                    possiblePlayScores.map {
-                        secondTeamScore - it
-                    }.map {
-                        secondTeamSubscore ->
-                        if (secondTeamSubscore < 0)
-                            null
-                        else {
-                            cacheOfGameScoreLeadChanges[
-                                    firstTeamScore][secondTeamSubscore]?.let {
-                                it.copy(countOfSecondTeamScoringPlays =
-                                it.countOfSecondTeamScoringPlays + 1)
-                            }
-                        }
+
+        fun maximumNumberOfTeamLeadChanges(firstTeamGameScore: Int, secondTeamGameScore: Int,
+                                           possiblePlayScores: List<Int>): Int {
+            val cacheOfGameScoreLeadChanges: MutableList<List<GameScoreLeadChanges?>> =
+                    mutableListOf()
+            (0..firstTeamGameScore).forEach {
+                firstTeamScore ->
+                val secondTeamLeadChanges: MutableList<GameScoreLeadChanges?> =
+                        mutableListOf()
+                cacheOfGameScoreLeadChanges.add(secondTeamLeadChanges)
+                // We only need to compute firstTeamLeadChanges once per
+                // row i.e. once per firstTeamSubscore.  It is unchanged
+                // from the firstTeamLeadChanges computed for the 0th
+                // column of each row.
+                val firstTeamLeadChanges = possiblePlayScores.map {
+                    firstTeamScore - it
+                }.map {
+                    firstTeamSubscore ->
+                    if (firstTeamSubscore < 0)
+                        null
+                    else
+                        cacheOfGameScoreLeadChanges[firstTeamSubscore][0]
+                                ?.playScoredBy(Team.A)
+                }
+                (0..secondTeamGameScore).forEach secondTeamScoreLoop@ {
+                    secondTeamScore ->
+                    if (firstTeamScore == 0 &&
+                            secondTeamScore == 0) {
+                        secondTeamLeadChanges.add(GameScoreLeadChanges())
+                        return@secondTeamScoreLoop
                     }
-            ).flatten().filterNotNull().let {
-                if (it.isEmpty())
-                    null
-                else it.reduce {
-                    acc, value ->
-                    if (value.leadChangeCount > acc.leadChangeCount) value else acc
+                    // Alternate approach would be to subtract combinations of play points
+                    // from both scores at once. So we'd be moving in 2 dimensions at once,
+                    // rather than one dimension as in the current approach. For this we'd
+                    // need to include 0 in the possible play points to allow for consecutive
+                    // point scoring by the same team.  But later we'd need to ignore those
+                    // 0 point scores as lead changes.
+                    secondTeamLeadChanges.add(listOf(
+                            firstTeamLeadChanges,
+                            possiblePlayScores.map {
+                                secondTeamScore - it
+                            }.map {
+                                secondTeamSubscore ->
+                                if (secondTeamSubscore < 0)
+                                    null
+                                else {
+                                    cacheOfGameScoreLeadChanges[
+                                            firstTeamScore][secondTeamSubscore]
+                                            ?.playScoredBy(Team.B)
+                                }
+                            }
+                    ).flatten().filterNotNull().let {
+                        if (it.isEmpty())
+                            null
+                        else it.reduce {
+                            acc, value ->
+                            if (value.leadChangeCount > acc.leadChangeCount) value else acc
+                        }
+                    })
                 }
-            })
+            }
+            return cacheOfGameScoreLeadChanges[
+                    firstTeamGameScore][secondTeamGameScore]?.leadChangeCount ?: 0
         }
     }
-    return cacheOfGameScoreLeadChanges[
-            firstTeamGameScore][secondTeamGameScore]?.leadChangeCount ?: 0
 }
--- java/src/test/kotlin/com/lagostout/elementsofprogramminginterviews/dynamicprogramming/MaximumNumberOfTeamLeadChangesSpek.kt
+++ java/src/test/kotlin/com/lagostout/elementsofprogramminginterviews/dynamicprogramming/MaximumNumberOfTeamLeadChangesSpek.kt
@@ -1,6 +1,5 @@
 package com.lagostout.elementsofprogramminginterviews.dynamicprogramming
 
-import com.lagostout.common.takeIfNotLast
 import org.jetbrains.spek.api.Spek
 import org.jetbrains.spek.api.dsl.describe
 import org.jetbrains.spek.api.dsl.given
@@ -17,8 +16,9 @@ class MaximumNumberOfTeamLeadChangesSpek : Spek({
             given("$score") {
                 it("computes $expected as the maximum " +
                         "possible number of team lead changes") {
-                    assertEquals(expected, maximumNumberOfTeamLeadChanges(
-                            score.firstTeam, score.secondTeam, possiblePlayPoints))
+                    assertEquals(expected, MaximumNumberOfTeamLeadChanges.
+                            maximumNumberOfTeamLeadChanges(
+                                    score.firstTeam, score.secondTeam, possiblePlayPoints))
                 }
             }
         }
@@ -41,10 +41,10 @@ class MaximumNumberOfTeamLeadChangesSpek : Spek({
 //                TestCase(GameScore(4,1), 0),
 //                TestCase(GameScore(2,2), 1),
 //                TestCase(GameScore(2,3), 1),
-//                TestCase(GameScore(4,3), 2),
+                TestCase(GameScore(4,3), 2),
 //                TestCase(GameScore(3,4), 2),
 //                TestCase(GameScore(2,4), 2),
-                TestCase(GameScore(4,4), 3),
+//                TestCase(GameScore(4,4), 3),
 //                TestCase(GameScore(3,7), 2),
 //                TestCase(GameScore(10,6), 4),
                 null
