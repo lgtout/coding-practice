a2fbb274b9bd796401b4deb56c38589bd0f0b2bd
lgtout <lagos.tout@gmail.com>
2017-07-05 11:24:11 -0700

    EP-K 10.8 Pre-order traversal without recursion - done!

---
+++ java/src/main/kotlin/com/lagostout/elementsofprogramminginterviews/binarytrees/PreOrderTraversalWithoutRecursion.kt
@@ -0,0 +1,42 @@
+package com.lagostout.elementsofprogramminginterviews.binarytrees
+
+import com.lagostout.common.BinaryTreeNode
+import java.util.*
+
+/**
+ * Problem 10.8 page 162
+ */
+fun pathOfPreOrderTraversalWithoutRecursion(
+        root: BinaryTreeNode<Int>?): List<BinaryTreeNode<Int>>? {
+    val path = mutableListOf<BinaryTreeNode<Int>>()
+    if (root == null) return path
+    data class Frame(var step: Int = 0, val node: BinaryTreeNode<Int>) {
+        fun nextStep() {
+            step++
+        }
+    }
+    val stack = LinkedList<Frame>()
+    stack.push(Frame(node = root))
+    while (stack.isNotEmpty()) {
+        val frame = stack.peek()
+        when (frame.step) {
+            0 -> {
+                path.add(frame.node)
+                frame.node.left?.let {
+                    stack.push(Frame(node = it))
+                }
+                frame.nextStep()
+            }
+            1 -> {
+                frame.node.right?.let {
+                    stack.push(Frame(node = it))
+                }
+                frame.nextStep()
+            }
+            2 -> {
+                stack.pop()
+            }
+        }
+    }
+    return path
+}
---
+++ java/src/test/kotlin/com/lagostout/elementsofprogramminginterviews/binarytrees/PreOrderTraversalWithoutRecursionSpek.kt
@@ -0,0 +1,76 @@
+package com.lagostout.elementsofprogramminginterviews.binarytrees
+
+import com.lagostout.common.BinaryTreeNode
+import com.lagostout.common.RawBinaryTreeNode
+import org.jetbrains.spek.api.Spek
+import org.jetbrains.spek.api.dsl.describe
+import org.jetbrains.spek.api.dsl.given
+import org.jetbrains.spek.api.dsl.it
+import org.junit.platform.runner.JUnitPlatform
+import org.junit.runner.RunWith
+import kotlin.test.assertEquals
+
+@RunWith(JUnitPlatform::class)
+class PreOrderTraversalWithoutRecursionSpek : Spek({
+    describe("pathOfPreOrderTraversalWithoutRecursion") {
+        testCases.forEach {
+            given("binary tree ${it.root}") {
+                it("computes the pre-order traversal " +
+                        "path as ${it.expectedPath}") {
+                    assertEquals(it.expectedPath,
+                            pathOfPreOrderTraversalWithoutRecursion(it.root))
+                }
+            }
+        }
+    }
+}) {
+    companion object {
+        data class TestCase(private val rawTree: List<RawBinaryTreeNode<Int>> = emptyList(),
+                            private val nodeIndicesOfExpectedPath: List<Int> = emptyList()) {
+            val root: BinaryTreeNode<Int>?
+            val expectedPath: List<BinaryTreeNode<Int>>
+            init {
+                val (root, expectedPath) = (fun (rawTree: List<RawBinaryTreeNode<Int>>):
+                        Pair<BinaryTreeNode<Int>?, List<BinaryTreeNode<Int>>> {
+                    val (root, nodes) = BinaryTreeNode.buildBinaryTree(rawTree)
+                    val pathNodes = nodeIndicesOfExpectedPath.map {
+                        nodes[it]
+                    }
+                    return Pair(root, pathNodes)
+                })(rawTree)
+                this.root = root
+                this.expectedPath = expectedPath
+            }
+        }
+        val testCases = listOf(
+                TestCase(),
+                TestCase(listOf(RawBinaryTreeNode(value = 0)), listOf(0)),
+                TestCase(listOf(
+                        RawBinaryTreeNode(value = 0, leftChildIndex = 1),
+                        RawBinaryTreeNode(value = 1)),
+                        listOf(0,1)),
+                TestCase(listOf(
+                        RawBinaryTreeNode(1, 2, value = 0),
+                        RawBinaryTreeNode(value = 1),
+                        RawBinaryTreeNode(value = 2)),
+                        listOf(0,1,2)),
+                TestCase(listOf(
+                        RawBinaryTreeNode(rightChildIndex = 1, value = 0),
+                        RawBinaryTreeNode(2, value = 1),
+                        RawBinaryTreeNode(3, 4, value = 2),
+                        RawBinaryTreeNode(value = 3),
+                        RawBinaryTreeNode(value = 4)),
+                        listOf(0,1,2,3,4)),
+                TestCase(listOf(
+                        RawBinaryTreeNode(1, 4, value = 0),
+                        RawBinaryTreeNode(2, 3, value = 1),
+                        RawBinaryTreeNode(value = 2),
+                        RawBinaryTreeNode(value = 3),
+                        RawBinaryTreeNode(5, 6, value = 4),
+                        RawBinaryTreeNode(value = 5),
+                        RawBinaryTreeNode(value = 6)),
+                        listOf(0,1,2,3,4,5,6)),
+                null
+        ).filterNotNull()
+    }
+}
