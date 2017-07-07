15750fbf58245c44dc454f7bc839ac3f70849d97
lgtout <lagos.tout@gmail.com>
2017-07-07 14:07:56 -0700

    EP-K 18.4 3-sum problem - WIP

---
+++ java/src/test/kotlin/com/lagostout/elementsofprogramminginterviews/dynamicprogramming/ThreeSumProblemSpek.kt
@@ -0,0 +1,12 @@
+package com.lagostout.elementsofprogramminginterviews.dynamicprogramming
+
+import org.jetbrains.spek.api.Spek
+import org.jetbrains.spek.api.dsl.describe
+
+class ThreeSumProblemSpek : Spek({
+    describe("") {
+
+    }
+}) {
+
+}
