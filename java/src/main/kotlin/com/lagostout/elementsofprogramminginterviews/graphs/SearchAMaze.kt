49ed49ec04118847ce4e1015dd6a3b3c1654c503
lgtout <lagos.tout@gmail.com>
2017-07-07 15:47:38 -0700

    EP-K 19.1 Search a maze - WIP

--- java/src/main/kotlin/com/lagostout/elementsofprogramminginterviews/graphs/SearchAMaze.kt
+++ java/src/main/kotlin/com/lagostout/elementsofprogramminginterviews/graphs/SearchAMaze.kt
@@ -4,30 +4,69 @@ import java.util.*
 
 object SearchMaze {
 
-    data class Point(val x: Int, val y: Int)
+    data class Point(val column: Int, val row: Int)
+    data class Result(val path: List<Point>? = null, val graph: Map<Point, Set<Point>>? = null)
 
     fun findPathThroughMaze(
-            maze: Map<Point, Set<Point>>,
-            entry: Point, exit: Point): List<Point> {
+            grid: List<List<Boolean>>,
+            entry: Point, exit: Point): Result {
+        if (!containsOpenPixel(grid, entry) ||
+                !containsOpenPixel(grid, exit)) return Result()
         data class Frame(val point: Point, val adjacentPoints: Iterator<Point>)
+        val graph = toGraph(grid)
         val stackOfFrames = LinkedList<Frame>()
-        stackOfFrames.push(Frame(point = entry,
-                adjacentPoints = maze[entry]?.iterator() ?:
-                        emptyList<Point>().iterator()))
+        stackOfFrames.push(
+                Frame(point = entry, adjacentPoints = graph[entry]!!.iterator()))
         while (stackOfFrames.isNotEmpty() &&
                 !(stackOfFrames.first().point == entry &&
                         stackOfFrames.last().point == exit)) {
-            val frame = stackOfFrames.peek()
-            val adjacentPoints = frame.adjacentPoints
+            val adjacentPoints = stackOfFrames.peek().adjacentPoints
             if (adjacentPoints.hasNext()) {
                 val adjacentPoint = adjacentPoints.next()
-                stackOfFrames.push(Frame(point = adjacentPoint,
-                        adjacentPoints = maze[adjacentPoint]?.iterator() ?:
-                                emptyList<Point>().iterator()))
+                stackOfFrames.push(
+                        Frame(point = adjacentPoint,
+                                adjacentPoints = graph[adjacentPoint]?.iterator() ?:
+                                        emptyList<Point>().iterator()))
             } else {
                 stackOfFrames.pop()
             }
         }
-        return listOf()
+        return Result(stackOfFrames.map { it.point }, graph)
     }
+
+    private fun containsOpenPixel(grid: List<List<Boolean>>, point: Point): Boolean {
+        return (point.row < grid.size && point.column < grid[0].size && grid[point.row][point.column])
+    }
+
+    private fun toGraph(grid: List<List<Boolean>>): Map<Point, Set<Point>> {
+        val adjacencies: MutableMap<Point, MutableSet<Point>> = mutableMapOf()
+        grid.forEachIndexed { rowIndex, list ->
+            list.forEachIndexed {
+                columnIndex, isOpen ->
+                if (isOpen) {
+                    val point = Point(columnIndex, rowIndex)
+                    val adjacentPoints = mutableSetOf<Point>()
+                    adjacencies.put(point, adjacentPoints)
+                    val previousRow = rowIndex - 1
+                    if (previousRow >= 0) {
+                        val previousRowPoint = Point(previousRow, columnIndex)
+                        if (adjacencies.containsKey(previousRowPoint)) {
+                            adjacencies[point]?.add(previousRowPoint)
+                            adjacencies[previousRowPoint]?.add(point)
+                        }
+                    }
+                    val previousColumn = columnIndex - 1
+                    if (previousColumn >= 0) {
+                        val previousColumnPoint = Point(rowIndex, previousColumn)
+                        if (adjacencies.containsKey(previousColumnPoint)) {
+                            adjacencies[point]?.add(previousColumnPoint)
+                            adjacencies[previousColumnPoint]?.add(point)
+                        }
+                    }
+                }
+            }
+        }
+        return adjacencies
+    }
+
 }
--- java/src/test/kotlin/com/lagostout/elementsofprogramminginterviews/graphs/SearchAMazeSpek.kt
+++ java/src/test/kotlin/com/lagostout/elementsofprogramminginterviews/graphs/SearchAMazeSpek.kt
@@ -8,19 +8,23 @@ import org.jetbrains.spek.api.dsl.given
 import org.jetbrains.spek.api.dsl.it
 import org.junit.platform.runner.JUnitPlatform
 import org.junit.runner.RunWith
+import kotlin.test.assertFalse
 import kotlin.test.assertTrue
 
-@RunWith(JUnitPlatform::class)
 class SearchAMazeSpek : Spek({
     describe("") {
         testCases.forEach {
-            (_, entry, exit, pathExists, adjacencies) ->
-            given("entry $entry exit $exit adjacencies $adjacencies") {
+            (grid, entry, exit, pathExists) ->
+            given("entry $entry exit $exit adjacencies $grid") {
+                val (path, graph) = findPathThroughMaze(grid, entry, exit)
                 it("${if (pathExists) "finds" else "doesn't find"} " +
-                        "a path from entry to exit") {
-                    val path = findPathThroughMaze(adjacencies, entry, exit)
-                    assertTrue(isValidPathThroughMaze(
-                            path, entry, exit, adjacencies))
+                        "a path $path from entry to exit") {
+                    if (path == null || graph == null) {
+                        assertFalse(pathExists)
+                    } else {
+                        assertTrue(isPathFromEntryToExit(
+                                path, entry, exit, graph))
+                    }
                 }
             }
         }
@@ -28,18 +32,24 @@ class SearchAMazeSpek : Spek({
 }){
     companion object {
 
-        fun isValidPathThroughMaze(
+        fun isPathFromEntryToExit(
                 path: List<SearchMaze.Point>, from: Point, to: Point,
                 adjacencies: Map<Point, Set<Point>>): Boolean {
             if (path.first() != from || path.last() != to) return false
             @Suppress("LoopToCallChain")
+            var isValidPath = true
             for ((index, point) in path.withIndex()) {
-                if (adjacencies.containsKey(point) && (index == 0 ||
-                        adjacencies[path[index - 1]]?.contains(point) ?: false)) {
-                    return false
+                isValidPath = if (adjacencies.containsKey(point)) {
+                    val isFirstPointInPath = index == 0
+                    isFirstPointInPath
+                } else {
+                    val edgeExistsFromPreviousPointInPath =
+                            adjacencies[path[index - 1]]?.contains(point) ?: false
+                    !edgeExistsFromPreviousPointInPath
                 }
+                if (!isValidPath) break
             }
-            return true
+            return isValidPath
         }
 
         val T = true
@@ -47,75 +57,43 @@ class SearchAMazeSpek : Spek({
 
         data class TestCase(val grid: List<List<Boolean>>,
                             val entry: Point, val exit: Point,
-                            val pathExists: Boolean) {
-            val adjacencies = (fun(grid: List<List<Boolean>>): Map<Point, Set<Point>> {
-                val adjacencies: MutableMap<Point, MutableSet<Point>> = mutableMapOf()
-                grid.forEachIndexed { columnIndex, list ->
-                    list.forEachIndexed {
-                        rowIndex, isOpen ->
-                        if (isOpen) {
-                            val point = Point(columnIndex, rowIndex)
-                            val adjacentPoints = mutableSetOf<Point>()
-                            adjacencies.put(point, adjacentPoints)
-                            val previousRow = rowIndex - 1
-                            if (previousRow >= 0) {
-                                val previousRowPoint = Point(previousRow, columnIndex)
-                                if (adjacencies.containsKey(previousRowPoint)) {
-                                    adjacencies[point]?.add(previousRowPoint)
-                                    adjacencies[previousRowPoint]?.add(point)
-                                }
-                            }
-                            val previousColumn = columnIndex - 1
-                            if (previousColumn >= 0) {
-                                val previousColumnPoint = Point(rowIndex, previousColumn)
-                                if (adjacencies.containsKey(previousColumnPoint)) {
-                                    adjacencies[point]?.add(previousColumnPoint)
-                                    adjacencies[previousColumnPoint]?.add(point)
-                                }
-                            }
-                        }
-                    }
-                }
-                return adjacencies
-            })(grid)
-            operator fun component5() = adjacencies
-        }
+                            val pathExists: Boolean)
 
         val testCases = listOf(
-                TestCase(listOf(
-                        listOf(T)
-                ), Point(0,0), Point(0,0), true),
-                TestCase(listOf(
-                        listOf(F)
-                ), Point(0,0), Point(0,0), false),
-                TestCase(listOf(
-                        listOf(F,T)
-                ), Point(0,0), Point(0,1), false),
-                TestCase(listOf(
-                        listOf(F,T)
-                ), Point(1,0), Point(1,0), true),
+//                TestCase(listOf(
+//                        listOf(T)
+//                ), Point(0,0), Point(0,0), true),
+//                TestCase(listOf(
+//                        listOf(F)
+//                ), Point(0,0), Point(0,0), false),
+//                TestCase(listOf(
+//                        listOf(F,T)
+//                ), Point(0,0), Point(0,1), false),
+//                TestCase(listOf(
+//                        listOf(F,T)
+//                ), Point(1,0), Point(1,0), true),
                 TestCase(listOf(
                         listOf(T,T)
                 ), Point(0,0), Point(1,0), true),
-                TestCase(listOf(
-                        listOf(T,T)
-                ), Point(1,0), Point(0,0), true),
-                TestCase(listOf(
-                        listOf(T,F),
-                        listOf(F,F)
-                ), Point(1,1), Point(0,0), false),
-                TestCase(listOf(
-                        listOf(T,F),
-                        listOf(F,F)
-                ), Point(0,0), Point(1,1), false),
-                TestCase(listOf(
-                        listOf(T,F),
-                        listOf(T,F)
-                ), Point(0,0), Point(0,1), true),
-                TestCase(listOf(
-                        listOf(T,F),
-                        listOf(T,T)
-                ), Point(0,1), Point(1,1), true),
+//                TestCase(listOf(
+//                        listOf(T,T)
+//                ), Point(1,0), Point(0,0), true),
+//                TestCase(listOf(
+//                        listOf(T,F),
+//                        listOf(F,F)
+//                ), Point(1,1), Point(0,0), false),
+//                TestCase(listOf(
+//                        listOf(T,F),
+//                        listOf(F,F)
+//                ), Point(0,0), Point(1,1), false),
+//                TestCase(listOf(
+//                        listOf(T,F),
+//                        listOf(T,F)
+//                ), Point(0,0), Point(0,1), true),
+//                TestCase(listOf(
+//                        listOf(T,F),
+//                        listOf(T,T)
+//                ), Point(0,1), Point(1,1), true),
                 // TODO More!!
                 null
         ).filterNotNull()
