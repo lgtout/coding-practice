c4d34bd8473aa03d85a5c01d87f103bfffce8c33
lgtout <lagos.tout@gmail.com>
2017-07-08 16:15:37 -0700

    EP-K 13.7.5 Rearrange array so no equal elements k or less apart - WIP

--- java/src/main/kotlin/com/lagostout/elementsofprogramminginterviews/hashtables/RearrarangeArraySoNoEqualElementsAreKOrLessApart.kt
+++ java/src/main/kotlin/com/lagostout/elementsofprogramminginterviews/hashtables/RearrarangeArraySoNoEqualElementsAreKOrLessApart.kt
@@ -1,2 +1,38 @@
 package com.lagostout.elementsofprogramminginterviews.hashtables
 
+import java.util.*
+
+/**
+ * Problem 13.7.5 page 225
+ */
+fun rearrangeArraySoNoEqualElementsAreKOrLessApart(
+        array: List<Int>, k: Int): List<Int> {
+    data class ValueCount(val value: Int, var count: Int)
+    val valueToCountMap = mutableMapOf<Int, Int>()
+    val sortedArray = mutableListOf<Int>()
+    array.forEach {
+        val count = valueToCountMap.getOrDefault(it, 0) + 1
+        valueToCountMap[it] = count
+    }
+    val sortedValueCountList = LinkedList<ValueCount>(
+            valueToCountMap.toSortedMap().map {
+                ValueCount(it.key, it.value) }.toList())
+    while (sortedValueCountList.isNotEmpty()) {
+        val iterator = sortedValueCountList.iterator().withIndex()
+        var groupSize = 0
+        val zeroCountValueIndices = mutableSetOf<Int>()
+        for ((index, valueCount) in iterator) {
+            if (groupSize >= k) break
+            sortedArray.add(valueCount.value)
+            valueCount.count--
+            groupSize++
+            if (valueCount.count == 0) {
+                zeroCountValueIndices.add(index)
+            }
+        }
+        zeroCountValueIndices.map {
+            sortedValueCountList.removeAt(it)
+        }
+    }
+    return sortedArray
+}
