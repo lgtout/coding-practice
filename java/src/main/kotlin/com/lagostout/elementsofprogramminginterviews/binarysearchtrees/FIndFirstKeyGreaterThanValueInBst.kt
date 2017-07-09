0f694aa5f5447dd4b886859af3400832b2c77cde
lgtout <lagos.tout@gmail.com>
2017-07-09 11:20:17 -0700

    EP-K 15.2 Find first key greater than value in BST - WIP

--- java/src/main/kotlin/com/lagostout/elementsofprogramminginterviews/binarysearchtrees/FIndFirstKeyGreaterThanValueInBst.kt
+++ java/src/main/kotlin/com/lagostout/elementsofprogramminginterviews/binarysearchtrees/FIndFirstKeyGreaterThanValueInBst.kt
@@ -3,33 +3,32 @@ package com.lagostout.elementsofprogramminginterviews.binarysearchtrees
 import com.lagostout.common.BinaryTreeNode
 import java.util.*
 
-fun findFirstKeyGreaterThanValueInBst(
+/**
+ * Problem 15.2 page 263
+ */
+fun firstKeyGreaterThanValueInBst(
         root: BinaryTreeNode<Int>, value: Int): Int? {
-    data class Frame(val node: BinaryTreeNode<Int>,
-                     var step: Int = 0,
-                     var previousKey: Int? = null)
-    val stack = LinkedList<Frame>()
-    var firstKeyGreaterThanValue = null
-    stack.add(Frame(root))
-    while (stack.isNotEmpty()) {
-        val frame = stack.peek()
-        val (node, step, previousKey) = frame
-        when (step) {
-            0 -> {
-                node.left.let {
-                    stack.push(Frame(node.left))
-                }
+    var firstKeyGreaterThanValue: Int? = null
+    var upperBound: Int? = null
+    var node = root
+    while (true) {
+        if (node.value <= value) {
+            if (node.right != null) {
+                upperBound = node.value
+                node = node.right
+            } else {
+                firstKeyGreaterThanValue = if (node.value > value) node.value
+                else upperBound
+                break
             }
-            1 -> {
-                val currentKey = node.value
-                if (previousKey == null) {
-                    if (currentKey < value) {
-
-                    }
-                }
+        } else {
+            if (node.left != null) {
+                node = node.left
+            } else {
+                firstKeyGreaterThanValue = node.value
+                break
             }
         }
-        frame.step++
     }
     return firstKeyGreaterThanValue
 }
--- java/src/test/kotlin/com/lagostout/elementsofprogramminginterviews/binarysearchtrees/FIndFirstKeyGreaterThanValueInBstSpek.kt
+++ java/src/test/kotlin/com/lagostout/elementsofprogramminginterviews/binarysearchtrees/FIndFirstKeyGreaterThanValueInBstSpek.kt
@@ -6,13 +6,15 @@ import org.jetbrains.spek.api.Spek
 import org.jetbrains.spek.api.dsl.describe
 import org.jetbrains.spek.api.dsl.given
 import org.jetbrains.spek.api.dsl.it
+import kotlin.test.assertEquals
 
 class FindFirstKeyGreaterThanValueInBstSpek : Spek({
-    describe("") {
+    describe("findFirstKeyGreaterThanValueInBst") {
         testCases.forEach {
-            given("") {
-                it("") {
-
+            (_, value, expected, root) ->
+            given("a binary search tree $root and value $value") {
+                it("finds $expected as the first key greater than value") {
+                    assertEquals(expected, firstKeyGreaterThanValueInBst(root, value))
                 }
             }
 
@@ -21,12 +23,17 @@ class FindFirstKeyGreaterThanValueInBstSpek : Spek({
 }){
     private companion object {
         private data class TestCase(
-                private val rawNodes: List<RawBinaryTreeNode<Int>>,
-                val value: Int, val expected: Int) {
-            val root = BinaryTreeNode.buildBinaryTree(rawNodes).left
+                val rawNodes: List<RawBinaryTreeNode<Int>>,
+                val value: Int = 0, val expected: Int? = null) {
+            val root: BinaryTreeNode<Int> =
+                    BinaryTreeNode.buildBinaryTree(rawNodes).left
+            operator fun component4() = root
         }
         private val testCases = listOf(
-                TestCase(listOf(), 0, 0),
+                TestCase(listOf(RawBinaryTreeNode(value = 10)), 0, 10),
+                TestCase(listOf(RawBinaryTreeNode(value = 10)), 10, null),
+                TestCase(listOf(RawBinaryTreeNode(value = 10)), 15, null),
+                // TODO More cases
                 null
         ).filterNotNull()
     }
