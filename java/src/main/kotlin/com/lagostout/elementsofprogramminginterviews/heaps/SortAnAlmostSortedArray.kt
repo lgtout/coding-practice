bb9c319dc9db83893c8a7dd92a41c5aa74b39d07
lgtout <lagos.tout@gmail.com>
2017-07-10 13:17:24 -0700

    EP-K Sort an almost-sorted array - done!

--- java/src/main/kotlin/com/lagostout/elementsofprogramminginterviews/heaps/SortAnAlmostSortedArray.kt
+++ java/src/main/kotlin/com/lagostout/elementsofprogramminginterviews/heaps/SortAnAlmostSortedArray.kt
@@ -22,9 +22,20 @@ fun sortAlmostSortedArray(array: MutableList<Int>, k: Int) {
             heap.add(Entry(array[j], j))
             j++
         }
-        val entry = heap.poll()
-        array[entry.index] = array[index]
-        array[index] = entry.value
+        val replacementEntry = heap.poll()
+        val currentEntry = Entry(value = array[index], index = index)
+        // Only swap if the currentEntry isn't the replacement.
+        // Otherwise replacementEntry will end up being added
+        // to the heap again, and remain as a candidate for
+        // future positions, even though we've already used it
+        // in the current position (i)
+        if (replacementEntry != currentEntry) {
+            heap.remove(currentEntry)
+            array[replacementEntry.index] = array[index]
+            heap.add(Entry(value = array[replacementEntry.index],
+                    index = replacementEntry.index))
+            array[index] = replacementEntry.value
+        }
         index++
     }
 }
--- java/src/test/kotlin/com/lagostout/elementsofprogramminginterviews/heaps/SortAnAlmostSortedArraySpek.kt
+++ java/src/test/kotlin/com/lagostout/elementsofprogramminginterviews/heaps/SortAnAlmostSortedArraySpek.kt
@@ -28,6 +28,9 @@ class SortAnAlmostSortedArraySpek : Spek({
         }
         val testCases = listOf(
                 TestCase(),
+                // Expected: -1, 2, 3, 4, 5, 6, 8
+                TestCase(mutableListOf(2,-1,3,5,4,8,6), k = 1),
+                TestCase(mutableListOf(3,-1,8,4,5,2,6), k = 4),
                 TestCase(mutableListOf(3,-1,2,6,4,5,8), k = 2),
                 null).filterNotNull()
     }
