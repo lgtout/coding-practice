18922ef2d37b11863bd0ceecf55451fc21af4548
lgtout <lagos.tout@gmail.com>
2017-07-12 16:37:43 -0700

    Binary search tree - WIP

---
+++ java/src/main/kotlin/com/lagostout/common/BinarySearchTree.kt
@@ -0,0 +1,39 @@
+package com.lagostout.common
+
+interface  BinarySearchTreeable {
+//    fun <K, T extends Comparable<K>> find(key: T): BinaryTreeNode<K>
+//    fun next(key: K): BinaryTreeNode<K>
+//    fun insert(key: K)
+//    fun delete(key: K)
+//    fun nearestNeighbors(key: K): List<BinaryTreeNode<K>>
+//    fun rangeSearch(startKey: K, endKey: K): List<BinaryTreeNode<K>>
+}
+
+//class BinarySearchTree<K> : BinarySearchTreeable<K> {
+//
+//    override fun next(key: K): BinaryTreeNode<K> {
+//
+//        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+//    }
+//
+//    override fun insert(key: K) {
+//        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+//    }
+//
+//    override fun delete(key: K) {
+//        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+//    }
+//
+//    override fun nearestNeighbors(key: K): List<BinaryTreeNode<K>> {
+//        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+//    }
+//
+//    override fun rangeSearch(startKey: K, endKey: K): List<BinaryTreeNode<K>> {
+//        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+//    }
+//
+//    override fun find(key: K): BinaryTreeNode<K> {
+//        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+//    }
+//
+//}
