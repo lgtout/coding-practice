1bfcaccf32447b9734df9d7cbf7a03da16404834
lgtout <lagos.tout@gmail.com>
2017-07-14 16:34:52 -0700

    Binary search tree - WIP

--- java/src/main/kotlin/com/lagostout/common/BinarySearchTree.kt
+++ java/src/main/kotlin/com/lagostout/common/BinarySearchTree.kt
@@ -1,30 +1,43 @@
 package com.lagostout.common
 
+import com.google.common.annotations.VisibleForTesting
+
 interface BinarySearchTreeable <T : Comparable<T>> {
-    fun find(key: T): BinaryTreeNode<T>
+    fun find(key: T): BinaryTreeNode<T>?
+    fun findInsertionPoint(key: T): BinaryTreeNode<T>
     fun next(key: T): BinaryTreeNode<T>
     fun insert(key: T)
     fun delete(key: T)
     fun nearestNeighbors(key: T): List<BinaryTreeNode<T>>
     fun rangeSearch(startKey: T, endKey: T): List<BinaryTreeNode<T>>
     fun contains(key: T): Boolean
+    @VisibleForTesting
+    val root: BinaryTreeNode<T>
 }
 
 class BinarySearchTree<T : Comparable<T>> : BinarySearchTreeable<T> {
-    override fun contains(key: T): Boolean {
+
+    override fun find(key: T): BinaryTreeNode<T>? {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 
-    override fun find(key: T): BinaryTreeNode<T> {
-
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
-
-    override fun next(key: T): BinaryTreeNode<T> {
+    // AKA modified find
+    override fun findInsertionPoint(key: T): BinaryTreeNode<T> {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 
     override fun insert(key: T) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override val root: BinaryTreeNode<T>
+        get() = TODO("not implemented") //To change initializer of created properties use File | Settings | File Templates.
+
+    override fun contains(key: T): Boolean {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun next(key: T): BinaryTreeNode<T> {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 
--- java/src/test/kotlin/com/lagostout/common/BinarySearchTreeSpek.kt
+++ java/src/test/kotlin/com/lagostout/common/BinarySearchTreeSpek.kt
@@ -4,7 +4,7 @@ import org.jetbrains.spek.api.Spek
 import org.jetbrains.spek.api.dsl.context
 import org.jetbrains.spek.api.dsl.describe
 import org.jetbrains.spek.api.dsl.it
-import org.jetbrains.spek.api.dsl.on
+import org.jetbrains.spek.api.dsl.xcontext
 import kotlin.test.assertEquals
 import kotlin.test.assertNull
 import kotlin.test.assertTrue
@@ -13,36 +13,67 @@ class BinarySearchTreeSpek : Spek({
     describe("BinarySearchTree") {
 
         val tree = BinarySearchTree<Int>()
+        val key = 1
 
-        context("tree is not empty") {
-            on("find") {
-                context("key is in tree") {
-                    val key = 1
+        describe("inserting a key") {
+            context("tree is empty") {
+                it("should contain only the inserted key") {
                     tree.insert(key)
-                    it("should return BinaryTreeNode containing the key") {
-                        assertEquals(key, tree.find(key).value)
+                    assertTrue(BinarySearchTreeSpek.contains(tree.root, key))
+                }
+            }
+            context("tree is not empty") {
+                beforeEachTest {
+                    tree.insert(key)
+                }
+                it("should contain the inserted and existing keys") {
+                    val key2 = 2
+                    tree.insert(key2)
+                    assertTrue(tree.contains(key2))
+                }
+                it("maintains the binary search tree property") {
+                    assertTrue(maintainsBinarySearchTreeProperty(tree.root))
+                }
+            }
+        }
+
+        describe("finding a key") {
+            context("tree is empty") {
+                it("should return null") {
+                    val keyToFind = 3
+                    assertNull(tree.find(keyToFind))
+                }
+            }
+            context("tree isn't empty") {
+                beforeEachTest {
+                    val keys = listOf(1,5,6,2,4)
+                    keys.forEach { tree.insert(it) }
+                }
+                context("key is in tree") {
+                    val keyToFind = 2
+                    it("should return the node containing the key") {
+                        assertEquals(keyToFind, tree.find(keyToFind)?.value)
+                    }
+                }
+                context("key isn't in tree") {
+                    it("should return null") {
+                        val keyToFind = 11
+                        assertNull(tree.find(keyToFind))
                     }
                 }
             }
         }
 
-        context("tree is empty") {
-            on("find") {
-                it("should return null") {
-                    assertNull(tree.find(1))
-                }
-            }
-            on("insert") {
-                val key = 1
-                tree.insert(key)
-                it("should contain the inserted key") {
-                    assertTrue(tree.contains(key))
-                }
-            }
-            on("contains") {
-
-            }
-        }
-
     }
-})
+}) {
+    companion object {
+        fun <T : Comparable<T>> maintainsBinarySearchTreeProperty(
+                root: BinaryTreeNode<T>): Boolean {
+            return false
+        }
+        fun <T : Comparable<T>> contains(
+                root: BinaryTreeNode<T>, key: T): Boolean {
+            return false
+        }
+    }
+}
