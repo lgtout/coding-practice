5ce4833adb7a2d771dd59e2e9918d717763f9f57
lgtout <lagos.tout@gmail.com>
2017-07-16 21:18:17 -0700

    EP-K 15.2 Find first key greater than value in BST - done!

--- java/src/test/kotlin/com/lagostout/elementsofprogramminginterviews/binarysearchtrees/FIndFirstKeyGreaterThanValueInBstSpek.kt
+++ java/src/test/kotlin/com/lagostout/elementsofprogramminginterviews/binarysearchtrees/FIndFirstKeyGreaterThanValueInBstSpek.kt
@@ -31,38 +31,50 @@ class FindFirstKeyGreaterThanValueInBstSpek : Spek({
             operator fun component4() = root
         }
         private val testCases = listOf(
+                // Single node
                 TestCase(listOf(RawBinaryTreeNode(value = 10)), 0, 10),
                 TestCase(listOf(RawBinaryTreeNode(value = 10)), 10, null),
                 TestCase(listOf(RawBinaryTreeNode(value = 10)), 15, null),
-                TestCase(listOf(
-                        RawBinaryTreeNode(leftChildIndex = 1, value = 10),
-                        RawBinaryTreeNode(value = 5)
-                ), 15, null),
+                // Node -> right node
                 TestCase(listOf(
                         RawBinaryTreeNode(rightChildIndex = 1, value = 10),
-                        RawBinaryTreeNode(value = 14)
-                ), 15, null),
+                        RawBinaryTreeNode(value = 20)
+                ), 15, 20),
                 TestCase(listOf(
                         RawBinaryTreeNode(rightChildIndex = 1, value = 10),
-                        RawBinaryTreeNode(value = 16)
-                ), 15, 16),
+                        RawBinaryTreeNode(value = 20)
+                ), 30, null),
+                // Node -> left node
                 TestCase(listOf(
-                        RawBinaryTreeNode(leftChildIndex = 1, value = 10),
-                        RawBinaryTreeNode(value = 5)
+                        RawBinaryTreeNode(leftChildIndex = 1, value = 20),
+                        RawBinaryTreeNode(value = 10)
                 ), 5, 10),
                 TestCase(listOf(
-                        RawBinaryTreeNode(leftChildIndex = 1, value = 10),
-                        RawBinaryTreeNode(value = 5)
-                ), 6, 10),
+                        RawBinaryTreeNode(leftChildIndex = 1, value = 20),
+                        RawBinaryTreeNode(value = 10)
+                ), 15, 20),
+                // Node -> right node -> left node
                 TestCase(listOf(
-                        RawBinaryTreeNode(leftChildIndex = 1, value = 10),
-                        RawBinaryTreeNode(value = 5)
-                ), 4, 5),
+                        RawBinaryTreeNode(rightChildIndex = 1, value = 10),
+                        RawBinaryTreeNode(leftChildIndex = 2, value = 20),
+                        RawBinaryTreeNode(value = 15)
+                ), 18, 20),
                 TestCase(listOf(
-                        RawBinaryTreeNode(leftChildIndex = 1, value = 10),
-                        RawBinaryTreeNode(value = 0)
-                ), -5, 0),
-                // TODO More cases
+                        RawBinaryTreeNode(rightChildIndex = 1, value = 10),
+                        RawBinaryTreeNode(leftChildIndex = 2, value = 20),
+                        RawBinaryTreeNode(value = 15)
+                ), 10, 15),
+                // Node -> left node -> right node
+                TestCase(listOf(
+                        RawBinaryTreeNode(leftChildIndex = 1, value = 20),
+                        RawBinaryTreeNode(rightChildIndex = 2, value = 10),
+                        RawBinaryTreeNode(value = 15)
+                ), 18, 20),
+                TestCase(listOf(
+                        RawBinaryTreeNode(leftChildIndex = 1, value = 20),
+                        RawBinaryTreeNode(rightChildIndex = 2, value = 10),
+                        RawBinaryTreeNode(value = 15)
+                ), 10, 15),
                 null
         ).filterNotNull()
     }
