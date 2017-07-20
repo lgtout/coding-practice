2bd49ea6747a248ef2e351f2b536e75a021d7797
lgtout <lagos.tout@gmail.com>
2017-07-20 12:00:32 -0700

    EP-K 15.3 Find k largest elements in BST - WIP

--- java/src/main/kotlin/com/lagostout/elementsofprogramminginterviews/binarysearchtrees/FindKLargestElementsInBST.kt
+++ java/src/main/kotlin/com/lagostout/elementsofprogramminginterviews/binarysearchtrees/FindKLargestElementsInBST.kt
@@ -1,2 +1,8 @@
 package com.lagostout.elementsofprogramminginterviews.binarysearchtrees
 
+import com.lagostout.common.BinaryTreeNode
+
+fun <T : Comparable<T>> findKLargestElementsInBST(
+        root: BinaryTreeNode<T>?, k: Int): Set<Int> {
+    return emptySet()
+}
--- java/src/test/kotlin/com/lagostout/elementsofprogramminginterviews/binarysearchtrees/FindKLargestElementsInBSTSpek.kt
+++ java/src/test/kotlin/com/lagostout/elementsofprogramminginterviews/binarysearchtrees/FindKLargestElementsInBSTSpek.kt
@@ -1,2 +1,120 @@
 package com.lagostout.elementsofprogramminginterviews.binarysearchtrees
 
+import com.lagostout.common.BinaryTreeNode
+import com.lagostout.common.BinaryTreeNode.buildBinaryTree
+import com.lagostout.datastructures.RawBinaryTreeNode
+import org.jetbrains.spek.api.Spek
+import org.jetbrains.spek.api.dsl.describe
+import org.jetbrains.spek.api.dsl.given
+import org.jetbrains.spek.api.dsl.it
+import kotlin.test.assertEquals
+
+class FindKLargestElementsInBSTSpek : Spek({
+    describe("findKLargestElementsInBST") {
+        testCases.forEach {
+            (_, k, expected, tree) ->
+            given("tree $tree k $k") {
+                it("finds k largest elements as $expected") {
+                    assertEquals(expected, findKLargestElementsInBST(tree, k))
+                }
+            }
+        }
+    }
+}) {
+    companion object {
+        data class TestCase(val rawNodes: List<RawBinaryTreeNode<Int>>,
+                            var k: Int, var expected: Set<Int>) {
+            val root: BinaryTreeNode<Int>? = buildBinaryTree(rawNodes).left
+            operator fun component4() = root
+        }
+        val testCases: List<TestCase> = listOf(
+                // empty
+                TestCase(listOf(), 0, emptySet()),
+                // n
+                TestCase(listOf(RawBinaryTreeNode(value = 1)), 0, emptySet()),
+                TestCase(listOf(RawBinaryTreeNode(value = 1)), 1, setOf(1)),
+                TestCase(listOf(RawBinaryTreeNode(value = 1)), 2, setOf(1)),
+                // n
+                //  \
+                //   n
+                TestCase(listOf(
+                        RawBinaryTreeNode(value = 1, rightChildIndex = 1),
+                        RawBinaryTreeNode(value = 2)),
+                        1, setOf(2)),
+                TestCase(listOf(
+                        RawBinaryTreeNode(value = 1, rightChildIndex = 1),
+                        RawBinaryTreeNode(value = 2)),
+                        2, setOf(2,1)),
+                TestCase(listOf(
+                        RawBinaryTreeNode(value = 1, rightChildIndex = 1),
+                        RawBinaryTreeNode(value = 2)),
+                        3, setOf(2,1)),
+                //   n
+                //  /
+                // n
+                TestCase(listOf(
+                        RawBinaryTreeNode(value = 2, leftChildIndex = 1),
+                        RawBinaryTreeNode(value = 1)),
+                        1, setOf(2)),
+                TestCase(listOf(
+                        RawBinaryTreeNode(value = 2, leftChildIndex = 1),
+                        RawBinaryTreeNode(value = 1)),
+                        2, setOf(2,1)),
+                TestCase(listOf(
+                        RawBinaryTreeNode(value = 2, leftChildIndex = 1),
+                        RawBinaryTreeNode(value = 1)),
+                        3, setOf(2, 1)),
+                // n
+                //  \
+                //   n
+                //  /
+                // n
+                TestCase(listOf(
+                        RawBinaryTreeNode(value = 2, rightChildIndex = 1),
+                        RawBinaryTreeNode(value = 4, leftChildIndex = 2),
+                        RawBinaryTreeNode(value = 3)),
+                        1, setOf(4)),
+                TestCase(listOf(
+                        RawBinaryTreeNode(value = 2, rightChildIndex = 1),
+                        RawBinaryTreeNode(value = 4, leftChildIndex = 2),
+                        RawBinaryTreeNode(value = 3)),
+                        2, setOf(4,3)),
+                TestCase(listOf(
+                        RawBinaryTreeNode(value = 2, rightChildIndex = 1),
+                        RawBinaryTreeNode(value = 4, leftChildIndex = 2),
+                        RawBinaryTreeNode(value = 3)),
+                        3, setOf(4,3,2)),
+                TestCase(listOf(
+                        RawBinaryTreeNode(value = 2, rightChildIndex = 1),
+                        RawBinaryTreeNode(value = 4, leftChildIndex = 2),
+                        RawBinaryTreeNode(value = 3)),
+                        4, setOf(4,3,2)),
+                //   n
+                //  /
+                // n
+                //  \
+                //   n
+                TestCase(listOf(
+                        RawBinaryTreeNode(value = 4, leftChildIndex = 1),
+                        RawBinaryTreeNode(value = 2, rightChildIndex = 2),
+                        RawBinaryTreeNode(value = 4)),
+                        1, setOf(4)),
+                TestCase(listOf(
+                        RawBinaryTreeNode(value = 4, leftChildIndex = 1),
+                        RawBinaryTreeNode(value = 2, rightChildIndex = 2),
+                        RawBinaryTreeNode(value = 4)),
+                        2, setOf(4,3)),
+                TestCase(listOf(
+                        RawBinaryTreeNode(value = 2, rightChildIndex = 1),
+                        RawBinaryTreeNode(value = 4, leftChildIndex = 2),
+                        RawBinaryTreeNode(value = 3)),
+                        3, setOf(4,3,2)),
+                TestCase(listOf(
+                        RawBinaryTreeNode(value = 2, rightChildIndex = 1),
+                        RawBinaryTreeNode(value = 4, leftChildIndex = 2),
+                        RawBinaryTreeNode(value = 3)),
+                        4, setOf(4,3,2)),
+                null
+        ).filterNotNull()
+    }
+}
