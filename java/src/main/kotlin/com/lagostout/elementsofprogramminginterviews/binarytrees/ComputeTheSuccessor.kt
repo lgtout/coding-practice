0a322efdba2242f7d4b4193d34dc858cd4299a70
lgtout <lagos.tout@gmail.com>
2017-07-21 09:41:34 -0700

    EP-K 10.10 Compute the successor - WIP

--- java/src/main/kotlin/com/lagostout/elementsofprogramminginterviews/binarytrees/ComputeTheSuccessor.kt
+++ java/src/main/kotlin/com/lagostout/elementsofprogramminginterviews/binarytrees/ComputeTheSuccessor.kt
@@ -3,11 +3,26 @@ package com.lagostout.elementsofprogramminginterviews.binarytrees
 import com.lagostout.common.BinaryTreeNode
 
 fun <T : Comparable<T>>computeTheSuccessor(
-        node: BinaryTreeNode<T>, root: BinaryTreeNode<T>) {
+        predecessor: T, root: BinaryTreeNode<T>): Int? {
+    var successor = null
     var currentNode = root
-    while (currentNode != null) {
-        if (currentNode.left != null) {
-
-        }
+    while (true) {
+        if (currentNode.value < predecessor) {
+            if (currentNode.right != null) {
+                currentNode = currentNode.right
+            } else break
+        } else if (currentNode.value > predecessor) {
+            if (currentNode.left != null) {
+                currentNode = currentNode.left
+            } else break
+        } else break
     }
+    if (currentNode.value <= predecessor) {
+        // Find right child left descendant.
+        // If none, find right ancestor.
+        // If none, no successor.
+    } else {
+        // Current node is successor.
+    }
+    return successor
 }
--- java/src/test/kotlin/com/lagostout/elementsofprogramminginterviews/binarytrees/ComputeTheSuccessorSpek.kt
+++ java/src/test/kotlin/com/lagostout/elementsofprogramminginterviews/binarytrees/ComputeTheSuccessorSpek.kt
@@ -13,7 +13,7 @@ import org.jetbrains.spek.api.Spek
  * is present.
  */
 class ComputeTheSuccessorSpek : Spek({
-
+    TODO()
 }) {
     companion object {
         data class TestCase(val rawNodes: List<RawBinaryTreeNode<Int>>,
@@ -21,12 +21,12 @@ class ComputeTheSuccessorSpek : Spek({
                             val expectedNodeIndex: Int? = null) {
             val root: BinaryTreeNode<Int>
             operator fun component4() = root
-            val expectedSuccessor: BinaryTreeNode<Int>?
+            val expectedSuccessor: Int?
             operator fun component5() = expectedSuccessor
             init {
                 val (root, nodes) = BinaryTreeNode.buildBinaryTree(rawNodes)
                 this.root = root
-                expectedSuccessor = expectedNodeIndex?.let { nodes[expectedNodeIndex] }
+                expectedSuccessor = expectedNodeIndex?.let { nodes[expectedNodeIndex].value }
             }
         }
         val testCases = listOf(
@@ -35,7 +35,11 @@ class ComputeTheSuccessorSpek : Spek({
                 // predecessor is not in the tree and is less than range in the tree
                 TestCase(listOf(
                         RawBinaryTreeNode(value = 10)
-                ), 0, null),
+                ), 0, 0),
+                // predecessor is the only node in the tree
+                TestCase(listOf(
+                        RawBinaryTreeNode(value = 10)
+                ), 10, null),
                 // predecessor is not in the tree and is greater than range in the tree
                 TestCase(listOf(
                         RawBinaryTreeNode(value = 10)
@@ -51,18 +55,52 @@ class ComputeTheSuccessorSpek : Spek({
                         RawBinaryTreeNode(value = 5, parentIndex = 0),
                         RawBinaryTreeNode(value = 15, parentIndex = 0)
                 ), 7, null),
-                // predecessor is in the tree and successor is right child
+                // predecessor is in the tree and has right child
                 TestCase(listOf(
                         RawBinaryTreeNode(leftChildIndex = 1, rightChildIndex = 2, value = 10),
                         RawBinaryTreeNode(value = 5, parentIndex = 0),
                         RawBinaryTreeNode(value = 15, parentIndex = 0)
                 ), 10, 2),
+                // predecessor is not in the tree and previous in tree has right child
+                TestCase(listOf(
+                        RawBinaryTreeNode(leftChildIndex = 1, rightChildIndex = 2, value = 10),
+                        RawBinaryTreeNode(value = 5, parentIndex = 0),
+                        RawBinaryTreeNode(value = 15, parentIndex = 0)
+                ), 13, 2),
                 // predecessor is in the tree, has no right child, and is left child
                 TestCase(listOf(
                         RawBinaryTreeNode(leftChildIndex = 1, rightChildIndex = 2, value = 10),
                         RawBinaryTreeNode(value = 5, parentIndex = 0),
                         RawBinaryTreeNode(value = 15, parentIndex = 0)
-                ), 10, 2),
+                ), 5, 0),
+                // predecessor is not in the tree, and previous in tree has no right child,
+                // and is left child
+                TestCase(listOf(
+                        RawBinaryTreeNode(leftChildIndex = 1, rightChildIndex = 2, value = 10),
+                        RawBinaryTreeNode(value = 5, parentIndex = 0),
+                        RawBinaryTreeNode(value = 15, parentIndex = 0)
+                ), 7, 0),
+                // predecessor is in the tree, has no right child, is right child,
+                // and has right ancestor
+                TestCase(listOf(
+                        RawBinaryTreeNode(leftChildIndex = 1, value = 10),
+                        RawBinaryTreeNode(rightChildIndex = 2, value = 5, parentIndex = 0),
+                        RawBinaryTreeNode(value = 7, parentIndex = 1)
+                ), 7, 0),
+                // predecessor is not in the tree, and previous has no right child,
+                // is right child, and has right ancestor
+                TestCase(listOf(
+                        RawBinaryTreeNode(leftChildIndex = 1, value = 10),
+                        RawBinaryTreeNode(rightChildIndex = 2, value = 5, parentIndex = 0),
+                        RawBinaryTreeNode(value = 7, parentIndex = 1)
+                ), 9, 0),
+                // predecessor is in the tree, has no right child, is right child,
+                // and has no right ancestor
+                TestCase(listOf(
+                        RawBinaryTreeNode(leftChildIndex = 1, rightChildIndex = 2, value = 10),
+                        RawBinaryTreeNode(value = 5, parentIndex = 0),
+                        RawBinaryTreeNode(value = 15, parentIndex = 0)
+                ), 15, null),
                 null
         ).filterNotNull()
     }
