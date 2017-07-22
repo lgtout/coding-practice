b6a7bcf2798f877ecab6f873d44b4bcf9203e639
lgtout <lagos.tout@gmail.com>
2017-07-22 10:44:05 -0700

    EP-K 13.7.5 Rearrange array so no equal elements k or less apart - WIP

--- java/src/main/kotlin/com/lagostout/elementsofprogramminginterviews/hashtables/RearrarangeArraySoNoEqualElementsAreKOrLessApart.kt
+++ java/src/main/kotlin/com/lagostout/elementsofprogramminginterviews/hashtables/RearrarangeArraySoNoEqualElementsAreKOrLessApart.kt
@@ -7,37 +7,37 @@ fun rearrangeArraySoNoEqualElementsAreKOrLessApart(
         array: List<Int>, k: Int): List<Int> {
     val sortedArray = mutableListOf<Int>()
     if (array.isEmpty()) return sortedArray
-    val countToValueMap = array.groupingBy { it }.eachCount().
-            asIterable().groupBy( { it.value }, { it.key }).
-            toMutableMap().mapValues(
-            { it.value.toMutableSet().iterator() })
-    val currentCount = countToValueMap.keys.max()!!
-    // Maybe maintain 2 maps?  Would that be simpler than trying to
-    // merge used values into value sets of lower counts?
-    while (currentCount > 0) {
-        var remainingK = k
-        val nextCount = currentCount
-        while (remainingK > 0) {
-            val values = countToValueMap[currentCount]!!
-            val usedValues = mutableSetOf<Int>()
-            values.forEach {
-                usedValues.add(it)
-                values.remove()
-            }
-            if (usedValues.size < remainingK) {
-                remainingK =- usedValues.size
-            }
-            var currentK = 0
-            while (currentK < k) {
-                val value = values.next()
-            }
-//            val kValues = values.take(k)
-//            if (values.)
-//            val nextValues = countToValueMap.getOrDefault(
-//                    nextCount, mutableListOf())
-//            nextValues.addAll(kValues)
-//            remainingK -= values.size
+    val valueCount = array.size
+    val valueToCountMap = array.groupingBy { it }.eachCount().toMutableMap()
+    val indexToAvailableValueMap = mutableMapOf<Int, Int>()
+    val allAvailableValues = mutableSetOf<Int>()
+            .apply { addAll(valueToCountMap.keys) }
+    val availableValues: MutableSet<Int> = allAvailableValues
+    0.rangeTo(valueCount).forEach {
+        // If a value has been freed up in this new position,
+        // let's add it to our set of available values.
+        // TODO We still need to prioritize which value we select by occurrence count.
+        // Maybe availableValues should be a map of counts to values.
+        // Or, for O(n), we could have a list of sets of values, where
+        // we move sets to ever lower indices, where an index represents
+        // a value's remaining occurrence count.
+        indexToAvailableValueMap[it]?.let {
+            availableValues.add(it)
         }
+        availableValues.apply {
+            val value = first()
+            remove(value)
+            valueToCountMap[value] =- 1
+            // Bump value to next index when it's available
+            if (valueToCountMap[value]!! > 0) {
+                val nextAvailableIndex = it + k
+                indexToAvailableValueMap[nextAvailableIndex] = value
+            }
+            return@forEach
+        }
+        // There's a problem:  We've run out of available values.
+        throw IllegalArgumentException("Array cannot be arranged so no " +
+                "equal elements k or less apart")
     }
     return sortedArray
 }
