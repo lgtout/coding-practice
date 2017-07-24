99f000b4245f13e487c8f44cf0839e78510b8d56
lgtout <lagos.tout@gmail.com>
2017-07-24 12:35:16 -0700

    EP-K 10.10 Compute the successor - WIP

--- java/src/main/kotlin/com/lagostout/elementsofprogramminginterviews/binarytrees/ComputeTheSuccessor.kt
+++ java/src/main/kotlin/com/lagostout/elementsofprogramminginterviews/binarytrees/ComputeTheSuccessor.kt
@@ -2,27 +2,40 @@ package com.lagostout.elementsofprogramminginterviews.binarytrees
 
 import com.lagostout.common.BinaryTreeNode
 
+/**
+ * Problem 10.10 page 164
+ */
 fun <T : Comparable<T>>computeTheSuccessor(
-        predecessor: T, root: BinaryTreeNode<T>): Int? {
-    var successor = null
+        predecessor: T, root: BinaryTreeNode<T>): T? {
+    val successor: T?
+    var rightAncestor: BinaryTreeNode<T>? = null
     var currentNode = root
+    // Find the predecessor and, along the way, track the
+    // most recent right ancestor.
+    // We will end up with a node whose value is ==, <,
+    // or > the predecessor.
     while (true) {
         if (currentNode.value < predecessor) {
-            if (currentNode.right != null) {
-                currentNode = currentNode.right
-            } else break
+            if (currentNode.right == null) break
+            currentNode = currentNode.right
         } else if (currentNode.value > predecessor) {
-            if (currentNode.left != null) {
-                currentNode = currentNode.left
-            } else break
+            if (currentNode.left == null) break
+            rightAncestor = currentNode
+            currentNode = currentNode.left
         } else break
     }
-    if (currentNode.value <= predecessor) {
-        // Find right child left descendant.
-        // If none, find right ancestor.
-        // If none, no successor.
+    successor = if (currentNode.value < predecessor ||
+            (currentNode.value == predecessor &&
+                    currentNode.right == null)) {
+        rightAncestor?.value
     } else {
-        // Current node is successor.
+        if (currentNode.value == predecessor) {
+            currentNode = currentNode.right
+            while (currentNode.left != null) {
+                currentNode = currentNode.left
+            }
+        }
+        currentNode.value
     }
     return successor
 }
