52ad79cac8f6a66646eb50e84ee8e546ac623fbc
lgtout <lagos.tout@gmail.com>
2017-07-26 21:24:29 -0700

    EP-K 13.7.5 Rearrange array so no equal elements k or less apart - WIP

--- java/src/main/kotlin/com/lagostout/elementsofprogramminginterviews/hashtables/RearrarangeArraySoNoEqualElementsAreKOrLessApart.kt
+++ java/src/main/kotlin/com/lagostout/elementsofprogramminginterviews/hashtables/RearrarangeArraySoNoEqualElementsAreKOrLessApart.kt
@@ -11,19 +11,19 @@ fun rearrangeArraySoNoEqualElementsAreKOrLessApart(
     val valueCount = array.size
     val countToValuesMap = array.groupingBy { it }.eachCount()
             .entries.groupBy({ it.value }, { it.key })
+            .mapValues { it.value.toMutableList()}
             .toSortedMap(Comparator { k1:Int, k2:Int -> k2.compareTo(k1)})
     val indexToAvailableValuesMap = mutableMapOf<Int, ValueCount>()
     val currentIndex = 0
     // TODO Use while(values.isNotEmpty()) to drain values while possibly adding to values
-    countToValuesMap.forEach {
-        (count, values) ->
-        values.forEach {
-            sortedArray.add(it)
-            indexToAvailableValuesMap.put(
-                    sortedArray.size + k,
-                    ValueCount(it, count - 1))
+    0.rangeTo(array.size - 1).forEach {
+        indexToAvailableValuesMap[it]?.also {
+            (value, count) ->
+            countToValuesMap[count]?: run {
+                countToValuesMap[count] = mutableListOf<Int>()
+                countToValuesMap[count]!!
+            }.apply { add(value) }
         }
-
     }
-    return emptyList()
+    return sortedArray
 }
