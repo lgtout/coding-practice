f6dfdd8bfc921492410b99eaa06aec7204e5fd07
lgtout <lagos.tout@gmail.com>
2017-07-27 17:52:56 -0700

    EP-K 17.2 Compute Levenshtein distance - WIP

--- java/src/main/kotlin/com/lagostout/elementsofprogramminginterviews/dynamicprogramming/ComputeLevenshteinDistance.kt
+++ java/src/main/kotlin/com/lagostout/elementsofprogramminginterviews/dynamicprogramming/ComputeLevenshteinDistance.kt
@@ -1,2 +1,18 @@
 package com.lagostout.elementsofprogramminginterviews.dynamicprogramming
 
+fun levenshteinDistance(from: String, to: String): Int {
+    var distance = -1
+    val cache = mutableListOf<MutableList<Int>>()
+    0.rangeTo(to.length).forEach {
+        val row = mutableListOf<Int>()
+        cache.add(row)
+    }
+    val firstRow = cache[0]
+    0.rangeTo(from.length).forEach {
+        firstRow.add(it)
+    }
+    0.rangeTo(to.length).forEach {
+        cache[it].add(it)
+    }
+    return distance
+}
