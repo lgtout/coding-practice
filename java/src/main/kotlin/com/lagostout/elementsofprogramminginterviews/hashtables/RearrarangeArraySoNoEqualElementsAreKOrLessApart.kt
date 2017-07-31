9804d6ca2d9cc061d9d20ffa2a930b5278a98889
lgtout <lagos.tout@gmail.com>
2017-07-30 21:55:56 -0700

    EP-K 13.7.5 Rearrange array so no equal elements k or less apart - WIP

--- java/src/main/kotlin/com/lagostout/elementsofprogramminginterviews/hashtables/RearrarangeArraySoNoEqualElementsAreKOrLessApart.kt
+++ java/src/main/kotlin/com/lagostout/elementsofprogramminginterviews/hashtables/RearrarangeArraySoNoEqualElementsAreKOrLessApart.kt
@@ -8,21 +8,32 @@ fun rearrangeArraySoNoEqualElementsAreKOrLessApart(
     data class ValueCount(val value: Int, val count: Int)
     val sortedArray = mutableListOf<Int>()
     if (array.isEmpty()) return sortedArray
-    val valueCount = array.size
     val countToValuesMap = array.groupingBy { it }.eachCount()
             .entries.groupBy({ it.value }, { it.key })
             .mapValues { it.value.toMutableList()}
-            .toSortedMap(Comparator { k1:Int, k2:Int -> k2.compareTo(k1)})
-    val indexToAvailableValuesMap = mutableMapOf<Int, ValueCount>()
-    val currentIndex = 0
+            // Highest to lowest (reversed) order
+            .toSortedMap(Comparator { o1, o2 -> o2.compareTo(o1)} )
+    val indexToFreedUpValuesMap = mutableMapOf<Int, ValueCount>()
+//    val currentIndex = 0
     // TODO Use while(values.isNotEmpty()) to drain values while possibly adding to values
-    0.rangeTo(array.size - 1).forEach {
-        indexToAvailableValuesMap[it]?.also {
+    array.indices.forEach {
+        indexToFreedUpValuesMap[it]?.also {
             (value, count) ->
-            countToValuesMap[count]?: run {
-                countToValuesMap[count] = mutableListOf<Int>()
-                countToValuesMap[count]!!
-            }.apply { add(value) }
+            countToValuesMap.getOrPut(count) {
+               mutableListOf<Int>()
+            }.add(value)
+        }
+        val countToValuesEntry = countToValuesMap.entries.first()
+        val value = countToValuesEntry.value.removeAt(0)
+        val count = countToValuesEntry.key
+        indexToFreedUpValuesMap[it + k + 1] =
+                ValueCount(value, count - 1)
+        // If there are no more items with the current count,
+        // remove the entry from the sorted array so that the
+        // next set of items with the next lower count can be
+        // used.
+        if (countToValuesEntry.value.size == 0) {
+            countToValuesMap.entries.remove(countToValuesEntry)
         }
     }
     return sortedArray
--- java/src/test/kotlin/com/lagostout/elementsofprogramminginterviews/hashtables/RearrarangeArraySoNoEqualElementsAreKOrLessApartSpek.kt
+++ java/src/test/kotlin/com/lagostout/elementsofprogramminginterviews/hashtables/RearrarangeArraySoNoEqualElementsAreKOrLessApartSpek.kt
@@ -1,2 +1,3 @@
 package com.lagostout.elementsofprogramminginterviews.hashtables
 
+
