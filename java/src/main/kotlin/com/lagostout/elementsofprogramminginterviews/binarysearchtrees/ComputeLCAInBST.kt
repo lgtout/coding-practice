1bef7445f957b742e2df8b6ffd79be0b089c766c
lgtout <lagos.tout@gmail.com>
2017-07-31 09:23:39 -0700

    EP-K 15.4 Compute LCA in BST - WIP

--- java/src/main/kotlin/com/lagostout/elementsofprogramminginterviews/binarysearchtrees/ComputeLCAInBST.kt
+++ java/src/main/kotlin/com/lagostout/elementsofprogramminginterviews/binarysearchtrees/ComputeLCAInBST.kt
@@ -2,19 +2,44 @@ package com.lagostout.elementsofprogramminginterviews.binarysearchtrees
 
 import com.lagostout.common.BinaryTreeNode
 
+/**
+ * Problem 15.4 page 265
+ */
 fun <T : Comparable<T>> computeLCAInBST(
         root: BinaryTreeNode<T>?,
         firstDescendant: T,
         secondDescendant: T): BinaryTreeNode<T>? {
     var lca: BinaryTreeNode<T>? = null
-    var currentNode = root?: return null
+    var currentNode: BinaryTreeNode<T>? = root?: return null
     val descendants = listOf(firstDescendant, secondDescendant).sorted()
-    while (true) {
-        if (!descendants.contains(currentNode.value)) break
-//        currentNode = currentNode.run {
-//            if (descendants[0] > value) right
-//            else if (descendants[1] < value ) left
-//            else
+    val (lowerDescendant, higherDescendant) = descendants
+    while (currentNode != null) {
+        if (lowerDescendant > currentNode.value) {
+            currentNode = currentNode.right
+        } else if (higherDescendant < currentNode.value) {
+            currentNode = currentNode.left
+        } else if ((lowerDescendant <= currentNode.value)
+                and (higherDescendant >= currentNode.value)) {
+            @Suppress("NAME_SHADOWING")
+            fun find(root: BinaryTreeNode<T>, value: T): BinaryTreeNode<T>? {
+                var node = root
+                while ((node.value != null) and
+                        (node.value != node.value)) {
+                    node = if (value < node.value) {
+                        node.left
+                    } else {
+                        node.right
+                    }
+                }
+                return node
+            }
+            if (find(currentNode, lowerDescendant) != null &&
+                    find(currentNode, higherDescendant) != null) {
+                // Not found. No LCA.
+                lca = currentNode
+            }
+            break
+        }
     }
     return lca
 }
--- java/src/test/kotlin/com/lagostout/elementsofprogramminginterviews/binarysearchtrees/ComputeLCAInBSTSpek.kt
+++ java/src/test/kotlin/com/lagostout/elementsofprogramminginterviews/binarysearchtrees/ComputeLCAInBSTSpek.kt
@@ -5,12 +5,19 @@ import com.lagostout.datastructures.RawBinaryTreeNode
 import org.jetbrains.spek.api.Spek
 import org.jetbrains.spek.api.dsl.describe
 import org.jetbrains.spek.api.dsl.given
+import org.jetbrains.spek.api.dsl.it
+import kotlin.test.assertEquals
 
 class ComputeLCAInBSTSpek : Spek({
-    describe("") {
+    describe("computeLCAInBST") {
         testCases.forEach {
-            given("") {
-
+            (root, firstDescendant, secondDescendant, expectedLCA) ->
+            given("root $root, firstDescendant $firstDescendant, " +
+                    "secondDescendant $secondDescendant") {
+                it("finds the LCA as $expectedLCA") {
+                    assertEquals(expectedLCA, computeLCAInBST(
+                            root, firstDescendant, secondDescendant))
+                }
             }
         }
     }
@@ -20,8 +27,20 @@ class ComputeLCAInBSTSpek : Spek({
         class TestCase(rawNodes: List<RawBinaryTreeNode<Int>> = emptyList(),
                        val firstDescendant: Int = -1,
                        val secondDescendant: Int = -1,
-                       val expectedLCA: Int? = null) {
-            val root: BinaryTreeNode<Int>? = BinaryTreeNode.buildBinaryTree(rawNodes).left
+                       val expectedLCAIndex: Int? = null) {
+            val root: BinaryTreeNode<Int>?
+            val expectedLCA: BinaryTreeNode<Int>?
+
+            init {
+                val result = BinaryTreeNode.buildBinaryTree(rawNodes)
+                root = result.left
+                expectedLCA = expectedLCAIndex?.run { result.right[this] }
+            }
+
+            operator fun component1() = root
+            operator fun component2() = firstDescendant
+            operator fun component3() = secondDescendant
+            operator fun component4() = expectedLCA
         }
 
         val testCases: List<TestCase> = run {
@@ -34,7 +53,7 @@ class ComputeLCAInBSTSpek : Spek({
                     TestCase(listOf(RawBinaryTreeNode(value = 1)), firstDescendant = 1),
                     TestCase(listOf(RawBinaryTreeNode(value = 1)), secondDescendant = 1),
                     TestCase(listOf(RawBinaryTreeNode(value = 1)),
-                            firstDescendant = 1, secondDescendant = 1, expectedLCA = 1),
+                            firstDescendant = 1, secondDescendant = 1, expectedLCAIndex = 0),
                     /**
                      *    1
                      *   /
@@ -42,17 +61,21 @@ class ComputeLCAInBSTSpek : Spek({
                      */
                     TestCase(listOf(RawBinaryTreeNode(leftChildIndex = 1, value = 1),
                             RawBinaryTreeNode(value = 0)), firstDescendant = 0,
-                            secondDescendant = 1, expectedLCA = 0),
+                            secondDescendant = 1, expectedLCAIndex = 0),
                     // First/last descendants reversed
                     TestCase(listOf(RawBinaryTreeNode(leftChildIndex = 1, value = 1),
                             RawBinaryTreeNode(value = 0)), firstDescendant = 1,
-                            secondDescendant = 0, expectedLCA = 0),
-                    // Descendants missing
+                            secondDescendant = 0, expectedLCAIndex = 0),
+                    // Descendant missing
                     TestCase(listOf(RawBinaryTreeNode(leftChildIndex = 1, value = 1),
                             RawBinaryTreeNode(value = 0)), firstDescendant = 2,
                             secondDescendant = 0),
                     TestCase(listOf(RawBinaryTreeNode(leftChildIndex = 1, value = 1),
                             RawBinaryTreeNode(value = 0)), firstDescendant = 1,
+                            secondDescendant = -1),
+                    // Descendants missing
+                    TestCase(listOf(RawBinaryTreeNode(leftChildIndex = 1, value = 1),
+                            RawBinaryTreeNode(value = 0)), firstDescendant = 2,
                             secondDescendant = -1),
                     /**
                      *  1
@@ -61,17 +84,18 @@ class ComputeLCAInBSTSpek : Spek({
                      */
                     TestCase(listOf(RawBinaryTreeNode(rightChildIndex = 1, value = 1),
                             RawBinaryTreeNode(value = 2)), firstDescendant = 0,
-                            secondDescendant = 1, expectedLCA = 1),
+                            secondDescendant = 1, expectedLCAIndex = 0),
                     // First/last descendants reversed
                     TestCase(listOf(RawBinaryTreeNode(rightChildIndex = 1, value = 1),
                             RawBinaryTreeNode(value = 2)), firstDescendant = 1,
                             secondDescendant = 0),
+                    // Descendant missing
+                    TestCase(listOf(RawBinaryTreeNode(rightChildIndex = 1, value = 1),
+                            RawBinaryTreeNode(value = 2)), firstDescendant = 1,
+                            secondDescendant = 3),
                     // Descendants missing
                     TestCase(listOf(RawBinaryTreeNode(rightChildIndex = 1, value = 1),
                             RawBinaryTreeNode(value = 2)), firstDescendant = 0,
-                            secondDescendant = 0),
-                    TestCase(listOf(RawBinaryTreeNode(rightChildIndex = 1, value = 1),
-                            RawBinaryTreeNode(value = 2)), firstDescendant = 1,
                             secondDescendant = 3),
                     // TODO More cases
                     null).filterNotNull()
