0fd2c228935ac503efa638553d96cbda84ba0268
lgtout <lagos.tout@gmail.com>
2017-08-05 13:50:02 -0700

    EP-K 10.11.2 Iterative post- and pre-order traversal O(1) space - WIP

---
+++ java/src/main/kotlin/com/lagostout/elementsofprogramminginterviews/binarytrees/IterativePreAndPostOrderTraversalWithLinearSpace.kt
@@ -0,0 +1,8 @@
+package com.lagostout.elementsofprogramminginterviews.binarytrees
+
+import com.lagostout.datastructures.BinaryTreeNode
+
+fun <T> preOrderTraversal(root: BinaryTreeNode<T>): List<T> {
+    var result = emptyList<T>()
+    return result
+}
