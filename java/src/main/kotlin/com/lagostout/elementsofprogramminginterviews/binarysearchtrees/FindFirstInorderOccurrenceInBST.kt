059990538a80f58478e7d0e8fd2d37c54280b644
lgtout <lagos.tout@gmail.com>
2017-08-06 13:30:15 -0700

    EP-K 15.2.2 Find first inorder occurrence in BST - WIP

--- java/src/main/kotlin/com/lagostout/elementsofprogramminginterviews/binarysearchtrees/FindFirstInorderOccurrenceInBST.kt
+++ java/src/main/kotlin/com/lagostout/elementsofprogramminginterviews/binarysearchtrees/FindFirstInorderOccurrenceInBST.kt
@@ -1,2 +1,19 @@
 package com.lagostout.elementsofprogramminginterviews.binarysearchtrees
 
+import com.lagostout.datastructures.BinaryTreeNode
+
+// TODO Does it make sense that root is nullable?
+fun <T : Comparable<T>> findFirstInorderOccurrence(
+        root: BinaryTreeNode<T>?, k: T): BinaryTreeNode<T>? {
+    var firstOccurrence: BinaryTreeNode<T>? = null
+    var currentNode: BinaryTreeNode<T>? = root
+    while (currentNode != null) {
+        currentNode = currentNode?.run {
+            if (value >= k) {
+                if (value == k) firstOccurrence = this
+                left
+            } else right
+        }
+    }
+    return firstOccurrence
+}
--- java/src/test/kotlin/com/lagostout/elementsofprogramminginterviews/binarysearchtrees/FindFirstInorderOccurrenceInBSTSpek.kt
+++ java/src/test/kotlin/com/lagostout/elementsofprogramminginterviews/binarysearchtrees/FindFirstInorderOccurrenceInBSTSpek.kt
@@ -1,2 +1,46 @@
 package com.lagostout.elementsofprogramminginterviews.binarysearchtrees
 
+import com.lagostout.datastructures.BinaryTreeNode
+import com.lagostout.datastructures.BinaryTreeNode.Companion.buildBinaryTree
+import com.lagostout.datastructures.RawBinaryTreeNode
+import org.jetbrains.spek.api.Spek
+import org.jetbrains.spek.api.dsl.describe
+import org.jetbrains.spek.api.dsl.given
+import org.jetbrains.spek.api.dsl.it
+import kotlin.test.assertEquals
+
+class FindFirstInorderOccurrenceInBSTSpek : Spek({
+    describe("") {
+        testCases.forEach {
+            (root, expectedNode, k) ->
+            given("") {
+                it("") {
+                    assertEquals(expectedNode, findFirstInorderOccurrence(root, k))
+                }
+            }
+        }
+    }
+}) {
+    companion object {
+        class TestCase<T : Comparable<T>>(rawNodes: List<RawBinaryTreeNode<T>> = emptyList(),
+                                          expectedNodeIndex: Int? = null,
+                                          val k: Int = 0) {
+            val root: BinaryTreeNode<T>?
+            val expected: BinaryTreeNode<T>?
+            init {
+                val (root, expected) = buildBinaryTree(rawNodes).let { (root, nodes) ->
+                    Pair(root, expectedNodeIndex?.let { nodes[it] })
+                }
+                this.root = root
+                this.expected = expected
+            }
+            operator fun component1(): BinaryTreeNode<T>? = root
+            operator fun component2(): BinaryTreeNode<T>? = expected
+            operator fun component3() = k
+        }
+        val testCases = listOf<TestCase<Int>?>(
+                TestCase(),
+                null
+        ).filterNotNull()
+    }
+}
