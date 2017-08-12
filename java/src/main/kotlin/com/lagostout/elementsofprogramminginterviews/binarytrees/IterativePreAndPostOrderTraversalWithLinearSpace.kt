9b73c2a7fd5dce7fce71d79c0db3ab6fe0071be1
lgtout <lagos.tout@gmail.com>
2017-08-11 20:42:40 -0700

    EP-K 10.11.2 Iterative post- and pre-order traversal O(1) space - WIP

--- java/src/main/kotlin/com/lagostout/elementsofprogramminginterviews/binarytrees/IterativePreAndPostOrderTraversalWithLinearSpace.kt
+++ java/src/main/kotlin/com/lagostout/elementsofprogramminginterviews/binarytrees/IterativePreAndPostOrderTraversalWithLinearSpace.kt
@@ -2,21 +2,43 @@ package com.lagostout.elementsofprogramminginterviews.binarytrees
 
 import com.lagostout.datastructures.BinaryTreeNode
 
-fun <T : Comparable<T>> preOrderTraversal(root: BinaryTreeNode<T>): List<T> {
-    var result = mutableListOf<T>()
-    var node: BinaryTreeNode<T>? = root
-    var previousNode: BinaryTreeNode<T>? = null
-    while (node != null) {
-        if (previousNode == node.parent) {
-            result.add(node.value)
-            node = node.left?: node.parent
-        } else
-        previousNode = node
-    }
-    return result
-}
+object IterativePreAndPostOrderTraversalWithLinearSpace {
 
-fun <T> postOrderTraversal(root: BinaryTreeNode<T>): List<T> {
-    var result = emptyList<T>()
-    return result
+    enum class Order {
+        PRE, POST
+    }
+
+    fun <T : Comparable<T>> traversal(root: BinaryTreeNode<T>, order: Order): List<T> {
+        val result = mutableListOf<T>()
+        var node: BinaryTreeNode<T>? = root
+        var previousNode: BinaryTreeNode<T>? = null
+        while (node != null) {
+            when (previousNode) {
+                node.parent -> {
+                    if (order == Order.PRE)
+                        result.add(node.value)
+                    previousNode = node
+                    node = node.left?: node.parent
+                }
+                node.left -> {
+                    node = node.right?: node.parent
+                    previousNode = node
+                }
+                node.right -> {
+                    if (order == Order.POST)
+                        result.add(node.value)
+                    previousNode = node
+                    node = node.parent
+                }
+            }
+        }
+        return result
+    }
+
+    fun <T : Comparable<T>> preOrderTraversal(root: BinaryTreeNode<T>): List<T>
+            = traversal(root, Order.PRE)
+
+    fun <T : Comparable<T>> postOrderTraversal(root: BinaryTreeNode<T>): List<T>
+            = traversal(root, Order.POST)
+
 }
--- java/src/test/kotlin/com/lagostout/elementsofprogramminginterviews/binarytrees/IterativePreAndPostOrderTraversalWithLinearSpaceSpek.kt
+++ java/src/test/kotlin/com/lagostout/elementsofprogramminginterviews/binarytrees/IterativePreAndPostOrderTraversalWithLinearSpaceSpek.kt
@@ -1,2 +1,68 @@
 package com.lagostout.elementsofprogramminginterviews.binarytrees
 
+import com.lagostout.common.BinaryTreeNode
+import com.lagostout.datastructures.RawBinaryTreeNode
+import org.jetbrains.spek.api.Spek
+import org.jetbrains.spek.api.dsl.describe
+import org.jetbrains.spek.api.dsl.given
+import org.jetbrains.spek.api.dsl.it
+
+class IterativePreAndPostOrderTraversalWithLinearSpaceSpek : Spek({
+    describe("preOrderTraversal") {
+        testCases.forEach {
+            given("") {
+                it("") {
+
+                }
+            }
+        }
+    }
+}) {
+    companion object {
+        // TODO - Recursive pre- and post-order traversal for solution comparison
+        data class TestCase<T : Comparable<T>>(
+                val rawTree: List<RawBinaryTreeNode<T>> = emptyList(),
+                val preOrderTraversalExpectedIndices: List<Int> = emptyList(),
+                val postOrderTraversalExpectedIndices: List<Int> = emptyList())
+        val testCases = listOf(
+                TestCase(),
+                TestCase(listOf(RawBinaryTreeNode(value = 0)), listOf(0)),
+                TestCase(listOf(
+                        RawBinaryTreeNode(value = 0, leftChildIndex = 1),
+                        RawBinaryTreeNode(value = 1)),
+                        listOf(0,1),
+                        listOf(1,0)),
+                TestCase(listOf(
+                        RawBinaryTreeNode(1, 2, value = 0),
+                        RawBinaryTreeNode(value = 1),
+                        RawBinaryTreeNode(value = 2)),
+                        listOf(0,1,2),
+                        listOf(1,2,0)),
+                //    0
+                //     \
+                //      1
+                //     /
+                //    2
+                //   /  \
+                //  3    4
+                TestCase(listOf(
+                        RawBinaryTreeNode(rightChildIndex = 1, value = 0),
+                        RawBinaryTreeNode(2, value = 1),
+                        RawBinaryTreeNode(3, 4, value = 2),
+                        RawBinaryTreeNode(value = 3),
+                        RawBinaryTreeNode(value = 4)),
+                        listOf(0,1,2,3,4),
+                        listOf(0,1,2,3,4)),
+                TestCase(listOf(
+                        RawBinaryTreeNode(1, 4, value = 0),
+                        RawBinaryTreeNode(2, 3, value = 1),
+                        RawBinaryTreeNode(value = 2),
+                        RawBinaryTreeNode(value = 3),
+                        RawBinaryTreeNode(5, 6, value = 4),
+                        RawBinaryTreeNode(value = 5),
+                        RawBinaryTreeNode(value = 6)),
+                        listOf(0,1,2,3,4,5,6)),
+                null
+        ).filterNotNull()
+    }
+}
