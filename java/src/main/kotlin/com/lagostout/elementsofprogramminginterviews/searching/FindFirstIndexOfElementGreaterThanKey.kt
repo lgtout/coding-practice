16979ff2c3585d512f0a912504be9c8104afa57d
lgtout <lagos.tout@gmail.com>
2017-08-14 14:18:19 -0700

    EP-K 12.1.2 Find first index of element greater than key - WIP

--- java/src/main/kotlin/com/lagostout/elementsofprogramminginterviews/searching/FindFirstIndexOfElementGreaterThanKey.kt
+++ java/src/main/kotlin/com/lagostout/elementsofprogramminginterviews/searching/FindFirstIndexOfElementGreaterThanKey.kt
@@ -1,2 +1,21 @@
 package com.lagostout.elementsofprogramminginterviews.searching
 
+fun findFirstIndexOfElementGreaterThanKey(sortedList: List<Int>, key: Int): Int? {
+    // Cases:
+    // Key is less than every element in the list.
+    // Key is greater than every element in the list.
+    // Key is in the range of elements in the list.
+    // List is empty.
+    var i = 0
+    var j = sortedList.size
+    // Condition i < j is safe when list is empty.
+    while (i < j) {
+        val mid = j + (i - j)/2
+        val value = sortedList[mid]
+        if (value <= key)
+            i = mid + 1
+        else if (value > key)
+            j = mid - 1
+    }
+    return if (i < sortedList.lastIndex) i else null
+}
--- java/src/test/kotlin/com/lagostout/elementsofprogramminginterviews/searching/FindFirstIndexOfElementGreaterThanKeySpek.kt
+++ java/src/test/kotlin/com/lagostout/elementsofprogramminginterviews/searching/FindFirstIndexOfElementGreaterThanKeySpek.kt
@@ -1,2 +1,38 @@
 package com.lagostout.elementsofprogramminginterviews.searching
 
+import org.apache.commons.math3.random.RandomDataGenerator
+import org.jetbrains.spek.api.Spek
+import org.jetbrains.spek.api.dsl.describe
+
+class FindFirstIndexOfElementGreaterThanKeySpek : Spek({
+    describe("") {
+        testCases.forEach {
+
+        }
+    }
+}) {
+    companion object {
+        class TestCase(val items: List<Int>)
+        val testCases = run {
+            val cases = mutableListOf<TestCase>()
+            val testCaseCount = 10
+            val listSizeRange = Pair(0,10)
+            val itemRange = Pair(-10,10)
+            val random = RandomDataGenerator().apply { reSeed(1) }
+            (1..testCaseCount).forEach {
+                val items = mutableListOf<Int>()
+                val listSize = listSizeRange.run {
+                    random.nextInt(first, second)
+                }
+                (1..listSize).forEach {
+                    itemRange.apply {
+                        items.add(random.nextInt(first, second))
+                    }
+                }
+                items.sort()
+                cases.add(TestCase(items))
+            }
+            cases
+        }
+    }
+}
