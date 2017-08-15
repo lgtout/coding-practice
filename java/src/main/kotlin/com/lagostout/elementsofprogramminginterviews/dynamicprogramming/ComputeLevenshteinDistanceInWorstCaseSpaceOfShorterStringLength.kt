d8acb3459385dac13a9475a758c5f8667c06b5bf
lgtout <lagos.tout@gmail.com>
2017-08-15 14:44:15 -0700

    EP-K 17.2.1 Compute Levenshtein distance in O(min(a,b)) space - done!

--- java/src/main/kotlin/com/lagostout/elementsofprogramminginterviews/dynamicprogramming/ComputeLevenshteinDistanceInWorstCaseSpaceOfShorterStringLength.kt
+++ java/src/main/kotlin/com/lagostout/elementsofprogramminginterviews/dynamicprogramming/ComputeLevenshteinDistanceInWorstCaseSpaceOfShorterStringLength.kt
@@ -1,24 +1,34 @@
 package com.lagostout.elementsofprogramminginterviews.dynamicprogramming
 
+/**
+ * Problem 17.2.2 page 316
+ */
 fun levenshteinDistanceInWorstCaseSpaceOfShorterStringLength(from: String, to: String): Int {
-    val (shorterString, longerString) = listOf(from, to).sorted().apply {
-        Pair(first(), get(1))
-    }
+    val (shorterString, longerString) = listOf(from, to).sorted()
     val cache = mutableListOf<Int>().apply {
         (0..shorterString.length).forEach {
             add(it)
         }
     }
-    for (i in 0..longerString.length) {
-        var oldPreviousRowDistance: Int
+    for (i in 1..longerString.length) {
+        var upperLeftDistance = cache[0]
         for (j in 0..shorterString.length) {
             if (j == 0) {
-                oldPreviousRowDistance = cache[0]
                 cache[0]++
             } else {
-
+                val deleteDistance = cache[j] + 1
+                val insertDistance = cache[j-1] + 1
+                val substituteOrDoNothingDistance =
+                        upperLeftDistance +
+                                if (shorterString[j-1] == longerString[i-1])
+                                    0  else 1
+                upperLeftDistance = cache[j]
+                listOf(deleteDistance, insertDistance,
+                        substituteOrDoNothingDistance).min()?.let {
+                    cache[j] = it
+                }
             }
         }
     }
-    return 0
+    return cache.last()
 }
--- java/src/test/kotlin/com/lagostout/elementsofprogramminginterviews/dynamicprogramming/ComputeLevenshteinDistanceSpek.kt
+++ java/src/test/kotlin/com/lagostout/elementsofprogramminginterviews/dynamicprogramming/ComputeLevenshteinDistanceSpek.kt
@@ -38,30 +38,5 @@ class ComputeLevenshteinDistanceSpek : Spek({
             ).filterNotNull()
         }
 
-        // Recursion in either direction - comparing the strings from left-right,
-        // or from right-left - are equally valid solutions.  However, the non-recursive
-        // dynamic programming solution is modeled on the right-left recursive solution.
-
-        fun bruteForceLeftToRightLevenshteinDistance(from: String, to: String): Int {
-            if (from.isEmpty()) return to.length
-            if (to.isEmpty()) return from.length
-            val substituteOrNoActionCost = bruteForceLeftToRightLevenshteinDistance(
-                    from.substring(1), to.substring(1)) +
-                    (if (from.first() == to.first()) 0 else 1)
-            val deleteCost = bruteForceLeftToRightLevenshteinDistance(from.substring(1), to) + 1
-            val insertCost = bruteForceLeftToRightLevenshteinDistance(from, to.substring(1)) + 1
-            return listOf(substituteOrNoActionCost, deleteCost, insertCost).min()!!
-        }
-
-        fun bruteForceRightToLeftLevenshteinDistance(from: String, to: String): Int {
-            if (from.isEmpty()) return to.length
-            if (to.isEmpty()) return from.length
-            val substituteOrNoActionCost = bruteForceRightToLeftLevenshteinDistance(
-                    from.dropLast(1), to.dropLast(1)) +
-                    (if (from.last() == to.last()) 0 else 1)
-            val deleteCost = bruteForceRightToLeftLevenshteinDistance(from.dropLast(1), to) + 1
-            val insertCost = bruteForceRightToLeftLevenshteinDistance(from, to.dropLast(1)) + 1
-            return listOf(substituteOrNoActionCost, deleteCost, insertCost).min()!!
-        }
     }
 }
