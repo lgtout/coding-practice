a0efc5d5008ea8567f2f32ee29ec6041cc246cb0
lgtout <lagos.tout@gmail.com>
2017-08-16 13:34:56 -0700

    EP-K 7.2 Base conversion - WIP

--- java/src/main/kotlin/com/lagostout/elementsofprogramminginterviews/strings/BaseConversion.kt
+++ java/src/main/kotlin/com/lagostout/elementsofprogramminginterviews/strings/BaseConversion.kt
@@ -1,7 +1,43 @@
 package com.lagostout.elementsofprogramminginterviews.strings
 
-fun convertBase(numberInBase1: String, base1: Int, base2: Int) {
-    // Convert numberInBase1 to integer.
+/**
+ * Problem 7.2 page 96
+ */
+fun convertBase(numberInFirstBase: String,
+                firstBase: Int, secondBase: Int): String {
 
-    // Convert integer to string in base 2.
+    // Convert numberInFirstBase to integer.
+    val isNegative = numberInFirstBase.first().isDigit()
+    val reversedNumberInFirstBase = numberInFirstBase.run {
+        takeLast(length - if (isNegative) 1 else 0)
+    }.reversed()
+    var numberInBase10 = 0
+    var magnitude = 1
+    val zeroCharInt = '0'.toInt()
+    val aCharInt = 'A'.toInt()
+    reversedNumberInFirstBase.forEach {
+        val digitCharInt = it.toInt()
+        numberInBase10 += magnitude * (if (it.isDigit())
+            digitCharInt - zeroCharInt
+        else
+            firstBase + (digitCharInt - aCharInt))
+        magnitude *= firstBase
+    }
+
+    // Convert integer to string in second base.
+    val builder = StringBuilder()
+    while (numberInBase10 > 0) {
+        val nextNumberInBase10 = numberInBase10 / secondBase
+        val digitInBase10 = numberInBase10 - nextNumberInBase10
+        val digitCharInSecondBase = (if (digitInBase10 < 10) {
+            digitInBase10 + zeroCharInt
+        } else {
+            aCharInt + digitInBase10 - 10
+        }).toChar()
+        builder.append(digitCharInSecondBase)
+    }
+
+    if (isNegative) builder.append('-')
+
+    return builder.reversed().toString()
 }
--- java/src/test/kotlin/com/lagostout/elementsofprogramminginterviews/strings/BaseConversionSpek.kt
+++ java/src/test/kotlin/com/lagostout/elementsofprogramminginterviews/strings/BaseConversionSpek.kt
@@ -1,2 +1,31 @@
 package com.lagostout.elementsofprogramminginterviews.strings
 
+import org.jetbrains.spek.api.Spek
+import org.jetbrains.spek.api.dsl.describe
+import org.jetbrains.spek.api.dsl.given
+import org.jetbrains.spek.api.dsl.it
+
+class BaseConversionSpek : Spek({
+    describe("") {
+        testCases.forEach {
+            given("") {
+                it("") {
+
+                }
+            }
+        }
+    }
+}) {
+    companion object {
+        data class TestCase(val numberInFirstBase: String,
+                       val firstBase: Int, val secondBase: Int) {
+            val expectedNumberAsStringInSecondBase: String =
+                    Integer.toString(Integer.parseInt(
+                            numberInFirstBase, firstBase), secondBase)
+            operator fun component4() = expectedNumberAsStringInSecondBase
+        }
+        val testCases = run {
+            listOf<TestCase>()
+        }
+    }
+}
