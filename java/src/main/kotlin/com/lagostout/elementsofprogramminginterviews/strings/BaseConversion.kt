640b8c9ebeb34962697ac688c997a9aa74125d22
lgtout <lagos.tout@gmail.com>
2017-08-15 20:57:13 -0700

    EP-K 7.2 Base conversion - WIP

---
+++ java/src/main/kotlin/com/lagostout/elementsofprogramminginterviews/strings/BaseConversion.kt
@@ -0,0 +1,7 @@
+package com.lagostout.elementsofprogramminginterviews.strings
+
+fun convertBase(numberInBase1: String, base1: Int, base2: Int) {
+    // Convert numberInBase1 to integer.
+
+    // Convert integer to string in base 2.
+}
