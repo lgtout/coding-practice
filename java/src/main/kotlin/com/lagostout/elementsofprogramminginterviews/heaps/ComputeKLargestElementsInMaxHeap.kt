59652b8cc080e787ee75d880bfaf6d1410840a50
lgtout <lagos.tout@gmail.com>
2017-08-16 21:43:16 -0700

    EP-K 11.6 Compute k-largest elements in max-heap - WIP

--- java/src/main/kotlin/com/lagostout/elementsofprogramminginterviews/heaps/ComputeKLargestElementsInMaxHeap.kt
+++ java/src/main/kotlin/com/lagostout/elementsofprogramminginterviews/heaps/ComputeKLargestElementsInMaxHeap.kt
@@ -1,33 +1,41 @@
 package com.lagostout.elementsofprogramminginterviews.heaps
 
+/**
+ * Problem 11.6 page 185
+ */
 fun kLargestElementsInMaxHeap(heap: List<Int>, k: Int): Set<Int> {
-    if (k < 0) throw IllegalArgumentException("K must be positive")
-    if (heap.isEmpty()) return emptySet()
+    var kOrHeapSize = k
+    when {
+        k < 0 -> throw IllegalArgumentException("K must be non-negative")
+        k == 0 || heap.isEmpty() -> return emptySet()
+        k > heap.size -> kOrHeapSize = heap.size
+    }
     fun log2(number: Int): Int {
         return (Math.log(number.toDouble())/Math.log(2.toDouble())).toInt()
     }
-    fun leftChild(parentIndex: Int): Int {
-        return parentIndex * 2 - 1
+    fun leftChildIndex(parentIndex: Int): Int {
+        return parentIndex * 2 + 1
     }
-    fun rightChild(parentIndex: Int): Int {
-        return leftChild(parentIndex) + 1
+    fun rightChildIndex(parentIndex: Int): Int {
+        return leftChildIndex(parentIndex) + 1
     }
     var currentLevelIndices = setOf(0)
     val kLargestElements = mutableSetOf<Int>()
-    while ((kLargestElements.size + currentLevelIndices.size <= log2(k))
-            && (kLargestElements.size < k)) {
+    val log2K = log2(kOrHeapSize)
+    while ((kLargestElements.size + currentLevelIndices.size <= log2K)
+            && (kLargestElements.size < kOrHeapSize)) {
         kLargestElements.addAll(currentLevelIndices)
         currentLevelIndices = currentLevelIndices.fold(mutableSetOf()) {
             acc, index ->
             acc.apply {
-                addAll(arrayOf(leftChild(index), rightChild(index))
+                addAll(arrayOf(leftChildIndex(index), rightChildIndex(index))
                         .filter { it < heap.size })
             }
         }
     }
     kLargestElements.addAll(
-            currentLevelIndices.toList().sortedDescending()
-                    .take(k - kLargestElements.size))
+            currentLevelIndices.sortedDescending()
+                    .take(kOrHeapSize - kLargestElements.size))
 
     return kLargestElements
 }
--- java/src/test/kotlin/com/lagostout/elementsofprogramminginterviews/heaps/ComputeKLargestElementsInMaxHeapSpek.kt
+++ java/src/test/kotlin/com/lagostout/elementsofprogramminginterviews/heaps/ComputeKLargestElementsInMaxHeapSpek.kt
@@ -1,2 +1,43 @@
 package com.lagostout.elementsofprogramminginterviews.heaps
 
+import org.jetbrains.spek.api.Spek
+import org.jetbrains.spek.api.dsl.describe
+import org.jetbrains.spek.api.dsl.given
+import org.jetbrains.spek.api.dsl.it
+import org.hamcrest.MatcherAssert.assertThat
+import org.hamcrest.Matchers
+import kotlin.test.assertFailsWith
+
+class ComputeKLargestElementsInMaxHeapSpek : Spek({
+    describe("kLargestElementsInMaxHeap") {
+        testCases.forEach {
+            (heap, k, expectedKLargestElements) ->
+            given("heap $heap k $k") {
+                it("returns $expectedKLargestElements") {
+                    assertThat(kLargestElementsInMaxHeap(heap, k),
+                            Matchers.contains(
+                                    *expectedKLargestElements.toTypedArray()))
+                }
+            }
+        }
+        describe("Throws an IllegalArgumentException when k is negative") {
+            val k = -1
+            given("k $k") {
+                it("throws an IllegalArgumentException") {
+                    assertFailsWith<IllegalArgumentException> {
+                        kLargestElementsInMaxHeap(listOf(1), k)
+                    }
+                }
+            }
+        }
+    }
+}) {
+    companion object {
+        data class TestCase(val heap: List<Int> = emptyList(), val k: Int = 0) {
+            operator fun component3() = heap.sorted().take(k)
+        }
+        val testCases = listOf(
+                TestCase(),
+                null).filterNotNull()
+    }
+}
