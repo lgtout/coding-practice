322d9db9e4960e74951bc073e80a455746d068f5
lgtout <lagos.tout@gmail.com>
2017-08-17 18:31:51 -0700

    EP-K 15.5 Reconstruct BST from traversal data - WIP

--- java/src/main/kotlin/com/lagostout/elementsofprogramminginterviews/binarysearchtrees/ReconstructBSTFromTraversalData.kt
+++ java/src/main/kotlin/com/lagostout/elementsofprogramminginterviews/binarysearchtrees/ReconstructBSTFromTraversalData.kt
@@ -1,2 +1,11 @@
 package com.lagostout.elementsofprogramminginterviews.binarysearchtrees
 
+import com.lagostout.datastructures.BinaryTreeNode
+
+/**
+ * Problem 15.5 page 266
+ */
+fun <T : Comparable<T>> reconstructBSTFromTraversal(path: List<Int>): BinaryTreeNode<T>? {
+
+    return null
+}
--- java/src/test/kotlin/com/lagostout/elementsofprogramminginterviews/binarysearchtrees/ReconstructBSTFromTraversalDataSpek.kt
+++ java/src/test/kotlin/com/lagostout/elementsofprogramminginterviews/binarysearchtrees/ReconstructBSTFromTraversalDataSpek.kt
@@ -1,2 +1,27 @@
 package com.lagostout.elementsofprogramminginterviews.binarysearchtrees
 
+import org.jetbrains.spek.api.Spek
+import org.jetbrains.spek.api.dsl.describe
+import org.jetbrains.spek.api.dsl.given
+import org.jetbrains.spek.api.dsl.it
+
+class ReconstructBSTFromTraversalDataSpek : Spek({
+    describe("") {
+        testCases.forEach {
+            given("") {
+                it("") {
+
+                }
+            }
+        }
+    }
+}) {
+    companion object {
+        class TestCase()
+        val testCases = run {
+            listOf(
+                    TestCase(),
+                    null).filterNotNull()
+        }
+    }
+}
