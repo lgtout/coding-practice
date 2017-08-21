600e6c8bf0280a3552d0b06fe5d74b895857f3b3
lgtout <lagos.tout@gmail.com>
2017-08-20 17:16:58 -0700

    EP-K 7.3.1 Compute spreadsheet column encoding - WIP

--- java/src/main/kotlin/com/lagostout/elementsofprogramminginterviews/strings/ComputeSpreadsheetColumnEncoding.kt
+++ java/src/main/kotlin/com/lagostout/elementsofprogramminginterviews/strings/ComputeSpreadsheetColumnEncoding.kt
@@ -1,2 +1,17 @@
 package com.lagostout.elementsofprogramminginterviews.strings
 
+fun numericEncodingOfColumn(alphabeticEncoding: String): Int {
+    if (alphabeticEncoding.isEmpty())
+        throw IllegalArgumentException(
+                "Alphabetic encoding must be one or more characters")
+    alphabeticEncoding.find { !it.isDigit() }.apply {
+        throw IllegalArgumentException(
+                "Alphabetic encoding must contain only digits") }
+    var magnitude = 1
+    var numericEncoding = 0
+    alphabeticEncoding.capitalize().reversed().forEach {
+        numericEncoding += (it - 'A' + 1) * magnitude
+        magnitude *= 26
+    }
+    return numericEncoding
+}
--- java/src/test/kotlin/com/lagostout/elementsofprogramminginterviews/strings/ComputeSpreadsheetColumnEncodingSpek.kt
+++ java/src/test/kotlin/com/lagostout/elementsofprogramminginterviews/strings/ComputeSpreadsheetColumnEncodingSpek.kt
@@ -1,2 +1,45 @@
 package com.lagostout.elementsofprogramminginterviews.strings
 
+import org.jetbrains.spek.api.Spek
+import org.jetbrains.spek.api.dsl.describe
+import org.jetbrains.spek.api.dsl.given
+import org.jetbrains.spek.api.dsl.it
+import kotlin.test.assertEquals
+
+class ComputeSpreadsheetColumnEncodingSpek : Spek({
+    describe("numericEncodingOfColumn") {
+        testCases.forEach {
+            (expectedNumericEncoding, alphabeticEncoding) ->
+            given("alphabetic encoding: $alphabeticEncoding") {
+                it("returns $expectedNumericEncoding") {
+                    assertEquals(expectedNumericEncoding,
+                            numericEncodingOfColumn(alphabeticEncoding))
+                }
+            }
+        }
+    }
+}) {
+    companion object {
+        data class TestCase(val expectedNumericEncoding: Int) {
+            private val alphabeticEncoding = run {
+                var number = expectedNumericEncoding
+                val stringBuilder = StringBuilder()
+                while (number > 0) {
+                    stringBuilder.append(((number % 26) - 1 + 'A'.toInt()).toChar())
+                    // TODO ???
+                    number = number / 26 * 26
+                }
+                stringBuilder.reverse().toString()
+            }
+            operator fun component2() = alphabeticEncoding
+        }
+        val testCases = listOf(
+                TestCase(1),
+                TestCase(4),
+                TestCase(26),
+                TestCase(27),
+                TestCase(702),
+                null).filterNotNull()
+    }
+}
+
