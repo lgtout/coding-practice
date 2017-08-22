926882c077e72c1423cdb243d6876cfe4db769b2
lgtout <lagos.tout@gmail.com>
2017-08-21 17:52:50 -0700

    EP-K 16.1 Towers of Hanoi - WIP

--- java/src/main/kotlin/com/lagostout/elementsofprogramminginterviews/recursion/TowersOfHanoi.kt
+++ java/src/main/kotlin/com/lagostout/elementsofprogramminginterviews/recursion/TowersOfHanoi.kt
@@ -10,7 +10,7 @@ object TowersOfHanoi {
 
     data class Ring(val size: Int)
 
-    class Peg(val position: PegPosition, var ringCount: Int = 0,
+    class Peg(val position: PegPosition, ringCount: Int = 0,
               private val rings: MutableList<Ring> = mutableListOf()) {
         init {
             var ringSize = ringCount
@@ -24,40 +24,45 @@ object TowersOfHanoi {
             get() = rings.isEmpty()
         val isNotEmpty: Boolean
             get() = !isEmpty
-//        val size: Int
-//            get() = rings.size
+        val size: Int
+            get() = rings.size
         fun push(ring: Ring) {
             rings.add(ring)
         }
         fun pop(): Ring {
             return rings.removeAt(rings.lastIndex)
         }
+
+//        override fun toString(): String {
+//            return "Peg(position=$position, rings=$rings)"
+//        }
     }
 
-    data class MoveRing(val from: PegPosition, val to: PegPosition)
+    data class RingMove(val from: PegPosition, val to: PegPosition, val ring: Ring)
+
+    fun transferRingsFromOnePegToAnother(ringCount: Int): List<RingMove> {
+        val operations = mutableListOf<RingMove>()
+        transferRingsFromOnePegToAnother(Peg(PegPosition.LEFT, ringCount),
+                Peg(PegPosition.MIDDLE), Peg(PegPosition.RIGHT), ringCount,
+                operations)
+        return operations
+    }
 
     fun transferRingsFromOnePegToAnother(fromPeg: Peg, toPeg: Peg,
                                          extraPeg: Peg, ringCount: Int,
-                                         operations: List<MoveRing>) {
-        fromPeg.apply {
-            when {
-                isEmpty -> return
-                isNotEmpty && toPeg.isNotEmpty &&
-                        topRingSize > toPeg.topRingSize -> {
-                    throw IllegalStateException(
-                            "Attempted to place a ring on the destination peg " +
-                                    "that's larger than that peg's top ring")
-                }
-            }
-        }
-        toPeg.apply {
-            if (isEmpty || topRingSize < fromPeg.topRingSize) {
-                push(fromPeg.pop())
-                return
-            }
-        }
+                                         operations: MutableList<RingMove>) {
+        if (fromPeg.isEmpty) return
         transferRingsFromOnePegToAnother(fromPeg, extraPeg, toPeg,
                 ringCount - 1, operations)
+        val ring = fromPeg.pop()
+        if (toPeg.isNotEmpty && ring.size > toPeg.topRingSize)
+            throw IllegalStateException(
+                    "Attempted to place a ring on the destination peg " +
+                            "that's larger than that peg's top ring.")
+        toPeg.push(ring)
+        operations.add(RingMove(fromPeg.position, toPeg.position, ring))
+        transferRingsFromOnePegToAnother(extraPeg, toPeg, fromPeg,
+                extraPeg.size, operations)
     }
 
 }
--- java/src/test/kotlin/com/lagostout/elementsofprogramminginterviews/recursion/TowersOfHanoiSpek.kt
+++ java/src/test/kotlin/com/lagostout/elementsofprogramminginterviews/recursion/TowersOfHanoiSpek.kt
@@ -1,2 +1,32 @@
 package com.lagostout.elementsofprogramminginterviews.recursion
 
+import org.jetbrains.spek.api.Spek
+import org.jetbrains.spek.api.dsl.describe
+import org.jetbrains.spek.api.dsl.given
+import org.jetbrains.spek.api.dsl.it
+import kotlin.test.assertTrue
+
+class TowersOfHanoiSpek : Spek({
+    describe("") {
+        testCases.forEach {
+            (ringCount) ->
+            given("") {
+                it("") {
+                    // TODO Pass Pegs to both verification and solution, so they don't make assumptions about them?
+                    assertTrue(operationSequenceIsValid(
+                            TowersOfHanoi.transferRingsFromOnePegToAnother(ringCount)))
+                }
+            }
+        }
+    }
+}) {
+    companion object {
+        data class TestCase(val ringCount: Int = 0)
+        val testCases = listOf(TestCase(),
+                null).filterNotNull()
+        fun operationSequenceIsValid(operations: List<TowersOfHanoi.RingMove>): Boolean {
+            // TODO Perform operations and reach goal
+            return false
+        }
+    }
+}
