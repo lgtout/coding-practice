4499f62acc2a6b2e5dc418075b08b36b0c0edbc9
lgtout <lagos.tout@gmail.com>
2017-08-24 00:00:26 -0700

    EP-K 11.7 Implement stack using heap - WIP

--- java/src/main/kotlin/com/lagostout/elementsofprogramminginterviews/heaps/ImplementStackUsingHeap.kt
+++ java/src/main/kotlin/com/lagostout/elementsofprogramminginterviews/heaps/ImplementStackUsingHeap.kt
@@ -1,2 +1,23 @@
 package com.lagostout.elementsofprogramminginterviews.heaps
 
+import java.util.*
+
+class StackUsingHeap<T> {
+    class PriorityQueueItem<T>(val value: T, val order: Int) :
+            Comparable<PriorityQueueItem<T>> {
+        override fun compareTo(other: PriorityQueueItem<T>): Int {
+            return order.compareTo(other.order)
+        }
+    }
+    val heap = PriorityQueue<PriorityQueueItem<T>>(
+            Comparator<PriorityQueueItem<T>> { item1, item2 ->
+                item2.compareTo(item1)
+    })
+    fun push(value: T) {
+        heap.add(PriorityQueueItem(value, heap.size))
+    }
+    fun pop(): T {
+        return heap.remove().value
+    }
+}
+
