c2ef55f27901f768462649ed3eeaac231671dd59
lgtout <lagos.tout@gmail.com>
2017-08-24 15:04:35 -0700

    EP-K 13.8 Find smallest subarray sequentially covering all values - WIP

--- java/src/main/kotlin/com/lagostout/elementsofprogramminginterviews/hashtables/FindSmallestSubarraySequentiallyCoveringAllValues.kt
+++ java/src/main/kotlin/com/lagostout/elementsofprogramminginterviews/hashtables/FindSmallestSubarraySequentiallyCoveringAllValues.kt
@@ -1,2 +1,22 @@
 package com.lagostout.elementsofprogramminginterviews.hashtables
 
+import org.apache.commons.lang3.Range
+
+fun smallestSubarraySequentiallyCoveringAllValues(
+        paragraph: List<String>, keywords: LinkedHashSet<String>): Range<Int>? {
+    var rangeOfSmallestSubarray: Range<Int>? = null
+    var nextStringSoughtToStartStringIndexMap = mutableMapOf<String, Int?>()
+    var nextStringSoughtSet = mutableSetOf(keywords.first())
+    paragraph.forEachIndexed {
+        index, word ->
+        var entry: Pair<String, Int>? = when (word) {
+            keywords.first() -> word to index
+            in nextStringSoughtToStartStringIndexMap ->
+                nextStringSoughtToStartStringIndexMap[word]?.let {
+                    word to (it + 1)
+                }
+            else -> null
+        }
+    }
+    return rangeOfSmallestSubarray
+}
