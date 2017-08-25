1c9d3bf36012c165274a52f272e83dfabca532f6
lgtout <lagos.tout@gmail.com>
2017-08-24 23:49:42 -0700

    EP-K 17.2.3 Longest character sequence subsequence of 2 strings - WIP

--- java/src/main/kotlin/com/lagostout/elementsofprogramminginterviews/dynamicprogramming/LongestSequenceOfCharactersThatIsASubsequenceOf2Strings.kt
+++ java/src/main/kotlin/com/lagostout/elementsofprogramminginterviews/dynamicprogramming/LongestSequenceOfCharactersThatIsASubsequenceOf2Strings.kt
@@ -5,24 +5,48 @@ package com.lagostout.elementsofprogramminginterviews.dynamicprogramming
  */
 fun longestSequenceOfCharactersThatIsASubsequenceOf(
         string1: String, string2: String): String {
-    data class Position(val row: Int, val column: Int)
+    data class Position(val row: Int, val column: Int) {
+        val aboveLeft
+            get() = copy(row = row - 1, column = column - 1)
+        val left
+            get() = copy(column = column - 1)
+        val above
+            get() = copy(row = row - 1)
+        val possiblePreviousPositions
+            get() = listOf(left, aboveLeft, above)
+    }
     val levenshteinDistanceCache = levenshteinDistanceCache(string1, string2)
-    val longestSequenceCache = Array(string2.length, { IntArray(string1.length) })
+    val longestSequenceCache = Array(string2.length + 1,
+            { IntArray(string1.length + 1) })
     (1..longestSequenceCache.size).forEach { rowIndex ->
         val row = longestSequenceCache[rowIndex]
         (1..row.size).forEach { columnIndex ->
-            val candidateNeighborPositions = listOf(
-                    Position(rowIndex - 1, columnIndex - 1),
-                    Position(rowIndex - 1, columnIndex),
-                    Position(rowIndex, columnIndex - 1)).filter {
+            listOf(Position(rowIndex - 1, columnIndex - 1), // above-left
+                    Position(rowIndex - 1, columnIndex), // above
+                    Position(rowIndex, columnIndex - 1) // left
+            ).filter {
                 levenshteinDistanceCache[it.row][it.column] <=
                         levenshteinDistanceCache[rowIndex][columnIndex]
-            }.maxBy { longestSequenceCache[it.row][it.column] }?.apply {
-//                longestSequenceCache[rowIndex][columnIndex] =
-//                        longestSequenceCache[rowIndex][columnIndex] +
-//                                if () 1 else
+            }.maxBy { longestSequenceCache[it.row][it.column] }?.let {
+                longestSequenceCache[rowIndex][columnIndex] +=
+                        if (string1[it.column] == string2[it.row]) 1 else 0
             }
         }
+    }
+    var longestSequenceLength = longestSequenceCache.last().last()
+    var rowIndex = longestSequenceCache.lastIndex
+    var columnIndex = longestSequenceCache.last().lastIndex
+    while (true) {
+        val sequenceLength = longestSequenceCache[rowIndex][columnIndex]
+        if (sequenceLength == 0) break
+        val position = Position(rowIndex, columnIndex)
+        position.possiblePreviousPositions.filter {
+            Math.abs(sequenceLength - longestSequenceCache[it.row][it.column]) <= 1
+        }
+        // TODO
+//                . {
+//            longestSequenceCache[it.row][it.column] <= longestSequenceCache[rowIndex][columnIndex]
+//        }
     }
     return ""
 }
--- java/src/test/kotlin/com/lagostout/elementsofprogramminginterviews/dynamicprogramming/LongestSequenceOfCharactersThatIsASubsequenceOf2StringsSpek.kt
+++ java/src/test/kotlin/com/lagostout/elementsofprogramminginterviews/dynamicprogramming/LongestSequenceOfCharactersThatIsASubsequenceOf2StringsSpek.kt
@@ -1,2 +1,42 @@
 package com.lagostout.elementsofprogramminginterviews.dynamicprogramming
 
+import org.jetbrains.spek.api.Spek
+import org.jetbrains.spek.api.dsl.describe
+import org.jetbrains.spek.api.dsl.given
+import org.jetbrains.spek.api.dsl.it
+import kotlin.test.assertEquals
+
+class LongestSequenceOfCharactersThatIsASubsequenceOf2StringsSpek: Spek({
+    describe("levenshteinDistance") {
+        testCases.forEach {
+            (from, to, expected) ->
+            given("from: $from to: $to") {
+                it("returns $expected") {
+                    assertEquals(expected, levenshteinDistance(from, to))
+                }
+            }
+        }
+    }
+}) {
+    companion object {
+
+        data class TestCase(val from: String, val to: String) {
+            val expected: Int = bruteForceRightToLeftLevenshteinDistance(from, to)
+            operator fun component3() = expected
+        }
+
+        val testCases: List<TestCase> = run {
+            listOf(
+                    TestCase("saturday", "sundays"), // distance: 4
+                    TestCase("carthorse", "orchestra"), // distance: 8
+                    TestCase("", ""),
+                    TestCase("", "a"),
+                    TestCase("a", ""),
+                    TestCase("a", "a"),
+                    TestCase("a", "b"),
+                    null
+            ).filterNotNull()
+        }
+
+    }
+}
