d3446229d371b6e984027ecefc10808b52925540
lgtout <lagos.tout@gmail.com>
2017-08-26 14:01:51 -0700

    EP-K 11.7 Implement stack using heap - WIP

--- java/src/main/kotlin/com/lagostout/elementsofprogramminginterviews/heaps/ImplementStackUsingHeap.kt
+++ java/src/main/kotlin/com/lagostout/elementsofprogramminginterviews/heaps/ImplementStackUsingHeap.kt
@@ -3,21 +3,23 @@ package com.lagostout.elementsofprogramminginterviews.heaps
 import java.util.*
 
 class StackUsingHeap<T> {
-    class PriorityQueueItem<T>(val value: T, val order: Int) :
-            Comparable<PriorityQueueItem<T>> {
-        override fun compareTo(other: PriorityQueueItem<T>): Int {
-            return order.compareTo(other.order)
-        }
-    }
+    data class PriorityQueueItem<out T>(val value: T, val priority: Int)
+
     val heap = PriorityQueue<PriorityQueueItem<T>>(
             Comparator<PriorityQueueItem<T>> { item1, item2 ->
-                item2.compareTo(item1)
+                item2.priority.compareTo(item1.priority)
     })
+
     fun push(value: T) {
         heap.add(PriorityQueueItem(value, heap.size))
     }
+
     fun pop(): T {
         return heap.remove().value
+    }
+
+    override fun toString(): String {
+        return "StackUsingHeap(heap=$heap)"
     }
 }
 
--- java/src/test/kotlin/com/lagostout/elementsofprogramminginterviews/heaps/ImplementStackUsingHeapSpek.kt
+++ java/src/test/kotlin/com/lagostout/elementsofprogramminginterviews/heaps/ImplementStackUsingHeapSpek.kt
@@ -1,11 +1,33 @@
 package com.lagostout.elementsofprogramminginterviews.heaps
 
-import org.jetbrains.spek.api.Spek
+import org.jetbrains.spek.api.dsl.describe
+import org.jetbrains.spek.api.dsl.it
+import org.jetbrains.spek.subject.SubjectSpek
+import kotlin.test.assertEquals
+import kotlin.test.assertFailsWith
 
-class ImplementStackUsingHeapSpek : Spek({
-
-}) {
-    companion object {
+class ImplementStackUsingHeapSpek : SubjectSpek<StackUsingHeap<Int>>({
+    subject {
+        StackUsingHeap()
+    }
+    describe("StackUsingHeap") {
+        describe("empty stack") {
+            describe("push") {
+                val item = 1
+                subject.push(item)
+                it("adds $item to the top of the heap") {
+                    assertEquals(item, subject.heap.element()?.value)
+                    assertEquals(1, subject.heap.size)
+                }
+            }
+            describe("pop") {
+                it("throws an exception") {
+                    assertFailsWith<NoSuchElementException> {
+                        subject.pop()
+                    }
+                }
+            }
+        }
 
     }
-}
+})
