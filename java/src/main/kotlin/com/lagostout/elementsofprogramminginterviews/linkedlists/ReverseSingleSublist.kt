e8cff2c920a1af84d6eccc295e043aa9b7980ed3
lgtout <lagos.tout@gmail.com>
2017-09-05 11:45:45 -0700

    EP-K 8.2.1 Reverse single sublist - WIP

--- java/src/main/kotlin/com/lagostout/elementsofprogramminginterviews/linkedlists/ReverseSingleSublist.kt
+++ java/src/main/kotlin/com/lagostout/elementsofprogramminginterviews/linkedlists/ReverseSingleSublist.kt
@@ -4,27 +4,32 @@ class ReverseSingleSublist {
     data class ListNode<T>(var data: T? = null, var next: ListNode<T>? = null)
 
     fun <T> reverseSingleSublist(list: ListNode<T>, start: Int, end: Int) {
-        if (start < 1 || end < 1) {
-            throw IllegalArgumentException("Start and end positions must be >= 1")
+        var lastNode = list
+        var listSize = 1
+        while (true) {
+            ++listSize
+            lastNode.next?.let {
+                lastNode = it
+            } ?: break
         }
-        // Simplifying solution to only handle start <= end
+        if (listOf(start, end).find { it < 1 || it > listSize} != null) {
+            throw IllegalArgumentException("Start and end positions must be >= 1 and <= list size")
+        }
+        // Nodes point in one direction only - left to right.
+        // So let's simplify solution by constraining start and end.
         if (start > end) {
             throw IllegalArgumentException("End position must be >= start position")
         }
-        if (list.next == null || start == end)
-            return
-        // TODO Handle empty list
-        // TODO No fast way to check if end exceeds list length, so we'll check as we move in the list.
         var nodeBeforeSublistStart: ListNode<T>? = null
-        var sublistEndNode: ListNode<T>? = null
+        var sublistEnd: ListNode<T>? = null
         var currentPosition = 0
-        var currentNode = ListNode<T>()
-        currentNode.next = list
+        val head = ListNode(next = list)
+        var currentNode = head
         while (true) {
             if (currentPosition + 1 == start)
                 nodeBeforeSublistStart = currentNode
             if (currentPosition == end) {
-                sublistEndNode = currentNode
+                sublistEnd = currentNode
                 break
             }
             currentNode.next?.apply {
@@ -32,19 +37,13 @@ class ReverseSingleSublist {
                 ++currentPosition
             } ?: break
         }
-        if (nodeBeforeSublistStart == null || sublistEndNode == null) {
-            throw IllegalArgumentException(
-                    "Start and end position values must be in the range " +
-                            "1 to the length of the list (${currentPosition + 1})")
-        }
-        // TODO Verify for null cases (end of list)
-        while (nodeBeforeSublistStart.next !== sublistEndNode) {
-            nodeBeforeSublistStart.next?.let {
+        while (nodeBeforeSublistStart?.next !== sublistEnd) {
+            nodeBeforeSublistStart?.next?.let {
                 nodeBeforeSublistStart?.next = it.next
-                val nodeAfterSublistEndNode = sublistEndNode?.next
-                sublistEndNode?.next = it
-                it.next = nodeAfterSublistEndNode
-                sublistEndNode = it
+                sublistEnd?.apply {
+                    it.next = next
+                    next = it
+                }
             }
         }
     }
--- java/src/test/kotlin/com/lagostout/elementsofprogramminginterviews/linkedlists/ReverseSingleSublistSpek.kt
+++ java/src/test/kotlin/com/lagostout/elementsofprogramminginterviews/linkedlists/ReverseSingleSublistSpek.kt
@@ -6,3 +6,4 @@ package com.lagostout.elementsofprogramminginterviews.linkedlists
 // Start/end outside range of list
 // Single item lists
 // Multiple items, single item sublist
+// Sublist includes first or last node
