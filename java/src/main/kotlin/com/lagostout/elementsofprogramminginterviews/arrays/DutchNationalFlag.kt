11375da415a4b6d5566142db3d38922aae55de58
lgtout <lagos.tout@gmail.com>
2017-09-04 20:07:09 -0700

    EP-K 6.1 Dutch national flag problem - WIP

--- java/src/main/kotlin/com/lagostout/elementsofprogramminginterviews/arrays/DutchNationalFlag.kt
+++ java/src/main/kotlin/com/lagostout/elementsofprogramminginterviews/arrays/DutchNationalFlag.kt
@@ -1,2 +1,31 @@
 package com.lagostout.elementsofprogramminginterviews.arrays
 
+fun rearrangeAsDutchNationalFlag(array: MutableList<Int>, pivotIndex: Int) {
+    if (array.isEmpty() || array.size == 1) return
+    if (pivotIndex > array.lastIndex || pivotIndex < 0)
+        throw IllegalArgumentException(
+                "Argument pivotIndex must be > 0 and <= array last index")
+    fun swap(array: MutableList<Int>, index1: Int, index2: Int) {
+        val temp = array[index1]
+        array[index1] = array[index2]
+        array[index2] = temp
+    }
+    val pivotValue = array[pivotIndex]
+    swap(array, 0, pivotIndex)
+    var boundaryIndex = 0
+    (1 until array.size).forEach { currentIndex ->
+        if (array[currentIndex] < pivotValue) {
+            swap(array, currentIndex, ++boundaryIndex)
+        }
+//        println(array)
+    }
+    swap(array, 0, boundaryIndex)
+    println(array)
+    (boundaryIndex + 1 until array.size).forEach {
+        currentIndex ->
+        println(currentIndex)
+        if (array[currentIndex] == pivotValue) {
+            swap(array, currentIndex, ++boundaryIndex)
+        }
+    }
+}
--- java/src/test/kotlin/com/lagostout/elementsofprogramminginterviews/arrays/DutchNationalFlagSpek.kt
+++ java/src/test/kotlin/com/lagostout/elementsofprogramminginterviews/arrays/DutchNationalFlagSpek.kt
@@ -4,39 +4,48 @@ import org.jetbrains.spek.api.Spek
 import org.jetbrains.spek.api.dsl.describe
 import org.jetbrains.spek.api.dsl.given
 import org.jetbrains.spek.api.dsl.it
+import kotlin.test.assertEquals
 
 class DutchNationalFlagSpek : Spek({
     describe("rearrangeAsDutchNationalFlag") {
-        testCases.forEach { (array, pivot, expected) ->
-            given("") {
-                it("") {
-
+        testCases.forEach { (array, pivotIndex, expected) ->
+            given("array: $array, pivot $pivotIndex") {
+                it("rearranges array as $expected") {
+                    val mutableArray = array.toMutableList()
+                    rearrangeAsDutchNationalFlag(mutableArray, pivotIndex)
+                    assertEquals(expected, mutableArray)
                 }
             }
         }
     }
 }) {
     companion object {
-        class TestCase(val array: Array<Int> = emptyArray(), val pivot: Int = 0) {
-            private val expected: Array<Int> = run {
+        class TestCase(private val array: List<Int> = emptyList(),
+                       private val pivotIndex: Int = 0) {
+            private val expected: List<Int> = run {
                 val lessThan = mutableListOf<Int>()
                 val equalTo = mutableListOf<Int>()
                 val greaterThan = mutableListOf<Int>()
                 array.forEach {
                     (when {
-                        it < pivot -> lessThan
-                        it > pivot -> greaterThan
+                        it < array[pivotIndex] -> lessThan
+                        it > array[pivotIndex] -> greaterThan
                         else -> equalTo
                     }).add(it)
                 }
-                (lessThan + equalTo + greaterThan).toTypedArray()
+                (lessThan + equalTo + greaterThan)
             }
             operator fun component1() = array
-            operator fun component2() = pivot
+            operator fun component2() = pivotIndex
             operator fun component3() = expected
         }
         val testCases = listOf(
                 TestCase(),
+                TestCase(listOf(1), 0),
+                TestCase(listOf(1,1), 0),
+                TestCase(listOf(1,1), 1),
+                TestCase(listOf(1,1,0), 0),
+                TestCase(listOf(1,1,0), 2),
                 null).filterNotNull()
     }
 }
