cf16eada59eba42c8dd2722cf366f8033c55aa8f
lgtout <lagos.tout@gmail.com>
2017-09-20 12:51:11 -0700

    EP-K 16.1.2 Towers of Hanoi without recursion - WIP

--- java/src/main/kotlin/com/lagostout/elementsofprogramminginterviews/recursion/TowersOfHanoiWithoutRecursion.kt
+++ java/src/main/kotlin/com/lagostout/elementsofprogramminginterviews/recursion/TowersOfHanoiWithoutRecursion.kt
@@ -7,44 +7,28 @@ import java.util.*
 
 object TowersOfHanoiWithoutRecursionSpek {
 
-    enum class Step {
-        MOVE_N_RINGS {
-            override fun next(): Step {
-                return MOVE_RING
-            }
-        },
-        MOVE_RING {
-            override fun next(): Step {
-                throw UnsupportedOperationException()
-            }
-        };
-        abstract fun next(): Step
-    }
-
     fun transferRingsBetweenPegsWithoutRecursion(
-            pegs: Pegs, from: Peg, to: Peg, countOfRingsToMove: Int) {
-        data class Frame(var step: Step, val from: Peg,
+            pegs: Pegs, from: Peg, to: Peg) {
+        data class Frame(val from: Peg,
                          val to: Peg, val countOfRingsToMove: Int)
         val operations: MutableList<RingMove> = mutableListOf()
         val stack = LinkedList<Frame>().apply {
-            push(Frame(Step.MOVE_N_RINGS, from, to, from.size - 1))
+            push(Frame(from, to, from.size - 1))
         }
-        while (true) {
+        while (stack.isNotEmpty()) {
             val frame = stack.pop()
-            when (frame.step) {
-                Step.MOVE_N_RINGS -> {
-//                    stack.push(frame. = Step.MOVE_RING
-                    stack.push(Frame(Step.MOVE_N_RINGS,
-                            from, pegs.extra(listOf(from, to)),
-                            frame.countOfRingsToMove - 1))
-                }
-                Step.MOVE_RING -> {
-
-                    frame.run {
-                        from.push(to.pop())
-                    }
+            if (frame.countOfRingsToMove == 1) {
+                from.pop().apply {
+                    to.push(this)
+                    operations.add(RingMove(from.position, to.position, this))
                 }
             }
+            if (frame.countOfRingsToMove <= 1) continue
+            stack.push(frame.copy(countOfRingsToMove = frame.countOfRingsToMove - 1))
+            val extraPeg = pegs.extra(listOf(from, to))
+            stack.push(Frame(extraPeg, to, extraPeg.size))
+            stack.push(Frame(from, to, 1))
+            stack.push(Frame(to, extraPeg, to.size))
         }
     }
 
