536ec9f443814cd1febc1b3d1ba381f1fa6dd502
lgtout <lagos.tout@gmail.com>
2017-09-24 17:24:22 -0700

    EP-K 13.8 Find smallest subarray sequentially covering all values - WIP

--- java/src/main/kotlin/com/lagostout/elementsofprogramminginterviews/hashtables/FindSmallestSubarraySequentiallyCoveringAllValues.kt
+++ java/src/main/kotlin/com/lagostout/elementsofprogramminginterviews/hashtables/FindSmallestSubarraySequentiallyCoveringAllValues.kt
@@ -1,5 +1,8 @@
 package com.lagostout.elementsofprogramminginterviews.hashtables
 
+/**
+ * Problem 13.8 page 225
+ */
 object FindSmallestSubarraySequentiallyCoveringAllValues {
 
     fun smallestSubarraySequentiallyCoveringAllValues(
@@ -7,21 +10,33 @@ object FindSmallestSubarraySequentiallyCoveringAllValues {
         if (paragraph.isEmpty() || keywords.isEmpty() ||
                 paragraph.size < keywords.size)
             return null
+        // TODO This may not be necessary as a special case - handled by general case
         if (paragraph.size == 1) {
             if (keywords.first() != paragraph.first())
                 return null
             return Pair(1,1)
         }
+        // We'll use this to figure out what the To be able to
         val keywordToIndexMap = keywords.withIndex().map {
             it.value to it.index }.toMap()
-        val subarrays = Array<Set<Int>>(keywords.size, { mutableSetOf() })
+//        val subarrays = Array<Set<Int>>(keywords.size, { mutableSetOf() })
+        val nextWordToSubarrayStart = mutableMapOf<String, Int>()
         var rangeOfSmallestSubarray: Pair<Int, Int>? = null
         paragraph.forEachIndexed {
             index, word ->
-            if (word == keywords.first()) {
-//                subarrays[]
+            val nextWord = keywords[keywordToIndexMap[word]!! + 1]
+            if (!nextWordToSubarrayStart.containsKey(nextWord) ||
+                    nextWordToSubarrayStart[word]!! >
+                    nextWordToSubarrayStart[nextWord]!!) {
+                val start = if (word == keywords.first())
+                    index else nextWordToSubarrayStart[word]!!
+                nextWordToSubarrayStart[nextWord] = start
+            }
+            if (word != keywords.first()) {
+                nextWordToSubarrayStart.remove(word)
             }
         }
+//        nextWordToSubarrayStart.filterKeys { it == keywords.last() }.maxBy { it.value }
         return rangeOfSmallestSubarray
     }
 }
