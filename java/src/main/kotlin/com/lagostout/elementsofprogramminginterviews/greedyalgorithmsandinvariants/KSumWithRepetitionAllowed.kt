95c89819dd76a8fa6439538595de23db87164497
lgtout <lagos.tout@gmail.com>
2017-09-26 14:10:04 -0700

    EP-K 18.4.3 Find k entries in array that add up to sum - WIP

--- java/src/main/kotlin/com/lagostout/elementsofprogramminginterviews/greedyalgorithmsandinvariants/KSumWithRepetitionAllowed.kt
+++ java/src/main/kotlin/com/lagostout/elementsofprogramminginterviews/greedyalgorithmsandinvariants/KSumWithRepetitionAllowed.kt
@@ -1,5 +1,12 @@
 package com.lagostout.elementsofprogramminginterviews.greedyalgorithmsandinvariants
 
-fun kElementsAddUpToNumber(k: Int, allElements: List<Int>, s: Int): Boolean {
+/**
+ * Problem 18.4.3 page 346
+ */
+fun kElementsAddUpToNumberWithRepetitionAllowed(k: Int, allElements: List<Int>, s: Int): Boolean {
     return false
+}
+
+fun permutationsWithRepetition(elements: List<Int>, repeatCount: Int) {
+
 }
--- java/src/test/kotlin/com/lagostout/elementsofprogramminginterviews/dynamicprogramming/KSumWithRepetitionAllowedSpek.kt
+++ java/src/test/kotlin/com/lagostout/elementsofprogramminginterviews/dynamicprogramming/KSumWithRepetitionAllowedSpek.kt
@@ -1,2 +1,19 @@
 package com.lagostout.elementsofprogramminginterviews.dynamicprogramming
 
+import org.jetbrains.spek.api.Spek
+import org.jetbrains.spek.api.dsl.describe
+import org.jetbrains.spek.data_driven.data
+import org.jetbrains.spek.data_driven.on
+
+class KSumWithRepetitionAllowedSpek : Spek({
+    describe("permutationsWithRepetition") {
+        val data = arrayOf(
+                data(arrayOf(1), 0),
+                data(arrayOf(1,2), 0)
+        )
+        on("generate permutations of %s with %s repeats allowed", with = *data) {
+            elements, repeats ->
+            // TODO
+        }
+    }
+})
