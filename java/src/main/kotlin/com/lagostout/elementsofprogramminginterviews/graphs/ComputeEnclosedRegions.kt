291e5ce5e8eee3620b6c06d5f802acd315e7e91f
lgtout <lagos.tout@gmail.com>
2017-09-26 15:47:18 -0700

    EP-K 19.3 Compute enclosed regions - WIP

--- java/src/main/kotlin/com/lagostout/elementsofprogramminginterviews/graphs/ComputeEnclosedRegions.kt
+++ java/src/main/kotlin/com/lagostout/elementsofprogramminginterviews/graphs/ComputeEnclosedRegions.kt
@@ -1,5 +1,7 @@
 package com.lagostout.elementsofprogramminginterviews.graphs
 
+import java.util.*
+
 /**
  * Problem 19.3 page 365
  */
@@ -10,25 +12,36 @@ fun computeEnclosedRegionsAndFill(booleanMatrix: List<List<Boolean>>) {
     // Let's assume true = black, false = white
     val adjacencies = toGraph(booleanMatrix)
     val exploredCells = mutableSetOf<Cell>()
-    val whiteBorderCells = mutableSetOf<Cell>()
-    val nonBorderWhiteCells = mutableSetOf<Cell>()
-    val borderCells = mutableSetOf<Cell>()
-    // TODO Create set of all border cells, then filter by value.
-    booleanMatrix.forEachIndexed { index, list ->
+    val enclosedComponents = mutableSetOf<Set<Cell>>()
+    val whiteBorderCells = booleanMatrix.withIndex().map { (index, list) ->
         when (index) {
             0, booleanMatrix.lastIndex -> {
-                list.forEachIndexed { column, value ->
-                    if (!value) whiteBorderCells.add(Cell(column, index))
+                list.withIndex().map { (column, value) ->
+                    Cell(column, index, value)
                 }
             }
             else -> {
                 listOf(Pair(list.first(), 0),
-                        Pair(list.last(), list.lastIndex)).forEach {
-                    (value, column) ->
-                    if (!value) whiteBorderCells.add(Cell(column, index))
+                        Pair(list.last(), list.lastIndex)).map {
+                    (value, column) -> Cell(column, index, value)
                 }
             }
         }
+    }.flatten().filter { it.value == true }.toSet()
+    adjacencies.keys.forEach {
+        val currentComponent = mutableSetOf<Cell>()
+        val stack = LinkedList<Cell>().apply { add (it) }
+        while (stack.isNotEmpty()) {
+            val currentCell = stack.pop()
+            if (exploredCells.contains(it)) continue
+            exploredCells.add(it)
+            currentComponent.add(currentCell)
+            adjacencies[currentCell]?.forEach {
+                stack.push(it)
+            }
+        }
+        if (currentComponent.any { whiteBorderCells.contains(it) })
+            enclosedComponents.add(currentComponent)
     }
-    // TODO Compute components
+    // TODO Replace White with Black
 }
