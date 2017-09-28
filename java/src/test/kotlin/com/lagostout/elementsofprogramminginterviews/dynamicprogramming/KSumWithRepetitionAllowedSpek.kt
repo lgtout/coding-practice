9b37021bbf7fd2fa00f30473f7f044cee8eee7be
lgtout <lagos.tout@gmail.com>
2017-09-28 16:44:31 -0700

    EP-K 18.4.3 Find k entries in array that add up to sum - WIP

--- java/src/test/kotlin/com/lagostout/elementsofprogramminginterviews/dynamicprogramming/KSumWithRepetitionAllowedSpek.kt
+++ java/src/test/kotlin/com/lagostout/elementsofprogramminginterviews/dynamicprogramming/KSumWithRepetitionAllowedSpek.kt
@@ -5,14 +5,17 @@ import org.jetbrains.spek.api.dsl.describe
 import org.jetbrains.spek.data_driven.data
 import org.jetbrains.spek.data_driven.on
 
+/**
+ * Problem 18.4.3 page 346
+ */
 class KSumWithRepetitionAllowedSpek : Spek({
     describe("permutationsWithRepetition") {
         val data = arrayOf(
-                data(arrayOf(1), 0),
-                data(arrayOf(1,2), 0)
+                data(arrayOf(1), 0, expected = false),
+                data(arrayOf(1,2), 0, expected = false)
         )
         on("generate permutations of %s with %s repeats allowed", with = *data) {
-            elements, repeats ->
+            elements, repeats, expected ->
             // TODO
         }
     }
