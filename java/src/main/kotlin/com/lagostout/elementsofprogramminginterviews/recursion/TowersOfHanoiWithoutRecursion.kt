73b7a84a31661abf99d6fb5c98d7ed048eea228a
lgtout <lagos.tout@gmail.com>
2017-09-30 10:33:56 -0700

    EP-K 16.1.2 Towers of Hanoi without recursion - done!

--- java/src/main/kotlin/com/lagostout/elementsofprogramminginterviews/recursion/TowersOfHanoiWithoutRecursion.kt
+++ java/src/main/kotlin/com/lagostout/elementsofprogramminginterviews/recursion/TowersOfHanoiWithoutRecursion.kt
@@ -19,28 +19,33 @@ object TowersOfHanoiWithoutRecursion {
             push(Frame(fromPeg, toPeg, fromPeg.size))
         }
         while (stack.isNotEmpty()) {
-            val frame = stack.pop()
-            if (frame.countOfRingsToMove == 1) {
-                fromPeg.pop().apply {
-                    toPeg.push(this)
-                    operations.add(RingMove(fromPeg.position, toPeg.position, this))
+            with (stack.pop()) {
+                when {
+                    countOfRingsToMove == 1 -> {
+                        from.pop().apply {
+                            to.push(this)
+                            operations.add(RingMove(from.position, to.position, this))
+                        }
+                    }
+                    countOfRingsToMove > 1 -> {
+                        val extraPeg = pegs.extra(listOf(from, to))
+
+                        // Since this is a stack we need to push frames in
+                        // reverse order of execution: from last operation to first.
+                        // This ensures the last operation pushed will be
+                        // the first operation executed.
+
+                        // 3rd: Move rings set aside on extraPeg to toPeg
+                        stack.push(copy(from = extraPeg, to = to,
+                                countOfRingsToMove = countOfRingsToMove - 1))
+                        // 2nd: Move bottom ring from fromPeg to toPeg
+                        stack.push(Frame(from, to, 1))
+                        // 1st: Set aside all rings but the bottom one: move them to extraPeg
+                        stack.push(Frame(from, extraPeg, countOfRingsToMove - 1))
+                    }
+                    else -> Unit
                 }
             }
-            if (frame.countOfRingsToMove <= 1) continue
-            val extraPeg = pegs.extra(listOf(fromPeg, toPeg))
-
-            // Since this is a stack we need to push frames in
-            // reverse order of execution: from last operation to first.
-            // This ensures the last operation pushed will be
-            // the first operation executed.
-
-            // 3rd: Move rings set aside on extraPeg to toPeg
-            stack.push(frame.copy(from = extraPeg, to = toPeg,
-                    countOfRingsToMove = frame.countOfRingsToMove - 1))
-            // 2nd: Move bottom ring from fromPeg to toPeg
-            stack.push(Frame(fromPeg, toPeg, 1))
-            // 1st: Set aside all rings but the bottom one: move them to extraPeg
-            stack.push(Frame(fromPeg, extraPeg, frame.countOfRingsToMove - 1))
         }
     }
 
