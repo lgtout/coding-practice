806c87b857f2712d593b6e5e861a7d415d89d809
lgtout <lagos.tout@gmail.com>
2017-09-30 13:38:21 -0700

    EP-K 18.4.3 Find k entries in array that add up to sum - WIP

--- java/src/main/kotlin/com/lagostout/elementsofprogramminginterviews/greedyalgorithmsandinvariants/KSumWithRepetitionAllowed.kt
+++ java/src/main/kotlin/com/lagostout/elementsofprogramminginterviews/greedyalgorithmsandinvariants/KSumWithRepetitionAllowed.kt
@@ -3,10 +3,24 @@ package com.lagostout.elementsofprogramminginterviews.greedyalgorithmsandinvaria
 /**
  * Problem 18.4.3 page 346
  */
-fun kElementsAddUpToNumberWithRepetitionAllowed(k: Int, allElements: List<Int>, s: Int): Boolean {
+fun kElementsAddUpToNumberWithRepetitionAllowed(
+        k: Int, allElements: List<Int>, s: Int): Boolean {
     return false
 }
 
-fun permutationsWithRepetition(elements: List<Int>, repeatCount: Int) {
-
+fun permutationsWithRepetition(
+        elements: List<Int>, repeatCount: Int = 1,
+        startIndex: Int = 0): List<List<Int>> {
+    // TODO This algorithm for generating permutations isn't right yet.
+    return if (repeatCount == 0) {
+        // Should we be returning the same number of empty lists as
+        // the size of the elements list?
+        emptyList()
+    } else {
+        (startIndex..elements.lastIndex).map { index ->
+            permutationsWithRepetition(elements, repeatCount - 1, index).map {
+                listOf(index) + it
+            }.flatten()
+        }
+    }
 }
--- java/src/test/kotlin/com/lagostout/elementsofprogramminginterviews/dynamicprogramming/KSumWithRepetitionAllowedSpek.kt
+++ java/src/test/kotlin/com/lagostout/elementsofprogramminginterviews/dynamicprogramming/KSumWithRepetitionAllowedSpek.kt
@@ -1,22 +1,30 @@
 package com.lagostout.elementsofprogramminginterviews.dynamicprogramming
 
+import com.lagostout.elementsofprogramminginterviews.greedyalgorithmsandinvariants.permutationsWithRepetition
 import org.jetbrains.spek.api.Spek
 import org.jetbrains.spek.api.dsl.describe
+import org.jetbrains.spek.api.dsl.it
+import org.jetbrains.spek.data_driven.Data2
 import org.jetbrains.spek.data_driven.data
 import org.jetbrains.spek.data_driven.on
+import kotlin.test.assertEquals
 
 /**
  * Problem 18.4.3 page 346
  */
 class KSumWithRepetitionAllowedSpek : Spek({
     describe("permutationsWithRepetition") {
-        val data = arrayOf(
-                data(arrayOf(1), 0, expected = false),
-                data(arrayOf(1,2), 0, expected = false)
-        )
+        val data = listOf(
+                data(listOf(1), 0, expected = emptyList<List<Int>>()),
+                data(listOf(1,2), 0, expected = emptyList()),
+                null
+        ).filterNotNull().toTypedArray()
         on("generate permutations of %s with %s repeats allowed", with = *data) {
-            elements, repeats, expected ->
-            // TODO
+            elements: List<Int>, repeatCount: Int, expected: List<List<Int>> ->
+            it("returns $expected") {
+                assertEquals(expected,
+                        permutationsWithRepetition(elements, repeatCount))
+            }
         }
     }
 })
