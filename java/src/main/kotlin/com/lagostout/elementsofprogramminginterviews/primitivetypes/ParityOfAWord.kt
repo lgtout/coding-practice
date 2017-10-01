07adc36f0638ce952bd8d3e5508b9cbc7b94f819
lgtout <lagos.tout@gmail.com>
2017-10-01 12:40:58 -0700

    EP-K 5.1 Parity of a word - WIP

--- java/src/main/kotlin/com/lagostout/elementsofprogramminginterviews/primitivetypes/ParityOfAWord.kt
+++ java/src/main/kotlin/com/lagostout/elementsofprogramminginterviews/primitivetypes/ParityOfAWord.kt
@@ -3,21 +3,37 @@ package com.lagostout.elementsofprogramminginterviews.primitivetypes
 /**
  * Problem 5.1 page 45
  */
-fun computeParity(words: Array<Long>): Array<Boolean> {
-    val parities = arrayOf<Boolean>()
-    // Build cache of 8-bit words
-    val cache = arrayOfNulls<Int>(1 shl 8)
-    // Compute parities
-    cache.withIndex().forEach { (index, value) ->
-        value ?: run {
-            var number = index
+fun computeParity(words: Array<Long>): List<Boolean> {
+    // TODO Handle corner/degenerate cases
+    val parities = mutableListOf<Boolean>()
+    // Build cache of 16-bit words
+    val cache = mutableListOf<Int>().apply {
+        (0..(1 shl 15)).forEach {
+            var number = it
             var parity = 0
             while (number > 0) {
                 number = (number and (number - 1)).apply {
                     parity++
                 }
             }
+            set(it, parity)
         }
+    }
+    // Compute word parities
+    words.forEach { word ->
+        // TODO Simplify/consolidate next 5 lines
+        val mask = (-1L ushr 48)
+        val right = (word and mask)
+        val middleRight = (word ushr 16) and mask
+        val middleLeft = (word ushr 32) and mask
+        val left = (word ushr 48) and mask
+        val parity = listOf(right, middleRight, middleLeft, left).fold(0) {
+            acc, subWord ->
+            // TODO Reduce integer to boolean
+            acc xor cache[subWord.toInt()]
+        }
+        // We represent parity 1 as boolean true
+        parities.add(parity == 1)
     }
     return parities
 }
