956b48f6dbaf009fc8c1996bdba9615492f4c726
lgtout <lagos.tout@gmail.com>
2017-10-02 18:12:05 -0700

    EP-K 12.1.3 Find local minimum - done!

--- java/src/main/kotlin/com/lagostout/elementsofprogramminginterviews/searching/FindLocalMinimum.kt
+++ java/src/main/kotlin/com/lagostout/elementsofprogramminginterviews/searching/FindLocalMinimum.kt
@@ -1,2 +1,23 @@
 package com.lagostout.elementsofprogramminginterviews.searching
 
+/**
+ * Problem 12.1.3 page 193
+ */
+fun findIndexOfLocalMinimum(array: List<Int>): Int {
+    val indexOfMinimum: Int
+    var left = 0
+    var right = array.size
+    while (true) {
+        val middle = (left + right) / 2
+        if (array[middle - 1] >= array[middle] &&
+                array[middle + 1] >= array[middle]) {
+            indexOfMinimum = middle
+            break
+        } else if (array[middle - 1] <= array[middle]) {
+            right = middle
+        } else {
+            left = middle
+        }
+    }
+    return indexOfMinimum
+}
--- java/src/test/kotlin/com/lagostout/elementsofprogramminginterviews/searching/FindLocalMinimumSpek.kt
+++ java/src/test/kotlin/com/lagostout/elementsofprogramminginterviews/searching/FindLocalMinimumSpek.kt
@@ -1,2 +1,30 @@
 package com.lagostout.elementsofprogramminginterviews.searching
 
+import org.jetbrains.spek.api.Spek
+import org.jetbrains.spek.api.dsl.describe
+import org.jetbrains.spek.api.dsl.it
+import org.jetbrains.spek.data_driven.data
+import org.jetbrains.spek.data_driven.on
+import kotlin.test.assertTrue
+
+object FindLocalMinimumSpek : Spek({
+    describe("findIndexOfLocalMinimum") {
+        val data = listOf(
+                data(listOf(2,1,2), expected = setOf(1)),
+                data(listOf(2,1,1,2), expected = setOf(1,2)),
+                data(listOf(2,1,2,1,2), expected = setOf(1,3)),
+                data(listOf(2,1,0,1,2), expected = setOf(2)),
+                data(listOf(5,4,5,6,7,4,3,2,3), expected = setOf(1,2,7)),
+                data(listOf(5,4,6,7,8,9,10,11), expected = setOf(1)),
+                data(listOf(7,6,5,4,3,2,1,2), expected = setOf(6)),
+                null
+        ).filterNotNull().toTypedArray()
+        on("array: %s", with = *data) { array, minima ->
+            it("returns one of $minima") {
+                val indexOfMinimum = findIndexOfLocalMinimum(array)
+                println(indexOfMinimum)
+                assertTrue(minima.contains(indexOfMinimum))
+            }
+        }
+    }
+})
