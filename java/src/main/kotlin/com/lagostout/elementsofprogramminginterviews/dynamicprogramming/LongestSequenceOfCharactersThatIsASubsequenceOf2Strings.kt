b85869c10b0ca146d5896d5369e13de629fb3944
lgtout <lagos.tout@gmail.com>
2017-10-03 15:49:12 -0700

    EP-K 17.2.3 Longest character sequence subsequence of 2 strings - WIP

--- java/src/main/kotlin/com/lagostout/elementsofprogramminginterviews/dynamicprogramming/LongestSequenceOfCharactersThatIsASubsequenceOf2Strings.kt
+++ java/src/main/kotlin/com/lagostout/elementsofprogramminginterviews/dynamicprogramming/LongestSequenceOfCharactersThatIsASubsequenceOf2Strings.kt
@@ -1,15 +1,9 @@
 package com.lagostout.elementsofprogramminginterviews.dynamicprogramming
 
-
-// TODO Implement recursive solution to improve understanding of DP solution.
-fun recursiveLongestSequenceOfCharactersThatIsASubsequenceOfStrings() {
-
-}
-
 /**
  * Problem 17.2.3 page 316
  */
-fun longestSequenceOfCharactersThatIsASubsequenceOfStrings(
+fun longestCommonSubsequence(
         string1: String, string2: String): String {
     data class Position(val row: Int, val column: Int) {
         val belowRight
@@ -18,36 +12,59 @@ fun longestSequenceOfCharactersThatIsASubsequenceOfStrings(
             get() = copy(column = column + 1)
         val below
             get() = copy(row = row + 1)
-        val possiblePreviousPositions
-            get() = listOf(right, below, belowRight)
     }
-    // Build cache
+
+    // Build grid cache
+    // string1 -> vertical, string2 -> horizontal
     val cache = mutableListOf<MutableList<Int>>().apply {
         val row = mutableListOf<Int>().apply {
-            (0..(string2.length + 1)).forEach { add(0) }
+            (0..string2.length).forEach { add(0) }
         }
-        (0..(string1.length + 1)).forEach {
-            add(mutableListOf(*row.toTypedArray()))
+        (0..string1.length).forEach {
+            add(row.toMutableList())
         }
     }
+
+    // Compute grid cache match counts
     cache.indices.reversed().forEach { rowIndex ->
         cache[rowIndex].reversed().forEach { colIndex ->
             val currentCellPosition = Position(rowIndex, colIndex)
-            // TODO Reduce duplication
             val rightCellMatchCount = currentCellPosition.right.run {
                 cache[row][column]
             }
             val belowCellMatchCount = currentCellPosition.below.run {
                 cache[row][column]
             }
-            val belowRightCellMatchCount = if (string1[currentCellPosition.column] == string2[currentCellPosition.row]) {
-                currentCellPosition.belowRight.run { cache[row][column] }
-            } else null
+            val belowRightCellMatchCount =
+                    if (string1[currentCellPosition.column] ==
+                            string2[currentCellPosition.row]) {
+                        currentCellPosition.belowRight.run { cache[row][column] } + 1
+                    } else 0
+            cache[rowIndex][colIndex] = listOf(rightCellMatchCount,
+                    belowCellMatchCount, belowRightCellMatchCount).max()!!
         }
     }
 
-    // Build common sequence from matrix
+    // Build common sequence from walking the grid
+    var position = Position(0,0)
+    val longestSequence = StringBuilder()
+    run {
+        while (true) {
+            with (position) {
+                val currentPositionMatchCount = cache[row][column]
+                if (currentPositionMatchCount == 0) return@run
+                position = if (string1[row] == string2[column]) {
+                    longestSequence.append(string1[row])
+                    position.belowRight
+                } else {
+                    listOf(position.below, position.right).find { (r, c) ->
+                        cache[r][c] == currentPositionMatchCount
+                    }!!
+                }
+            }
+        }
+    }
 
-    return ""
+    return longestSequence.reverse().toString()
 
 }
--- java/src/test/kotlin/com/lagostout/elementsofprogramminginterviews/dynamicprogramming/LongestSequenceOfCharactersThatIsASubsequenceOf2StringsSpek.kt
+++ java/src/test/kotlin/com/lagostout/elementsofprogramminginterviews/dynamicprogramming/LongestSequenceOfCharactersThatIsASubsequenceOf2StringsSpek.kt
@@ -4,15 +4,16 @@ import org.jetbrains.spek.api.Spek
 import org.jetbrains.spek.api.dsl.describe
 import org.jetbrains.spek.api.dsl.given
 import org.jetbrains.spek.api.dsl.it
-import kotlin.test.assertEquals
+import kotlin.test.assertTrue
 
 class LongestSequenceOfCharactersThatIsASubsequenceOf2StringsSpek: Spek({
     describe("levenshteinDistance") {
         testCases.forEach {
-            (from, to, expected) ->
-            given("from: $from to: $to") {
-                it("returns $expected") {
-                    assertEquals(expected, levenshteinDistance(from, to))
+            (string1, string2, expected) ->
+            given("string1: $string1 string2: $string2") {
+                it("returns one of $expected") {
+                    assertTrue(expected.contains(
+                            longestCommonSubsequence(string1, string2)))
                 }
             }
         }
@@ -20,15 +21,40 @@ class LongestSequenceOfCharactersThatIsASubsequenceOf2StringsSpek: Spek({
 }) {
     companion object {
 
-        data class TestCase(val from: String, val to: String) {
-            val expected: Int = bruteForceRightToLeftLevenshteinDistance(from, to)
+        // Not the most efficient.  But it captures the essential idea
+        // of a recursive solution.
+        fun bruteForceLongestCommonSubsequence(
+                string1: String, string2: String): List<String> {
+            val subsequences = mutableListOf<String>()
+            bruteForceCommonSubsequences(subsequences, string1, string2)
+            return subsequences.apply {
+                sortByDescending { it.length }
+            }.let {
+                it.filter { it.length < subsequences[0].length }
+            }
+        }
+
+        // TODO
+        private fun bruteForceCommonSubsequences(
+                commonSubsequences: MutableList<String>,
+                string1: String, string2: String,
+                string1Start: Int = 0, string2Start: Int = 0) {
+            // Could use better names for these cases
+//            val lengthWhenFirstCharacterNotIncluded =
+//                    bruteForceCommonSubsequences(commonSubsequences)
+            val lengthWhenFirstCharacterIncluded = 0
+            val lengthWhenFirstCharactersMatch = 0
+        }
+
+        data class TestCase(val string1: String, val string2: String) {
+            val expected = bruteForceLongestCommonSubsequence(string1, string2)
             operator fun component3() = expected
         }
 
         val testCases: List<TestCase> = run {
             listOf(
-                    TestCase("saturday", "sundays"), // distance: 4
-                    TestCase("carthorse", "orchestra"), // distance: 8
+                    TestCase("saturday", "sundays"),
+                    TestCase("carthorse", "orchestra"),
                     TestCase("", ""),
                     TestCase("", "a"),
                     TestCase("a", ""),
