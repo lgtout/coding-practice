382ba9754e61cb6b0da4d2df1586265d7078c3f2
lgtout <lagos.tout@gmail.com>
2017-10-04 19:54:40 -0700

    EP-K 7.4.1 Replace and remove - WIP

--- java/src/main/kotlin/com/lagostout/elementsofprogramminginterviews/strings/ReplaceAndRemove.kt
+++ java/src/main/kotlin/com/lagostout/elementsofprogramminginterviews/strings/ReplaceAndRemove.kt
@@ -3,6 +3,30 @@ package com.lagostout.elementsofprogramminginterviews.strings
 /**
  * Problem 7.4.1 page 98
  */
-fun replaceAndRemove(chars: List<Char>) {
-
+fun replaceAndRemove(chars: MutableList<Char>) {
+    var aCount = 0
+    var shiftLeftCount = 0
+    var charCount = 0
+    chars.forEachIndexed { index, char ->
+        if (char != 'b') {
+            if (char == 'a') aCount++
+            chars[index - shiftLeftCount]
+            charCount++
+        } else {
+            shiftLeftCount++
+        }
+    }
+    var sourceIndex = charCount - 1
+    var destinationIndex = sourceIndex + aCount
+    (destinationIndex..0).forEach {
+        val char = chars[sourceIndex--].let {
+            if (it == 'a') {
+                'd'
+            } else it
+        }
+        chars[destinationIndex--] = char
+        if (char == 'd') {
+            chars[destinationIndex--] = char
+        }
+    }
 }
--- java/src/test/kotlin/com/lagostout/elementsofprogramminginterviews/strings/ReplaceAndRemoveSpek.kt
+++ java/src/test/kotlin/com/lagostout/elementsofprogramminginterviews/strings/ReplaceAndRemoveSpek.kt
@@ -1,2 +1,29 @@
 package com.lagostout.elementsofprogramminginterviews.strings
 
+import org.jetbrains.spek.api.Spek
+import org.jetbrains.spek.api.dsl.describe
+import org.jetbrains.spek.api.dsl.it
+import org.jetbrains.spek.data_driven.data
+import org.jetbrains.spek.data_driven.on
+import kotlin.test.assertEquals
+
+@Suppress("NAME_SHADOWING")
+object ReplaceAndRemoveSpek : Spek({
+    describe("replaceAndRemove()") {
+        val data = listOf(
+                data(listOf('c'), expected = listOf('c')),
+                data(listOf('d'), expected = listOf('d')),
+                data(listOf('d','d'), expected = listOf('d','d')),
+                data(listOf('e','d'), expected = listOf('e','d')),
+                null
+        ).filterNotNull().toTypedArray()
+        on("replace and remove operations on %s", with = *data) {
+            array, expected ->
+            it("modifies array to $expected") {
+                val array = array.toMutableList()
+                replaceAndRemove(array)
+                assertEquals(expected, array)
+            }
+        }
+    }
+})
