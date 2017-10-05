fed0f140f01f9dd721e9cb183f214a713bc45a51
lgtout <lagos.tout@gmail.com>
2017-10-04 17:33:56 -0700

    EP-K 5.1 Parity of a word - WIP
    This solution is certainly better than brute force, but it's not
    optimal, according to the book solution.

--- java/src/main/kotlin/com/lagostout/elementsofprogramminginterviews/primitivetypes/ParityOfAWord.kt
+++ java/src/main/kotlin/com/lagostout/elementsofprogramminginterviews/primitivetypes/ParityOfAWord.kt
@@ -3,9 +3,9 @@ package com.lagostout.elementsofprogramminginterviews.primitivetypes
 /**
  * Problem 5.1 page 45
  */
-fun computeParity(words: Array<Long>): List<Boolean> {
-    // TODO Handle corner/degenerate cases
-    val parities = mutableListOf<Boolean>()
+fun computeParity(words: Array<Long>): List<Int> {
+    if (words.isEmpty()) return emptyList()
+    val parities = mutableListOf<Int>()
     // Build cache of 16-bit words
     val cache = mutableListOf<Int>().apply {
         (0..(1 shl 15)).forEach {
@@ -13,27 +13,22 @@ fun computeParity(words: Array<Long>): List<Boolean> {
             var parity = 0
             while (number > 0) {
                 number = (number and (number - 1)).apply {
-                    parity++
+                    parity = parity xor 1
                 }
             }
-            set(it, parity)
+            add(it, parity)
         }
     }
     // Compute word parities
+    val mask = (-1L ushr 48)
     words.forEach { word ->
-        // TODO Simplify/consolidate next 5 lines
-        val mask = (-1L ushr 48)
-        val right = (word and mask)
-        val middleRight = (word ushr 16) and mask
-        val middleLeft = (word ushr 32) and mask
-        val left = (word ushr 48) and mask
-        val parity = listOf(right, middleRight, middleLeft, left).fold(0) {
+        val parity = (0..3).map {
+            (word ushr it * 16) and mask
+        }.fold(0) {
             acc, subWord ->
-            // TODO Reduce integer to boolean
             acc xor cache[subWord.toInt()]
         }
-        // We represent parity 1 as boolean true
-        parities.add(parity == 1)
+        parities.add(parity)
     }
     return parities
 }
--- java/src/test/kotlin/com/lagostout/elementsofprogramminginterviews/primitivetypes/ParityOfAWordSpek.kt
+++ java/src/test/kotlin/com/lagostout/elementsofprogramminginterviews/primitivetypes/ParityOfAWordSpek.kt
@@ -1,2 +1,24 @@
 package com.lagostout.elementsofprogramminginterviews.primitivetypes
 
+import org.jetbrains.spek.api.Spek
+import org.jetbrains.spek.api.dsl.describe
+import org.jetbrains.spek.api.dsl.it
+import org.jetbrains.spek.data_driven.data
+import org.jetbrains.spek.data_driven.on
+import kotlin.test.assertEquals
+
+object ParityOfAWordSpek : Spek({
+    describe("computeParity()") {
+        val data = listOf(
+                data(arrayOf(), expected = listOf()),
+                data(arrayOf<Long>(0), expected = listOf(0)),
+                data(arrayOf<Long>(0,3,5,7,11), expected = listOf(0,0,0,1,1)),
+                null
+        ).filterNotNull().toTypedArray()
+        on("compute parity of words %s", with = *data) { words, expected ->
+            it("returns $expected") {
+                assertEquals(expected, computeParity(words))
+            }
+        }
+    }
+})
