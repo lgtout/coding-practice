44dcd4a86eea8024af56a2c8b27608e1c7cdc5fe
lgtout <lagos.tout@gmail.com>
2017-10-05 17:21:20 -0700

    EP-K 12.1.5 Find string with prefix in array of sorted strings - WIP

--- java/src/main/kotlin/com/lagostout/elementsofprogramminginterviews/searching/FindStringWithPrefixInArrayOfSortedStrings.kt
+++ java/src/main/kotlin/com/lagostout/elementsofprogramminginterviews/searching/FindStringWithPrefixInArrayOfSortedStrings.kt
@@ -3,3 +3,23 @@ package com.lagostout.elementsofprogramminginterviews.searching
 /**
  * Problem 12.1.5 page 193
  */
+fun findStringWithPrefix(prefix: String, strings: List<String>): String? {
+    var lo = 0
+    var hi = strings.size
+    var result: String? = null
+    if (strings.isEmpty() || prefix.isEmpty()) return result
+    run {
+        while (lo < hi) {
+            val mid = (hi - lo)/2 + lo
+            when (prefix.compareTo(strings[mid].substring(0, prefix.length))) {
+                0 -> {
+                    result = strings[mid]
+                    return@run
+                }
+                1 -> lo = mid + 1
+                -1 -> hi = mid - 1
+            }
+        }
+    }
+    return result
+}
--- java/src/test/kotlin/com/lagostout/elementsofprogramminginterviews/searching/FindStringWithPrefixInArrayOfSortedStringsSpek.kt
+++ java/src/test/kotlin/com/lagostout/elementsofprogramminginterviews/searching/FindStringWithPrefixInArrayOfSortedStringsSpek.kt
@@ -1,2 +1,31 @@
 package com.lagostout.elementsofprogramminginterviews.searching
 
+import org.jetbrains.spek.api.Spek
+import org.jetbrains.spek.api.dsl.describe
+import org.jetbrains.spek.api.dsl.it
+import org.jetbrains.spek.data_driven.Data2
+import org.jetbrains.spek.data_driven.data
+import org.jetbrains.spek.data_driven.on
+import kotlin.test.assertEquals
+
+object FindStringWithPrefixInArrayOfSortedStringsSpek : Spek({
+    describe("findStringWithPrefix()") {
+        val data = listOf<Data2<List<String>, String, String?>?>(
+//                data(listOf("a"), "", expected = null),
+//                data(listOf("a"), "a", expected = "a"),
+                data(listOf("a","b"), "a", expected = "a"),
+//                data(listOf("a","b"), "b", expected = "b"),
+//                data(listOf("ab","bc"), "b", expected = "bc"),
+//                data(listOf("ab","bc"), "a", expected = "ab"),
+//                data(listOf("ab","ac"), "a", expected = "ab"),
+//                data(listOf("ab","ac","bc","bd","cd","cee"), "ce", expected = "cee"),
+                null
+        ).filterNotNull().toTypedArray()
+        on("strings: %s prefix: %s", with = *data) {
+            strings: List<String>, prefix: String, expected: String? ->
+            it("returns $expected") {
+                assertEquals(expected, findStringWithPrefix(prefix, strings))
+            }
+        }
+    }
+})
