6c4c91bf0145d6bcbe3e43c98958fa0ffde02be0
lgtout <lagos.tout@gmail.com>
2017-10-06 12:30:42 -0700

    EP-K 14.2 Merge sorted arrays - WIP

--- java/src/main/kotlin/com/lagostout/elementsofprogramminginterviews/sorting/MergeSortedArrays.kt
+++ java/src/main/kotlin/com/lagostout/elementsofprogramminginterviews/sorting/MergeSortedArrays.kt
@@ -1,7 +1,47 @@
 package com.lagostout.elementsofprogramminginterviews.sorting
 
-fun mergeSortedArrays(array1: Array<Int?>, array2: Array<Int?>) {
-
-
-
+/**
+ * Problem 14.2 page 242
+ *
+ * Assumptions -
+ * Empty spaces are contiguous and exist only at the end of [toArray].
+ * There may be more empty spaces than are needed to contain the merged
+ * values.
+ */
+fun mergeSortedArrays(toArray: Array<Int?>, fromArray: Array<Int?>) {
+    if (toArray.isEmpty()) return
+    var lastNonEmptyToArrayIndex = 0
+    while (true) {
+        val nextIndex = lastNonEmptyToArrayIndex + 1
+        if (nextIndex > toArray.lastIndex ||
+                toArray[nextIndex] == null) break
+        else lastNonEmptyToArrayIndex = nextIndex
+    }
+    var toArrayIndex = toArray.size
+    (lastNonEmptyToArrayIndex downTo 0).forEach {
+        toArray[--toArrayIndex] = toArray[it]
+    }
+    var mergedArrayIndex = 0
+    var fromArrayIndex = 0
+    while (true) {
+        val pastEndOfFromArray = fromArrayIndex > fromArray.lastIndex
+        val pastEndOfToArray = toArrayIndex > toArray.lastIndex
+        toArray[mergedArrayIndex++] = if (!pastEndOfFromArray && !pastEndOfToArray) {
+            val toArrayValue = toArray[toArrayIndex]!!
+            val fromArrayValue = fromArray[fromArrayIndex]!!
+            if (toArrayValue <= fromArrayValue) {
+                toArrayIndex++
+                toArrayValue
+            } else {
+                fromArrayIndex++
+                fromArrayValue
+            }
+        } else if (!pastEndOfToArray) {
+            toArrayIndex++
+            fromArray[fromArrayIndex]
+        } else if (!pastEndOfFromArray) {
+            toArrayIndex++
+            toArray[toArrayIndex]
+        } else break
+    }
 }
