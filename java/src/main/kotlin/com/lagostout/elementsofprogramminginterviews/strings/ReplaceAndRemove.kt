01b49ab19059ba46026debf48c18445210d2815c
lgtout <lagos.tout@gmail.com>
2017-10-08 19:01:07 -0700

    EP-K 7.4.1 Replace and remove - done!

--- java/src/main/kotlin/com/lagostout/elementsofprogramminginterviews/strings/ReplaceAndRemove.kt
+++ java/src/main/kotlin/com/lagostout/elementsofprogramminginterviews/strings/ReplaceAndRemove.kt
@@ -4,33 +4,37 @@ package com.lagostout.elementsofprogramminginterviews.strings
  * Problem 7.4.1 page 98
  */
 fun replaceAndRemove(chars: MutableList<Char?>) {
+    // We'll assume all empty positions are contiguous and
+    // at the right end of the list.
     var aCount = 0
     var shiftLeftCount = 0
     var charCount = 0
     chars.forEachIndexed { index, char ->
+        val shiftedIndex = index - shiftLeftCount
         if (char == 'b') {
+            // In case all b's, for example.
+            chars[index] = null
             shiftLeftCount++
         } else if (char != null) {
             if (char == 'a') aCount++
-            chars[index - shiftLeftCount] = char
+            chars[shiftedIndex] = char
             charCount++
         }
     }
     var sourceIndex = charCount - 1
     var destinationIndex = sourceIndex + aCount
-//    println(destinationIndex)
-    (destinationIndex downTo 0).forEach {
-        println(it)
+    while (sourceIndex >= 0) {
         chars[sourceIndex--].let {
-//            println(it)
             var charsToWrite = listOf(it)
             if (it == 'a') {
-                charsToWrite = listOf('d','d')
+                charsToWrite = listOf('d', 'd')
             }
             charsToWrite.forEach {
-                chars[destinationIndex--] = it
+                if (destinationIndex <= chars.lastIndex) {
+                    chars[destinationIndex] = it
+                }
+                --destinationIndex
             }
         }
     }
-//    println(chars)
 }
--- java/src/test/kotlin/com/lagostout/elementsofprogramminginterviews/strings/ReplaceAndRemoveSpek.kt
+++ java/src/test/kotlin/com/lagostout/elementsofprogramminginterviews/strings/ReplaceAndRemoveSpek.kt
@@ -3,6 +3,7 @@ package com.lagostout.elementsofprogramminginterviews.strings
 import org.jetbrains.spek.api.Spek
 import org.jetbrains.spek.api.dsl.describe
 import org.jetbrains.spek.api.dsl.it
+import org.jetbrains.spek.data_driven.Data1
 import org.jetbrains.spek.data_driven.data
 import org.jetbrains.spek.data_driven.on
 import kotlin.test.assertEquals
@@ -10,13 +11,27 @@ import kotlin.test.assertEquals
 @Suppress("NAME_SHADOWING")
 object ReplaceAndRemoveSpek : Spek({
     describe("replaceAndRemove()") {
-        val data = listOf(
-//                data(listOf('c'), expected = listOf('c')),
-//                data(listOf('d'), expected = listOf('d')),
-//                data(listOf('d','d'), expected = listOf('d','d')),
-//                data(listOf('e','d'), expected = listOf('e','d')),
-                data(listOf('a', null), expected = listOf<Char?>('d', 'd')),
-//                data(listOf('e','d','a'), expected = listOf('e','d','d')),
+        // Interesting cases: empty; all b's; all a's;
+        // replacement d's spill outside list;
+        // nulls at end of list; no nulls at end of list.
+        val data = listOf<Data1<List<Char?>, List<Char?>>?>(
+                data(listOf(), expected = listOf()),
+                data(listOf(null), expected = listOf(null)),
+                data(listOf('c'), expected = listOf('c')),
+                data(listOf('d'), expected = listOf('d')),
+                data(listOf('d','d'), expected = listOf('d','d')),
+                data(listOf('e','d'), expected = listOf('e','d')),
+                data(listOf('a', null), expected = listOf('d', 'd')),
+                data(listOf('a','a'), expected = listOf('d','d')),
+                data(listOf('e','d','a'), expected = listOf('e','d','d')),
+                data(listOf('e','d','a', null), expected = listOf('e','d','d','d')),
+                data(listOf('e','d','a', null, null), expected = listOf('e','d','d','d', null)),
+                data(listOf('b'), expected = listOf(null)),
+                data(listOf('b','b'), expected = listOf(null, null)),
+                data(listOf('a','b','b'), expected = listOf('d','d', null)),
+                data(listOf('a','b','b','a','a','a'), expected = listOf('d','d','d','d','d','d')),
+                data(listOf('a','b','b','a','a','c', null, null),
+                        expected = listOf('d','d','d','d','d','d','c', null)),
                 null
         ).filterNotNull().toTypedArray()
         on("replace and remove operations on %s", with = *data) {
