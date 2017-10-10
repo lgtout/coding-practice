30ef6fc9e179d54650ba96c7a1cc9307615c467d
lgtout <lagos.tout@gmail.com>
2017-10-10 16:00:30 -0700

    EP-K 16.1.3 Towers of Hanoi with one peg always involved - WIP

--- java/src/main/kotlin/com/lagostout/elementsofprogramminginterviews/recursion/TowersOfHanoiMinimumOperationsWithOnePegAlwaysInvolved.kt
+++ java/src/main/kotlin/com/lagostout/elementsofprogramminginterviews/recursion/TowersOfHanoiMinimumOperationsWithOnePegAlwaysInvolved.kt
@@ -3,6 +3,7 @@ package com.lagostout.elementsofprogramminginterviews.recursion
 import com.lagostout.elementsofprogramminginterviews.recursion.TowersOfHanoi.Pegs
 import com.lagostout.elementsofprogramminginterviews.recursion.TowersOfHanoi.Peg
 import com.lagostout.elementsofprogramminginterviews.recursion.TowersOfHanoi.RingMove
+import com.lagostout.elementsofprogramminginterviews.recursion.TowersOfHanoi.PegPosition
 import java.util.*
 
 /**
@@ -11,14 +12,34 @@ import java.util.*
 fun minimumNumberOfOperationsWithOnePegAlwaysInvolved(
         pegs: Pegs, fromPeg: Peg, toPeg: Peg,
         operations: MutableList<RingMove>) {
+    // We'll assume the right peg is the one that should
+    // be involved in every operation.
+    // TODO Make required peg a parameter.
     data class Frame(val from: Peg, val to: Peg,
                      val countOfRingsToMove: Int)
-
     val stack = LinkedList<Frame>().apply {
         push(Frame(fromPeg, toPeg, fromPeg.size))
     }
+    val rightPegPosition = PegPosition.RIGHT
+    val rightPeg = pegs.at(rightPegPosition)
     while (stack.isNotEmpty()) {
-        with(stack.pop()) { }
-        // TODO
+        stack.pop().let { frame ->
+            if (frame.to != rightPeg) {
+                listOf(Frame(frame.from, rightPeg, frame.countOfRingsToMove),
+                        Frame(rightPeg, frame.to, frame.countOfRingsToMove)).reversed().forEach {
+                    stack.push(it)
+                }
+            } else if (frame.to == rightPeg) {
+                // TODO Is this right?  Have we covered all to/from cases?
+            } else if (frame.from == rightPeg) {
+                val extraPeg = pegs.extra(listOf(frame.from, frame.to))
+                val ringCount = frame.countOfRingsToMove - 1
+                listOf(Frame(frame.from, extraPeg, ringCount),
+                        Frame(frame.from, frame.to, 1),
+                        Frame(extraPeg, frame.to, ringCount)).reversed().forEach {
+                    stack.push(it)
+                }
+            }
+        }
     }
 }
