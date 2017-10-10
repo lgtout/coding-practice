231bd6cf66189b16876378c3e1e6021aef45702c
lgtout <lagos.tout@gmail.com>
2017-10-10 14:03:08 -0700

    EP-K 14.2 Merge sorted arrays - WIP

--- java/src/main/kotlin/com/lagostout/elementsofprogramminginterviews/sorting/MergeSortedArrays.kt
+++ java/src/main/kotlin/com/lagostout/elementsofprogramminginterviews/sorting/MergeSortedArrays.kt
@@ -2,46 +2,52 @@ package com.lagostout.elementsofprogramminginterviews.sorting
 
 /**
  * Problem 14.2 page 242
- *
- * Assumptions -
- * Empty spaces are contiguous and exist only at the end of [toArray].
- * There may be more empty spaces than are needed to contain the merged
- * values.
  */
 fun mergeSortedArrays(toArray: Array<Int?>, fromArray: Array<Int?>) {
-    if (toArray.isEmpty()) return
-    var lastNonEmptyToArrayIndex = 0
-    while (true) {
-        val nextIndex = lastNonEmptyToArrayIndex + 1
-        if (nextIndex > toArray.lastIndex ||
-                toArray[nextIndex] == null) break
-        else lastNonEmptyToArrayIndex = nextIndex
+    // We'll assume that empty spaces are contiguous and exist only at
+    // the end of toArray.
+    // Also, the size of toArray will be equal to or
+    // greater than the number of elements in both arrays.
+    if (fromArray.isEmpty()) return
+    // At this point we know that, since fromArray is not empty,
+    // toArray has at least one empty position.  So we can start
+    // looking for firstNonEmptyToArrayIndex at toArray.lastIndex
+    val nonNullElementCount = toArray.withIndex().find {
+        it.value == null
+    }?.index!!
+    (0 until nonNullElementCount).forEach { index ->
+        toArray[index + nonNullElementCount] = toArray[index]
+        // Not necessary, but might be useful during debugging.
+        toArray[index] = null
     }
-    var toArrayIndex = toArray.size
-    (lastNonEmptyToArrayIndex downTo 0).forEach {
-        toArray[--toArrayIndex] = toArray[it]
-    }
+    var toArrayIndex = toArray.size - nonNullElementCount
     var mergedArrayIndex = 0
     var fromArrayIndex = 0
     while (true) {
         val pastEndOfFromArray = fromArrayIndex > fromArray.lastIndex
         val pastEndOfToArray = toArrayIndex > toArray.lastIndex
+        val fromValue = if (!pastEndOfFromArray) fromArray[fromArrayIndex] else null
+        val toValue = if (!pastEndOfToArray) toArray[toArrayIndex] else null
         toArray[mergedArrayIndex++] = if (!pastEndOfFromArray && !pastEndOfToArray) {
-            val toArrayValue = toArray[toArrayIndex]!!
-            val fromArrayValue = fromArray[fromArrayIndex]!!
-            if (toArrayValue <= fromArrayValue) {
-                toArrayIndex++
-                toArrayValue
-            } else {
-                fromArrayIndex++
-                fromArrayValue
+            compareValues(fromValue, toValue).let {
+                when (it) {
+                    1,0 -> {
+                        ++toArrayIndex
+                        toValue
+                    }
+                    else -> {
+                        ++fromArrayIndex
+                        fromValue
+                    }
+                }
             }
+        } else if (!pastEndOfFromArray) {
+            fromArrayIndex++
+            fromValue
         } else if (!pastEndOfToArray) {
             toArrayIndex++
-            fromArray[fromArrayIndex]
-        } else if (!pastEndOfFromArray) {
-            toArrayIndex++
-            toArray[toArrayIndex]
+            toValue
         } else break
     }
+
 }
--- java/src/test/kotlin/com/lagostout/elementsofprogramminginterviews/sorting/MergeSortedArraysSpek.kt
+++ java/src/test/kotlin/com/lagostout/elementsofprogramminginterviews/sorting/MergeSortedArraysSpek.kt
@@ -10,20 +10,21 @@ import kotlin.test.assertEquals
 object MergeSortedArraysSpek : Spek({
     describe("mergeSortedArrays()") {
         val data = listOf(
-                data(emptyArray(), emptyArray(), expected = emptyArray()),
-                data(arrayOf<Int?>(1), emptyArray(), expected = arrayOf<Int?>(1)),
-                data(arrayOf<Int?>(null), arrayOf<Int?>(1), expected = arrayOf<Int?>(1)),
-                data(arrayOf<Int?>(null, null), arrayOf<Int?>(1), expected = arrayOf(1, null)),
-                data(arrayOf<Int?>(null, null), arrayOf<Int?>(1, 2), expected = arrayOf<Int?>(1, 2)),
+//                data(emptyArray(), emptyArray(), expected = emptyArray()),
+//                data(arrayOf<Int?>(1), emptyArray(), expected = arrayOf<Int?>(1)),
+//                data(arrayOf<Int?>(null), arrayOf<Int?>(1), expected = arrayOf<Int?>(1)),
+//                data(arrayOf<Int?>(null, null), arrayOf<Int?>(1), expected = arrayOf(1, null)),
+//                data(arrayOf<Int?>(null, null), arrayOf<Int?>(1, 2), expected = arrayOf<Int?>(1, 2)),
                 data(arrayOf(1, null, null), arrayOf<Int?>(2, 3), expected = arrayOf<Int?>(1, 2, 3)),
-                data(arrayOf(1, 3, null, null, null), arrayOf<Int?>(2, 4), expected = arrayOf(1, 2, 3, 4, null)),
-                data(arrayOf(5, 6, 8, null, null), arrayOf<Int?>(2, 7), expected = arrayOf<Int?>(2, 5, 6, 7, 8)),
+//                data(arrayOf(1, 3, null, null, null), arrayOf<Int?>(2, 4), expected = arrayOf(1, 2, 3, 4, null)),
+//                data(arrayOf(5, 6, 8, null, null), arrayOf<Int?>(2, 7), expected = arrayOf<Int?>(2, 5, 6, 7, 8)),
                 null
         ).filterNotNull().toTypedArray()
-        on("merging %s and %a", with = *data) { array1, array2, expected ->
+        println(data.toList())
+        on("merging %s and %s", with = *data) { array1, array2, expected ->
             it("modifies the first array to $expected") {
                 mergeSortedArrays(array1, array2)
-                assertEquals(expected, array1)
+                assertEquals(expected.toList(), array1.toList())
             }
         }
     }
