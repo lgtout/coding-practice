025985ddbaeba8027a913598614f4a2d3e46d3d2
lgtout <lagos.tout@gmail.com>
2017-10-11 08:43:22 -0700

    EP-K 16.1.3 Towers of Hanoi with one peg always involved - done!

--- java/src/main/kotlin/com/lagostout/elementsofprogramminginterviews/recursion/TowersOfHanoiMinimumOperationsWithOnePegAlwaysInvolved.kt
+++ java/src/main/kotlin/com/lagostout/elementsofprogramminginterviews/recursion/TowersOfHanoiMinimumOperationsWithOnePegAlwaysInvolved.kt
@@ -1,37 +1,57 @@
 package com.lagostout.elementsofprogramminginterviews.recursion
 
-import com.lagostout.elementsofprogramminginterviews.recursion.TowersOfHanoi.Pegs
 import com.lagostout.elementsofprogramminginterviews.recursion.TowersOfHanoi.Peg
+import com.lagostout.elementsofprogramminginterviews.recursion.TowersOfHanoi.Pegs
 import com.lagostout.elementsofprogramminginterviews.recursion.TowersOfHanoi.RingMove
-import com.lagostout.elementsofprogramminginterviews.recursion.TowersOfHanoi.PegPosition
 import java.util.*
 
 /**
  * Problem 16.1.3 page 290
  */
 fun minimumNumberOfOperationsWithOnePegAlwaysInvolved(
-        pegs: Pegs, fromPeg: Peg, toPeg: Peg,
+        pegs: Pegs, fromPeg: Peg, toPeg: Peg, requiredPeg: Peg,
         operations: MutableList<RingMove>) {
-    // We'll assume the right peg is the one that should
-    // be involved in every operation.
-    // TODO Make required peg a parameter.
+
+    if (fromPeg.isEmpty) return
+
     data class Frame(val from: Peg, val to: Peg,
                      val countOfRingsToMove: Int)
+
     val stack = LinkedList<Frame>().apply {
         push(Frame(fromPeg, toPeg, fromPeg.size))
     }
-    val rightPegPosition = PegPosition.RIGHT
-    val rightPeg = pegs.at(rightPegPosition)
+
     while (stack.isNotEmpty()) {
-        stack.pop().let { frame ->
-            if (frame.to != rightPeg) {
-                listOf(Frame(frame.from, rightPeg, frame.countOfRingsToMove),
-                        Frame(rightPeg, frame.to, frame.countOfRingsToMove)).reversed().forEach {
-                    stack.push(it)
+        val frame = stack.pop()
+        if (frame.from != requiredPeg && frame.to != requiredPeg) {
+            listOf(Frame(frame.from, requiredPeg, frame.countOfRingsToMove),
+                    Frame(requiredPeg, frame.to, frame.countOfRingsToMove)).reversed().forEach {
+                stack.push(it)
+            }
+        } else if (frame.to == requiredPeg) {
+            if (frame.countOfRingsToMove == 1) {
+                frame.from.pop().let {
+                    frame.to.push(it)
+                    operations.add(RingMove(
+                            frame.from.position, frame.to.position, it))
                 }
-            } else if (frame.to == rightPeg) {
-                // TODO Is this right?  Have we covered all to/from cases?
-            } else if (frame.from == rightPeg) {
+            } else {
+                val extraPeg = pegs.extra(listOf(frame.from, frame.to))
+                val ringCount = frame.countOfRingsToMove - 1
+                listOf(Frame(frame.from, requiredPeg, ringCount),
+                        Frame(requiredPeg, extraPeg, ringCount),
+                        Frame(frame.from, frame.to, 1),
+                        Frame(extraPeg, frame.to, ringCount))
+                        .reversed().forEach { stack.push(it) }
+            }
+        } else if (frame.from == requiredPeg) {
+            if (frame.countOfRingsToMove == 1) {
+                frame.from.pop().let {
+                    frame.to.push(it)
+                    operations.add(RingMove(
+                            frame.from.position, frame.to.position, it))
+                }
+            } else {
                 val extraPeg = pegs.extra(listOf(frame.from, frame.to))
                 val ringCount = frame.countOfRingsToMove - 1
                 listOf(Frame(frame.from, extraPeg, ringCount),
--- java/src/test/kotlin/com/lagostout/elementsofprogramminginterviews/recursion/TowersOfHanoiMinimumOperationsWithOnePegAlwaysInvolvedSpek.kt
+++ java/src/test/kotlin/com/lagostout/elementsofprogramminginterviews/recursion/TowersOfHanoiMinimumOperationsWithOnePegAlwaysInvolvedSpek.kt
@@ -1,2 +1,81 @@
 package com.lagostout.elementsofprogramminginterviews.recursion
 
+import com.lagostout.elementsofprogramminginterviews.recursion.TowersOfHanoi.PegPosition
+import com.lagostout.elementsofprogramminginterviews.recursion.TowersOfHanoi.PegPosition.*
+import com.lagostout.elementsofprogramminginterviews.recursion.TowersOfHanoi.Pegs
+import com.lagostout.elementsofprogramminginterviews.recursion.TowersOfHanoi.RingMove
+import org.jetbrains.spek.api.Spek
+import org.jetbrains.spek.api.dsl.describe
+import org.jetbrains.spek.api.dsl.given
+import org.jetbrains.spek.api.dsl.it
+import kotlin.test.assertEquals
+
+object TowersOfHanoiMinimumOperationsWithOnePegAlwaysInvolvedSpek : Spek({
+
+    describe("minimumNumberOfOperationsWithOnePegAlwaysInvolved") {
+
+        fun pegsFromRunningOperations(
+                ringCount: Int,
+                fromPosition: PegPosition,
+                operations: List<TowersOfHanoi.RingMove>): Pegs {
+            val pegs = Pegs(ringCount, fromPosition)
+            operations.forEach { (fromPosition, toPosition) ->
+                pegs.at(toPosition).let { toPeg ->
+                    pegs.at(fromPosition).let { fromPeg ->
+                        toPeg.push(fromPeg.pop())
+                    }
+                }
+            }
+            return pegs
+        }
+
+        data class TestCase(val fromPegPosition: PegPosition,
+                            val toPegPosition: PegPosition,
+                            val ringCount: Int = 0,
+                            val requiredPegPosition: PegPosition = PegPosition.RIGHT,
+                            val expectedOperationCount: Int? = null)
+
+        val testCases = listOf(
+                TestCase(TowersOfHanoi.PegPosition.LEFT, MIDDLE),
+                TestCase(LEFT, MIDDLE, 1),
+                TestCase(LEFT, MIDDLE, 2),
+                TestCase(LEFT, MIDDLE, 3, expectedOperationCount = 26),
+                TestCase(RIGHT, RIGHT, 5),
+                TestCase(MIDDLE, RIGHT, 5),
+                TestCase(RIGHT, MIDDLE, 5),
+                TestCase(RIGHT, LEFT, 5),
+                null).filterNotNull()
+
+        testCases.forEach {
+            (fromPegPosition, toPegPosition, ringCount,
+                    requiredPegPosition, expectedOperationCount) ->
+            given("ringCount: $ringCount, from peg: $fromPegPosition, " +
+                    "to peg: $toPegPosition, required peg: $requiredPegPosition, " +
+                    "expected operation count: $expectedOperationCount") {
+                it("moves rings between pegs") {
+                    val pegs = TowersOfHanoi.Pegs(ringCount, fromPegPosition)
+                    val operations = mutableListOf<RingMove>()
+                            minimumNumberOfOperationsWithOnePegAlwaysInvolved(
+                                    pegs, pegs.at(fromPegPosition),
+                                    pegs.at(toPegPosition), pegs.at(requiredPegPosition),
+                                    operations)
+                    println(operations.size)
+                    // Too much work, by hand, to figure out what the expected number
+                    // of operations should be.  And I don't want to spend time on an
+                    // alternate solution.  But I'm fairly confident my solution
+                    // results in the minimum number.  So we just need to verify that
+                    // the operations put the pegs and rings in the final state expected.
+                    // However, I did go through all the steps for one case (moving 3 rings
+                    // from left to middle peg), so I can verify expected operation count
+                    // for that one.
+                    assertEquals(pegs, pegsFromRunningOperations(
+                            ringCount, fromPegPosition, operations))
+                    expectedOperationCount?.let {
+                        assertEquals(it, operations.size)
+                    }
+                }
+            }
+        }
+    }
+
+})
