06f7ea47b8a87caa33346599d3da656b373d4438
lgtout <lagos.tout@gmail.com>
2017-10-12 18:45:38 -0700

    EP-K 5.2 Swap bits - WIP

---
+++ java/src/main/kotlin/com/lagostout/elementsofprogramminginterviews/primitivetypes/SwapBits.kt
@@ -0,0 +1,6 @@
+package com.lagostout.elementsofprogramminginterviews.primitivetypes
+
+fun swapBits(number: Long, from: Int, to: Int) {
+    // Let's assume position 0 is the LSB, and 64 is the MSB.
+
+}
