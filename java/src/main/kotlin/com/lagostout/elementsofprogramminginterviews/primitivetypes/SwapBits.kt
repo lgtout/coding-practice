20b546f84ed8adb9089099f1649ec895fbbae63e
lgtout <lagos.tout@gmail.com>
2017-10-17 11:15:25 -0700

    EP-K 5.2 Swap bits - WIP

--- java/src/main/kotlin/com/lagostout/elementsofprogramminginterviews/primitivetypes/SwapBits.kt
+++ java/src/main/kotlin/com/lagostout/elementsofprogramminginterviews/primitivetypes/SwapBits.kt
@@ -1,6 +1,21 @@
 package com.lagostout.elementsofprogramminginterviews.primitivetypes
 
-fun swapBits(number: Long, from: Int, to: Int) {
-    // Let's assume position 0 is the LSB, and 64 is the MSB.
-
+fun swapBits(number: Long, from: Int, to: Int): Long {
+    // Let's assume position 0 is the LSB, and 63 is the MSB.
+    var swappedNumber = number
+    // Extract the values at both positions
+    val fromMask = (1L shl from)
+    val toMask = (1L shl to)
+    val fromValue = number and fromMask
+    val toValue = number and toMask
+    if (fromValue != toValue) {
+        listOf(fromValue to toMask, toValue to fromMask).forEach {
+            (value, mask) ->
+            swappedNumber = swappedNumber.let {
+                if (value == 0L) it and mask.inv()
+                else it or mask
+            }
+        }
+    }
+    return swappedNumber
 }
--- java/src/test/kotlin/com/lagostout/elementsofprogramminginterviews/primitivetypes/SwapBitsSpek.kt
+++ java/src/test/kotlin/com/lagostout/elementsofprogramminginterviews/primitivetypes/SwapBitsSpek.kt
@@ -1,2 +1,29 @@
 package com.lagostout.elementsofprogramminginterviews.primitivetypes
 
+import org.jetbrains.spek.api.Spek
+import org.jetbrains.spek.api.dsl.describe
+import org.jetbrains.spek.api.dsl.it
+import org.jetbrains.spek.data_driven.Data3
+import org.jetbrains.spek.data_driven.data
+import org.jetbrains.spek.data_driven.on
+import kotlin.test.assertEquals
+
+object SwapBitsSpek : Spek({
+    describe("swapBits()") {
+        // Negative numbers aren't a special case.
+        val data = listOf<Data3<Long, Int, Int, Long>?>(
+                data(0, 0, 0, 0),
+                data(0, 1, 2, 0),
+                data(1, 0, 1, 2),
+                data(1, 0, 1, 2),
+                data(10, 0, 1, 2),
+                null
+        ).filterNotNull().toTypedArray()
+        on("number %s, from %s, to %s", with = *data) {
+            number, from, to, expected ->
+            it("returns ${java.lang.Long.toBinaryString(expected)}") {
+                assertEquals(expected, swapBits(number, from, to))
+            }
+        }
+    }
+})
