550d8d79c608a86378e3384604a9548c4aa66f50
lgtout <lagos.tout@gmail.com>
2017-10-22 09:19:58 -0700

    EP-K 9.5 Search postings list - done!

--- java/src/main/kotlin/com/lagostout/elementsofprogramminginterviews/stacksandqueues/SearchPostingsList.kt
+++ java/src/main/kotlin/com/lagostout/elementsofprogramminginterviews/stacksandqueues/SearchPostingsList.kt
@@ -1,15 +1,24 @@
 package com.lagostout.elementsofprogramminginterviews.stacksandqueues
 
+import com.google.common.base.MoreObjects.toStringHelper
 import java.util.*
 
+/**
+ * Problem 9.5 page 140
+ */
 object SearchPostingsList {
 
     data class Entry<T>(val value: T,
                         var jumpNext: Entry<T>? = null,
                         var next: Entry<T>? = null,
-                        var order: Int = -1)
+                        var order: Int = -1) {
+        override fun toString(): String {
+            return toStringHelper(this).add("value", value)
+                    .add("order", order).toString()
+        }
+    }
 
-    data class RawEntry<T>(val value: T, val jumpNext: Int? = null)
+    data class RawEntry<out T>(val value: T, val jumpNext: Int? = null)
 
     fun <T> iterativeAssignJumpFirstOrder(head: Entry<T>) {
         val stack = LinkedList<Entry<T>>()
@@ -17,9 +26,11 @@ object SearchPostingsList {
         stack.add(head)
         while (!stack.isEmpty()) {
             val entry = stack.pop()
+            // If order has been set, we've
+            // already explored this entry.
             if (entry.order >= 0) continue
             entry.order = order++
-            listOf(entry.next, entry.jumpNext).forEach {
+            listOf(entry.next, entry.jumpNext).filterNotNull().forEach {
                 stack.push(it)
             }
         }
--- java/src/test/kotlin/com/lagostout/elementsofprogramminginterviews/stacksandqueues/SearchPostingsListSpek.kt
+++ java/src/test/kotlin/com/lagostout/elementsofprogramminginterviews/stacksandqueues/SearchPostingsListSpek.kt
@@ -1,26 +1,33 @@
 package com.lagostout.elementsofprogramminginterviews.stacksandqueues
 
-import org.jetbrains.spek.api.Spek
-import org.jetbrains.spek.api.dsl.describe
-import com.lagostout.elementsofprogramminginterviews.stacksandqueues.SearchPostingsList.RawEntry
 import com.lagostout.elementsofprogramminginterviews.stacksandqueues.SearchPostingsList.Entry
+import com.lagostout.elementsofprogramminginterviews.stacksandqueues.SearchPostingsList.RawEntry
 import com.lagostout.elementsofprogramminginterviews.stacksandqueues.SearchPostingsList.iterativeAssignJumpFirstOrder
 import com.lagostout.elementsofprogramminginterviews.stacksandqueues.SearchPostingsList.recursiveAssignJumpFirstOrder
-import org.jetbrains.spek.data_driven.data
-import org.jetbrains.spek.data_driven.on
+import org.jetbrains.spek.api.Spek
+import org.jetbrains.spek.api.dsl.describe
+import org.jetbrains.spek.api.dsl.it
+import org.jetbrains.spek.api.dsl.on
+import kotlin.test.assertEquals
 
 typealias RE = RawEntry<Char>
+
 object SearchPostingsListSpek : Spek({
-    describe("") {
+    describe("assign jump order") {
         val data = listOf(
                 Pair(listOf(RE('a')), listOf(0)),
+                Pair(listOf(RE('a', 0)), listOf(0)),
                 Pair(listOf(RE('a', 2), RE('b', 3), RE('c', 1), RE('d', 3)),
                         listOf(0, 2, 1, 3)),
+                Pair(listOf(RE('a', 2), RE('b', 3), RE('c'), RE('d')),
+                        listOf(0, 3, 1, 2)),
+                Pair(listOf(RE('a', 3), RE('b', 3), RE('c'), RE('d')),
+                        listOf(0, 2, 3, 1)),
                 null).filterNotNull().map { (rawEntries, expectedOrder) ->
             val entries = rawEntries.map { Entry(it.value) }.let { entries ->
                 entries.forEachIndexed { index, entry ->
                     with (entry) {
-                        if (index <= rawEntries.lastIndex) {
+                        if (index < rawEntries.lastIndex) {
                             next = entries[index + 1]
                         }
                         rawEntries[index].jumpNext?.let {
@@ -30,10 +37,29 @@ object SearchPostingsListSpek : Spek({
                 }
                 entries
             }
-            data(entries, expectedOrder)
-        }.toTypedArray()
-        on("", with = *data) { entries, expectedOrder ->
-            // TODO
+            Pair(entries, expectedOrder)
+        }
+        describe("iterativeAssignJumpFirstOrder()") {
+            var count = 0
+            data.forEach { (entries, expectedOrder) ->
+                on("#${++count} $entries") {
+                    it("returns assigned order: $expectedOrder") {
+                        iterativeAssignJumpFirstOrder(entries[0])
+                        assertEquals(expectedOrder, entries.map { it.order })
+                    }
+                }
+            }
+        }
+        describe("recursiveAssignJumpFirstOrder()") {
+            var count = 0
+            data.forEach { (entries, expectedOrder) ->
+                on("#${++count} $entries") {
+                    it("[${++count}] returns assigned order: $expectedOrder") {
+                        recursiveAssignJumpFirstOrder(entries[0])
+                        assertEquals(expectedOrder, entries.map { it.order })
+                    }
+                }
+            }
         }
     }
 })
