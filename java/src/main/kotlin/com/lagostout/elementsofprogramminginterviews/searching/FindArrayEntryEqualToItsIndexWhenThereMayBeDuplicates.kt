c08ad2602cd6edfc1b5d019a952d377022318b02
lgtout <lagos.tout@gmail.com>
2017-10-23 09:08:52 -0700

    EP-K 12.2.2 Find array entry equal to its index with duplicates - WIP

--- java/src/main/kotlin/com/lagostout/elementsofprogramminginterviews/searching/FindArrayEntryEqualToItsIndexWhenThereMayBeDuplicates.kt
+++ java/src/main/kotlin/com/lagostout/elementsofprogramminginterviews/searching/FindArrayEntryEqualToItsIndexWhenThereMayBeDuplicates.kt
@@ -1,20 +1,44 @@
 package com.lagostout.elementsofprogramminginterviews.searching
 
+import java.util.*
+
+/**
+ * Problem 12.2.2 page 194
+ */
 fun findArrayEntryEqualToItsIndexWhenThereMayBeDuplicates(
         array: List<Int>): Int? {
-    var left = 0
-    var right = array.lastIndex
     var entryEqualToIndex: Int? = null
-    while (left <= right) {
-        val mid = (right - left) + (left/2)
-        val midEntry = array[mid]
-        if (mid == midEntry) {
-            entryEqualToIndex = mid
-            break
-        } else if (mid < midEntry || (mid > midEntry)) {
-            right = mid - 1
-        } else if (mid > midEntry) {
-            left = mid + 1
+    if (array.isEmpty()) return entryEqualToIndex
+    data class Frame(val left: Int, val right: Int) {
+        val mid: Int
+            get() = (left - right) + right/2
+    }
+    val stack = LinkedList<Frame>().apply {
+        push(Frame(0, array.lastIndex))
+    }
+    while (stack.isNotEmpty() || entryEqualToIndex != null) {
+        val frame = stack.pop()
+        if (with (frame) {
+            left > right || left < 0 || right > array.lastIndex
+        }) continue
+        val mid = frame.mid
+        val entry = array[mid]
+        when {
+            entry == mid -> entryEqualToIndex = entry
+            entry < mid ->
+                listOf(Frame(frame.left, array[entry]),
+                        Frame(mid + 1, frame.right))
+                        .reversed()
+                        .forEach {
+                            stack.push(it)
+                        }
+            else -> {
+                listOf(Frame(array[entry], frame.right),
+                        Frame(frame.left, mid - 1))
+                        .forEach {
+                            stack.push(it)
+                        }
+            }
         }
     }
     return entryEqualToIndex
--- java/src/test/kotlin/com/lagostout/elementsofprogramminginterviews/searching/FindArrayEntryEqualToItsIndexWhenThereMayBeDuplicatesSpek.kt
+++ java/src/test/kotlin/com/lagostout/elementsofprogramminginterviews/searching/FindArrayEntryEqualToItsIndexWhenThereMayBeDuplicatesSpek.kt
@@ -1,7 +1,48 @@
 package com.lagostout.elementsofprogramminginterviews.searching
 
+import org.assertj.core.api.Assertions.anyOf
+import org.assertj.core.api.Assertions.assertThat
+import org.assertj.core.api.Condition
 import org.jetbrains.spek.api.Spek
+import org.jetbrains.spek.api.dsl.describe
+import org.jetbrains.spek.api.dsl.it
+import org.jetbrains.spek.data_driven.Data1
+import org.jetbrains.spek.data_driven.data
+import org.jetbrains.spek.data_driven.on
 
 object FindArrayEntryEqualToItsIndexWhenThereMayBeDuplicatesSpek : Spek({
-
+    describe("findArrayEntryEqualToItsIndexWhenThereMayBeDuplicates()") {
+        val data = listOf<Data1<List<Int>, List<Int>>?>(
+//                data(emptyList(), emptyList()),
+                data(listOf(1), emptyList()),
+//                data(listOf(-1), emptyList()),
+//                data(listOf(-1,0), emptyList()),
+//                data(listOf(0), listOf(0)),
+//                data(listOf(0,1), listOf(0,1)),
+//                data(listOf(0,0,2), listOf(0)),
+//                data(listOf(-1,1), listOf(1)),
+//                data(listOf(-1,-1,1), emptyList()),
+//                data(listOf(-1,1,1,1,1,1,1), listOf(1)),
+//                data(listOf(-1,1,1,2,3,4,6,7,8,9,10), listOf(1)),
+//                data(listOf(-1,0,1,2,3,4,5,7,8,9,10), listOf(7,8,9,10)),
+//                data(listOf(-1,0,1,2,3,4,5,6,6,9,11), listOf(9)),
+                // TODO More cases - especially those that examine both sides of mid
+//                data(listOf(), listOf()),
+                null
+        ).filterNotNull().toTypedArray()
+        on("array %s", with = *data) { array, expected ->
+            it("returns one of $expected") {
+                assertThat(findArrayEntryEqualToItsIndexWhenThereMayBeDuplicates(array))
+                        .`is`(anyOf(object: Condition<Int?>(){
+                            override fun matches(value: Int?): Boolean {
+                                return value == null && expected.isEmpty()
+                            }
+                        }, object: Condition<Int?>() {
+                            override fun matches(value: Int?): Boolean {
+                                return expected.contains(value)
+                            }
+                        }))
+            }
+        }
+    }
 })
