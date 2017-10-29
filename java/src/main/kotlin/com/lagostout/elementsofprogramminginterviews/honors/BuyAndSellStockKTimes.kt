a642c5d11f4957e4f2582e2a6d1d36ccd6d90ace
lgtout <lagos.tout@gmail.com>
2017-10-29 14:53:45 -0700

    EP-K 25.3.1 Buy and sell stock k times - WIP

--- java/src/main/kotlin/com/lagostout/elementsofprogramminginterviews/honors/BuyAndSellStockKTimes.kt
+++ java/src/main/kotlin/com/lagostout/elementsofprogramminginterviews/honors/BuyAndSellStockKTimes.kt
@@ -1,5 +1,28 @@
 package com.lagostout.elementsofprogramminginterviews.honors
 
+/**
+ * Problem 25.3.1 page 445
+ */
 fun buyAndSellStockKTimes(prices: List<Double>, k: Int) {
-
+    data class BuySell(val buy: Int, val sell: Int)
+    val requiredWidth = (k * 2).also {
+        if (it > prices.size)
+            throw IllegalArgumentException(
+                    "Prices list must be at least $it in size")
+    }
+    val buySellPairs = mutableListOf<BuySell>().apply {
+        (0 until requiredWidth step 2).forEach {
+            add(BuySell(it, it + 1))
+        }
+    }
+    val rightBound = prices.lastIndex
+    val buySellPairsIterator = buySellPairs.reversed().iterator()
+    val buySellPair = buySellPairsIterator.next()
+    while (true) {
+        val profit = prices[buySellPair.buy] -
+                prices[buySellPair.sell]
+        while (true) {
+//            if ()
+        }
+    }
 }
