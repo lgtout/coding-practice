a67145c76d5fdf5d57da0a6607515beeb3839330
lgtout <lagos.tout@gmail.com>
2017-10-29 12:17:16 -0700

    EP-K 19.4.3 Graph remains connected after removing any edge - done!

--- java/src/main/kotlin/com/lagostout/elementsofprogramminginterviews/graphs/GraphRemainsConnectedAfterRemovingAnyEdge.kt
+++ java/src/main/kotlin/com/lagostout/elementsofprogramminginterviews/graphs/GraphRemainsConnectedAfterRemovingAnyEdge.kt
@@ -3,23 +3,25 @@ package com.lagostout.elementsofprogramminginterviews.graphs
 /**
  * Problem 19.4.3 page 369
  */
+// Might have been simpler and more appropriate to represent the
+// graph with an edge list, instead of an adjacency list.
 object GraphRemainsConnectedAfterRemovingAnyEdge {
     data class Node<out K>(val value: K)
     fun <K> graphRemainsConnectedAfterRemovingAnyEdge(
-            graph: Map<Node<K>, List<Node<K>>>): Boolean {
+            graph: Map<Node<K>, Set<Node<K>>>): Boolean {
         val nodeToIncomingEdgeCountMap = mutableMapOf<Node<K>, Int>()
         graph.values.forEach { adjacentNodes ->
             adjacentNodes.forEach { node ->
                 nodeToIncomingEdgeCountMap.let {
-                    it.put(node,
-                            it.getOrPut(node, { 0 }) + 1)
+                    it.put(node, it.getOrPut(node, { 0 }) + 1)
                 }
             }
         }
-        nodeToIncomingEdgeCountMap.let {
+        return nodeToIncomingEdgeCountMap.let {
             if (it.any { it.value == 0 })
-                throw IllegalArgumentException("Graph must be connected, to begin with.")
-            return it.any { it.value == 1 }
+                throw IllegalArgumentException(
+                        "Graph must be connected, to begin with.")
+            graph.size == 1 || !it.containsValue(1)
         }
     }
 }
--- java/src/test/kotlin/com/lagostout/elementsofprogramminginterviews/graphs/GraphRemainsConnectedAfterRemovingAnyEdgeSpek.kt
+++ java/src/test/kotlin/com/lagostout/elementsofprogramminginterviews/graphs/GraphRemainsConnectedAfterRemovingAnyEdgeSpek.kt
@@ -11,25 +11,40 @@ import org.jetbrains.spek.data_driven.on
 
 object GraphRemainsConnectedAfterRemovingAnyEdgeSpek : Spek({
     fun toGraph(rawMap: Map<Node<Char>, List<Int>>):
-            Map<Node<Char>, List<Node<Char>>> {
+            Map<Node<Char>, Set<Node<Char>>> {
         val keys = rawMap.keys.toList()
-        return rawMap.mapValues { (_, value) ->
-            value.map {
+        return rawMap.mapValues {
+            // Convert indices to nodes
+            it.value.map {
                 keys[it]
+            }.toMutableSet()
+        }.apply {
+            // Add back edges
+            forEach { (key, adjacentNodes) ->
+                adjacentNodes.forEach {
+                    get(it)?.add(key)
+                }
             }
         }
     }
     describe("graphRemainsConnectedAfterRemovingAnyEdge()") {
         val data = listOf(
-                Pair(mapOf(Node('A') to emptyList()), false),
-                Pair(mapOf(Node('A') to listOf(1), Node('B') to emptyList()), true),
-                Pair(mapOf(Node('A') to listOf(1), Node('B') to listOf(0)), false),
+                Pair(mapOf(Node('A') to emptyList()), true),
+                Pair(mapOf(Node('A') to listOf(1), Node('B') to emptyList()), false),
                 Pair(mapOf(Node('A') to listOf(2), Node('B') to listOf(2),
-                        Node('C') to listOf(3), Node('D') to emptyList()), true),
+                        Node('C') to listOf(3), Node('D') to emptyList()), false),
+                Pair(mapOf(Node('A') to listOf(1,2), Node('B') to listOf(2),
+                        Node('C') to listOf(3), Node('D') to emptyList()), false),
+                Pair(mapOf(Node('A') to listOf(1), Node('B') to listOf(2),
+                        Node('C') to emptyList()), false),
+                Pair(mapOf(Node('A') to listOf(1), Node('B') to listOf(2),
+                        Node('C') to listOf(1)), false),
+                Pair(mapOf(Node('A') to listOf(1), Node('B') to listOf(2),
+                        Node('C') to listOf(0)), true),
                 null
         ).filterNotNull().map { data(toGraph(it.first), it.second) }.toTypedArray()
         on("graph: %s", with = *data) {
-            graph: Map<Node<Char>, List<Node<Char>>>, expected: Boolean ->
+            graph: Map<Node<Char>, Set<Node<Char>>>, expected: Boolean ->
             it("returns $expected") {
                 assertThat(graphRemainsConnectedAfterRemovingAnyEdge(graph))
                         .isEqualTo(expected)
