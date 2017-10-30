7a6c323937bd90f47bd73dd88250c39aec314c0e
lgtout <lagos.tout@gmail.com>
2017-10-30 12:00:54 -0700

    EP-K 5.3 Reverse bits - WIP

--- java/src/main/kotlin/com/lagostout/elementsofprogramminginterviews/primitivetypes/ReverseBits.kt
+++ java/src/main/kotlin/com/lagostout/elementsofprogramminginterviews/primitivetypes/ReverseBits.kt
@@ -2,23 +2,35 @@ package com.lagostout.elementsofprogramminginterviews.primitivetypes
 
 import com.google.common.math.IntMath
 
-fun reverseBits(number: Long): Int {
+/**
+ * Problem 5.3 page 49
+ */
+fun reverseBits(number: Long): Long {
     // Construct 16-bit cache
-    val cache = mapOf(0 to 0, 1 to 1)
-    var level = 1
-    while (level <= 15) {
-        val bitCount = IntMath.pow(2, level)
-        bitCount.let {
+    val cache = mutableMapOf(0 to 0, 1 to 1)
+    var exponent = 1
+    while (exponent <= 15) {
+        IntMath.pow(2, exponent).let {
             IntRange(it, it * 2 - 1)
-        }.forEach {
-            (bitCount / 2).let { shiftCount ->
-                Pair(it ushr shiftCount, it shl shiftCount shr shiftCount)
-                // Lookup left and right halves.
-                // Combine in reverse.
-                // Add to cache.
+        }.forEach { cacheNumber ->
+            ((exponent + 1) / 2).let { bitCount ->
+                val rightHalfShift = 64 - bitCount
+                Pair(cacheNumber ushr bitCount,
+                        cacheNumber shl rightHalfShift shr rightHalfShift).let {
+                    cache[cacheNumber] = it.second shl bitCount and it.first
+                }
             }
         }
-        level *= 2
+        ++exponent
     }
-    return 0
+    // Reverse bits
+    return (0..3).map {
+        (number shl it * 16 ushr 48).toInt()
+    }.withIndex().map { (index, value) ->
+        (cache[value]!! shl (index * 16)).toLong()
+    }.reduce { acc, l ->
+        acc and l
+    }.apply {
+        println(this)
+    }
 }
