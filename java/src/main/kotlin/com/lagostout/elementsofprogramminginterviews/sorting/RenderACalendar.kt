3acb60adfd600d2ec3493215b1c0916b6d4b760d
lgtout <lagos.tout@gmail.com>
2017-11-01 14:20:42 -0700

    EP-K 14.4.1 Render a calendar - WIP

---
+++ java/src/main/kotlin/com/lagostout/elementsofprogramminginterviews/sorting/RenderACalendar.kt
@@ -0,0 +1,42 @@
+package com.lagostout.elementsofprogramminginterviews.sorting
+
+import org.apache.commons.lang3.builder.ReflectionToStringBuilder
+import org.apache.commons.lang3.builder.ToStringStyle
+import java.util.*
+
+object RenderACalendar {
+    data class Event(val start: Int = 0, val end: Int = 0)
+    class Level : Comparable<Level> {
+        private val events = mutableListOf<Event>()
+        val end: Int
+            get() = if (events.isEmpty()) 0
+            else events.last().end
+        fun add(event: Event) {
+            events.add(event)
+        }
+        override fun compareTo(other: Level): Int {
+            return end.compareTo(other.end)
+        }
+        override fun toString(): String {
+            return ReflectionToStringBuilder.reflectionToString(
+                    this, ToStringStyle.SHORT_PREFIX_STYLE)
+        }
+    }
+    fun computeMaximumNumberOfEventsThatCanTakePlaceConcurrently(
+            events: List<Event>): Int {
+        val levels = PriorityQueue<Level>(Comparator { l1, l2 ->
+            l2.compareTo(l1)
+        }).apply { if (events.isNotEmpty()) add(Level()) }
+        events.sortedBy { it.start }.forEach { event ->
+            levels.peek()?.let { levelWithEarliestEnd ->
+                if (levelWithEarliestEnd.end > event.start) {
+                    levels.add(Level().apply { add(event) })
+                } else {
+                    levelWithEarliestEnd.add(event)
+                }
+            }
+        }
+//        println(PriorityQueue(levels))
+        return levels.size
+    }
+}
---
+++ java/src/test/kotlin/com/lagostout/elementsofprogramminginterviews/sorting/RenderACalendarSpek.kt
@@ -0,0 +1,34 @@
+package com.lagostout.elementsofprogramminginterviews.sorting
+
+import org.jetbrains.spek.api.Spek
+import org.jetbrains.spek.api.dsl.describe
+import org.jetbrains.spek.data_driven.data
+import com.lagostout.elementsofprogramminginterviews.sorting.RenderACalendar.Event
+import com.lagostout.elementsofprogramminginterviews.sorting.RenderACalendar.computeMaximumNumberOfEventsThatCanTakePlaceConcurrently
+import org.assertj.core.api.Assertions.assertThat
+import org.jetbrains.spek.api.dsl.it
+import org.jetbrains.spek.data_driven.on
+
+object RenderACalendarSpek : Spek({
+    val data = listOf(
+//            data(emptyList(), 0),
+//            data(listOf(Event(0,0)), 1),
+//            data(listOf(Event(0,1)), 1),
+//            data(listOf(Event(0,1), Event(1,2)), 1),
+//            data(listOf(Event(0,1), Event(2,3)), 1),
+//            data(listOf(Event(0,1), Event(0,2)), 2),
+//            data(listOf(Event(0,2), Event(1,3)), 2),
+//            data(listOf(Event(1,3), Event(0,2)), 2),
+//            data(listOf(Event(1,3), Event(0,2), Event(2,3)), 2),
+            data(listOf(Event(1,3), Event(0,2), Event(2,3)), 2),
+            null
+    ).filterNotNull().toTypedArray()
+    describe("computeMaximumNumberOfEventsThatCanTakePlaceConcurrently()") {
+        on("events %s", with = *data) { events, expected ->
+            it("returns $expected") {
+                assertThat(computeMaximumNumberOfEventsThatCanTakePlaceConcurrently(events))
+                        .isEqualTo(expected)
+            }
+        }
+    }
+})
