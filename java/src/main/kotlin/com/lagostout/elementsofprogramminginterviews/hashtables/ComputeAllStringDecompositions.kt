dd106206da5d30cbbd3878594d80b91a8cb311fb
lgtout <lagos.tout@gmail.com>
2017-11-01 10:07:22 -0700

    EP-K 13.12 Compute all string decompositions - WIP

--- java/src/main/kotlin/com/lagostout/elementsofprogramminginterviews/hashtables/ComputeAllStringDecompositions.kt
+++ java/src/main/kotlin/com/lagostout/elementsofprogramminginterviews/hashtables/ComputeAllStringDecompositions.kt
@@ -1,15 +1,63 @@
 package com.lagostout.elementsofprogramminginterviews.hashtables
 
 fun computeAllStringDecompositions(sentence: String, words: List<String>): List<Int> {
-    var wordToCount = mutableMapOf<String, Int>().apply {
+    if (words.isEmpty()) return emptyList()
+
+    val wordToCountMap = mutableMapOf<String, Int>().apply {
         words.forEach {
             put(it, getOrPut(it, { 0 }) + 1)
         }
     }
-    val lengthOfConcatenation = words.size *
-            if (words.isEmpty()) 0 else words.first().length
-//    val currentConcatenation
-    // How about moving forward one word length at a time,
-    // instead of entire length of concatenated words at a time?
-    return emptyList()
+    val missingWordsSet = mutableSetOf<String>().apply {
+        addAll(words)
+    }
+    val wordLength = words.first().length
+    val currentWordToCountMap = mutableMapOf<String, Int>()
+    val wordEndOffset = wordLength - 1
+    var currentIndex = 0
+    var previousFirstWord: String?
+    val startingIndicesOfConcatenations = mutableListOf<Int>()
+
+    while (true) {
+        val wordEndIndex = currentIndex + wordEndOffset
+        if (wordEndIndex > sentence.lastIndex) break
+
+        // Remove the first word-length substring in the current
+        // sentence substring.
+        previousFirstWord = (currentIndex - wordLength * words.size).let {
+            previousWordIndex ->
+            if (previousWordIndex >= 0)
+                sentence.substring(previousWordIndex,
+                        previousWordIndex + wordEndOffset)
+            else null
+        }
+        previousFirstWord?.let { word ->
+            currentWordToCountMap.computeIfPresent(
+                    word, { _, count -> count - 1 })?.let {
+                missingWordsSet.add(word)
+            }
+        }
+
+        // Examine the next word-length substring in the sentence
+        sentence.substring(currentIndex, wordEndIndex).let {
+            currentWord ->
+            if (currentWord in missingWordsSet) {
+                with(currentWordToCountMap) {
+                    put(currentWord, getOrPut(currentWord, { 0 }) + 1)?.let {
+                        if (it == wordToCountMap[currentWord])
+                            with (missingWordsSet) {
+                                remove(currentWord)
+                                if (isEmpty())
+                                    startingIndicesOfConcatenations.add(currentIndex)
+                            }
+                    }
+                }
+            }
+        }
+
+        // Next word in sentence
+        currentIndex += wordEndOffset
+    }
+
+    return startingIndicesOfConcatenations
 }
