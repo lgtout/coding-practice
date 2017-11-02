1ced0a0f5eb0ecac19899b2c13e352f21a87ae3f
lgtout <lagos.tout@gmail.com>
2017-11-02 11:05:07 -0700

    EP-K 17.2.4 Minimum characters to delete to make palindrome - WIP

--- java/src/main/kotlin/com/lagostout/elementsofprogramminginterviews/dynamicprogramming/ComputeMinimumCharsToDeleteToMakePalindrome.kt
+++ java/src/main/kotlin/com/lagostout/elementsofprogramminginterviews/dynamicprogramming/ComputeMinimumCharsToDeleteToMakePalindrome.kt
@@ -1,13 +1,74 @@
 package com.lagostout.elementsofprogramminginterviews.dynamicprogramming
 
+fun computeMinimumCharsToDeleteToMakePalindrome(string: String): Int {
+    calls = LinkedHashMap(0, 0.75F, false)
+    return computeMinimumCharsToDeleteToMakePalindrome(
+            string.toCharArray().toList(), 0, string.lastIndex).also {
+        println("size ${calls.size}")
+        println("recalls ${calls.filterValues { it > 1 }.size}")
+        println(calls.asIterable().toList())
+        println()
+    }
+}
+
+var calls = mutableMapOf<Triple<List<Char>, Int, Int>, Int>()
+
+// TODO How do we translate this top-down recursive solution to DP bottom-up?
+// Find right char matching left char by recursion, not looping.
 fun computeMinimumCharsToDeleteToMakePalindrome(
         chars: List<Char>, left: Int, right: Int): Int {
+    if (left >= right) return 0
+    calls.compute(Triple(chars, left, right), {
+        _, u -> (u ?: 0) + 1
+    })
+    val deletionCountWhenFirstCharIncluded: Int =
+            (if (chars[right] == chars[left]) Pair(1, 0) else Pair(0, 1))
+                    .let { (leftOffset, deletedCharCount) ->
+                        computeMinimumCharsToDeleteToMakePalindrome(
+                                chars, left + leftOffset, right - 1) + deletedCharCount
+                    }
+    val deletionCountWhenFirstCharNotIncluded =
+        computeMinimumCharsToDeleteToMakePalindrome(
+                chars, left + 1, right) + 1
+    return listOf(deletionCountWhenFirstCharIncluded,
+            deletionCountWhenFirstCharNotIncluded).min()!!
+}
+
+@Suppress("NAME_SHADOWING")
+// Find right char matching left char by looping, not recursion.
+fun computeMinimumCharsToDeleteToMakePalindrome2(
+        chars: List<Char>, left: Int, right: Int): Int {
+    calls.compute(Triple(chars, left, right), {
+        _, u -> (u ?: 0) + 1
+    })
+    if (left >= right) return 0
+    var right = right
+    val deletionCountWhenFirstCharIncluded = run {
+        var deletionCount = 0
+        while (true) {
+            if (chars[right] == chars[left]) break
+            deletionCount++
+            --right
+        }
+        deletionCount +
+                computeMinimumCharsToDeleteToMakePalindrome2(
+                        chars, left + 1, right - 1)
+    }
+    val deletionCountWhenFirstCharNotIncluded =
+            computeMinimumCharsToDeleteToMakePalindrome2(
+                    chars, left + 1, right) + 1
+    return listOf(deletionCountWhenFirstCharIncluded,
+            deletionCountWhenFirstCharNotIncluded).min()!!
+}
+
+fun computeMinimumCharsToDeleteToMakePalindrome1(
+        chars: List<Char>, left: Int, right: Int): Int {
     val deletionCountWhenFirstCharDeleted = 1 +
-            computeMinimumCharsToDeleteToMakePalindrome(chars, left + 1, right)
+            computeMinimumCharsToDeleteToMakePalindrome1(chars, left + 1, right)
     val deletionCountWhenComparingFirstAndLastChar = if (chars[left] != chars[right]) 0 else 2 +
-            computeMinimumCharsToDeleteToMakePalindrome(chars, left + 1, right - 1)
+            computeMinimumCharsToDeleteToMakePalindrome1(chars, left + 1, right - 1)
     val deletionCountWhenLastCharDeleted = 1 +
-            computeMinimumCharsToDeleteToMakePalindrome(chars, left, right - 1)
+            computeMinimumCharsToDeleteToMakePalindrome1(chars, left, right - 1)
     // TODO
     // Figure out a way to track what's being deleted, for debugging.
     // Cache results.
--- java/src/test/kotlin/com/lagostout/elementsofprogramminginterviews/dynamicprogramming/ComputeMinimumCharsToDeleteToMakePalindromeSpek.kt
+++ java/src/test/kotlin/com/lagostout/elementsofprogramminginterviews/dynamicprogramming/ComputeMinimumCharsToDeleteToMakePalindromeSpek.kt
@@ -1,7 +1,30 @@
 package com.lagostout.elementsofprogramminginterviews.dynamicprogramming
 
+import org.assertj.core.api.Assertions.assertThat
 import org.jetbrains.spek.api.Spek
+import org.jetbrains.spek.api.dsl.describe
+import org.jetbrains.spek.api.dsl.it
+import org.jetbrains.spek.data_driven.data
+import org.jetbrains.spek.data_driven.on
 
 object ComputeMinimumCharsToDeleteToMakePalindromeSpek : Spek({
-
+    val data = listOf(
+            data("", 0),
+            data("a", 0),
+            data("ab", 1),
+            data("aba", 0),
+            data("aaa", 0),
+            data("aaab", 1),
+            data("aaba", 1),
+            data("abaa", 1),
+            null
+    ).filterNotNull().toTypedArray()
+    describe("computeMinimumCharsToDeleteToMakePalindrome()") {
+        on("string %s", with = *data) { string, expected ->
+            it("returns $expected") {
+                assertThat(computeMinimumCharsToDeleteToMakePalindrome(string))
+                        .isEqualTo(expected)
+            }
+        }
+    }
 })
