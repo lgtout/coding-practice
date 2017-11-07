91f3aa090f60959f3bbbcb094213b286b7edd7bf
lgtout <lagos.tout@gmail.com>
2017-11-07 14:29:50 -0800

    EP-K 19.5 Clone a graph - WIP

--- java/src/main/kotlin/com/lagostout/elementsofprogramminginterviews/graphs/CloneAGraph.kt
+++ java/src/main/kotlin/com/lagostout/elementsofprogramminginterviews/graphs/CloneAGraph.kt
@@ -6,27 +6,33 @@ object CloneAGraph {
 
     data class Vertex(val label: Int, val adjacents: MutableList<Vertex> = mutableListOf())
 
-    fun cloneGraph(node: Vertex): Vertex {
-        val stack = LinkedList<Vertex>().apply {
-            push(node)
-        }
+    @Suppress("NAME_SHADOWING")
+    fun cloneGraph(vertices: List<Vertex>): List<Vertex> {
+        val vertices = vertices.toMutableSet()
+        val stack = LinkedList<Vertex>()
         val vertexToCloneMap = mutableMapOf<Vertex, Vertex>()
                 .withDefault { key -> Vertex(key.label) }
-        val explored = mutableSetOf<Vertex>()
-        while (stack.isNotEmpty()) {
-            val vertex = stack.pop()
-            if (vertex in explored) continue
-            explored.add(vertex)
-            vertexToCloneMap.getValue(vertex).let { clone ->
-                // We haven't visited this vertex before, so it's
-                // not possible to add duplicate adjacent vertices.
-                vertex.adjacents.forEach {
-                    clone.adjacents.add(
-                            vertexToCloneMap.getValue(it))
+        while (vertices.isNotEmpty()) {
+            val explored = mutableSetOf<Vertex>()
+            stack.push(vertices.first())
+            while (stack.isNotEmpty()) {
+                val vertex = stack.pop()
+                if (vertex in explored) continue
+                explored.add(vertex)
+                vertices.remove(vertex)
+                vertexToCloneMap.getValue(vertex).let { clone ->
+                    // We haven't visited this vertex before, so it's
+                    // not possible to add duplicate adjacent vertices.
+                    println(clone)
+                    vertex.adjacents.forEach {
+                        clone.adjacents.add(
+                                vertexToCloneMap.getValue(it))
+                    }
                 }
             }
         }
-        return vertexToCloneMap.getValue(node)
+        println(vertexToCloneMap)
+        return vertexToCloneMap.values.toList()
     }
 
 }
--- java/src/test/kotlin/com/lagostout/elementsofprogramminginterviews/graphs/CloneAGraphSpek.kt
+++ java/src/test/kotlin/com/lagostout/elementsofprogramminginterviews/graphs/CloneAGraphSpek.kt
@@ -1,25 +1,50 @@
 package com.lagostout.elementsofprogramminginterviews.graphs
 
+import com.lagostout.elementsofprogramminginterviews.graphs.CloneAGraph.cloneGraph
+import org.assertj.core.api.Assertions.assertThat
 import org.jetbrains.spek.api.Spek
 import org.jetbrains.spek.api.dsl.describe
+import org.jetbrains.spek.api.dsl.given
+import org.jetbrains.spek.api.dsl.it
 
 object CloneAGraphSpek : Spek({
     describe("cloneGraph()") {
         val data = listOf(
-                listOf(Pair(0, emptyList())),
-                listOf(Pair(0, listOf(1)), Pair(1, emptyList())),
+                listOf(Pair(0, emptyList<Int>())),
+//                listOf(Pair(0, listOf(1)), Pair(1, emptyList())),
                 null
         ).filterNotNull().map {
-            it.map { Pair(CloneAGraph.Vertex(it.first), it.second) }.also {
-                it.forEach { (vertex, indicesOfAdjacents) ->
-                    indicesOfAdjacents.map {}
-//                    vertex.adjacents.add(it[index])
+            it.map { Pair(CloneAGraph.Vertex(it.first), it.second) }.run {
+                map { (vertex, indicesOfAdjacents) ->
+                    indicesOfAdjacents.forEach {
+                        vertex.adjacents.add(get(it).first)
+                    }
+                    vertex
                 }
             }
         }
-        data.forEach {
-
+        data.forEachIndexed { index, vertices ->
+            given("vertices $vertices") {
+                it("#$index returns a clone of the graph") {
+                    val clone = cloneGraph(vertices)
+//                    assertThat(clone).containsExactlyInAnyOrder(
+//                            *vertices.toTypedArray())
+//                    vertices.filter { vertex ->
+//                        clone.any { it === vertex }
+//                    }.let {
+//                        assertThat(it).containsExactlyInAnyOrder(
+//                                *clone.toTypedArray())
+//                    }
+                    assertThat(clone).containsExactlyInAnyOrder(
+                            *vertices.toTypedArray())
+                    vertices.filter { vertex ->
+                        clone.any { it === vertex }
+                    }.let {
+                        assertThat(it).containsExactlyInAnyOrder(
+                                *clone.toTypedArray())
+                    }
+                }
+            }
         }
-//        given()
     }
 })
