64f9320f0e12902055aeacfbe4353adc4ce93288
lgtout <lagos.tout@gmail.com>
2017-11-10 11:43:53 -0800

    EP-K 13.12 Compute all string decompositions - done!

--- java/src/main/kotlin/com/lagostout/elementsofprogramminginterviews/hashtables/ComputeAllStringDecompositions.kt
+++ java/src/main/kotlin/com/lagostout/elementsofprogramminginterviews/hashtables/ComputeAllStringDecompositions.kt
@@ -1,74 +1,40 @@
 package com.lagostout.elementsofprogramminginterviews.hashtables
 
+import java.util.regex.Pattern
+
+/**
+ * Problem 13.12 page 232
+ */
 fun computeAllStringDecompositions(sentence: String, words: List<String>): List<Int> {
     if (words.isEmpty() || sentence.isEmpty()) return emptyList()
 
-    val wordToCountMap = mutableMapOf<String, Int>()
-    val substringWordToCountMap = mutableMapOf<String, Int>()
-    words.forEach {
-        with (wordToCountMap) {
-            compute(it, {_, count -> (count ?: 0) + 1})
+    fun createWordToCountMap(words: List<String>): Map<String, Int> {
+        val map = mutableMapOf<String, Int>()
+        words.forEach {
+            with (map) {
+                compute(it, {_, count -> (count ?: 0) + 1})
+            }
         }
-        with (substringWordToCountMap) {
-            put(it, 0)
-        }
+        return map
+
     }
-    val missingWordsSet = mutableSetOf<String>().apply {
-        addAll(words)
-    }
-    val wordLength = words.first().length
+    val wordToCountMap = createWordToCountMap(words)
+    var substringWordToCountMap: Map<String, Int>
     var currentIndex = 0
-    val startingIndicesOfConcatenations = mutableListOf<Int>()
-
+    val wordLength = words.first().length
+    val concatenations = mutableListOf<Int>()
+    val substringLength = wordLength * words.size
     while (true) {
-        val wordEndIndex = currentIndex + wordLength
-        if (wordEndIndex > sentence.length) break
-
-        // Remove the first word-length substring from the current
-        // sentence substring.
-        (currentIndex - wordLength * words.size).let {
-            substringFirstWordIndex ->
-            if (substringFirstWordIndex >= 0)
-                sentence.substring(substringFirstWordIndex,
-                        substringFirstWordIndex + wordLength)
-            else null
-        }?.let { previousFirstWord ->
-            substringWordToCountMap.computeIfPresent(
-                    previousFirstWord, { _, count -> count - 1 })?.let {
-                missingWordsSet.add(previousFirstWord)
-            }
-        }
-
-        println(wordToCountMap)
-        println(substringWordToCountMap)
-        println(missingWordsSet)
-        println()
-        // Examine the next word-length substring in the sentence
-        sentence.substring(currentIndex, wordEndIndex).let {
-            currentWord ->
-            println(currentWord)
-            if (currentWord in missingWordsSet) {
-                with(substringWordToCountMap) {
-                    println(currentWord)
-                    compute(currentWord, { _, count -> count?.let {it  + 1} })?.let {
-                        println(it)
-                        if (it == wordToCountMap[currentWord])
-                            with (missingWordsSet) {
-                                remove(currentWord)
-                                if (isEmpty()) {
-                                    startingIndicesOfConcatenations
-                                            .add(currentIndex -
-                                                    (wordLength * (words.size - 1)))
-                                }
-                            }
-                    }
-                }
-            }
-        }
-
-        // Next word in sentence
-        currentIndex += wordLength
+        val substringEndIndex = currentIndex + substringLength - 1
+        if (substringEndIndex > sentence.lastIndex) break
+        substringWordToCountMap = createWordToCountMap(
+                sentence.substring(currentIndex, substringEndIndex + 1)
+                        .split(Pattern.compile(
+                                "(?<=\\G[\\w ]{$wordLength})(?!$)")))
+        if (wordToCountMap == substringWordToCountMap)
+            concatenations.add(currentIndex)
+        ++currentIndex
     }
 
-    return startingIndicesOfConcatenations
+    return concatenations
 }
--- java/src/test/kotlin/com/lagostout/elementsofprogramminginterviews/hashtables/ComputeAllStringDecompositionsSpek.kt
+++ java/src/test/kotlin/com/lagostout/elementsofprogramminginterviews/hashtables/ComputeAllStringDecompositionsSpek.kt
@@ -8,20 +8,21 @@ import org.jetbrains.spek.data_driven.on
 
 object ComputeAllStringDecompositionsSpek : Spek({
     val data = listOf(
-//            data("", listOf(""), emptyList()),
-//            data("man", listOf("man"), listOf(0)),
-//            data("manman", listOf("man"), listOf(0,3)),
-//            data("mancan", listOf("man","can"), listOf(0)),
-//            data("mancan", listOf("can","man"), listOf(0)),
-//            data("aplcanman", listOf("man","can"), listOf(3)),
-//            data("aplcanman", listOf("man","can"), listOf(3)),
-//            data("canaplman", listOf("man","can"), listOf()),
-//            data("canxman", listOf("man","can"), listOf()),
-//            data("canmancan", listOf("man","can"), listOf(0,3)),
-//            data("aplcanmanaplcan", listOf("man","can"), listOf(3)),
-            // TODO
-            // Move forward one character at a time, not wordLength.
+            data("", listOf(""), emptyList()),
+            data("man", listOf("man"), listOf(0)),
+            data("manman", listOf("man"), listOf(0,3)),
+            data("mancan", listOf("man","can"), listOf(0)),
+            data("mancan", listOf("can","man"), listOf(0)),
+            data("aplcanman", listOf("man","can"), listOf(3)),
+            data("aplcanman", listOf("man","can"), listOf(3)),
+            data("canaplman", listOf("man","can"), listOf()),
+            data("canxman", listOf("man","can"), listOf()),
+            data("canmancan", listOf("man","can"), listOf(0,3)),
+            data("aplcanmanaplcan", listOf("man","can"), listOf(3)),
             data("amanaplanacanal", listOf("can","apl","ana"), listOf(4)),
+            data("amanacaplacanal", listOf("ca","na"), listOf(3,10)),
+            data("amanacaplacanal", listOf("xa","na"), listOf()),
+            data("amanacaplacanal", listOf("pl","na"), listOf()),
             null
     ).filterNotNull().toTypedArray()
     on("sentence: %s, words: %s", with = *data) { sentence, words, expected ->
