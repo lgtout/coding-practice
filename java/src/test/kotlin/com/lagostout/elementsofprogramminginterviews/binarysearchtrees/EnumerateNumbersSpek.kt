ce7c924dbfe6b1855fb1c9f2f300d6d14c1df036
lgtout <lagos.tout@gmail.com>
2017-11-23 12:07:00 -0800

    EP-K 15.7 Enumerate numbers - WIP

--- java/src/test/kotlin/com/lagostout/elementsofprogramminginterviews/binarysearchtrees/EnumerateNumbersSpek.kt
+++ java/src/test/kotlin/com/lagostout/elementsofprogramminginterviews/binarysearchtrees/EnumerateNumbersSpek.kt
@@ -1,2 +1,14 @@
 package com.lagostout.elementsofprogramminginterviews.binarysearchtrees
 
+import org.paukov.combinatorics3.Generator
+
+fun generateSample() {
+    Generator.permutation((0..15).toList())
+            .withRepetitions(2)
+            .map { (first, second) ->
+                Triple(first, second, first + second * Math.sqrt(2.0))
+            }.sortedBy { it.third }
+            .forEach {
+                println(it)
+            }
+}
