66ee534f903008c81bce72e5b52a422986e37280
lgtout <lagos.tout@gmail.com>
2017-11-23 16:59:47 -0800

    EP-K 17.3.1 Number of ways to traverse 2D array - done!

---
+++ java/src/main/kotlin/com/lagostout/elementsofprogramminginterviews/dynamicprogramming/NumberOfWaysToTraverse2DArray.kt
@@ -0,0 +1,26 @@
+package com.lagostout.elementsofprogramminginterviews.dynamicprogramming
+
+/**
+ * Problem 17.3 page 317
+ */
+fun numberOfWaysToTraverse2DArray(rowCount: Int, columnCount: Int): Int {
+    val array = mutableListOf<MutableList<Int>>().apply {
+        val row = mutableListOf<Int>().apply {
+            (0 until columnCount).forEach {
+                (if (it == 0) 1 else 0).let {
+                    add(it)
+                }
+            }
+        }
+        addAll((0 until rowCount).map { row.toMutableList() })
+    }.apply {
+        get(0).apply { (1 until columnCount).forEach { set(it, 1) } }
+    }
+    (1 until rowCount).forEach { rowIndex ->
+        (1 until columnCount).forEach { columnIndex ->
+            array[rowIndex][columnIndex] = array[rowIndex - 1][columnIndex] +
+                    array[rowIndex][columnIndex - 1]
+        }
+    }
+    return array.last().last()
+}
---
+++ java/src/test/kotlin/com/lagostout/elementsofprogramminginterviews/dynamicprogramming/NumberOfWaysToTraverse2DArraySpek.kt
@@ -0,0 +1,30 @@
+package com.lagostout.elementsofprogramminginterviews.dynamicprogramming
+
+import org.assertj.core.api.Assertions.assertThat
+import org.jetbrains.spek.api.Spek
+import org.jetbrains.spek.api.dsl.describe
+import org.jetbrains.spek.api.dsl.it
+import org.jetbrains.spek.data_driven.data
+import org.jetbrains.spek.data_driven.on
+
+object NumberOfWaysToTraverse2DArraySpek : Spek({
+    describe("numberOfWaysToTraverse2DArray()") {
+        val data = listOfNotNull(
+                data(1,1,1),
+                data(1,2,1),
+                data(2,1,1),
+                data(2,2,2),
+                data(2,3,3),
+                data(4,4,20),
+                data(5,5,70),
+                null
+        ).toTypedArray()
+        on("array: %sx%s", with = *data) {
+            rowCount, columnCount, expected ->
+            it("returns $expected") {
+                assertThat(numberOfWaysToTraverse2DArray(rowCount, columnCount))
+                        .isEqualTo(expected)
+            }
+        }
+    }
+})
