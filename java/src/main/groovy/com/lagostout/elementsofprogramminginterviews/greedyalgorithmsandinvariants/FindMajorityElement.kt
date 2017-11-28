4094b744f6077e9b7cce3fe6c0c425d2f84f4ae3
lgtout <lagos.tout@gmail.com>
2017-11-28 11:08:29 -0800

    EP-K 18.5 Find majority element - WIP

---
+++ java/src/main/groovy/com/lagostout/elementsofprogramminginterviews/greedyalgorithmsandinvariants/FindMajorityElement.kt
@@ -0,0 +1,8 @@
+package com.lagostout.elementsofprogramminginterviews.greedyalgorithmsandinvariants
+
+/**
+ * Problem 18.5 page 347
+ */
+fun findMajorityElement(strings: List<String>): String {
+    return ""
+}
