5c207858194dda38ad27a9463133ac0cdd5df1e5
lgtout <lagos.tout@gmail.com>
2017-11-30 13:16:52 -0800

    EP-K 6.4.2 Advancing through array - minimum steps

--- java/src/main/kotlin/com/lagostout/elementsofprogramminginterviews/arrays/MinimumStepsToAdvanceThroughArray.kt
+++ java/src/main/kotlin/com/lagostout/elementsofprogramminginterviews/arrays/MinimumStepsToAdvanceThroughArray.kt
@@ -1,2 +1,21 @@
 package com.lagostout.elementsofprogramminginterviews.arrays
 
+/**
+ * Problem 6.4.2 page 69
+ */
+// Assumptions:
+// -- The last location is already reachable.  We already
+// solved the problem of determining if it's reachable in 6.4.1, so
+// no benefit to repeating that here.
+// -- The supplied list is non-empty.
+fun minimumStepsToAdvanceThroughArray(maxSteps: List<Int>): Int {
+    var destinationIndex = maxSteps.lastIndex
+    var stepCount: Int? = null
+    var candidateStepIndex: Int
+    (maxSteps.lastIndex - 1 downTo 0).forEach {
+       if (it + maxSteps[it] >= destinationIndex) {
+           destinationIndex = it
+       }
+    }
+    return 0
+}
