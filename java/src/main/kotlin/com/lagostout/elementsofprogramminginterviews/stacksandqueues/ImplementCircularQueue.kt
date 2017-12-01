59cb4b801e87475407c37112661077c71452d899
lgtout <lagos.tout@gmail.com>
2017-12-01 13:25:37 -0800

    EP-K 9.8 Implement circular queue - WIP

--- java/src/main/kotlin/com/lagostout/elementsofprogramminginterviews/stacksandqueues/ImplementCircularQueue.kt
+++ java/src/main/kotlin/com/lagostout/elementsofprogramminginterviews/stacksandqueues/ImplementCircularQueue.kt
@@ -1,17 +1,39 @@
 package com.lagostout.elementsofprogramminginterviews.stacksandqueues
 
-class CircularQueue<T>(initialSize: Int) {
-    @Suppress("UNCHECKED_CAST")
-    var array = arrayOfNulls<Any>(initialSize) as Array<T?>
-    var startIndex = 0
-    var endIndex = 0
-    fun enqueue(entry: T) {
+import java.util.*
 
+@Suppress("UNCHECKED_CAST")
+class CircularQueue<T>(initialSize: Int) {
+
+    private var array = arrayOfNulls<Any>(initialSize) as Array<T?>
+    private var startIndex = 0
+    private var endIndex = 0
+    private var _size = 0
+
+    fun enqueue(entry: T) {
+        if (_size == array.size) {
+            Collections.rotate(array.asList(), -startIndex)
+            array = (arrayOfNulls<Any>(_size * 2) as Array<T?>).apply {
+                (0 until size).forEach {
+                    set(it, array[it])
+                }
+            }
+        }
+        ++_size
+        endIndex = (startIndex + _size) % _size
+        array[endIndex] = entry
     }
+
     fun dequeue(): T {
-        throw IllegalStateException()
+        val entry = array[startIndex]
+        array[startIndex] = null
+        startIndex = (startIndex + 1) % _size
+        return entry!!
     }
-    fun size(): Int {
-        return 0
-    }
+
+    val size: Int
+        get() {
+            return _size
+        }
+
 }
--- java/src/test/kotlin/com/lagostout/elementsofprogramminginterviews/stacksandqueues/ImplementCircularQueueSpek.kt
+++ java/src/test/kotlin/com/lagostout/elementsofprogramminginterviews/stacksandqueues/ImplementCircularQueueSpek.kt
@@ -1,2 +1,45 @@
 package com.lagostout.elementsofprogramminginterviews.stacksandqueues
 
+import org.assertj.core.api.Assertions.assertThat
+import org.jetbrains.spek.api.Spek
+import org.jetbrains.spek.api.dsl.describe
+import org.jetbrains.spek.api.dsl.it
+import org.jetbrains.spek.api.dsl.on
+import org.jetbrains.spek.api.lifecycle.CachingMode
+
+object ImplementCircularQueueSpek : Spek({
+    describe("CircularQueue") {
+        val queue by memoized (CachingMode.TEST){ CircularQueue<Int>(5) }
+        describe("empty queue") {
+            on("get size") {
+                val expected = 0
+                it("returns $expected") {
+                    assertThat(queue.size)
+                            .isEqualTo(expected)
+                }
+            }
+            on("enqueue an entry") {
+                queue.enqueue(1)
+                val expected = 1
+                it("has size $expected") {
+                    assertThat(queue.size)
+                            .isEqualTo(expected)
+                }
+            }
+        }
+        describe("queue containing 1 entry") {
+            beforeGroup {
+                queue.enqueue(1)
+            }
+            on("enqueue another entry") {
+                queue.enqueue(1)
+                val expected = 2
+                it("has size $expected") {
+                    assertThat(queue.size)
+                            .isEqualTo(expected)
+                }
+            }
+        }
+    }
+    // TODO Continue
+})
