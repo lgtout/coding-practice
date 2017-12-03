2c0da041972fd9eee9520c1d4b62cf85a36573e3
lgtout <lagos.tout@gmail.com>
2017-12-02 22:16:05 -0800

    EP-K 15.7 Enumerate numbers - WIP

--- java/src/main/kotlin/com/lagostout/elementsofprogramminginterviews/binarysearchtrees/EnumerateNumbers.kt
+++ java/src/main/kotlin/com/lagostout/elementsofprogramminginterviews/binarysearchtrees/EnumerateNumbers.kt
@@ -1,2 +1,28 @@
 package com.lagostout.elementsofprogramminginterviews.binarysearchtrees
 
+/**
+ * Problem 15.7 page 271
+ */
+fun enumerateNumbers(count: Int): List<Double> {
+    data class Expression(val a: Int, val b: Int) {
+        val value: Double
+            get() {
+                return a + b * Math.sqrt(2.0)
+            }
+    }
+    val numbers = mutableListOf<Expression>(). apply {
+        add(Expression(0, 0))
+    }
+    var currentCoeffA = 1
+    var currentCoeffB = 1
+    var maxCoeffA = 1
+    var nextZeroCoeffAExpression = Expression(0, 1)
+    var nextZeroCoeffBExpression = Expression(1, 0)
+    while (numbers.size < count) {
+        (maxCoeffA downTo 1).forEach {
+
+        }
+        ++maxCoeffA
+    }
+    return emptyList()
+}
