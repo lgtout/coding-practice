d1998ecfe011b7f8142b2c80ec6a994b5978630c
lgtout <lagos.tout@gmail.com>
2017-12-06 14:17:59 -0800

    EP-K 5.4.1 Find closest integer with same weight - WIP

--- java/src/main/kotlin/com/lagostout/elementsofprogramminginterviews/primitivetypes/ClosestIntegerWithSameWeight.kt
+++ java/src/main/kotlin/com/lagostout/elementsofprogramminginterviews/primitivetypes/ClosestIntegerWithSameWeight.kt
@@ -4,5 +4,16 @@ package com.lagostout.elementsofprogramminginterviews.primitivetypes
  * Problem 5.4.1 page 50
  */
 fun findClosestIntegerWithSameWeight(number: Int): Int {
+    // Find position of rightmost 0
+    val rightMostZeroBitMask = (number.inv() - 1).let {
+        it.inv().and(it)
+    }
+    if (rightMostZeroBitMask.and(1) > 0) {
+        // Rightmost 0 bit is rightmost bit.
+        // Find position of rightmost 1 bit.
+        
+    } else {
+
+    }
     return 0
 }
--- java/src/test/kotlin/com/lagostout/elementsofprogramminginterviews/primitivetypes/ClosestIntegerWithSameWeightSpek.kt
+++ java/src/test/kotlin/com/lagostout/elementsofprogramminginterviews/primitivetypes/ClosestIntegerWithSameWeightSpek.kt
@@ -1,24 +1,2 @@
 package com.lagostout.elementsofprogramminginterviews.primitivetypes
 
-import org.paukov.combinatorics3.Generator
-import java.math.BigInteger
-
-fun findClosestNeighborsWithSameBitCount(v: String = "111100"): List<Pair<String, BigInteger>> {
-    val bv = BigInteger(v, 2)
-    return Generator.permutation(*(v.toCharArray().toTypedArray())).simple()
-            .map {
-                val s = it.joinToString(separator = "")
-                val n = BigInteger(s, 2)
-                Pair(s, n)
-            }.sortedBy{ it.second }.withIndex().let { all ->
-        all.find {
-            it.value.second == bv
-        }!!.let {
-            mutableListOf<Pair<String, BigInteger>>().apply {
-                if (it.index > 0) add(all.elementAt(it.index - 1).value)
-                if (it.index < all.last().index)
-                    add(all.elementAt(it.index + 1).value)
-            }
-        }
-    }
-}
