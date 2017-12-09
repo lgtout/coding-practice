5abc59349adee4c81c85aa871a578155a31dc231
lgtout <lagos.tout@gmail.com>
2017-12-09 14:27:21 -0800

    EP-K 16.1.8 Towers of Hanoi with colored disks - done!

--- java/src/main/kotlin/com/lagostout/elementsofprogramminginterviews/recursion/towersofhanoi/TowersOfHanoiWithColoredDisks.kt
+++ java/src/main/kotlin/com/lagostout/elementsofprogramminginterviews/recursion/towersofhanoi/TowersOfHanoiWithColoredDisks.kt
@@ -3,6 +3,7 @@ package com.lagostout.elementsofprogramminginterviews.recursion.towersofhanoi
 import com.lagostout.elementsofprogramminginterviews.recursion.towersofhanoi.TowersOfHanoi.Peg
 import com.lagostout.elementsofprogramminginterviews.recursion.towersofhanoi.TowersOfHanoi.Pegs
 import com.lagostout.elementsofprogramminginterviews.recursion.towersofhanoi.TowersOfHanoi.RingMove
+import com.lagostout.elementsofprogramminginterviews.recursion.towersofhanoi.TowersOfHanoiWithColoredDisks.Color.BLACK
 import com.lagostout.elementsofprogramminginterviews.recursion.towersofhanoi.TowersOfHanoiWithColoredDisks.Color.WHITE
 
 /**
@@ -22,8 +23,10 @@ object TowersOfHanoiWithColoredDisks {
     // This makes for a much simpler approach than the previous Tower of Hanoi
     // problems.  We don't need a stack and conditionals are simpler.
 
-    // TODO
-    // See posted question on forum
+    // This problem is not challenging enough to make it worth writing tests
+    // to verify.
+
+    // Forum post http://bit.ly/2BnIWz1
 
     fun computeMinimumNumberOfOperationsWithColoredDisksAreColored(
             pegs: Pegs<ColoredRing>, fromPeg: Peg<ColoredRing>, toPeg: Peg<ColoredRing>,
@@ -32,13 +35,28 @@ object TowersOfHanoiWithColoredDisks {
         if (fromPeg.isEmpty) return
         val extraPeg = pegs.extra(fromPeg, toPeg)
 
-        while (true) {
-            val ring = fromPeg.pop()
-            if (ring.color == WHITE) break
-            extraPeg.push(ring)
-            operations.add(RingMove(fromPeg, extraPeg, ring))
+        // According to Tsung-Hsien Lee, OK to peek at a peg's bottom disk
+        // without removing disks from the peg.
+
+        val bottomDisk = fromPeg.bottom!!
+        val topDisk = fromPeg.peek()!!
+        if (bottomDisk.color == WHITE && topDisk.color == BLACK) {
+            // Move all black disks aside to get to the white ones
+            var done = false
+            while (!done) {
+                fromPeg.peek()?.let {
+                    if (it.color == WHITE) {
+                        done = true
+                    } else {
+                        fromPeg.pop()
+                        extraPeg.push(it)
+                        operations.add(RingMove(fromPeg, extraPeg, it))
+                    }
+                } ?: break
+            }
         }
 
+        // Move all white disks to destination peg
         while (true) {
             if (fromPeg.isEmpty) break
             fromPeg.pop().let {
@@ -47,6 +65,7 @@ object TowersOfHanoiWithColoredDisks {
             }
         }
 
+        // Move black disks that were moved aside to destination peg
         while (true) {
             if (extraPeg.isEmpty) break
             extraPeg.pop().let {
