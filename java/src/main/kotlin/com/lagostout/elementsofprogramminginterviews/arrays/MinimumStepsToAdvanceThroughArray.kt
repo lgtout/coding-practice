688054f476eb0588c536b0e8e2966509adbd5f34
lgtout <lagos.tout@gmail.com>
2017-12-12 15:12:11 -0800

    EP-K 6.4.2 Advancing through array - minimum steps - WIP

--- java/src/main/kotlin/com/lagostout/elementsofprogramminginterviews/arrays/MinimumStepsToAdvanceThroughArray.kt
+++ java/src/main/kotlin/com/lagostout/elementsofprogramminginterviews/arrays/MinimumStepsToAdvanceThroughArray.kt
@@ -3,19 +3,35 @@ package com.lagostout.elementsofprogramminginterviews.arrays
 /**
  * Problem 6.4.2 page 69
  */
-// Assumptions:
+// Assumptions and implications:
+// -- The supplied array is non-empty.
 // -- The last location is already reachable.  We already
-// solved the problem of determining if it's reachable in 6.4.1, so
-// no benefit to repeating that here.
-// -- The supplied list is non-empty.
-fun minimumStepsToAdvanceThroughArray(maxSteps: List<Int>): Int {
-    var destinationIndex = maxSteps.lastIndex
-    var stepCount: Int? = null
-    var candidateStepIndex: Int
-    (maxSteps.lastIndex - 1 downTo 0).forEach {
-       if (it + maxSteps[it] >= destinationIndex) {
-           destinationIndex = it
-       }
+// solved the problem of determining if it's reachable in
+// 6.4.1, so no need to repeat that here.
+// -- There is a sequence of positions such that, at each
+// position, the subsequent positions in the sequence are
+// reachable.
+// -- The first position in the array has a value.
+// Otherwise it would be impossible to proceed.
+fun minimumStepsToAdvanceThroughArray(moves: List<Int>): Int {
+    var steps = 0
+    var currentIndex = 0
+    var nextMoveIndex: Int
+    while (currentIndex < moves.lastIndex) {
+        var movesAvailable = moves[currentIndex]
+        var maxDistance = Pair(currentIndex, movesAvailable)
+        nextMoveIndex = currentIndex
+        while (movesAvailable > 0 &&
+                nextMoveIndex < moves.lastIndex - 1) {
+            movesAvailable--
+            nextMoveIndex++
+            val movesPossible = movesAvailable + moves[nextMoveIndex]
+            maxDistance = listOf(
+                    maxDistance, Pair(nextMoveIndex, movesPossible))
+                    .maxBy { it.first + it.second }!! // ???
+        }
+        currentIndex += maxDistance.first // ???
+        steps++
     }
-    return 0
+    return steps
 }
--- java/src/test/kotlin/com/lagostout/elementsofprogramminginterviews/arrays/MinimumStepsToAdvanceThroughArraySpek.kt
+++ java/src/test/kotlin/com/lagostout/elementsofprogramminginterviews/arrays/MinimumStepsToAdvanceThroughArraySpek.kt
@@ -1,2 +1,31 @@
 package com.lagostout.elementsofprogramminginterviews.arrays
 
+import org.assertj.core.api.Assertions.assertThat
+import org.jetbrains.spek.api.Spek
+import org.jetbrains.spek.api.dsl.describe
+import org.jetbrains.spek.api.dsl.it
+import org.jetbrains.spek.data_driven.data
+import org.jetbrains.spek.data_driven.on
+
+object MinimumStepsToAdvanceThroughArraySpek : Spek({
+    describe("minimumStepsToAdvanceThroughArray()") {
+        val data = listOfNotNull(
+//                data(listOf(0), 0),
+//                data(listOf(1), 0),
+                data(listOf(1,0), 1),
+//                data(listOf(2,0), 1),
+//                data(listOf(2,0,0), 1),
+//                data(listOf(2,1,0), 1),
+//                data(listOf(1,2,0), 2),
+//                data(listOf(1,3,0), 2),
+//                data(listOf(2,0,3,1,0,1,0), 3),
+                null
+        ).toTypedArray()
+        on("array: %s", with = *data) { array, expected ->
+            it("returns $expected") {
+                assertThat(minimumStepsToAdvanceThroughArray(array))
+                        .isEqualTo(expected)
+            }
+        }
+    }
+})
