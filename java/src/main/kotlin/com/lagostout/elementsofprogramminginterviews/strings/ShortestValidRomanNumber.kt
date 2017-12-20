6f8534436170f7d155f01734e212bdc5734baf18
lgtout <lagos.tout@gmail.com>
2017-12-20 14:11:01 -0800

    EP-K 7.9.3 Shortest valid roman number - WIP

--- java/src/main/kotlin/com/lagostout/elementsofprogramminginterviews/strings/ShortestValidRomanNumber.kt
+++ java/src/main/kotlin/com/lagostout/elementsofprogramminginterviews/strings/ShortestValidRomanNumber.kt
@@ -1,42 +1,83 @@
 package com.lagostout.elementsofprogramminginterviews.strings
+import java.util.*
 
-fun computeShortestValidRomanNumber(number: Int): String {
-    // Idea: Use TreeSet to cut searching this down to O(log n).
-    // Or apply binary search.
-    val romanToDecimalMap = mapOf(
-            'I' to 1, 'V' to 5, 'X' to 10,
-            'L' to 50, 'C' to 100, 'D' to 500,
-            'M' to 1000)
-    val symbolOrderExceptionMap = mapOf(
-            'I' to setOf('V', 'X'),
-            'X' to setOf('L', 'C'),
-            'C' to setOf('D','M')
-    )
-//    val exceptionSymbols =
-    val symbols = romanToDecimalMap.keys
-    val symbolToOrderMap = symbols.withIndex().map {
-        (index, value) -> value to index
-    }.toMap()
-    var remainder = number
-    val romanToDecimalList = romanToDecimalMap.toList()
-    val stringBuilder = StringBuilder()
-    while (true) {
-        val romanToDecimalIndex = romanToDecimalList
-                .binarySearch { it.second.compareTo(remainder) }
-        val romanToDecimal = romanToDecimalList[romanToDecimalIndex]
-        if (romanToDecimal.second == remainder) {
-            stringBuilder.append(romanToDecimal.first)
-            break
-        } else {
-            val lowerSymbol = if (romanToDecimalList[romanToDecimalIndex].second == remainder)
-                romanToDecimalList[romanToDecimalIndex]
-            else romanToDecimalList[romanToDecimalIndex - 1]
-//            val higherSymbol = if (romanToDecimalList[romanToDecimalIndex].second == remainder) ( == lastIndex) null else get(it + 1)
-//            if (remainder == lowerSymbol) {
-//
-//            }
-//            Pair(lowerSymbol, higherSymbol)
-        }
+object ShortestValidRomanNumber {
+    @Suppress("PropertyName", "unused")
+    object RomanDigits {
+        class RomanDigit (val value: Int, val symbol: Char)
+        val I = RomanDigit(1,'I')
+        val V = RomanDigit(5,'V')
+        val X = RomanDigit(10,'X')
+        val L = RomanDigit(50,'L')
+        val C = RomanDigit(100,'C')
+        val D = RomanDigit(500,'D')
+        val M = RomanDigit(1000,'M')
+        val values = listOf(I,V,X,L,C,D,M)
     }
-    return ""
+    fun computeShortestValidRomanNumber(number: Int): String {
+        // Idea: Use TreeSet to cut searching this down to O(log n).
+        // Or apply binary search.
+        // Is there a solution that used O(1) space?  Or does using
+        // stack not increase space used beyond O(1), since there
+        // cannot be more entries in the stack than there are roman
+        // digits available.
+        val exceptionMap = with (RomanDigits) {
+            mapOf(V to I, X to I, L to X,
+                    C to X, D to C, M to C)
+        }
+        val reversedRomanDigits = RomanDigits.values.toList().reversed()
+        val stringBuilder = StringBuilder()
+        var romanDigitsIndex = 0
+        val stack = LinkedList<Int>().apply {
+            push(number)
+        }
+        val romanDigitsTree = TreeSet<RomanDigits.RomanDigit>(
+                { o1, o2 -> o1.value.compareTo(o2.value) })
+        var remainder: Int
+        while (true) {
+            if (stack.isEmpty()) break
+            remainder = stack.pop()
+            // Find appropriate position in reversedRomanDigits for current
+            // remainder either by searching (tree, binary search) or iterating.
+            while (true) {
+                val lowerRomanDigit = reversedRomanDigits[romanDigitsIndex]
+                val higherRomanDigit = if (romanDigitsIndex == 0) null else
+                    reversedRomanDigits[romanDigitsIndex + 1]
+                if (remainder > lowerRomanDigit.value) {
+
+                }
+                break
+            }
+//            romanDigitsIndex <= reversedRomanDigits.lastIndex
+            val romanDigit = reversedRomanDigits[romanDigitsIndex]
+            if (number == romanDigit.value) {
+                stringBuilder.append(romanDigit.symbol)
+                stack.push(remainder - romanDigit.value)
+            } else if (number > romanDigit.value) {
+                if (romanDigitsIndex == 0) {
+                    val romanDigitCount = remainder / romanDigit.value
+                    (1..romanDigitCount).forEach {
+                        stringBuilder.append(romanDigit.symbol)
+                    }
+                    stack.push(remainder % romanDigit.value)
+                } else {
+                    val higherRomanDigit = reversedRomanDigits[romanDigitsIndex - 1]
+                    if (higherRomanDigit in exceptionMap.keys) {
+                        val halfwayBetweenAdjacentRomanDigits =
+                                ((higherRomanDigit.value - romanDigit.value) / 2) +
+                                        romanDigit.value
+                        if (remainder > halfwayBetweenAdjacentRomanDigits) {
+                            val difference = higherRomanDigit.value - remainder
+
+                        } else {
+
+                        }
+                    } else {
+
+                    }
+                }
+            }
+        }
+        return ""
+    }
 }
