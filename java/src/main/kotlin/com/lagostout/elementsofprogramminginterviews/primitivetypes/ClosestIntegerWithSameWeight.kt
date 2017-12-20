d817c31392c528eaf35f9a41d8d05ec988bda2cc
lgtout <lagos.tout@gmail.com>
2017-12-20 11:11:44 -0800

    EP-K 5.4.1 Find closest integer with same weight - WIP

--- java/src/main/kotlin/com/lagostout/elementsofprogramminginterviews/primitivetypes/ClosestIntegerWithSameWeight.kt
+++ java/src/main/kotlin/com/lagostout/elementsofprogramminginterviews/primitivetypes/ClosestIntegerWithSameWeight.kt
@@ -11,30 +11,34 @@ fun findClosestIntegerWithSameWeight(number: Int): Int {
     fun findRightmostOneBit(number: Int): Int {
         return (number - 1).inv() and number
     }
+    if (number == 0) return 0
     // Find rightmost 0 bit
     val rightmostZeroBitMask = (number.inv() - 1).let {
         it.inv() and it
     }
     return when {
         rightmostZeroBitMask == 1 ->
-            // Rightmost 0 bit is the leftmost bit.
+            // Rightmost 0 bit is the rightmost bit.
+            // 0010000
             // Find rightmost 1 bit.
             findRightmostOneBit(number).let {
                 // Shift rightmost 1 bit right by 1 bit.
                 shiftRightmostOneBitRightByOneBit(number, it)
             }
         rightmostZeroBitMask > number ->
-            // Rightmost 0 bit is left of the rightmost 1 bit.
+            // Rightmost 0 bit is left of the leftmost 1 bit.
+            // 0011111
             // Shift the leftmost 1 bit left by 1 bit.
             (number or rightmostZeroBitMask) xor (rightmostZeroBitMask ushr 1)
         else ->
-            // Rightmost 0 bit is right of the leftmost 1 bit and
-            // left of the rightmost 1 bit.
-            // Shift the rightmost 1 bit that's to the left of the
-            // rightmost 0 bit right by 1 bit.
+            // Rightmost 0 bit is between 1 bits.
+            // 00110011
+            // 00110111
+            // Shift the 1 bit that's to the left of the rightmost
+            // 0 bit right by 1 bit.
             // Clear all 1 bits to the right of the rightmost 0 bit.
             ((rightmostZeroBitMask - 1) xor number).let {
-                // Find the rightmost 1 bit.
+                // Find the 1 bit to the left of the 0 bit.
                 // This is the position of the 1 bit that we need
                 // to move right by 1 bit.
                 findRightmostOneBit(it).let {
--- java/src/test/kotlin/com/lagostout/elementsofprogramminginterviews/primitivetypes/ClosestIntegerWithSameWeightSpek.kt
+++ java/src/test/kotlin/com/lagostout/elementsofprogramminginterviews/primitivetypes/ClosestIntegerWithSameWeightSpek.kt
@@ -1,28 +1,38 @@
 package com.lagostout.elementsofprogramminginterviews.primitivetypes
 
+import com.lagostout.common.toBinaryString
+import org.assertj.core.api.Assertions.assertThat
 import org.jetbrains.spek.api.Spek
 import org.jetbrains.spek.api.dsl.describe
+import org.jetbrains.spek.api.dsl.given
+import org.jetbrains.spek.api.dsl.it
+import org.jetbrains.spek.data_driven.data
 import org.paukov.combinatorics3.Generator
 import java.math.BigInteger
 
 object ClosestIntegerWithSameWeightSpek : Spek({
     describe("findClosestIntegerWithSameWeight()") {
         fun findClosestIntegerWithBruteForce(number: Int):
-                Pair<String, BigInteger> {
+                Pair<String, Int> {
             val bits = number.toBigInteger()
-            return Generator.permutation(bits.toString(2).toCharArray())
+            val bitsChars = listOf('0') + bits.toString(2).toList()
+//            println(bitsChars)
+            return Generator.permutation(bitsChars)
                     .simple()
                     .map {
                         val s = it.joinToString(separator = "")
                         val n = BigInteger(s, 2)
+//                        println(s)
                         Pair(s, n)
                     }
                     .sortedBy { it.second }
                     .withIndex()
                     .let { all ->
+//                        println(all.toList())
                         all.find {
                             it.value.second == bits
                         }!!.let { (numberIndex, _) ->
+//                            println(numberIndex)
                             mutableListOf<Pair<String, BigInteger>>()
                                     .apply {
                                         if (numberIndex > 0)
@@ -32,6 +42,7 @@ object ClosestIntegerWithSameWeightSpek : Spek({
                                             add(all.elementAt(numberIndex + 1).value)
                                     }
                                     .map {
+                                        println(it)
                                         Pair(it, it.second - all.elementAt(numberIndex).value.second)
                                     }
                                     .filterNot { it.second == 0.toBigInteger() }
@@ -39,7 +50,20 @@ object ClosestIntegerWithSameWeightSpek : Spek({
                                     .first().first
                         }
                     }
-
+                    .run {
+                        Pair(first, second.toInt())
+                    }
+        }
+        val data = (1..1).map {
+            data(it, findClosestIntegerWithBruteForce(it))
+        }.toTypedArray()
+        data.forEach { (number, expected) ->
+            given("number: ${number.toBinaryString()}") {
+                it("returns ${expected.first}") {
+                    assertThat(findClosestIntegerWithSameWeight(number))
+                            .isEqualTo(expected.second)
+                }
+            }
         }
     }
 })
