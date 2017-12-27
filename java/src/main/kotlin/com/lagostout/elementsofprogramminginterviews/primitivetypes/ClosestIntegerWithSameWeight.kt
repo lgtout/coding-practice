022d1d48549e9c28a03f2cd232cde47ceb377ae8
lgtout <lagos.tout@gmail.com>
2017-12-26 20:26:17 -0800

    EP-K 5.4.1 Find closest integer with same weight - done!

--- java/src/main/kotlin/com/lagostout/elementsofprogramminginterviews/primitivetypes/ClosestIntegerWithSameWeight.kt
+++ java/src/main/kotlin/com/lagostout/elementsofprogramminginterviews/primitivetypes/ClosestIntegerWithSameWeight.kt
@@ -4,22 +4,28 @@ package com.lagostout.elementsofprogramminginterviews.primitivetypes
  * Problem 5.4.1 page 50
  */
 fun findClosestIntegerWithSameWeight(number: Int): Int {
+
     fun shiftRightmostOneBitRightByOneBit(
             number: Int, mask: Int): Int {
         return (number xor mask) or (mask ushr 1)
     }
+
     fun findRightmostOneBit(number: Int): Int {
         return (number - 1).inv() and number
     }
+
     if (number == 0) return 0
+
     // Find rightmost 0 bit
-    val rightmostZeroBitMask = (number.inv() - 1).let {
-        it.inv() and it
+    val rightmostZeroBitMask = (number.inv()).let { inverse ->
+        (inverse - 1).inv() and inverse
     }
+    // TODO Cleanup
     return when {
         rightmostZeroBitMask == 1 ->
             // Rightmost 0 bit is the rightmost bit.
             // 0010000
+            //       ^
             // Find rightmost 1 bit.
             findRightmostOneBit(number).let {
                 // Shift rightmost 1 bit right by 1 bit.
@@ -28,23 +34,44 @@ fun findClosestIntegerWithSameWeight(number: Int): Int {
         rightmostZeroBitMask > number ->
             // Rightmost 0 bit is left of the leftmost 1 bit.
             // 0011111
+            //  ^
             // Shift the leftmost 1 bit left by 1 bit.
             (number or rightmostZeroBitMask) xor (rightmostZeroBitMask ushr 1)
         else ->
             // Rightmost 0 bit is between 1 bits.
             // 00110011
+            //      ^
             // 00110111
-            // Shift the 1 bit that's to the left of the rightmost
+            //     ^
+            // Shift the first 1 bit that's to the left of the rightmost
             // 0 bit right by 1 bit.
-            // Clear all 1 bits to the right of the rightmost 0 bit.
-            ((rightmostZeroBitMask - 1) xor number).let {
+            (rightmostZeroBitMask - 1).let { rightOneBits ->
+                // Clear all 1 bits to the right of the rightmost 0 bit.
+                var numberWithoutRightOneBits = number xor rightOneBits
                 // Find the 1 bit to the left of the 0 bit.
                 // This is the position of the 1 bit that we need
                 // to move right by 1 bit.
-                findRightmostOneBit(it).let {
-                    // Shift that 1 bit right by 1 bit.
-                    shiftRightmostOneBitRightByOneBit(number, it)
+                var rightmostOneBitOfNumberWithoutRightOneBits =
+                        findRightmostOneBit(numberWithoutRightOneBits)
+
+                // Shift that 1 bit right by 1 bit.
+                numberWithoutRightOneBits = shiftRightmostOneBitRightByOneBit(
+                        numberWithoutRightOneBits,
+                        rightmostOneBitOfNumberWithoutRightOneBits)
+
+                // Shift 1 bits that were cleared left, so they are
+                // adjacent to the 1 bit we moved right.
+                rightmostOneBitOfNumberWithoutRightOneBits =
+                        rightmostOneBitOfNumberWithoutRightOneBits ushr 1
+                var oneBits = rightOneBits
+                while (true) {
+                    val nextOneBits = oneBits shl 1
+                    if (nextOneBits < rightmostOneBitOfNumberWithoutRightOneBits)
+                        oneBits = nextOneBits else break
                 }
+
+                // Reapply the removed right 1 bits to the number.
+                numberWithoutRightOneBits or oneBits
             }
     }
 }
--- java/src/test/kotlin/com/lagostout/elementsofprogramminginterviews/primitivetypes/ClosestIntegerWithSameWeightSpek.kt
+++ java/src/test/kotlin/com/lagostout/elementsofprogramminginterviews/primitivetypes/ClosestIntegerWithSameWeightSpek.kt
@@ -14,25 +14,22 @@ object ClosestIntegerWithSameWeightSpek : Spek({
     describe("findClosestIntegerWithSameWeight()") {
         fun findClosestIntegerWithBruteForce(number: Int):
                 Pair<String, Int> {
+            if (number == 0) return Pair(0.toString(), 0)
             val bits = number.toBigInteger()
             val bitsChars = listOf('0') + bits.toString(2).toList()
-//            println(bitsChars)
             return Generator.permutation(bitsChars)
                     .simple()
                     .map {
                         val s = it.joinToString(separator = "")
                         val n = BigInteger(s, 2)
-//                        println(s)
                         Pair(s, n)
                     }
                     .sortedBy { it.second }
                     .withIndex()
                     .let { all ->
-//                        println(all.toList())
                         all.find {
                             it.value.second == bits
                         }!!.let { (numberIndex, _) ->
-//                            println(numberIndex)
                             mutableListOf<Pair<String, BigInteger>>()
                                     .apply {
                                         if (numberIndex > 0)
@@ -42,7 +39,6 @@ object ClosestIntegerWithSameWeightSpek : Spek({
                                             add(all.elementAt(numberIndex + 1).value)
                                     }
                                     .map {
-                                        println(it)
                                         Pair(it, it.second - all.elementAt(numberIndex).value.second)
                                     }
                                     .filterNot { it.second == 0.toBigInteger() }
@@ -54,7 +50,7 @@ object ClosestIntegerWithSameWeightSpek : Spek({
                         Pair(first, second.toInt())
                     }
         }
-        val data = (1..1).map {
+        val data = (0..100).map {
             data(it, findClosestIntegerWithBruteForce(it))
         }.toTypedArray()
         data.forEach { (number, expected) ->
