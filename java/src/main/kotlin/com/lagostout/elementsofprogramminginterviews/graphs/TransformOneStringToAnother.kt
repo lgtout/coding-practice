fd32ea5ec4e2642aec98f3ee558e930c12c55b6d
lgtout <lagos.tout@gmail.com>
2018-01-01 19:13:50 -0800

    EP-K 19.7.1 Transform one string to another - WIP

--- java/src/main/kotlin/com/lagostout/elementsofprogramminginterviews/graphs/TransformOneStringToAnother.kt
+++ java/src/main/kotlin/com/lagostout/elementsofprogramminginterviews/graphs/TransformOneStringToAnother.kt
@@ -1,2 +1,60 @@
 package com.lagostout.elementsofprogramminginterviews.graphs
 
+import java.util.*
+
+/**
+ *  Problem 19.7.1 page 372
+ */
+fun lengthOfShortestProductionSequence(from: String, to: String,
+                                       dictionary: List<String>): Int? {
+    // Build the graph
+    val graph = mutableMapOf<String, MutableList<String>>()
+    (0..from.lastIndex).forEach { indexOfCharToIgnore ->
+        val adjacentWords = dictionary.groupBy {
+            it.removeRange(indexOfCharToIgnore,
+                    indexOfCharToIgnore + 1)
+        }
+        adjacentWords.values.forEach {
+            for (source in 0..(it.lastIndex - 1)) {
+                for (destination in (source + 1)..it.lastIndex) {
+                    graph.getOrPut(dictionary[source], { mutableListOf() })
+                            .add(dictionary[destination])
+                }
+            }
+        }
+    }
+    // Search the graph
+    data class Frame(val word: String, val adjacentWordIndex: Int = 0)
+    val explored = mutableSetOf<String>()
+    val stack = LinkedList<Frame>().apply {
+        add(Frame(from))
+    }
+    explored.add(from)
+    var distance: Int? = null
+    while (stack.isNotEmpty()) {
+        run {
+            stack.pop().let { (word, adjacentWordIndex) ->
+                if (adjacentWordIndex >= graph[word]?.size ?: 0) {
+                    return@run
+                }
+                stack.push(Frame(word, adjacentWordIndex.inc()))
+                graph[word]?.let {
+                    it[adjacentWordIndex].let {
+                        // Don't add _to_ to explored
+                        if (it == to) {
+                            val pathLength = stack.size - 1
+                            distance = distance?.let {
+                                minOf(pathLength, it)
+                            } ?: pathLength
+                            return@run
+                        }
+                        if (it in explored) return@run
+                        stack.push(Frame(it, 0))
+                        explored.add(it)
+                    }
+                }
+            }
+        }
+    }
+    return distance
+}
--- java/src/test/kotlin/com/lagostout/elementsofprogramminginterviews/graphs/TransformOneStringToAnotherSpek.kt
+++ java/src/test/kotlin/com/lagostout/elementsofprogramminginterviews/graphs/TransformOneStringToAnotherSpek.kt
@@ -1,2 +1,7 @@
 package com.lagostout.elementsofprogramminginterviews.graphs
 
+import org.jetbrains.spek.api.Spek
+
+object TransformOneStringToAnother : Spek({
+
+})
