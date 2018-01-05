3b6023ba37ca906b64cf153fa6f578ff78f9ca4f
lgtout <lagos.tout@gmail.com>
2018-01-04 18:52:54 -0800

    EP-K 8.7 Remove kth last element from list - WIP

---
+++ java/src/main/kotlin/com/lagostout/elementsofprogramminginterviews/linkedlists/RemoveKthLastElement.kt
@@ -0,0 +1,12 @@
+package com.lagostout.elementsofprogramminginterviews.linkedlists
+
+import java.util.*
+
+/**
+ * Problem 8.7 page 123
+ */
+fun <T> removeKthLastElement(list: LinkedList<T>) {
+    // Find the end of the list
+
+    
+}
