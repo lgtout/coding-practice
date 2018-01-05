8cd54a27a8096b2a162e58f5af47c6c91d6fea92
lgtout <lagos.tout@gmail.com>
2018-01-05 15:01:43 -0800

    EP-K 14.6 Compute the union of intervals - WIP

--- java/src/main/kotlin/com/lagostout/elementsofprogramminginterviews/sorting/UnionOfIntervals.kt
+++ java/src/main/kotlin/com/lagostout/elementsofprogramminginterviews/sorting/UnionOfIntervals.kt
@@ -1,12 +1,14 @@
 package com.lagostout.elementsofprogramminginterviews.sorting
 
+import com.google.common.base.MoreObjects
 import com.lagostout.elementsofprogramminginterviews.sorting.UnionOfIntervals.EndPoint.Closed
+import com.lagostout.elementsofprogramminginterviews.sorting.UnionOfIntervals.EndPoint.Open
 import org.apache.commons.collections4.iterators.PeekingIterator
 
 /**
  * Problem 14.6 page 248
  */
-class UnionOfIntervals {
+object UnionOfIntervals {
 
     sealed class EndPoint(val value: Int) {
         class Open(value: Int): EndPoint(value)
@@ -14,9 +16,23 @@ class UnionOfIntervals {
         operator fun compareTo(other: EndPoint): Int {
             return value - other.value
         }
+
+        override fun toString(): String {
+            return MoreObjects.toStringHelper(
+                    this.javaClass.canonicalName.split(".").last())
+                    .add("value", value).toString()
+        }
+        companion object {
+            fun o(value: Int) = Open(value)
+            fun c(value: Int) = Closed(value)
+        }
     }
 
-    data class Interval(val start: EndPoint, val end: EndPoint)
+    data class Interval(val start: EndPoint, val end: EndPoint) {
+        companion object {
+            fun i(start: EndPoint, end: EndPoint) = Interval(start, end)
+        }
+    }
 
     fun computeUnionOfIntervals(intervals: List<Interval>): List<Interval> {
         val mergedIntervals = mutableListOf<Interval>()
@@ -24,20 +40,33 @@ class UnionOfIntervals {
         intervals.sortedBy { it.start.value }
         val iterator = PeekingIterator(intervals.iterator())
         var interval = iterator.next()
-        var start = interval.start
         var end = interval.end
         while (iterator.hasNext()) {
-            val nextInterval = iterator.next()
-            if (end > nextInterval.start ||
-                    (end == nextInterval.start &&
-                            (end is Closed || nextInterval.start is Closed))) {
-                end = nextInterval.end
-            } else {
+            val noOverlapWithNextInterval = run {
+                iterator.peek()?.let {
+                    interval.end < it.start ||
+                            (interval.end == it.start &&
+                                    listOf(interval.end, it.start)
+                                            .all { it is Open })
+                } ?: true
+            }
+            if (noOverlapWithNextInterval) {
                 mergedIntervals.add(interval.copy(end = end))
                 iterator.peek()?.let {
                     interval = it
-                    start = it.start
                     end = it.end
+                }
+            } else {
+                end = iterator.next().end.let {
+                    when {
+                        it.value < interval.end.value -> interval.end
+                        it.value > interval.end.value -> it
+                        else -> {
+                            (if (listOf(interval.end, it).any { it is Closed })
+                                ::Closed else ::Open)
+                                    .call("value" to it.value)
+                        }
+                    }
                 }
             }
         }
--- java/src/test/kotlin/com/lagostout/elementsofprogramminginterviews/sorting/UnionOfIntervalsSpek.kt
+++ java/src/test/kotlin/com/lagostout/elementsofprogramminginterviews/sorting/UnionOfIntervalsSpek.kt
@@ -1,2 +1,26 @@
 package com.lagostout.elementsofprogramminginterviews.sorting
 
+import com.lagostout.elementsofprogramminginterviews.sorting.UnionOfIntervals.EndPoint.Companion.o
+import com.lagostout.elementsofprogramminginterviews.sorting.UnionOfIntervals.Interval.Companion.i
+import com.lagostout.elementsofprogramminginterviews.sorting.UnionOfIntervals.computeUnionOfIntervals
+import org.assertj.core.api.Assertions.assertThat
+import org.jetbrains.spek.api.Spek
+import org.jetbrains.spek.api.dsl.describe
+import org.jetbrains.spek.api.dsl.it
+import org.jetbrains.spek.data_driven.data
+import org.jetbrains.spek.data_driven.on
+
+object UnionOfIntervalsSpek : Spek({
+    describe("computeUnionOfIntervals") {
+        val data = listOfNotNull(
+                data(listOf(i(o(1),o(1))), listOf(i(o(1),o(1)))),
+                null
+        ).toTypedArray()
+        on("intervals: %s", with = *data) { intervals, expected ->
+            it("returns $expected") {
+                assertThat(computeUnionOfIntervals(intervals))
+                        .isEqualTo(expected)
+            }
+        }
+    }
+})
