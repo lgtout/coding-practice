add6672aea8a61a7a4a8f610598db6891900efed
lgtout <lagos.tout@gmail.com>
2018-01-08 22:13:35 -0800

    EP-K 19.7.1 Transform one string to another - WIP

--- java/src/main/kotlin/com/lagostout/elementsofprogramminginterviews/graphs/TransformOneStringToAnother.kt
+++ java/src/main/kotlin/com/lagostout/elementsofprogramminginterviews/graphs/TransformOneStringToAnother.kt
@@ -2,27 +2,46 @@ package com.lagostout.elementsofprogramminginterviews.graphs
 
 import java.util.*
 
-/**
- *  Problem 19.7.1 page 372
- */
+/*  Problem 19.7.1 page 372 */
+/*
+    We'll assume that
+    -- If there are words in the dictionary, they're
+    the same length as [from] and [to].
+    -- The source and destination strings are in the
+    dictionary.
+    -- The length of the production sequence is the
+    length of the path in the graph from the source
+    string to the destination string.  This is one
+    less than the number of words on that path.
+*/
 fun lengthOfShortestProductionSequence(from: String, to: String,
                                        dictionary: List<String>): Int? {
+    if (from == to) return 0
+
     // Build the graph
     val graph = mutableMapOf<String, MutableList<String>>()
     (0..from.lastIndex).forEach { indexOfCharToIgnore ->
-        val adjacentWords = dictionary.groupBy {
+        // Words that differ by one character are adjacent
+        // in the graph e.g cod cad.
+        val adjacentWordGroups = dictionary.groupBy {
             it.removeRange(indexOfCharToIgnore,
                     indexOfCharToIgnore + 1)
         }
-        adjacentWords.values.forEach {
-            for (source in 0..(it.lastIndex - 1)) {
-                for (destination in (source + 1)..it.lastIndex) {
-                    graph.getOrPut(dictionary[source], { mutableListOf() })
-                            .add(dictionary[destination])
-                }
+        println(adjacentWordGroups)
+        adjacentWordGroups.values.forEach { adjacentWords ->
+            for (source in 0..adjacentWords.lastIndex) {
+                (0..adjacentWords.lastIndex).filterNot { it == source }
+                        .forEach { destination ->
+                            graph.getOrPut(adjacentWords[source],
+                                    { mutableListOf() })
+                                    .add(adjacentWords[destination])
+                        }
             }
         }
     }
+
+    println(graph)
+
     // Search the graph
     data class Frame(val word: String, val adjacentWordIndex: Int = 0)
     val explored = mutableSetOf<String>()
@@ -42,10 +61,9 @@ fun lengthOfShortestProductionSequence(from: String, to: String,
                     it[adjacentWordIndex].let {
                         // Don't add _to_ to explored
                         if (it == to) {
-                            val pathLength = stack.size - 1
-                            distance = distance?.let {
-                                minOf(pathLength, it)
-                            } ?: pathLength
+                            val pathLength = stack.size
+                            distance = listOfNotNull(
+                                    distance, pathLength).min()
                             return@run
                         }
                         if (it in explored) return@run
--- java/src/test/kotlin/com/lagostout/elementsofprogramminginterviews/graphs/TransformOneStringToAnotherSpek.kt
+++ java/src/test/kotlin/com/lagostout/elementsofprogramminginterviews/graphs/TransformOneStringToAnotherSpek.kt
@@ -1,7 +1,34 @@
 package com.lagostout.elementsofprogramminginterviews.graphs
 
+import org.assertj.core.api.Assertions.assertThat
 import org.jetbrains.spek.api.Spek
+import org.jetbrains.spek.api.dsl.describe
+import org.jetbrains.spek.api.dsl.it
+import org.jetbrains.spek.data_driven.data
+import org.jetbrains.spek.data_driven.on
 
 object TransformOneStringToAnother : Spek({
-
+    describe("lengthOfShortestProductionSequence") {
+        val data = listOfNotNull(
+//                data("a", "a", emptyList(), 0),
+//                data("a", "a", listOf("a"), 0),
+//                data("a", "a", listOf("c","d"), 0),
+//                data("a", "b", listOf("a", "b"), 1),
+//                data("a", "b", listOf("c", "b", "a"), 1),
+//                data("a", "b", listOf("b", "a"), 1),
+//                data("a", "b", listOf("b", "a", "c"), 1),
+//                data("ab", "cb", listOf("ab", "ac", "cb"), 2),
+                // TODO More cases
+                data("ab", "bc", listOf("ab", "ad", "cb", "bc"), -1),
+//                data("cat", "dog", listOf("bat", "cot", "dog", "dag", "dot", "cat"), 3),
+                null
+        ).toTypedArray()
+        on("from: %s, to: %s, dictionary: %s", with = *data) {
+            from: String, to: String, dictionary: List<String>, expected: Int? ->
+            it("returns $expected") {
+                assertThat(lengthOfShortestProductionSequence(
+                        from, to, dictionary) ?: -1).isEqualTo(expected)
+            }
+        }
+    }
 })
