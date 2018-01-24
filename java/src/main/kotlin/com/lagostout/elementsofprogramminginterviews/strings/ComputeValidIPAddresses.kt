0c9c0a6aabb41482cd765bf0ee1ceff329773e03
lgtout <lagos.tout@gmail.com>
2018-01-23 20:43:15 -0800

    EP-K 7.10.1 Compute all IP addresses - WIP

--- java/src/main/kotlin/com/lagostout/elementsofprogramminginterviews/strings/ComputeValidIPAddresses.kt
+++ java/src/main/kotlin/com/lagostout/elementsofprogramminginterviews/strings/ComputeValidIPAddresses.kt
@@ -2,28 +2,27 @@ package com.lagostout.elementsofprogramminginterviews.strings
 
 /* Problem 7.10.1  Page 106 */
 
-fun computeValidIPAddresses(address:String): Int {
-    data class Frame(val octetLength: Int,
-                     val lengthOfIPStringRemainder: Int,
-                     val remainingOctetCount: Int)
-
-    fun computeOctets(frames: List<Frame>): List<Frame> {
-        return frames.map { frame ->
-            (1..3).map {
-                Frame(it, frame.lengthOfIPStringRemainder - it,
-                        frame.remainingOctetCount - 1)
-            }.filter {
-                it.run {
-                    lengthOfIPStringRemainder in
-                            (remainingOctetCount * 1)..
-                                    (remainingOctetCount * 3)
-                }
+fun computeValidIPAddresses(addressWithNoPeriods:String): List<String> {
+    // TODO Should octetOrdinal be increasing or decreasing?
+    fun computeOctets(addressWithNoPeriods: String,
+                      start: Int, octetOrdinal: Int): List<String> {
+        val addressWithPeriods = mutableListOf<String>()
+        Pair(addressWithNoPeriods.length - start !in
+                (octetOrdinal..octetOrdinal * 3),
+                (octetOrdinal > 4)).let {
+            (tooManyOrTooFewNumbersRemaining, illegalOctetOrdinal) ->
+            if (tooManyOrTooFewNumbersRemaining || illegalOctetOrdinal)
+                return addressWithPeriods
+        }
+        var currentOctet: String
+        (1..4).forEach {
+            val nextOctetStart = start + it
+            currentOctet = addressWithNoPeriods.substring(start, nextOctetStart)
+            computeOctets(addressWithNoPeriods, nextOctetStart, octetOrdinal + 1).map {
+                addressWithPeriods.add("$currentOctet.$it")
             }
-        }.flatten()
+        }
+        return addressWithPeriods
     }
-    (0..3).map {
-        { frames: List<Frame> -> computeOctets(frames) }
-    }.fold(listOf(Frame(0, address.length, 4))) {
-        acc, fn -> fn(acc) }
-    return 0
+    return computeOctets(addressWithNoPeriods, 0, 1)
 }
