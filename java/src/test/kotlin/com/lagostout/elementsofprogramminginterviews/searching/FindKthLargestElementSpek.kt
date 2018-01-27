95084a08bd0ef1adc594f9e2c4f226ace757de17
lgtout <lagos.tout@gmail.com>
2018-01-27 14:58:42 -0800

    EP-K 12.8.1 Find kth largest element - done!
    Reduce space complexity from O(log n) to O(1).
    Randomize pivot selection to ensure average time complexity O(n).

--- java/src/test/kotlin/com/lagostout/elementsofprogramminginterviews/searching/FindKthLargestElementSpek.kt
+++ java/src/test/kotlin/com/lagostout/elementsofprogramminginterviews/searching/FindKthLargestElementSpek.kt
@@ -2,31 +2,33 @@ package com.lagostout.elementsofprogramminginterviews.searching
 
 import org.assertj.core.api.Assertions.assertThat
 import org.jetbrains.spek.api.Spek
+import org.jetbrains.spek.api.dsl.describe
 import org.jetbrains.spek.api.dsl.it
 import org.jetbrains.spek.data_driven.data
 import org.jetbrains.spek.data_driven.on
 
 object FindKthLargestElementSpek : Spek({
-    val data = listOfNotNull(
-            data(listOf(2), 1, 2),
-            data(listOf(3,1), 1, 3),
-            data(listOf(3,1), 2, 1),
-            data(listOf(6,1,2,5,4,3), 1, 6),
-            data(listOf(6,1,2,5,4,3), 2, 5),
-            data(listOf(3,1,2,5,4,6), 2, 5),
-            data(listOf(6,1,2,5,4,3), 3, 4),
-            data(listOf(6,1,2,5,4,3), 4, 3),
-            data(listOf(3,1,2,5,4,6), 5, 2),
-            data(listOf(3,1,2,5,4,6), 6, 1),
-            data(listOf(3,1,2,5,4,6), 6, 1),
-            data(listOf(6,1,2,5,4,3), 6, 1),
-            null
-    ).toTypedArray()
-    on("list: %s, k: %s", with = *data) {
-        list, k, expected ->
-        it("expected: $expected") {
-            assertThat(findKthLargestElement(list.toMutableList(), k))
-                    .isEqualTo(expected)
+    describe("findKthLargestElement") {
+        val data = listOfNotNull(
+                data(listOf(2), 1, 2),
+                data(listOf(3, 1), 1, 3),
+                data(listOf(3, 1), 2, 1),
+                data(listOf(6, 1, 2, 5, 4, 3), 1, 6),
+                data(listOf(6, 1, 2, 5, 4, 3), 2, 5),
+                data(listOf(3, 1, 2, 5, 4, 6), 2, 5),
+                data(listOf(6, 1, 2, 5, 4, 3), 3, 4),
+                data(listOf(6, 1, 2, 5, 4, 3), 4, 3),
+                data(listOf(3, 1, 2, 5, 4, 6), 5, 2),
+                data(listOf(3, 1, 2, 5, 4, 6), 6, 1),
+                data(listOf(3, 1, 2, 5, 4, 6), 6, 1),
+                data(listOf(6, 1, 2, 5, 4, 3), 6, 1),
+                null
+        ).toTypedArray()
+        on("list: %s, k: %s", with = *data) { list, k, expected ->
+            it("expected: $expected") {
+                assertThat(findKthLargestElement(list.toMutableList(), k))
+                        .isEqualTo(expected)
+            }
         }
     }
 })
