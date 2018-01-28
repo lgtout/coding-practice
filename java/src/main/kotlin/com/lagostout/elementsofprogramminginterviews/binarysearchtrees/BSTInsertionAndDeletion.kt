d8e4795acd0e5e9c95eb40533898a8802926845c
lgtout <lagos.tout@gmail.com>
2018-01-28 11:17:54 -0800

    EP-K 15.10.1 Insertion and deletion in a BST - WIP

--- java/src/main/kotlin/com/lagostout/elementsofprogramminginterviews/binarysearchtrees/BSTInsertionAndDeletion.kt
+++ java/src/main/kotlin/com/lagostout/elementsofprogramminginterviews/binarysearchtrees/BSTInsertionAndDeletion.kt
@@ -4,62 +4,86 @@ import com.lagostout.common.isLeftChild
 import com.lagostout.common.isRightChild
 import com.lagostout.datastructures.BinaryTreeNode
 
+/* Problem 15.10.1 page 277 */
+
 object BSTInsertionAndDeletion {
 
     private fun <T : Comparable<T>> find(
-            value: T, root: BinaryTreeNode<T>):
+            root: BinaryTreeNode<T>,
+            compareTo: (T) -> Int):
             BinaryTreeNode<T> {
         var currentNode = root
-        run {
-            while (true) {
-                if (value == currentNode.value) break
-                else if (currentNode.value > value) {
+        while (true) {
+            when (compareTo(currentNode.value)) {
+                0 -> null
+                1 -> {
                     currentNode.left?.let {
                         currentNode = it
-                    } ?: return@run
-                } else {
+                    }
+                }
+                else -> {
                     currentNode.right?.let {
                         currentNode = it
-                    } ?: return@run
+                    }
                 }
-            }
+            } ?: break
         }
         return currentNode
     }
+
+    private fun <T : Comparable<T>> find(
+            value: T, root: BinaryTreeNode<T>): BinaryTreeNode<T> {
+        return find(root, { it.compareTo(value) })
+    }
+
     fun <T : Comparable<T>> insert(value: T, root: BinaryTreeNode<T>) {
         val duplicateOrParent = find(value, root)
         if (duplicateOrParent.value == value) return
         duplicateOrParent.let {
+            // We know the parent is a leaf, so we don't need to
+            // consider its left/right children.
             (if (it.value > value) it::right
             else it::left).set(it)
         }
     }
 
-    fun <T : Comparable<T>> delete(value: T, root: BinaryTreeNode<T>): BinaryTreeNode<T>? {
+    fun <T : Comparable<T>> delete(value: T, root: BinaryTreeNode<T>):
+            BinaryTreeNode<T>? {
         val node =  find(value, root)
         if (node.value != value) return root
-        val parent = node.parent
-        // Cases: Deleting
-        // -- root
-        // -- leaf
-        // -- root AND leaf
-        // Detach subtree
-        if (node.isRightChild) {
-            Pair(node.left, node.right).apply {
-                node.left = null
-            }
-        } else if (node.isLeftChild) {
-            Pair(node.right, node.left).apply {
-                node.right = null
-            }
-        } else {
-            // TODO
-            // root
-            null
-        }?.let { (detachedNode, root) ->
-            // Check for nulls firsts
+        // If there's a left subtree, find within it the node
+        // with the max value.
+        // Otherwise, if there's a right subtree, find within
+        // it the node with the min value.
+        val replacementNode = node.left?.let {
+            find(it, { 1 })
+        } ?: node.right?.let {
+            find (it, { -1 })
         }
-        return null
+        return replacementNode?.let {
+            // Detach the replacement node from its parent.
+            it.parent?.let { parent ->
+                (if (it.isRightChild)
+                    parent::right else parent::left).set(null)
+            }
+            // Detach the node to delete from its parent.
+            if (node.isRightChild) {
+                node.parent?.right = replacementNode
+            } else if (node.isLeftChild){
+                node.parent?.left = replacementNode
+            }
+            // Attach the replacement node to its new parent.
+            // Attach the deleted nodes subtrees to the replacement node.
+            with (it) {
+                right = node.right
+                left = node.left
+                parent = node.parent
+            }
+            // Return the root.
+            // If replacement node was null, the node to delete is the
+            // root, and we return null.
+            root
+        }
     }
 
 }
--- java/src/test/kotlin/com/lagostout/elementsofprogramminginterviews/binarysearchtrees/BSTInsertionAndDeletionSpek.kt
+++ java/src/test/kotlin/com/lagostout/elementsofprogramminginterviews/binarysearchtrees/BSTInsertionAndDeletionSpek.kt
@@ -1,2 +1,16 @@
 package com.lagostout.elementsofprogramminginterviews.binarysearchtrees
 
+import com.lagostout.datastructures.BinarySearchTree
+import com.lagostout.kotlin.common.Alphabet
+import org.jetbrains.spek.api.Spek
+import org.jetbrains.spek.api.dsl.describe
+
+object BSTInsertionAndDeletionSpek : Spek({
+    val tree:BinarySearchTree<Alphabet>? by memoized { null }
+    describe("insert") {
+
+    }
+    describe("delete") {
+
+    }
+})
