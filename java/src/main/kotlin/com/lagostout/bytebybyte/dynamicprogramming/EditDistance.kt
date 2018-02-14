3ea7f00da8a9dde133f6e86c6aa4324b37e49df6
lgtout <lagos.tout@gmail.com>
2018-02-13 21:28:41 -0800

    BB Edit distance - WIP

--- java/src/main/kotlin/com/lagostout/bytebybyte/dynamicprogramming/EditDistance.kt
+++ java/src/main/kotlin/com/lagostout/bytebybyte/dynamicprogramming/EditDistance.kt
@@ -1,6 +1,6 @@
 package com.lagostout.bytebybyte.dynamicprogramming
 
-import kotlin.math.max
+import kotlin.math.absoluteValue
 import kotlin.math.min
 
 /* Given two strings, write a function that determines the minimum edit distance
@@ -70,66 +70,24 @@ object EditDistance {
     // TODO Matrix cache implementation
     fun computeWithMemoizationBottomUp(
             string1: String, string2: String): Int {
+        //  TODO Redo
+        if (string1.isEmpty() || string2.isEmpty()) {
+            return (string1.length - string2.length).absoluteValue
+        }
         // We're converting string1 into string2.
         val cache: MutableMap<Pair<Int, Int>, Int> = mutableMapOf()
-        for (string1Index in min(0, string1.lastIndex)..string1.lastIndex) {
-            /*
-            It's impossible for a character in string1 to be aligned with one
-            in string2 such that string1Index < string2Index. So we start comparing
-            at string1Index within string2.  This is because only inserts or updates
-            could have been applied to positions prior to string1Index, and both
-            operations can only push any following characters right, such that they
-            align with higher indices of string2 than they did prior to the operations.
-            */
-            for (string2Index in min(string1Index, string2.lastIndex)
-                    ..max(string1.lastIndex, string2.lastIndex)) {
-                /*
-                Find out the possible edit distances we'd have to have incurred
-                to reach the current alignment of the characters at string1Index
-                and string2Index.  To get here, in the prior move, either we modified
-                the character before string1Index in string1, or the character at
-                string1Index was previously at string1Index - 1 and we did an insert
-                which pushed the character to string1Index.  These are the only paths
-                by which we the current alignment can be reached.
-                */
-                val previousPositionByInsert = Pair(string1Index, string2Index - 1)
-                val previousPositionByModify = Pair(string1Index - 1, string2Index - 1)
-                /*
-                If string2Index is 0, it's not possible for the current alignment
-                between the strings to have been reached by any series of inserts
-                or updates to string1. So the edit distances by insert and modify
-                are both 0.
-                */
-                val keys = listOf(previousPositionByInsert, previousPositionByModify).filter {
-                            it.toList().all { it >= 0 } }.map {
-                    println(it)
-                    cache[it]!! }
-                /*
-                If string1 is longer than string2 we must be careful not to access positions
-                within string2 that are greater than string2.lastIndex.  If it is greater, we
-                can assume we need to do an update to string1.  So we increment the edit distance.
-                */
-                cache[Pair(string1Index, string2Index)] = (keys.min() ?: 0) +
-                        if ((string1Index > string2.lastIndex ||
-                                        // If either index is at an index before the start
-                                        // of a string...
-                                        ((string2Index < 0 || string1Index < 0)
-                                                // But they're not both at an index before the start
-                                                // of a string (checking this because we shouldn't
-                                                // increment edit distance if there are no characters
-                                                // at the current indices)...
-                                                && !(string2Index < 0 && string1Index < 0)) ||
-                                        // Prevent accessing a negative index in either string.
-                                        (!(string2Index < 0 || string1Index < 0)
-                                                && string1[string1Index] != string1[string2Index]))) 1 else 0
+        var string1Index = if (string1.isEmpty()) - 1 else 0
+        var string2Index = if (string2.isEmpty()) - 1 else 0
+        while (true) {
+            if (string1Index > string1.lastIndex &&
+                    string2Index > string2.lastIndex) break
+            val key = Pair(string1Index, string2Index)
+            listOf(Pair(string1Index - 1, string2Index),
+                Pair(string1Index, string2Index)).filter {
+                it.toList().all { it >= 0 }
             }
         }
-        /*
-        After string1 is transformed to string2, the last position of string1 will be aligned
-        with the last one of string2.  The edit distance at this alignment is the minimum edit
-        distance for the entire transformation of string1.
-        */
-        return cache[Pair(string1.lastIndex, string2.lastIndex)]!!
+        return 0
     }
 
 }
