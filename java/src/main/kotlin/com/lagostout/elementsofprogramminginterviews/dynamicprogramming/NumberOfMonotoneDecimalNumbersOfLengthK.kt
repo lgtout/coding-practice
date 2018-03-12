ed3ebac794b51b3dcb5c4174a8885fbfab5d2fa3
lgtout <lagos.tout@gmail.com>
2018-03-12 12:15:58 -0700

    EP-K 17.3.6 Number of monotone decimal numbers of length k - done!

--- java/src/main/kotlin/com/lagostout/elementsofprogramminginterviews/dynamicprogramming/NumberOfMonotoneDecimalNumbersOfLengthK.kt
+++ java/src/main/kotlin/com/lagostout/elementsofprogramminginterviews/dynamicprogramming/NumberOfMonotoneDecimalNumbersOfLengthK.kt
@@ -5,15 +5,56 @@ object NumberOfMonotoneDecimalNumbersOfLengthK {
     fun computeWithRecursionAndBruteForce(k: Int): Int {
         return if (k == 0) 0
         else {
+            val digits = (1..9)
             fun compute(k: Int, start: Int): Int {
                 return when (k) {
                     0 -> 1
-                    else -> (start..(9 - k + 1)).map {
+                    else -> (start..digits.last).map {
                         compute(k - 1, it)
                     }.sum()
                 }
             }
-            compute(k, 0)
+            compute(k, 1)
+        }
+    }
+
+    fun computeWithRecursionAndMemoization(k: Int): Int {
+        return if (k == 0) 0
+        else {
+            val digits = (1..9)
+            val cache = List(k + 1) {
+                MutableList<Int?>(digits.last + 1) { null }
+            }
+            fun compute(k: Int, start: Int): Int {
+                return cache[k][start] ?: when (k) {
+                    0 -> 1
+                    else -> (start..digits.last).map {
+                        compute(k - 1, it)
+                    }.sum()
+                }.also {
+                    cache[k][start] = it
+                }
+            }
+            compute(k, 1)
+        }
+    }
+
+    @Suppress("NAME_SHADOWING")
+    fun computeBottomUpWithMemoization(k: Int): Int {
+        return if (k == 0) 0 else {
+            val digits = (1..9)
+            val cache = List(k + 1) { k ->
+                MutableList(digits.last + 1) { if (k == 0) 1 else null }
+            }
+            (1..k).forEach { k ->
+                digits.reversed().forEach { digit ->
+                    cache[k][digit] = (digit..digits.last()).mapNotNull {
+                        cache[k-1][it]
+                    }.sum()
+                }
+            }
+            println(cache)
+            cache[k][1]!!
         }
     }
 
--- java/src/test/kotlin/com/lagostout/elementsofprogramminginterviews/dynamicprogramming/NumberOfMonotoneDecimalNumbersOfLengthKSpek.kt
+++ java/src/test/kotlin/com/lagostout/elementsofprogramminginterviews/dynamicprogramming/NumberOfMonotoneDecimalNumbersOfLengthKSpek.kt
@@ -1,7 +1,63 @@
 package com.lagostout.elementsofprogramminginterviews.dynamicprogramming
 
+import com.lagostout.elementsofprogramminginterviews.dynamicprogramming.NumberOfMonotoneDecimalNumbersOfLengthK.computeBottomUpWithMemoization
+import com.lagostout.elementsofprogramminginterviews.dynamicprogramming.NumberOfMonotoneDecimalNumbersOfLengthK.computeWithRecursionAndBruteForce
+import com.lagostout.elementsofprogramminginterviews.dynamicprogramming.NumberOfMonotoneDecimalNumbersOfLengthK.computeWithRecursionAndMemoization
+import org.assertj.core.api.Assertions.assertThat
 import org.jetbrains.spek.api.Spek
+import org.jetbrains.spek.api.dsl.describe
+import org.jetbrains.spek.api.dsl.it
+import org.jetbrains.spek.data_driven.data
+import org.jetbrains.spek.data_driven.on
+import org.paukov.combinatorics3.Generator
 
-object NumberOfMonotoneDecimalNumbersOfLengthK : Spek({
+object NumberOfMonotoneDecimalNumbersOfLengthKSpek : Spek({
+
+    val randomData by memoized {
+        val kRange = (0..5)
+        val digits = (1..9)
+        kRange.map { k ->
+            val expected = Generator.permutation(digits.toList())
+                    .withRepetitions(k).filter {
+                        it.sorted() == it
+                    }.also{
+                        if (k==0) println(it)
+                    }.run { if (size == 1 && first().isEmpty()) 0 else count() }
+            data(k, expected)
+        }.toSet()
+    }
+
+    val data = (listOfNotNull(
+        data(0, 0),
+        data(1, 9),
+        null
+    ) + randomData).toTypedArray()
+
+    describe("computeWithRecursionAndBruteForce") {
+        on("k: %s", with = *data) { k, expected ->
+            it("returns $expected") {
+                assertThat(computeWithRecursionAndBruteForce(k))
+                        .isEqualTo(expected)
+            }
+        }
+    }
+
+    describe("computeWithRecursionAndMemoization") {
+        on("k: %s", with = *data) { k, expected ->
+            it("returns $expected") {
+                assertThat(computeWithRecursionAndMemoization(k))
+                        .isEqualTo(expected)
+            }
+        }
+    }
+
+    describe("computeBottomUpWithMemoization") {
+        on("k: %s", with = *data) { k, expected ->
+            it("returns $expected") {
+                assertThat(computeBottomUpWithMemoization(k))
+                        .isEqualTo(expected)
+            }
+        }
+    }
 
 })
