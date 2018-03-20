cdb58b0824c5a38c9d806a667b431c060d116dbf
lgtout <lagos.tout@gmail.com>
2018-03-20 14:35:33 -0700

    EP-K 17.5.3 Occurrences of sequence in 2D array - single visit - done!

--- java/src/main/kotlin/com/lagostout/elementsofprogramminginterviews/dynamicprogramming/EnumerateOccurrencesOfSequenceIn2DArrayWithSingleVisitPerPositionLimit.kt
+++ java/src/main/kotlin/com/lagostout/elementsofprogramminginterviews/dynamicprogramming/EnumerateOccurrencesOfSequenceIn2DArrayWithSingleVisitPerPositionLimit.kt
@@ -23,26 +23,25 @@ object EnumerateOccurrencesOfSequenceIn2DArrayWithSingleVisitPerPositionLimit {
                         Pair(rowIndex, it)
                     }
                 }
-                fun compute(start: Pair<Int, Int>, patternIndex: Int,
-                            visited: Set<Pair<Int, Int>> = setOf(),
-                            path: List<Pair<Int, Int>> = listOf()):
+                fun compute(start: Pair<Int, Int>, patternIndex: Int):
                         Set<List<Pair<Int, Int>>> {
                     return when {
                         grid[start.first][start.second] !=
                                 pattern[patternIndex] -> emptySet()
                         patternIndex == pattern.lastIndex ->
-                            setOf(path + listOf(start))
+                            setOf(listOf(start))
                         else -> start.run {
                             listOf(up, down, left, right)
                         }.filter {
-                            it !in visited && it.first in (0..rowCount) &&
+                            it.first in (0..rowCount) &&
                                     it.second in (0..colCount)
-                        }.map {
-                            compute(it, patternIndex + 1, visited + setOf(start),
-                                path + listOf(start))
-                        }.fold(mutableSetOf()) { acc, curr ->
-                            acc.apply { addAll(curr) }
-                        }
+                        }.flatMap {
+                            compute(it, patternIndex + 1).filter {
+                                start !in it
+                            }.map {
+                                listOf(start) + it
+                            }
+                        }.toSet()
                     }
                 }
                 fun search(positionsIndex: Int): Set<List<Pair<Int, Int>>> {
@@ -57,10 +56,9 @@ object EnumerateOccurrencesOfSequenceIn2DArrayWithSingleVisitPerPositionLimit {
     }
 
     fun computeWithRecursionAndMemoization(
-            grid: List<List<Int>>, pattern: List<Int>): Boolean {
+            grid: List<List<Int>>, pattern: List<Int>): Set<List<Pair<Int, Int>>> {
         return when {
-            grid.isEmpty() -> pattern.isEmpty()
-            pattern.isEmpty() -> true
+            grid.isEmpty() || pattern.isEmpty() -> emptySet()
             else -> {
                 val lastRow = grid.lastIndex
                 val lastCol = grid.first().lastIndex
@@ -69,38 +67,39 @@ object EnumerateOccurrencesOfSequenceIn2DArrayWithSingleVisitPerPositionLimit {
                         Pair(rowIndex, it)
                     }
                 }
-                val cache = mutableMapOf<Pair<Int, Int>, MutableList<Boolean?>>()
-                fun compute(
-                        start: Pair<Int, Int>, patternIndex: Int,
-                        visited: MutableSet<Pair<Int, Int>> = mutableSetOf()): Boolean {
-                    val isValidPosition = start.first in (0..lastRow) &&
-                                start.second in (0..lastCol)
+                val cache = mutableMapOf<Pair<Int, Int>,
+                        MutableList<Set<List<Pair<Int, Int>>>?>>()
+                fun compute(start: Pair<Int, Int>, patternIndex: Int):
+                        Set<List<Pair<Int, Int>>> {
                     return cache[start]?.get(patternIndex) ?: when {
-                        !isValidPosition -> false
                         grid[start.first][start.second] !=
-                                pattern[patternIndex] -> false
-                        patternIndex == pattern.lastIndex -> true
-                        else -> start.run {
-                            listOf(up, down, left, right).filter {
-                                it !in visited
+                                pattern[patternIndex] -> emptySet()
+                        patternIndex == pattern.lastIndex -> setOf(listOf(start))
+                        else -> run {
+                            start.run {
+                                listOf(up, down, left, right).filter {
+                                    it.first in (0..lastRow) &&
+                                            it.second in (0..lastCol)
+                                }.flatMap {
+                                    compute(it, patternIndex + 1).filter {
+                                        start !in it
+                                    }.map {
+                                        listOf(start) + it
+                                    }
+                                }.toSet()
                             }
-                        }.map {
-                            compute(it, patternIndex + 1, visited.apply { add(start) })
-                        }.any { it }
-                    }.also {
-                        cache.getOrPut(start){
+                        }
+                    }.also { paths ->
+                        cache.getOrPut(start) {
                             MutableList(pattern.size) { null }
-                        }[patternIndex] = it
+                        }.let {
+                            it[patternIndex] = paths
+                        }
                     }
                 }
-                fun search(positionsIndex: Int): Boolean {
-                    return if (positionsIndex > positions.lastIndex) false
-                    // Swapping the OR operands short-circuits the search,
-                    // completing as soon as the first sequence is found.
-                    // This prevents the cache from being useful.  Having
-                    // them as follows makes use of the cache, as every
-                    // possible sequence in the grid is explored.
-                    else search(positionsIndex + 1) ||
+                fun search(positionsIndex: Int): Set<List<Pair<Int, Int>>> {
+                    return if (positionsIndex > positions.lastIndex) emptySet()
+                    else search(positionsIndex + 1) +
                             compute(positions[positionsIndex], 0)
 
                 }
@@ -110,65 +109,49 @@ object EnumerateOccurrencesOfSequenceIn2DArrayWithSingleVisitPerPositionLimit {
     }
 
     fun computeBottomUpWithMemoization(
-            grid: List<List<Int>>, pattern: List<Int>): Boolean {
+            grid: List<List<Int>>, pattern: List<Int>):
+            Set<List<Pair<Int, Int>>> {
         return when {
-            grid.isEmpty() -> pattern.isEmpty()
-            pattern.isEmpty() -> true
+            grid.isEmpty() || pattern.isEmpty() -> emptySet()
             else -> {
-                val lastCacheRow = grid.lastIndex
-                val lastCacheCol = grid.first().lastIndex
-                val positions = (0..lastCacheRow).flatMap { row ->
-                    (0..lastCacheCol).map { col ->
-                        Pair(row, col)
+                val lastRow = grid.lastIndex
+                val lastCol = grid.first().lastIndex
+                val positions = grid.withIndex().flatMap { (rowIndex, row) ->
+                    row.indices.map {
+                        Pair(rowIndex, it)
                     }
                 }
-                val cache = positions.map { position ->
-                    position to (-1..pattern.lastIndex).map { patternIndex ->
-                        patternIndex to Pair(
-                            if (patternIndex == -1) true else null,
-                            setOf<Set<Pair<Int, Int>>>())
-                    }.toMap().toMutableMap()
-                }.toMap()
-                (-1 until pattern.lastIndex).forEach { patternIndex ->
-                    positions.filter {
-                        cache[it]?.let {
-                            it[patternIndex]?.first
-                        } == true
-                    }.forEach { position ->
-                        position.run {
-                            (if (patternIndex == -1) listOf(
-                                Pair(position, setOf(emptySet())))
-                            else (listOf(up, down, left, right).map { nextPosition ->
-                                val paths = cache[position]?.get(patternIndex)?.second!!
-                                Pair(nextPosition, paths.filter { nextPosition !in it }.toSet())
-                            }.filter {
-                                it.second.isNotEmpty()
-                            })).map {
-                                Pair(it.first, it.second.map {
-                                    it.toMutableSet().apply { add(position) }.toSet()
-                                }.toMutableSet())
-                            }.toSet()
-                        }.forEach { (nextPosition, newPaths) ->
-                            val nextPatternIndex = patternIndex + 1
-                            cache[nextPosition]?.let { patternIndexToPathsMap ->
-                                patternIndexToPathsMap.merge(
-                                    nextPatternIndex,
-                                    Pair(pattern[nextPatternIndex] ==
-                                            grid[nextPosition.first][nextPosition.second],
-                                        setOf())) { old, new ->
-                                    if (new.first == true) {
-                                        val oldPaths = patternIndexToPathsMap[nextPatternIndex]!!.second
-                                        Pair(new.first, new.second.toMutableSet().apply {
-                                            addAll(oldPaths)
-                                            addAll(newPaths)
-                                        })
-                                    } else old
-                                }
+                val cache = mutableMapOf<Pair<Int, Int>,
+                        MutableList<Set<List<Pair<Int, Int>>>?>>()
+                (pattern.indices).reversed().forEach { patternIndex ->
+                    positions.forEach { start ->
+                        val paths = when {
+                            grid[start.first][start.second] !=
+                                    pattern[patternIndex] -> emptySet()
+                            patternIndex == pattern.lastIndex -> setOf(listOf(start))
+                            else -> start.run {
+                                listOf(up, down, left, right).filter {
+                                    it.first in (0..lastRow) &&
+                                            it.second in (0..lastCol)
+                                }.flatMap {
+                                    cache[it]?.get(patternIndex + 1)?.filter {
+                                        start !in it
+                                    }?.map {
+                                        listOf(start) + it
+                                    } ?: emptyList()
+                                }.toSet()
                             }
+                        }
+                        cache.getOrPut(start) {
+                            MutableList(pattern.size) { null }
+                        }.let {
+                            it[patternIndex] = paths
                         }
                     }
                 }
-                cache.any { it.value[pattern.lastIndex]?.first == true }
+                return cache.values.flatMap {
+                    it[0] ?: emptySet()
+                }.toSet()
             }
         }
     }
