66ec2ff6d127eabef51361c3565866acbe7deaed
lgtout <lagos.tout@gmail.com>
2018-04-25 15:49:11 -0700

    EP-S 17.12.3 Longest alternating subsequence - WIP

---
+++ swift/CodingPractice/CodingPractice/LongestAlternatingSubsequence.swift
@@ -0,0 +1,40 @@
+// Copyright (c) 2018 lagostout. All rights reserved.
+
+import Foundation
+
+/* Problem 17.12.3 page 336 */
+
+class LongestAlternatingSubsequence {
+    
+    static func computeBottomUpWithMemoization(sequence: [Int]) -> [[Int]] {
+        if sequence.isEmpty {
+            return [[]]
+        }
+        var cache: [[(index: Int, value: Int)]] = []
+        var currIndex = 0
+        while currIndex < sequence.endIndex {
+            let currValue: Int = sequence[currIndex]
+            for subsequence in cache {
+                if let lastValueInSubsequence = subsequence.last?.value {
+                    let isEvenIndex = subsequence.count % 2 == 0
+                    var newSubsequence: [(Int, Int)]? = nil
+                    if (isEvenIndex && lastValueInSubsequence > currValue) ||
+                               (!isEvenIndex && lastValueInSubsequence < currValue) {
+                        newSubsequence = subsequence + [(currIndex, currValue)]
+                    }
+                    if let subsequence = newSubsequence {
+                        cache.append(subsequence)
+                    }
+                }
+            }
+            cache.append([(currIndex, currValue)])
+            currIndex += 1
+        }
+        let sortedCache = cache.sorted { $0.count >= $1.count }
+        let result = sortedCache.prefix { $0.count == sortedCache.first!.count }
+                .map { $0.map { $0.value } }
+        return result
+    }
+    
+}
+
---
+++ swift/CodingPractice/CodingPracticeTests/LongestAlternatingSubsequenceSpec.swift
@@ -0,0 +1,48 @@
+// Copyright (c) 2018 lagostout. All rights reserved.
+
+import Foundation
+import Quick
+import Nimble
+
+class LongestAlternatingSubsequenceSpec : QuickSpec {
+
+    static let cases = [
+        ([], []),
+        ([1], [1]),
+        ([1,2], [1,2]),
+        ([1,2,3], [1,2]),
+        ([1,2,3], [2,3]),
+        ([1,3,2], [1,3,2]),
+        ([3,2,3], [2,3]),
+        ([3,2,1], [3]),
+        ([3,2,1], [2]),
+        ([3,2,1], [1]),
+        ([4,1,3,5,2,3], [1,3,2,3]),
+        ([4,1,3,5,2,3], [4,5,2,3]),
+    ]
+
+    class SharedExamples : QuickConfiguration {
+        override class func configure(_ configuration: Configuration!) {
+            sharedExamples("longestAlternatingSubsequence") { context in
+                let fn = context()["fn"] as! ([Int]) -> [[Int]]
+                for (sequence, expected) in cases {
+                    describe("sequence \(sequence)") {
+                        it("should return \(expected)") {
+                            expect(fn(sequence)).to(contain(expected))
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    override func spec() {
+        describe("computeBottomUpWithMemoization") {
+            itBehavesLike("longestAlternatingSubsequence") {
+                ["fn" : LongestAlternatingSubsequence
+                        .computeBottomUpWithMemoization]
+            }
+        }
+    }
+
+}
