fa4d02ca93977ad108b1861e669b73268ec86c24
lgtout <lagos.tout@gmail.com>
2018-04-25 21:03:32 -0700

    EP-S 17.12.4 Longest weakly alternating subsequence - WIP

---
+++ swift/CodingPractice/CodingPractice/LongestWeaklyAlternatingSubsequence.swift
@@ -0,0 +1,49 @@
+// Copyright (c) 2018 lagostout. All rights reserved.
+
+import Foundation
+
+/* Problem 17.12.4 page 337 */
+
+class LongestWeaklyAlternatingSubsequence {
+
+    static func computeBottomUpWithMemoization(sequence: [Int]) -> [[Int]] {
+        if sequence.isEmpty {
+            return [[]]
+        }
+        var cache: [[(index: Int, value: Int)]] = []
+        var currIndex = 0
+        while currIndex < sequence.endIndex {
+            let currValue = sequence[currIndex]
+            for subsequence in cache {
+                var appendCurrValue = false
+                if subsequence.count >= 3 {
+                    let firstValue = subsequence[
+                            subsequence.endIndex - 3].value
+                    let secondValue = subsequence[
+                            subsequence.endIndex - 2].value
+                    let thirdValue = subsequence[
+                            subsequence.endIndex - 1].value
+                    if !(firstValue < secondValue && secondValue < thirdValue
+                            && thirdValue < currValue) &&
+                               !(firstValue > secondValue && secondValue > thirdValue
+                                       && thirdValue > currValue) {
+                        appendCurrValue = true
+                    }
+                } else {
+                    appendCurrValue = true
+                }
+                if appendCurrValue {
+                    let newSubsequence = subsequence + [(currIndex, currValue)]
+                    cache.append(newSubsequence)
+                }
+            }
+            cache.append([(currIndex, currValue)])
+            currIndex += 1
+        }
+        let sortedCache = cache.sorted { $0.count >= $1.count }
+        let result = sortedCache.prefix { $0.count == sortedCache.first!.count }
+                .map { $0.map { $0.value } }
+        return result
+    }
+
+}
---
+++ swift/CodingPractice/CodingPracticeTests/LongestWeaklyAlternatingSubsequenceSpec.swift
@@ -0,0 +1,52 @@
+// Copyright (c) 2018 lagostout. All rights reserved.
+
+import Foundation
+import Quick
+import Nimble
+
+class LongestWeaklyAlternatingSubsequenceSpec : QuickSpec {
+
+    static let cases = [
+        ([], []),
+        ([1], [1]),
+        ([1,2], [1,2]),
+        ([1,2,3], [1,2,3]),
+        ([1,3,2], [1,3,2]),
+        ([3,2,2], [3,2,2]),
+        ([3,2,1], [3,2,1]),
+        ([3,2,3], [3,2,3]),
+        ([3,2,1,1], [3,2,1,1]),
+        ([3,2,1,0], [3,2,1]),
+        ([3,2,1,0], [2,1,0]),
+        ([3,2,2,3], [3,2,2,3]),
+        ([4,3,2,1,2,3,4,5], [4,3,2,2,3,4]),
+        ([4,3,2,1,2,3,4,5], [4,3,2,2,3,5]),
+        ([4,3,2,1,2,3,4,5], [4,3,2,2,4,5]),
+    ]
+
+    class SharedExamples : QuickConfiguration {
+        override class func configure(_ configuration: Configuration!) {
+            sharedExamples("longestWeaklyAlternatingSubsequence") { context in
+                let fn = context()["fn"] as! ([Int]) -> [[Int]]
+                for (sequence, expected) in cases {
+                    describe("sequence \(sequence)") {
+                        it("should return \(expected)") {
+                            expect(fn(sequence)).to(contain(expected))
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    override func spec() {
+        describe("computeBottomUpWithMemoization") {
+            itBehavesLike("longestWeaklyAlternatingSubsequence") {
+                ["fn" : LongestWeaklyAlternatingSubsequence
+                        .computeBottomUpWithMemoization]
+            }
+        }
+    }
+
+
+}
