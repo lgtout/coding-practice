f7251f45f6f462cca8cd5ce14e0174fdfada3969
lgtout <lagos.tout@gmail.com>
2018-06-30 20:17:23 -0700

    EP-K 5.5 Compute product without arithmetic operators - WIP

---
+++ java/src/main/kotlin/com/lagostout/elementsofprogramminginterviews/primitivetypes/ComputeProductWithoutArithmeticOperators.kt
@@ -0,0 +1,39 @@
+package com.lagostout.elementsofprogramminginterviews.primitivetypes
+
+/* EP 5.5 page 51 */
+
+fun productWithoutArithmeticOperators(left: Int, right: Int): Int {
+
+    fun sum(left: Int, right: Int): Int {
+        var result = 0
+        var carryOver = 0
+        var currentBitMask = 1
+        var leftOrRight = left or right
+        while (leftOrRight != 0) {
+            val currentLeft = left and currentBitMask
+            val currentRight = right and currentBitMask
+            val currentSum = currentLeft xor currentRight xor carryOver
+            carryOver = ((currentLeft and currentRight) or
+                    (currentLeft and carryOver) or
+                    (currentRight and carryOver)) shl 1
+            result = result or currentSum
+            currentBitMask = currentBitMask shl 1
+            leftOrRight = leftOrRight shr 1
+        }
+        return result
+    }
+
+    var currentSummand = left
+    val rightBitMask = 1
+    var currentRight = right
+    var product = 0
+    while (currentRight != 0) {
+        if (currentRight and rightBitMask == 1) {
+            product = sum(product, currentSummand)
+        }
+        currentSummand = currentSummand shl 1
+        currentRight = currentRight shr 1
+    }
+    return product
+
+}
---
+++ java/src/test/kotlin/com/lagostout/elementsofprogramminginterviews/primitivetypes/ComputeProductWithoutArithmeticOperatorsSpek.kt
@@ -0,0 +1,37 @@
+package com.lagostout.elementsofprogramminginterviews.primitivetypes
+
+import org.assertj.core.api.Assertions.assertThat
+import org.jetbrains.spek.api.Spek
+import org.jetbrains.spek.api.dsl.describe
+import org.jetbrains.spek.api.dsl.it
+import org.jetbrains.spek.data_driven.data
+import org.jetbrains.spek.data_driven.on
+
+object ComputeProductWithoutArithmeticOperatorsSpek : Spek({
+
+    val data = listOfNotNull(
+//        data(Pair(0,0), 0),
+//        data(Pair(1,0), 0),
+//        data(Pair(0,1), 0),
+//        data(Pair(1,1), 1),
+//        data(Pair(2,1), 2),
+//        data(Pair(5,1), 5),
+//        data(Pair(5,1), 5),
+//        data(Pair(3,2), 6),
+        data(Pair(3,3), 9),
+//        data(Pair(3,5), 15),
+//        data(Pair(3,8), 24),
+        data(Pair(3,11), 33),
+        null
+    ).toTypedArray()
+
+    describe("productWithoutArithmeticOperators") {
+        on("operands: %s", with = *data) { (left, right), expected ->
+            it("returns $expected") {
+                assertThat(productWithoutArithmeticOperators(left, right))
+                        .isEqualTo(expected)
+            }
+        }
+    }
+
+})
