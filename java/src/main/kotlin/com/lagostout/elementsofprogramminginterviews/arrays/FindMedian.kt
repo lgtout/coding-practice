c29fb7668676bcde69036ece9abf3e0886dac58e
lgtout <lagos.tout@gmail.com>
2018-07-04 20:24:31 -0700

    EP-K 12.8.2 Find median of array - WIP

--- java/src/main/kotlin/com/lagostout/elementsofprogramminginterviews/arrays/FindMedian.kt
+++ java/src/main/kotlin/com/lagostout/elementsofprogramminginterviews/arrays/FindMedian.kt
@@ -1,2 +1,65 @@
+@file:Suppress("NAME_SHADOWING")
+
 package com.lagostout.elementsofprogramminginterviews.arrays
 
+import com.lagostout.common.nextInt
+import org.apache.commons.math3.random.RandomDataGenerator
+
+/* Problem 12.8.2 page 204 */
+
+fun findMedian(array: List<Int>): Double? {
+
+    if (array.isEmpty()) return null
+
+    val array = array.toMutableList()
+    var leftBounds: Pair<Int, Int>
+    var rightBounds = Pair(0, array.lastIndex)
+    var boundsToSearch = rightBounds
+    val random = RandomDataGenerator().apply { reSeed(1) }
+    val medianBounds = (array.count() / 2).let {
+        val countIsOdd = array.count() % 2 == 1
+        Pair(it, it).let {
+            if (countIsOdd) it
+            else it.copy(first = it.first - 1)
+        }
+    }
+
+    fun swap(from: Int, to: Int) {
+        val temp = array[from]
+        array[from] = array[to]
+        array[to] = temp
+    }
+
+    fun contains(bounds1: Pair<Int, Int>,
+                 bounds2: Pair<Int, Int>): Boolean {
+        return bounds2.toList().all {
+            it >= bounds1.first && it <= bounds1.second
+        }
+    }
+
+    while (boundsToSearch != medianBounds) {
+        val pivotIndex = random.nextInt(boundsToSearch)
+        val pivotValue = array[pivotIndex]
+        swap(boundsToSearch.first, pivotIndex)
+        var sortedIndex = boundsToSearch.first
+        var unsortedIndex = boundsToSearch.second
+        while (sortedIndex < unsortedIndex) {
+            val unsortedValue = array[unsortedIndex]
+            if (unsortedValue <= pivotValue) {
+                swap(sortedIndex + 1, unsortedIndex)
+                sortedIndex += 1
+            }
+            unsortedIndex -= 1
+        }
+        swap(boundsToSearch.first, sortedIndex)
+        leftBounds = boundsToSearch.copy(second = sortedIndex)
+        rightBounds = boundsToSearch.copy(first = sortedIndex + 1)
+        boundsToSearch = if (contains(leftBounds, medianBounds))
+            leftBounds else rightBounds
+    }
+
+    return boundsToSearch.let {
+        (array[it.first] + array[it.second]) / 2.0
+    }
+
+}
--- java/src/test/kotlin/com/lagostout/elementsofprogramminginterviews/arrays/FindMedianSpek.kt
+++ java/src/test/kotlin/com/lagostout/elementsofprogramminginterviews/arrays/FindMedianSpek.kt
@@ -1,2 +1,56 @@
 package com.lagostout.elementsofprogramminginterviews.arrays
 
+import com.lagostout.common.nextInt
+import org.apache.commons.math3.random.RandomDataGenerator
+import org.assertj.core.api.Assertions.assertThat
+import org.jetbrains.spek.api.Spek
+import org.jetbrains.spek.api.dsl.describe
+import org.jetbrains.spek.api.dsl.it
+import org.jetbrains.spek.data_driven.data
+import org.jetbrains.spek.data_driven.on
+
+object FindMedianSpek : Spek({
+
+    fun computeExpected(list: List<Int>): Double? {
+        fun medianBounds(array: List<Int>): Pair<Int, Int> {
+            return (array.count() / 2).let {
+                Pair(it, it).let {
+                    val countIsOdd = array.count() % 2 == 1
+                    if (countIsOdd) it
+                    else it.copy(first = it.first - 1)
+                }
+            }
+        }
+        return if (list.isEmpty()) null
+        else list.sorted().let { sortedList ->
+            medianBounds(sortedList).let {
+                (sortedList[it.first] + sortedList[it.second]) / 2.0
+            }
+        }
+    }
+
+    val randomData by memoized {
+        val caseCount = 10
+        val numberRange = Pair(-5, 5)
+        val numberCount = Pair(0, 10)
+        val random = RandomDataGenerator().apply { reSeed(1) }
+        (0 until caseCount).map {
+            random.nextInt(numberCount).let { numberCount ->
+                (0..numberCount).map {
+                    random.nextInt(numberRange)
+                }
+            }.let {
+                data(it, computeExpected(it))
+            }
+        }.toTypedArray()
+    }
+
+    describe("findMedian") {
+        on("array: %s", with = *randomData) { array, expected ->
+            it("should return $expected") {
+                assertThat(findMedian(array)).isEqualTo(expected)
+            }
+        }
+    }
+
+})
