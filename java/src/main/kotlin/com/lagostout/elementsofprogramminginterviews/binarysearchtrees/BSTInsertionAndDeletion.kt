c7b14a3ba5dc4618429598e7bdcaaed2ed06f425
lgtout <lagos.tout@gmail.com>
2018-07-05 22:56:43 -0700

    EP-K 15.10.1 Insertion and deletion in BST - WIP

--- java/src/main/kotlin/com/lagostout/elementsofprogramminginterviews/binarysearchtrees/BSTInsertionAndDeletion.kt
+++ java/src/main/kotlin/com/lagostout/elementsofprogramminginterviews/binarysearchtrees/BSTInsertionAndDeletion.kt
@@ -8,6 +8,83 @@ import com.lagostout.datastructures.BinaryTreeNode
 
 object BSTInsertionAndDeletion {
 
+    // We assume the tree is never empty i.e. the root is never null.
+
+    fun <T : Comparable<T>> insert(value: T, root: BinaryTreeNode<T>) {
+        val duplicateOrParent = find(value, root)
+        if (duplicateOrParent.value == value) return
+        duplicateOrParent.let {
+            // We know the parent is a leaf, so we don't need to
+            // consider its left/right children.
+            (if (it.value > value) it::right else it::left).set(it)
+        }
+    }
+
+    fun <T : Comparable<T>> delete(value: T, root: BinaryTreeNode<T>):
+            BinaryTreeNode<T>? {
+
+        @Suppress("NAME_SHADOWING")
+        var root = root
+        val node =  find(value, root)
+        if (node.value != value) return root
+
+        // If there's a left subtree, find within it the node
+        // with the max value.  Otherwise, use the right node
+        // as the replacement.
+
+        val replacementNode = node.left?.let {
+            find(it) { -1 }
+        } ?: node.right
+
+        // If replacement node is null, the node to delete is the
+        // root, so we return null.
+
+        return replacementNode?.let {
+
+            // Detach the node to delete from its parent,
+            // swapping it with the replacement.  If the
+            // node to delete is the root, this operation
+            // has no effect.
+
+            if (node.isRightChild) {
+                node.parent?.right = replacementNode
+            } else if (node.isLeftChild){
+                node.parent?.left = replacementNode
+            }
+
+            // Attach the deleted node's subtrees to the replacement node.
+            // Attach the replacement node to its new parent.
+
+            with (it) {
+
+                // Prevent a cycle from being created on replacement node
+                // when the replacement node is the child of node to
+                // delete.
+
+                if (node.right != this) right = node.right
+                if (node.left != this) left = node.left
+                parent = node.parent.apply {
+
+                    // If we're deleting the root, let's make the
+                    // replacement node the new root.
+
+                    if (node == root) root = it
+                }
+            }
+
+            // Detach the replacement node from its parent.
+
+            it.parent?.let { parent ->
+                (if (it.isRightChild)
+                    parent::right else parent::left).set(null)
+            }
+
+            // Return the root.
+
+            root
+        }
+    }
+
     private fun <T : Comparable<T>> find(
             root: BinaryTreeNode<T>,
             compareTo: (T) -> Int):
@@ -33,57 +110,7 @@ object BSTInsertionAndDeletion {
 
     private fun <T : Comparable<T>> find(
             value: T, root: BinaryTreeNode<T>): BinaryTreeNode<T> {
-        return find(root, { it.compareTo(value) })
-    }
-
-    fun <T : Comparable<T>> insert(value: T, root: BinaryTreeNode<T>) {
-        val duplicateOrParent = find(value, root)
-        if (duplicateOrParent.value == value) return
-        duplicateOrParent.let {
-            // We know the parent is a leaf, so we don't need to
-            // consider its left/right children.
-            (if (it.value > value) it::right
-            else it::left).set(it)
-        }
-    }
-
-    fun <T : Comparable<T>> delete(value: T, root: BinaryTreeNode<T>):
-            BinaryTreeNode<T>? {
-        val node =  find(value, root)
-        if (node.value != value) return root
-        // If there's a left subtree, find within it the node
-        // with the max value.
-        // Otherwise, if there's a right subtree, find within
-        // it the node with the min value.
-        val replacementNode = node.left?.let {
-            find(it, { 1 })
-        } ?: node.right?.let {
-            find (it, { -1 })
-        }
-        return replacementNode?.let {
-            // Detach the replacement node from its parent.
-            it.parent?.let { parent ->
-                (if (it.isRightChild)
-                    parent::right else parent::left).set(null)
-            }
-            // Detach the node to delete from its parent.
-            if (node.isRightChild) {
-                node.parent?.right = replacementNode
-            } else if (node.isLeftChild){
-                node.parent?.left = replacementNode
-            }
-            // Attach the replacement node to its new parent.
-            // Attach the deleted nodes subtrees to the replacement node.
-            with (it) {
-                right = node.right
-                left = node.left
-                parent = node.parent
-            }
-            // Return the root.
-            // If replacement node was null, the node to delete is the
-            // root, and we return null.
-            root
-        }
+        return find(root) { it.compareTo(value) }
     }
 
 }
