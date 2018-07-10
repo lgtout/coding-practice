f17270ee7caf3e1759785c5bcc953b0a560a0a21
lgtout <lagos.tout@gmail.com>
2018-07-10 16:50:56 -0700

    EP-K 6.6.2 Longest subarray of equal entries - done!

--- java/src/main/kotlin/com/lagostout/elementsofprogramminginterviews/arrays/LengthOfLongestSubarrayOfEqualEntries.kt
+++ java/src/main/kotlin/com/lagostout/elementsofprogramminginterviews/arrays/LengthOfLongestSubarrayOfEqualEntries.kt
@@ -1,3 +1,22 @@
 package com.lagostout.elementsofprogramminginterviews.arrays
 
 /* Problem 6.6.2 page 71 */
+
+fun lengthOfLongestSubarrayOfEqualEntries(array: List<Int>): Int {
+    if (array.count() <= 1) return array.count()
+    var longestSubarrayCount = 1
+    var currentSubarrayCount = 1
+    (1 until array.count()).forEach { index ->
+        val currentNumber = array[index]
+        val previousNumber = array[index - 1]
+        if (currentNumber == previousNumber) {
+            currentSubarrayCount += 1
+            if (currentSubarrayCount > longestSubarrayCount) {
+                longestSubarrayCount = currentSubarrayCount
+            }
+        } else {
+            currentSubarrayCount = 1
+        }
+    }
+    return longestSubarrayCount
+}
--- java/src/test/kotlin/com/lagostout/elementsofprogramminginterviews/arrays/LengthOfLongestSubarrayOfEqualEntriesSpek.kt
+++ java/src/test/kotlin/com/lagostout/elementsofprogramminginterviews/arrays/LengthOfLongestSubarrayOfEqualEntriesSpek.kt
@@ -1,2 +1,35 @@
 package com.lagostout.elementsofprogramminginterviews.arrays
 
+import org.assertj.core.api.Assertions.assertThat
+import org.jetbrains.spek.api.Spek
+import org.jetbrains.spek.api.dsl.describe
+import org.jetbrains.spek.api.dsl.it
+import org.jetbrains.spek.data_driven.data
+import org.jetbrains.spek.data_driven.on
+
+class LengthOfLongestSubarrayOfEqualEntriesSpek : Spek({
+
+    val data = run {
+        listOfNotNull(
+            data(emptyList(), 0),
+            data(listOf(1), 1),
+            data(listOf(1,1), 2),
+            data(listOf(1,2), 1),
+            data(listOf(1,2,2), 2),
+            data(listOf(1,2,2,1,1,1,2,2), 3),
+            data(listOf(1,2,2,1,1,1,2,3,3,3,3,2), 4),
+            data(listOf(1,2,2,1,1,1,2,3,3,3,3,2,1,1,1), 4),
+            null
+        ).toTypedArray()
+    }
+
+    describe("lengthOfLongestSubarrayOfEqualEntries") {
+        on("array %s", with = *data) { array, expected ->
+            it("should return $expected") {
+                assertThat(lengthOfLongestSubarrayOfEqualEntries(array))
+                        .isEqualTo(expected)
+            }
+        }
+    }
+
+})
