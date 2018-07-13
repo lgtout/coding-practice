2811d565ae7b3d342cd3e4dcced221b87016538f
lgtout <lagos.tout@gmail.com>
2018-07-13 16:34:27 -0700

    EP-K Queue with max API - done!

--- java/src/main/kotlin/com/lagostout/elementsofprogramminginterviews/stacksandqueues/ImplementQueueWithMaxAPI.kt
+++ java/src/main/kotlin/com/lagostout/elementsofprogramminginterviews/stacksandqueues/ImplementQueueWithMaxAPI.kt
@@ -1,13 +1,40 @@
 package com.lagostout.elementsofprogramminginterviews.stacksandqueues
 
-class QueueWithMaxAPI<T> {
-    fun enqueue(entry: T) {
+import java.util.*
 
+/* Problem 9.10 page 148 */
+
+class QueueWithMaxAPI {
+
+    data class MaxCount(val max: Int, val count: Int)
+
+    private val queue = LinkedList<Int>()
+    private val deque = LinkedList<MaxCount>()
+
+    fun enqueue(entry: Int) {
+        queue.add(entry)
+        var count = 0
+        while (true) {
+            if (deque.isEmpty()) break
+            val (previousMax, previousCount) = deque.last()
+            if (previousMax > entry) break
+            count += previousCount
+            deque.removeLast()
+        }
+        deque.addLast(MaxCount(entry, count + 1))
     }
-    fun dequeue(): T? {
-        return null
+
+    fun dequeue(): Int {
+        deque.removeFirst().let {
+            val count = it.count - 1
+            if (count > 0)
+                deque.addFirst(it.copy(count = count))
+        }
+        return queue.remove()
     }
-    fun max(): T? {
-        return null
+
+    fun max(): Int {
+        return deque.first().max
     }
+
 }
--- java/src/test/kotlin/com/lagostout/elementsofprogramminginterviews/stacksandqueues/ImplementQueueWithMaxAPISpek.kt
+++ java/src/test/kotlin/com/lagostout/elementsofprogramminginterviews/stacksandqueues/ImplementQueueWithMaxAPISpek.kt
@@ -1,2 +1,205 @@
 package com.lagostout.elementsofprogramminginterviews.stacksandqueues
 
+import org.assertj.core.api.Assertions.assertThat
+import org.jetbrains.spek.api.Spek
+import org.jetbrains.spek.api.dsl.describe
+import org.jetbrains.spek.api.dsl.it
+import org.jetbrains.spek.api.dsl.on
+
+object ImplementQueueWithMaxAPISpek : Spek({
+
+    describe("QueueWithMaxAPI") {
+
+        val queue by memoized { QueueWithMaxAPI() }
+
+        describe("queue enqueue/dequeue") {
+            val entries = listOf(1, 2, 3)
+            on("enqueue $entries") {
+                entries.forEach { queue.enqueue(it) }
+                it("dequeues $entries") {
+                    val dequeuedEntries = (0 until entries.count()).map {
+                        queue.dequeue()
+                    }
+                    assertThat(dequeuedEntries)
+                            .containsExactlyElementsOf(entries)
+                }
+            }
+        }
+
+        Pair(1, 1).let { (entry, expected) ->
+            describe("enqueued with $entry") {
+                on("max") {
+                    queue.enqueue(entry)
+                    val max = queue.max()
+                    it("should return $expected") {
+                        assertThat(max).isEqualTo(expected)
+                    }
+                }
+            }
+        }
+
+        Pair(listOf(1, 2), 2).let { (entries, expected) ->
+            describe("enqueued with $entries") {
+                on("max") {
+                    entries.forEach {
+                        queue.enqueue(it)
+                    }
+                    val max = queue.max()
+                    it("should return $expected") {
+                        assertThat(max).isEqualTo(expected)
+                    }
+                }
+            }
+        }
+
+        Pair(listOf(1, 2, 3), 3).let { (entries, expected) ->
+            describe("enqueued with $entries") {
+                on("max") {
+                    entries.forEach {
+                        queue.enqueue(it)
+                    }
+                    val max = queue.max()
+                    it("should return $expected") {
+                        assertThat(max).isEqualTo(expected)
+                    }
+                }
+            }
+        }
+
+        Pair(listOf(3, 2, 1), 3).let { (entries, expected) ->
+            describe("enqueued with $entries") {
+                on("max") {
+                    entries.forEach {
+                        queue.enqueue(it)
+                    }
+                    val max = queue.max()
+                    it("should return $expected") {
+                        assertThat(max).isEqualTo(expected)
+                    }
+                }
+            }
+        }
+
+        Triple(listOf(1, 2, 3), 1, 3).let { (entries, dequeueCount, expected) ->
+            describe("enqueued with $entries, then dequeued x$dequeueCount") {
+                on("max") {
+                    entries.forEach {
+                        queue.enqueue(it)
+                    }
+                    (1..dequeueCount).forEach { queue.dequeue() }
+                    val max = queue.max()
+                    it("should return $expected") {
+                        assertThat(max).isEqualTo(expected)
+                    }
+                }
+            }
+        }
+
+        Triple(listOf(1, 2, 3), 2, 3).let { (entries, dequeueCount, expected) ->
+            describe("enqueued with $entries, then dequeued x$dequeueCount") {
+                on("max") {
+                    entries.forEach {
+                        queue.enqueue(it)
+                    }
+                    (1..dequeueCount).forEach { queue.dequeue() }
+                    val max = queue.max()
+                    it("should return $expected") {
+                        assertThat(max).isEqualTo(expected)
+                    }
+                }
+            }
+        }
+
+        Triple(listOf(1, 2, 3, 1, 2), 4, 2).let { (entries, dequeueCount, expected) ->
+            describe("enqueued with $entries, then dequeued x$dequeueCount") {
+                beforeEachTest {
+                    entries.forEach {
+                        queue.enqueue(it)
+                    }
+                }
+                on("max") {
+                    (1..dequeueCount).forEach { queue.dequeue() }
+                    val max = queue.max()
+                    it("should return $expected") {
+                        assertThat(max).isEqualTo(expected)
+                    }
+                }
+            }
+        }
+
+        Triple(listOf(5, 4, 3, 1, 2), 1, 4).let { (entries, dequeueCount, expected) ->
+            describe("enqueued with $entries, then dequeued x$dequeueCount") {
+                beforeEachTest {
+                    entries.forEach {
+                        queue.enqueue(it)
+                    }
+                }
+                on("max") {
+                    (1..dequeueCount).forEach { queue.dequeue() }
+                    val max = queue.max()
+                    it("should return $expected") {
+                        assertThat(max).isEqualTo(expected)
+                    }
+                }
+            }
+        }
+
+        Triple(listOf(5, 4, 3, 1, 2), 2, 3).let { (entries, dequeueCount, expected) ->
+            describe("enqueued with $entries, then dequeued x$dequeueCount") {
+                beforeEachTest {
+                    entries.forEach {
+                        queue.enqueue(it)
+                    }
+                }
+                on("max") {
+                    (1..dequeueCount).forEach { queue.dequeue() }
+                    val max = queue.max()
+                    it("should return $expected") {
+                        assertThat(max).isEqualTo(expected)
+                    }
+                }
+            }
+        }
+
+        Triple(listOf(5, 4, 3, 1, 2), 3, 2).let { (entries, dequeueCount, expected) ->
+            describe("enqueued with $entries, then dequeued x$dequeueCount") {
+                beforeEachTest {
+                    entries.forEach {
+                        queue.enqueue(it)
+                    }
+                }
+                on("max") {
+                    (1..dequeueCount).forEach { queue.dequeue() }
+                    val max = queue.max()
+                    it("should return $expected") {
+                        assertThat(max).isEqualTo(expected)
+                    }
+                }
+            }
+        }
+
+        listOf(5, 4, 3, 1, 2).let { entries ->
+            val dequeueCount = 2
+            val entriesToEnqueueAfterDequeue = listOf(6,2)
+            val expected = 6
+            describe("enqueued with $entries, then dequeued x$dequeueCount, then enqueued with $entriesToEnqueueAfterDequeue") {
+                beforeEachTest {
+                    entries.forEach {
+                        queue.enqueue(it)
+                    }
+                    (1..dequeueCount).forEach { queue.dequeue() }
+                    entriesToEnqueueAfterDequeue.forEach {
+                        queue.enqueue(it)
+                    }
+                }
+                on("max") {
+                    val max = queue.max()
+                    it("should return $expected") {
+                        assertThat(max).isEqualTo(expected)
+                    }
+                }
+            }
+        }
+    }
+
+})
