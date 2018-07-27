a9a47f80be35ab7a4faec384038bb6e4178c1881
lgtout <lagos.tout@gmail.com>
2018-07-26 17:46:41 -0700

    Upgrade Kotlin to 1.3-M1

--- java/src/main/kotlin/com/lagostout/scripts/hamcrest.kts
+++
@@ -1,6 +0,0 @@
-import org.hamcrest.MatcherAssert.assertThat
-import org.hamcrest.Matchers.containsInAnyOrder
-
-// Assert that one collection contains the same items as another, with
-// the same cardinality.
-assertThat(listOf(1,2), containsInAnyOrder(*(listOf(2,1).toTypedArray())))
--- java/src/main/kotlin/com/lagostout/scripts/hamkrest.kts
+++
@@ -1,5 +0,0 @@
-import com.natpryce.hamkrest.assertion.assertThat
-
-// Assert that one collection contains the same items as another, with
-// the same cardinality.
-assertThat(listOf(1,2), Collection<Int>::containsAll, listOf(2,1))
