137e529759dbb662c7cf1cc02f40e19838bddc95
lgtout <lagos.tout@gmail.com>
2018-07-27 20:42:52 -0700

    EP-K 16.2.1 Generate all non-attacking placements of n queens- done!

--- java/src/main/kotlin/com/lagostout/elementsofprogramminginterviews/recursion/GenerateAllNonAttackingPlacementsOfNQueens.kt
+++ java/src/main/kotlin/com/lagostout/elementsofprogramminginterviews/recursion/GenerateAllNonAttackingPlacementsOfNQueens.kt
@@ -6,19 +6,27 @@ object GenerateAllNonAttackingPlacementsOfNQueens {
 
     data class Position(val row: Int, val col: Int)
 
-    fun compute(n: Int): List<List<Position>> {
+    fun compute(n: Int): Set<Set<Position>> {
         val allPositions = (1..n).flatMap { row ->
             (1..n).map { col -> Position(row, col) }
         }
-        return compute(n, n, allPositions.toMutableSet())
+        val cache = mutableMapOf<Pair<Int, Set<Position>>, Set<Set<Position>>>()
+        return compute(n, n, allPositions.toSet(), cache)
+                .filter { it.isNotEmpty() }.toSet()
     }
 
     private fun compute(
             size: Int, queenCount: Int,
-            freePositions: MutableSet<Position>): List<List<Position>> {
-        if (queenCount == 0 || freePositions.isEmpty()) return mutableListOf()
+            freePositions: Set<Position>,
+            cache: MutableMap<Pair<Int, Set<Position>>,
+                    Set<Set<Position>>>): Set<Set<Position>> {
+        val key = Pair(queenCount, freePositions)
+        cache[key]?.let { return it }
+        if (queenCount == 0) return setOf(emptySet())
+        else if (freePositions.isEmpty()) return emptySet()
         return freePositions.flatMap { freePosition ->
             val nextFreePositions = freePositions.toMutableSet()
+            nextFreePositions.remove(freePosition)
             // Row and column
             (1..size).forEach {
                 nextFreePositions.removeAll(
@@ -30,25 +38,31 @@ object GenerateAllNonAttackingPlacementsOfNQueens {
             var rightCol = freePosition.col + 1
             for (row in (freePosition.row - 1 downTo 1)) {
                 if (leftCol >= 1) {
-                    nextFreePositions.remove(Position(row, leftCol.dec()))
+                    nextFreePositions.remove(Position(row, leftCol))
                 }
                 if (rightCol <= size) {
-                    nextFreePositions.remove(Position(row, rightCol.inc()))
+                    nextFreePositions.remove(Position(row, rightCol))
                 }
+                leftCol -= 1
+                rightCol += 1
             }
             leftCol = freePosition.col - 1
             rightCol = freePosition.col + 1
             for (row in (freePosition.row + 1)..size) {
                 if (leftCol >= 1) {
-                    nextFreePositions.remove(Position(row, leftCol.dec()))
+                    nextFreePositions.remove(Position(row, leftCol))
                 }
                 if (rightCol <= size) {
-                    nextFreePositions.remove(Position(row, rightCol.inc()))
+                    nextFreePositions.remove(Position(row, rightCol))
                 }
+                leftCol -= 1
+                rightCol += 1
             }
-            compute(size, queenCount.inc(), nextFreePositions).map {
+            compute(size, queenCount.dec(), nextFreePositions, cache).map {
                 it + freePosition
             }
+        }.toSet().also {
+            cache[key] = it
         }
     }
 
--- java/src/test/kotlin/com/lagostout/elementsofprogramminginterviews/recursion/GenerateAllNonAttackingPlacementsOfNQueensSpek.kt
+++ java/src/test/kotlin/com/lagostout/elementsofprogramminginterviews/recursion/GenerateAllNonAttackingPlacementsOfNQueensSpek.kt
@@ -1,9 +1,7 @@
 package com.lagostout.elementsofprogramminginterviews.recursion
 
-import com.lagostout.common.nextInt
 import com.lagostout.elementsofprogramminginterviews.recursion.GenerateAllNonAttackingPlacementsOfNQueens.Position
 import com.lagostout.elementsofprogramminginterviews.recursion.GenerateAllNonAttackingPlacementsOfNQueens.compute
-import org.apache.commons.math3.random.RandomDataGenerator
 import org.assertj.core.api.Assertions.assertThat
 import org.jetbrains.spek.api.Spek
 import org.jetbrains.spek.api.dsl.describe
@@ -13,43 +11,54 @@ import org.jetbrains.spek.data_driven.on
 import org.paukov.combinatorics3.Generator
 import kotlin.math.absoluteValue
 
+@Suppress("NestedLambdaShadowedImplicitParameter")
 object GenerateAllNonAttackingPlacementsOfNQueensSpek : Spek({
 
-    val randomData by memoized {
-        val caseCount = 10
-        val nRange = Pair(0,6)
-        val random = RandomDataGenerator().apply { reSeed(1) }
-        (1..caseCount).map {
-            val n = random.nextInt(nRange)
-            val allPositions = (1..n).flatMap { row ->
-                (1..n).map { col -> Position(row, col) }
-            }
-            val expected = Generator.combination(allPositions)
-                    .simple(n).filter {
-                        it.map { it.row }.toSet().count() == n &&
-                                it.map { it.col }.toSet().count() == n && run {
-                            Generator.combination(it).simple(2).asSequence().none { (first, second) ->
-                                (first.row - second.row).absoluteValue ==
-                                        (first.col - second.col).absoluteValue
-                            }
+    fun computeWithBruteForce(n: Int): Set<Set<Position>> {
+        val allPositions = (1..n).flatMap { row ->
+            (1..n).map { col -> Position(row, col) }
+        }
+        return Generator.combination(allPositions)
+                .simple(n).filter {
+                    it.map { it.row }.toSet().count() == n &&
+                            it.map { it.col }.toSet().count() == n && run {
+                        Generator.combination(it).simple(2).asSequence().none { (first, second) ->
+                            (first.row - second.row).absoluteValue ==
+                                    (first.col - second.col).absoluteValue
                         }
                     }
-            data(n, expected)
+                }.map { it.toSet() }.toSet()
+    }
+
+    val randomData by memoized {
+        val nRange = Pair(0,7)
+        (nRange.first..nRange.second).map {
+            data(it, computeWithBruteForce(it))
         }.toTypedArray()
     }
 
     val data = listOfNotNull(
-//        data(2, emptyList<List<Position>>())
-        data(1, listOf(listOf(Position(1,1)))),
-        null)
-            .toTypedArray()
+        data(2, setOf()),
+        data(1, setOf(setOf(Position(1,1)))),
+        data(4, setOf(
+            setOf(Position(row=1, col=2),
+                Position(row=2, col=4),
+                Position(row=3, col=1),
+                Position(row=4, col=3)),
+            setOf(Position(row=1, col=3),
+                Position(row=2, col=1),
+                Position(row=3, col=4),
+                Position(row=4, col=2)))),
+        data(0, emptySet()),
+        null
+    ).toTypedArray()
 
     describe("generateAllNonAttackingPlacementsOfNQueens") {
-        on("n: %s", with = *data) { n, expected ->
-//        on("n: %s", with = *randomData) { n, expected ->
+//        on("n: %s", with = *data) { n, expected ->
+        on("n: %s", with = *randomData) { n, expected ->
+            val placements = compute(n)
             it("should return $expected") {
-                assertThat(compute(n))
-                        .containsOnlyElementsOf(expected)
+                assertThat(placements).containsOnlyElementsOf(expected)
             }
         }
     }
