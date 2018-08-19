eff330e3971edc1c8a2dd80ad34eefa949b4abe0
lgtout <lagos.tout@gmail.com>
2018-08-19 14:09:36 -0700

    EP-K 10.16.2 Compute right sibling tree using right child field - WIP

---
+++ java/src/main/kotlin/com/lagostout/elementsofprogramminginterviews/binarytrees/ComputeRightSiblingTreeUsingRightChildField.kt
@@ -0,0 +1,11 @@
+package com.lagostout.elementsofprogramminginterviews.binarytrees
+
+import com.lagostout.datastructures.BinaryTreeNode
+
+/* Problem 10.16.2 page 173 */
+
+fun <T> computeRightSiblingTreeUsingRightChildField(root: BinaryTreeNode<T>) {
+
+
+
+}
