cbd253f125f9749f74e5fd5bb4135f72ab0b7991
lgtout <lagos.tout@gmail.com>
2018-08-24 13:47:02 -0700

    EP-K 15.10.2 Insertion/deletion in BST when only links can change - WIP

---
+++ java/src/main/kotlin/com/lagostout/elementsofprogramminginterviews/binarysearchtrees/BSTInsertionAndDeletionWhenOnlyLinksCanChange.kt
@@ -0,0 +1,122 @@
+package com.lagostout.elementsofprogramminginterviews.binarysearchtrees
+
+import com.lagostout.common.isLeftChild
+import com.lagostout.common.isRightChild
+import com.lagostout.datastructures.BinaryTreeNode
+
+/* Problem 15.10.2 page 280 */
+
+object BSTInsertionAndDeletionWhenOnlyLinksCanChange {
+
+    // We assume the tree is never empty i.e. the root is never null.
+
+    fun <T : Comparable<T>> insert(value: T, root: BinaryTreeNode<T>) {
+        val duplicateOrParent = find(value, root)
+        if (duplicateOrParent.value == value) return
+        duplicateOrParent.let {
+            // We know the parent is a leaf, so we don't need to
+            // consider its left/right children.
+            val node = BinaryTreeNode(value = value, parent = it)
+            (if (value > it.value) it::right else it::left).set(node)
+
+        }
+    } 
+
+    /* We see two possible approaches.  The first is to push the node we're
+    deleting down the tree, till it's a leaf, then pruning it from the tree.
+    The second is to replace the node-to-delete with either the node with the
+    largest value in the node-to-delete's left subtree or the one with the
+    smallest value in the node-to-delete's right subtree.  We choose to
+    implement the second approach, as it seems simpler. */
+
+    fun <T : Comparable<T>> delete(value: T, root: BinaryTreeNode<T>):
+            BinaryTreeNode<T>? {
+        val nodeToDelete = find(value, root)
+
+        // Node to delete isn't in the tree.
+        if (nodeToDelete.value != value) return root
+
+        // Find the replacement node.
+        val replacementNode = nodeToDelete.run {
+            left?.let { it ->
+                find(it) { 1 }
+            } ?: right?.let { it ->
+                find(it) { -1 }
+            }
+        }
+
+        replacementNode?.apply {
+
+            // Extract the replacement node, promoting its single child,
+            // if it has one. The replacement node will always have <= 1
+            // child nodes, because it will be the leftmost or rightmost
+            // descendant (of the left or right subtree of the node to
+            // delete).
+            // If the replacement node is not null, it will have a parent.
+            (left ?: right).let {
+                it?.parent = parent
+                (if (isLeftChild) parent!!::left
+                else parent!!::right).set(it)
+            }
+            parent = null; left = null; right = null
+
+        }
+
+        // Swap the node to delete and the replacement node.
+
+        when {
+            nodeToDelete.isLeftChild -> nodeToDelete.parent?.left = replacementNode
+            nodeToDelete.isRightChild -> nodeToDelete.parent?.right = replacementNode
+        }
+        replacementNode?.left = nodeToDelete.left
+        replacementNode?.right = nodeToDelete.right
+        nodeToDelete.left?.parent = replacementNode
+        nodeToDelete.right?.parent = replacementNode
+
+//        println("replacementNode.value ${replacementNode?.value}")
+//        println("root $root")
+
+        // Clean up.
+        nodeToDelete.parent = null
+
+        // If the node to delete is the root then:
+        // - If there's no replacement node (i.e. the root is the only node
+        // in the tree) we return the replacement node, which is null.
+        // - If there's a replacement node, we return it because it's the new root.
+        // If the node to delete is not the root, we return the root.
+        return if (nodeToDelete == root) replacementNode else root
+    }
+
+    /* We compare the node we're searching for with each node in the tree.  If
+    the former's value is > the latter's, we go right.  If the value is <, we
+    go left.  Otherwise, we've found the node we're searching for, or we've
+    gone as far as we can and found a node that would be its parent if it was
+    present. */
+    private fun <T : Comparable<T>> find(
+            root: BinaryTreeNode<T>, compareTo: (T) -> Int): BinaryTreeNode<T> {
+        var currentNode = root
+        while (true) {
+            val comparison = compareTo(currentNode.value)
+            when {
+                comparison > 0 -> {
+                    currentNode.right?.let {
+                        currentNode = it
+                    }
+                }
+                comparison < 0 -> {
+                    currentNode.left?.let {
+                        currentNode = it
+                    }
+                }
+                else -> null
+            } ?: break
+        }
+        return currentNode
+    }
+
+    private fun <T : Comparable<T>> find(
+            value: T, root: BinaryTreeNode<T>): BinaryTreeNode<T> {
+        return find(root) { value.compareTo(it) }
+    }
+
+}
---
+++ java/src/test/kotlin/com/lagostout/elementsofprogramminginterviews/binarysearchtrees/BSTInsertionAndDeletionWhenOnlyLinksCanChangeSpek.kt
@@ -0,0 +1,124 @@
+package com.lagostout.elementsofprogramminginterviews.binarysearchtrees
+
+import com.lagostout.datastructures.BinaryTreeNode
+import com.lagostout.datastructures.BinaryTreeNode.Companion.bbtr
+import com.lagostout.datastructures.RawBinaryTreeNode.Companion.rbt
+import com.lagostout.kotlin.common.Alphabet
+import com.lagostout.kotlin.common.Alphabet.*
+import org.assertj.core.api.Assertions.assertThat
+import org.jetbrains.spek.api.Spek
+import org.jetbrains.spek.api.dsl.describe
+import org.jetbrains.spek.api.dsl.it
+import org.jetbrains.spek.data_driven.data
+import org.jetbrains.spek.data_driven.on
+
+object BSTInsertionAndDeletionWhenOnlyLinksCanChangeSpek : Spek({
+
+    describe("insert") {
+        val data = listOfNotNull(
+            data(bbtr(listOf(rbt(A))), B, bbtr(listOf(rbt(A, right = 1), rbt(B)))),
+            data(bbtr(listOf(rbt(B))), A, bbtr(listOf(rbt(B, left = 1), rbt(A)))),
+            data(bbtr(listOf(rbt(D, left = 1, right = 2), rbt(B), rbt(F))), A,
+                bbtr(listOf(rbt(D, left = 1, right = 2), rbt(B, left = 3), rbt(F), rbt(A)))),
+            data(bbtr(listOf(rbt(D, left = 1, right = 2), rbt(B), rbt(F))), C,
+                bbtr(listOf(rbt(D, left = 1, right = 2), rbt(B, right = 3), rbt(F), rbt(C)))),
+            data(bbtr(listOf(rbt(D, left = 1, right = 2), rbt(B), rbt(F))), E,
+                bbtr(listOf(rbt(D, left = 1, right = 2), rbt(B), rbt(F, left = 3), rbt(E)))),
+            data(bbtr(listOf(rbt(D, left = 1, right = 2), rbt(B), rbt(F))), G,
+                bbtr(listOf(rbt(D, left = 1, right = 2), rbt(B), rbt(F, right = 3), rbt(G)))),
+            null
+        ).toTypedArray()
+
+        on("tree: %s, valueToInsert: %s", with = *data) { root, valueToInsert, expected ->
+            it("should return $expected") {
+                BSTInsertionAndDeletionWhenOnlyLinksCanChange.insert(valueToInsert, root!!)
+                assertThat(root).isEqualTo(expected)
+            }
+        }
+    }
+
+    describe("delete") {
+
+        val data = listOfNotNull(
+
+            // Delete non-existent node
+            data(bbtr(listOf(rbt(A))), B, bbtr(listOf(rbt(A)))),
+            data(bbtr(listOf(rbt(B, left = 1, right = 2), rbt(A), rbt(C))), D,
+                bbtr(listOf(rbt(B, left = 1, right = 2), rbt(A), rbt(C)))),
+            data(bbtr(listOf(rbt(C, left = 1, right = 2), rbt(B), rbt(D))), A,
+                bbtr(listOf(rbt(C, left = 1, right = 2), rbt(B), rbt(D)))),
+
+            // Delete root node
+
+            // Delete root node when there is no replacement node
+            data(bbtr(listOf(rbt(A))), A, null as BinaryTreeNode<Alphabet>?),
+
+            // Delete root node when replacement node is child of root node and has no children
+            data(bbtr(listOf(rbt(B, left = 1), rbt(A))), B, bbtr(listOf(rbt(A)))),
+            data(bbtr(listOf(rbt(A, right = 1), rbt(B))), A, bbtr(listOf(rbt(B)))),
+            data(bbtr(listOf(rbt(B, left = 1, right = 2), rbt(A), rbt(C))), B,
+                bbtr(listOf(rbt(A, right = 1), rbt(C)))),
+
+            // Delete root node when replacement node is child of root and has a child
+            data(bbtr(listOf(rbt(D, left = 1, right = 2), rbt(B, left = 3), rbt(E), rbt(A))), D,
+                bbtr(listOf(rbt(B, left = 1, right = 2), rbt(A), rbt(E)))),
+            data(bbtr(listOf(rbt(A, right = 1), rbt(B, right = 2), rbt(C))), A,
+                bbtr(listOf(rbt(B, right = 1), rbt(C)))),
+
+            // Delete root node when replacement node is not a child of root and has no children
+            data(bbtr(listOf(rbt(A, right = 1), rbt(B, right = 2), rbt(C))), A,
+                bbtr(listOf(rbt(B, right = 1), rbt(C)))),
+            data(bbtr(listOf(rbt(C, left = 1, right = 3), rbt(A, right = 2), rbt(B), rbt(D))), C,
+                bbtr(listOf(rbt(B, left = 1, right = 2), rbt(A), rbt(D)))),
+            data(bbtr(listOf(rbt(C, left = 1), rbt(B, left = 2), rbt(C))), C,
+                bbtr(listOf(rbt(B, left = 1), rbt(C)))),
+
+            // Delete root node when replacement node is not a child of root and has a child
+            data(bbtr(listOf(rbt(A, right = 1), rbt(D, left = 2), rbt(B, right = 3), rbt(C))), A,
+                bbtr(listOf(rbt(B, right = 1), rbt(D, left = 2), rbt(C)))),
+            data(bbtr(listOf(rbt(D, left = 1, right = 2), rbt(A, right = 3), rbt(E), rbt(C, left = 4), rbt(B))), D,
+                bbtr(listOf(rbt(C, left = 1, right = 2), rbt(A, right = 3), rbt(E), rbt(B)))),
+
+            // Delete non-root node
+            // These sub-cases are not exhaustive, but I believe I've covered enough to be somewhat
+            // confident of the correctness of the solution.
+
+            // Delete non-root node that is a left or right child, when there is no replacement node.
+            data(bbtr(listOf(rbt(A, right = 1), rbt(B))), B, bbtr(listOf(rbt(A)))),
+            data(bbtr(listOf(rbt(B, left = 1), rbt(A))), A, bbtr(listOf(rbt(B)))),
+
+            // Delete non-root node that is a left or right child, when there is a replacement node,
+            // the replacement node is a child of the node to delete, and the replacement node has
+            // no children.
+            data(bbtr(listOf(rbt(C, left = 1), rbt(B, right = 2), rbt(C))), B,
+                bbtr(listOf(rbt(C, left = 1), rbt(C)))),
+            data(bbtr(listOf(rbt(A, right = 1), rbt(C, left = 2), rbt(B))), C,
+                bbtr(listOf(rbt(A, right = 1), rbt(B)))),
+
+            // Delete non-root node that is a left or right child, when there is a replacement node,
+            // the replacement node is a child of the node to delete, and the replacement node has
+            // a child. This case is already covered.
+
+            // Delete non-root node that is a left or right child, when there is a replacement node,
+            // the replacement node is not a child of the node to delete, and the replacement node has
+            // a child.
+
+            data(bbtr(listOf(rbt(A, right = 1), rbt(E, left = 2), rbt(B, right = 3), rbt(D, left = 4), rbt(C))), E,
+                bbtr(listOf(rbt(A, right = 1), rbt(D, left = 2), rbt(B, right = 3), rbt(C)))),
+            data(bbtr(listOf(rbt(E, left = 1), rbt(A, right = 2), rbt(D, left = 3), rbt(B, right = 4), rbt(C))), A,
+                bbtr(listOf(rbt(E, left = 1), rbt(B, right = 2), rbt(D, left = 3), rbt(C)))),
+
+            null
+        ).toTypedArray()
+
+        on("tree: %s, valueToDelete: %s", with = *data) { root, valueToDelete, expected ->
+            it("should return $expected") {
+                val tree = BSTInsertionAndDeletionWhenOnlyLinksCanChange
+                        .delete(valueToDelete, root!!)
+                println(tree)
+                assertThat(tree).isEqualTo(expected)
+            }
+        }
+    }
+
+})
