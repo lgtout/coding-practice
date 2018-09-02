2305e4e5c0e7c0ffad4e737fe3cbadcd433385f3
lgtout <lagos.tout@gmail.com>
2018-09-01 18:10:21 -0700

    EP-K 19.7.2 Shortest addition chain exponentiation - WIP

--- java/src/main/kotlin/com/lagostout/elementsofprogramminginterviews/graphs/ShortestAdditionChainExponentiation.kt
+++ java/src/main/kotlin/com/lagostout/elementsofprogramminginterviews/graphs/ShortestAdditionChainExponentiation.kt
@@ -4,48 +4,66 @@ import java.util.*
 
 /* Problem 19.7.2 page 373 */
 
-object ShortestAdditionChainExponentiation {
+/* From a complexity perspective, in the case where the graph isn't reused,
+I don't see the advantage of constructing a graph, versus using just recursion
+with caching. */
 
-    @Suppress("NAME_SHADOWING")
-    fun shortestAdditionChainExponentiation(exponent: Int): Pair<List<Int>, Int> {
+fun shortestAdditionChainExponentiation(n: Int): List<Int> {
 
-        val exponentToDecompositionsMap = mutableMapOf<Int, Set<Set<Int>>>()
+    // The map value field contains a set of sets of 1 or 2 exponent decompositions.
+    // Examples of the inner set: (3,2) - a decomposition of 5;  (2) - a decomposition of 4.
+    // We initialize with 1 - the exponent of x^1.
+    val exponentToDecompositionsMap = mutableMapOf<Int, Set<Set<Int>>>(Pair(1, emptySet()))
 
-        // Construct the graph.
+    // Construct a graph of chains.
 
-        run {
-            val pending = LinkedList<Int>().apply {
-                push(exponent)
-            }
-            while (pending.isNotEmpty()) {
-                val exponent = pending.pop()
-                if (exponentToDecompositionsMap.containsKey(exponent)) continue
-                val middleExponent = exponent / 2
-                if (exponent % 2 == 0) {
-                    exponentToDecompositionsMap[exponent] = setOf(setOf(middleExponent))
-                    if (!exponentToDecompositionsMap.containsKey(middleExponent)) {
-                        pending.push(middleExponent)
-                    }
-                } else {
-                    (1..middleExponent).map {
-                        setOf(exponent - it, it)
-                    }.toSet().let { decompositions ->
-                        exponentToDecompositionsMap[exponent] = decompositions
-                        decompositions.flatMap { it }.forEach {
-                            if (!exponentToDecompositionsMap.containsKey(it)) {
-                                pending.push(it)
-                            }
-                        }
+    run {
+        val pending = LinkedList<Int>().apply {
+            push(n)
+        }
+        while (pending.isNotEmpty()) {
+            val exponent = pending.pop()
+            if (exponentToDecompositionsMap.containsKey(exponent)) continue
+            val middleExponent = exponent / 2
+            if (exponent % 2 == 0) {
+                exponentToDecompositionsMap[exponent] = setOf(setOf(middleExponent))
+                if (!exponentToDecompositionsMap.containsKey(middleExponent)) {
+                    pending.push(middleExponent)
+                }
+            } else {
+                (1..middleExponent).map {
+                    setOf(exponent - it, it)
+                }.toSet().let { decompositions ->
+                    exponentToDecompositionsMap[exponent] = decompositions
+                    decompositions.flatMap { it }.forEach {
+                        pending.push(it)
                     }
                 }
             }
         }
-
-        // Find the shortest chain.
-
-        val pending = LinkedList<Set<Set<Int>>>()
-                .apply { push(setOf(setOf(exponent))) }
-
-        TODO()
     }
+
+    // Find the shortest chain.
+
+    val cache = mutableMapOf(Pair(1, setOf(1)))
+
+    fun compute(exponent: Int): Set<Int> {
+        return cache[exponent] ?:
+        exponentToDecompositionsMap[exponent]?.map { decomposition ->
+            val chainExponents = mutableSetOf<Int>()
+            decomposition.map {
+                listOf(it) + compute(it)
+            }.fold(chainExponents) { acc, list ->
+                acc.apply { addAll(list) }
+            }
+            chainExponents
+        }?.reduce { shortestChain, chain ->
+            if (chain.size < shortestChain.size) chain
+            else shortestChain
+        }?.also {
+            cache[exponent] = it
+        }!!
+    }
+
+    return (compute(n) + setOf(n)).sorted()
 }
--- java/src/test/kotlin/com/lagostout/elementsofprogramminginterviews/graphs/ShortestAdditionChainExponentiationSpek.kt
+++ java/src/test/kotlin/com/lagostout/elementsofprogramminginterviews/graphs/ShortestAdditionChainExponentiationSpek.kt
@@ -1,7 +1,32 @@
 package com.lagostout.elementsofprogramminginterviews.graphs
 
+import org.assertj.core.api.Assertions.assertThat
 import org.jetbrains.spek.api.Spek
+import org.jetbrains.spek.api.dsl.describe
+import org.jetbrains.spek.api.dsl.it
+import org.jetbrains.spek.data_driven.data
+import org.jetbrains.spek.data_driven.on
 
 object ShortestAdditionChainExponentiation : Spek({
+
+    // TODO More cases. What does brute force solution look like?
+
+    val data = listOfNotNull(
+        data(1, listOf(1)),
+        data(2, listOf(1,2)),
+        data(3, listOf(1,2,3)),
+        data(4, listOf(1,2,4)),
+        data(5, listOf(1,2,4,5)),
+        null
+    ).toTypedArray()
+
+    describe("shortestAdditionChainExponentiation") {
+        on("exponent: %s", with = *data) { exponent, expected ->
+            it("should return $expected") {
+                val chain = shortestAdditionChainExponentiation(exponent)
+                assertThat(chain).isEqualTo(expected)
+            }
+        }
+    }
 
 })
